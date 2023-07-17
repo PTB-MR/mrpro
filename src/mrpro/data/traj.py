ffb0edc21fb0ef5086297044a51135feca2fa4e1
ckolbPTB <christoph.kolbitsch@ptb.de>
2023-07-17 21:36:38 +0200

    copyright added

--- src/mrpro/data/traj.py
+++ src/mrpro/data/traj.py
@@ -1,3 +1,17 @@
+"""Data classes for MR raw data."""
+
+# Copyright 2023 Physikalisch-Technische Bundesanstalt
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#       http://www.apache.org/licenses/LICENSE-2.0
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 from abc import ABC
 from abc import abstractmethod
 
@@ -7,9 +21,9 @@ from mrpro.data.raw import KHeader
 
 
 class KTrajectory(ABC):
-    def __init__(self) -> None:
-        self.traj: torch.Tensor | None = None
+    def __init__(self):
+        self.traj: torch.Tensor
 
     @abstractmethod
-    def calc_traj(self, header: KHeader) -> None:
+    def calc_traj(self, header: KHeader):
         pass
