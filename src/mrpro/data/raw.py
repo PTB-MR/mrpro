ecab4b11af9814485ecd0b7ff2133ca536ca6581
ckolbPTB <christoph.kolbitsch@ptb.de>
2023-07-17 17:21:35 +0200

    coil_label_dict added

--- src/mrpro/data/raw.py
+++ src/mrpro/data/raw.py
@@ -1,6 +1,6 @@
 from abc import ABC
 from abc import abstractmethod
-from pathlib import Path
+from pathlib import Path, List, Dict
 from typing import Union, Tuple, Sequence, Optional
 
 import ismrmrd
@@ -10,42 +10,42 @@ import torch
 import re, os
 
 ACQ_FLAGS = ('ACQ_NO_FLAG',
-    'ACQ_FIRST_IN_ENCODE_STEP1',                
-    'ACQ_LAST_IN_ENCODE_STEP1',                        
-    'ACQ_FIRST_IN_ENCODE_STEP2',                          
-    'ACQ_LAST_IN_ENCODE_STEP2',                          
-    'ACQ_FIRST_IN_AVERAGE',                       
+    'ACQ_FIRST_IN_ENCODE_STEP1',
+    'ACQ_LAST_IN_ENCODE_STEP1',
+    'ACQ_FIRST_IN_ENCODE_STEP2',
+    'ACQ_LAST_IN_ENCODE_STEP2',
+    'ACQ_FIRST_IN_AVERAGE',
     'ACQ_LAST_IN_AVERAGE',
     'ACQ_FIRST_IN_SLICE',
-    'ACQ_LAST_IN_SLICE', 
+    'ACQ_LAST_IN_SLICE',
     'ACQ_FIRST_IN_CONTRAST',
-    'ACQ_LAST_IN_CONTRAST', 
-    'ACQ_FIRST_IN_PHASE',   
-    'ACQ_LAST_IN_PHASE',    
+    'ACQ_LAST_IN_CONTRAST',
+    'ACQ_FIRST_IN_PHASE',
+    'ACQ_LAST_IN_PHASE',
     'ACQ_FIRST_IN_REPETITION',
-    'ACQ_LAST_IN_REPETITION', 
-    'ACQ_FIRST_IN_SET',     
-    'ACQ_LAST_IN_SET',      
-    'ACQ_FIRST_IN_SEGMENT', 
-    'ACQ_LAST_IN_SEGMENT',  
-    'ACQ_IS_NOISE_MEASUREMENT',  
-    'ACQ_IS_PARALLEL_CALIBRATION', 
-    'ACQ_IS_PARALLEL_CALIBRATION_AND_IMAGING', 
-    'ACQ_IS_REVERSE',        
-    'ACQ_IS_NAVIGATION_DATA',  
-    'ACQ_IS_PHASECORR_DATA',      
-    'ACQ_LAST_IN_MEASUREMENT',  
-    'ACQ_IS_HPFEEDBACK_DATA',  
-    'ACQ_IS_DUMMYSCAN_DATA', 
-    'ACQ_IS_RTFEEDBACK_DATA',  
-    'ACQ_IS_SURFACECOILCORRECTIONSCAN_DATA', 
-    'ACQ_USER1',                               
-    'ACQ_USER2',                               
-    'ACQ_USER3',                               
-    'ACQ_USER4',                               
-    'ACQ_USER5',                               
-    'ACQ_USER6',                               
-    'ACQ_USER7',                               
+    'ACQ_LAST_IN_REPETITION',
+    'ACQ_FIRST_IN_SET',
+    'ACQ_LAST_IN_SET',
+    'ACQ_FIRST_IN_SEGMENT',
+    'ACQ_LAST_IN_SEGMENT',
+    'ACQ_IS_NOISE_MEASUREMENT',
+    'ACQ_IS_PARALLEL_CALIBRATION',
+    'ACQ_IS_PARALLEL_CALIBRATION_AND_IMAGING',
+    'ACQ_IS_REVERSE',
+    'ACQ_IS_NAVIGATION_DATA',
+    'ACQ_IS_PHASECORR_DATA',
+    'ACQ_LAST_IN_MEASUREMENT',
+    'ACQ_IS_HPFEEDBACK_DATA',
+    'ACQ_IS_DUMMYSCAN_DATA',
+    'ACQ_IS_RTFEEDBACK_DATA',
+    'ACQ_IS_SURFACECOILCORRECTIONSCAN_DATA',
+    'ACQ_USER1',
+    'ACQ_USER2',
+    'ACQ_USER3',
+    'ACQ_USER4',
+    'ACQ_USER5',
+    'ACQ_USER6',
+    'ACQ_USER7',
     'ACQ_USER8')
 
 class ELimits():
@@ -58,10 +58,10 @@ class ELimits():
                 "repetition",
                 "set",
                 "segment")
-    
+
     def __init__(self) -> None:
         pass
-    
+
 
 class AcqInfo():
     __slots__ = ("acquisition_time_stamp",
@@ -118,10 +118,10 @@ class AcqInfo():
     def from_ismrmrd_acq_header(self, curr_idx: int, acq: ismrmrd.Acquisition) -> None:
         for slot in self.__slots__:
             curr_attr = getattr(self, slot)
-            if slot in ("kspace_encode_step_1", "kspace_encode_step_2", "average", "slice", 
+            if slot in ("kspace_encode_step_1", "kspace_encode_step_2", "average", "slice",
                         "contrast", "phase", "repetition", "set", "segment"):
                 curr_attr[curr_idx,...] = torch.tensor(getattr(acq.idx, slot), dtype=curr_attr.dtype)
-                
+
             else:
                 curr_attr[curr_idx,...] = torch.tensor(getattr(acq, slot), dtype=curr_attr.dtype)
             setattr(self, slot, curr_attr)
@@ -134,7 +134,7 @@ def _return_par_tensor(par, array_attr) -> Union(torch.tensor, None):
             for attr in array_attr:
                 par_tensor.append(getattr(par, attr))
             return(torch.tensor(par_tensor))
-    
+
 def return_par_matrix_tensor(par: ismrmrd.xsd.ismrmrdschema.ismrmrd.matrixSizeType) -> Union(torch.tensor, None):
     return(_return_par_tensor(par, array_attr=("x", "y", "z")))
 
@@ -147,14 +147,20 @@ def return_acc_factor_tensor(par: ismrmrd.xsd.ismrmrdschema.ismrmrd.acceleration
 def bitmask_flag_to_strings(flag: int):
     if flag > 0:
         bmask = "{0:064b}".format(flag)
-        bitmask_idx = [m.start() + 1 for m in re.finditer('1', bmask[::-1])]  
+        bitmask_idx = [m.start() + 1 for m in re.finditer('1', bmask[::-1])]
     else:
         bitmask_idx = [0, ]
     flag_strings = []
     for knd in range(len(bitmask_idx)):
         flag_strings.append(ACQ_FLAGS[bitmask_idx[knd]])
     return(flag_strings)
-        
+
+def return_coil_label_dict(coil_label: List[ismrmrd.xsd.ismrmrdschema.ismrmrd.coilLabelType]) -> Dict:
+    coil_label_dict = {}
+    for idx, label in enumerate(coil_label):
+        coil_label_dict[idx] = [label.coilNumber, label.coilName]
+    return(coil_label_dict)
+
 class KHeader():
     __slots__ = ("protocol_name", "pat_pos", "meas_id", "institution", "receiver_noise_bwdth", "b0", "model", "vendor",
     "elimits", "acc_factor", "rec_matrix", "rec_fov", "enc_matrix", "enc_fov", "etl", "num_coils", "acq_info", "calib_mode",
@@ -166,7 +172,7 @@ class KHeader():
 
     def __init__(self) -> None:
         pass
- 
+
     def from_ismrmrd_header(self, header: ismrmrd.xsd.ismrmrdschema.ismrmrd.ismrmrdHeader) -> None:
 
         # Encoding
@@ -183,12 +189,12 @@ class KHeader():
         self.multiband = enc.parallelImaging.multiband
         self.traj_type = enc.trajectory.value
         self.traj_description = enc.trajectoryDescription
-        
-        
+
+
         self.elimits = ELimits()
         for climit in self.elimits.__slots__:
             setattr(self.elimits, climit, return_par_enc_limits_tensor(getattr(enc.encodingLimits, climit)))
-            
+
         # AcquisitionSystemInformation
         self.vendor = header.acquisitionSystemInformation.systemVendor
         self.model = header.acquisitionSystemInformation.systemModel
@@ -200,10 +206,10 @@ class KHeader():
         self.receiver_noise_bwdth = header.acquisitionSystemInformation.relativeReceiverNoiseBandwidth
         self.institution = header.acquisitionSystemInformation.institutionName
         self.num_coils = header.acquisitionSystemInformation.receiverChannels
-        
+
         # ExperimentalConditions
         self.h1freq = header.experimentalConditions.H1resonanceFrequency_Hz
-        
+
         # MeasurementInformation
         self.ref_frame_uid = header.measurementInformation.frameOfReferenceUID
         self.series_num = header.measurementInformation.initialSeriesNumber
@@ -218,7 +224,7 @@ class KHeader():
         self.series_description = header.measurementInformation.seriesDescription
         self.series_uid = header.measurementInformation.seriesInstanceUIDRoot
         self.series_time = header.measurementInformation.seriesTime
-        
+
         # SequenceParameters
         self.te = header.sequenceParameters.TE
         self.ti = header.sequenceParameters.TI
@@ -229,7 +235,7 @@ class KHeader():
         self.diffusion = header.sequenceParameters.diffusion
         self.diffusion_dim = header.sequenceParameters.diffusionDimension
         self.diffusion_scheme = header.sequenceParameters.diffusionScheme
-        
+
         # StudyInformation
         self.accession_number = header.studyInformation.accessionNumber
         self.body_part = header.studyInformation.bodyPartExamined
@@ -253,25 +259,25 @@ class KData():
     def from_file(cls,
                   filename: Union[str, Path],
                   ktrajectory_calculator: KTrajectory) -> KData:
-        
+
         # Check file is valid
         if not os.path.isfile(filename):
             print("%s is not a valid file" % filename)
             raise SystemExit
-        
+
         # Read header
         dset = ismrmrd.Dataset(filename, "dataset", create_if_needed=False)
         hdr_xml = dset.read_xml_header()
         hdr = ismrmrd.xsd.CreateFromDocument(hdr_xml)
         dset.close()
-        
+
         kheader = KHeader()
         kheader.from_ismrmrd_header(hdr)
-        
+
         # Read k-space data
         with ismrmrd.File(filename) as mrd:
             acqs = mrd["dataset"].acquisitions[:]
-  
+
         # Get indices for imaging data
         im_idx = []
         unique_acq_flags = set()
@@ -280,10 +286,10 @@ class KData():
                 unique_acq_flags.add(el)
             if 'ACQ_IS_NOISE_MEASUREMENT' not in bitmask_flag_to_strings(acq.flags):
                 im_idx.append(idx)
-                
+
         # Create AcqInfo
         kheader.acq_info = AcqInfo(len(im_idx))
-                
+
         # Get k-space data
         kdata = torch.zeros((len(im_idx), kheader.num_coils, acqs[im_idx[0]].number_of_samples), dtype=torch.complex64)
         for idx in range(len(im_idx)):
@@ -291,17 +297,17 @@ class KData():
             kdata[idx,:,:] = torch.tensor(acq.data, dtype=torch.complex64)
             # TODO: Make this faster
             kheader.acq_info.from_ismrmrd_acq_header(idx, acq)
-            
+
         # Calculate trajectory
         ktraj = ktrajectory_calculator.calc_traj(kheader)
-        
-            
+
+
         # TODO: Check for partial Fourier and reflected readouts
-        
+
         # Sort k-space data into (dim4, ncoils, k2, k1, k0)
         kdim_labels = ("kspace_encode_step_1", "kspace_encode_step_2", "average", "slice", "contrast", "phase", "repetition", "set")
         kdim_num = np.asarray([len(np.unique(getattr(kheader.acq_info, acq_label))) for acq_label in kdim_labels])
-        
+
         # Ensure each dim4 covers the same number of k2 and k1 points
         for idx, acq_label in enumerate(kdim_labels[2:]):
             label_values = np.unique(getattr(kheader.acq_info, acq_label))
@@ -311,19 +317,19 @@ class KData():
                 kdim_label_k2 = len(np.unique(kheader.acq_info.kspace_encode_step_2[cidx_curr_label]))
                 assert kdim_label_k1 == kdim_num[0], f'{acq_label} has {kdim_label_k1} k1 points instead of {kdim_num[0]}'
                 assert kdim_label_k2 == kdim_num[1], f'{acq_label} has {kdim_label_k2} k2 points instead of {kdim_num[1]}'
-            
-        sort_ki = np.stack((kheader.acq_info.kspace_encode_step_1, kheader.acq_info.kspace_encode_step_2, kheader.acq_info.average, 
-                                    kheader.acq_info.slice, kheader.acq_info.contrast, kheader.acq_info.phase, 
+
+        sort_ki = np.stack((kheader.acq_info.kspace_encode_step_1, kheader.acq_info.kspace_encode_step_2, kheader.acq_info.average,
+                                    kheader.acq_info.slice, kheader.acq_info.contrast, kheader.acq_info.phase,
                                     kheader.acq_info.repetition, kheader.acq_info.set), axis=0)
         sort_idx = np.lexsort(sort_ki)
 
         new_shape = (np.prod(kdim_num[2:]), kdim_num[1], kdim_num[0],)
         kdata = torch.reshape(kdata[sort_idx, :, :], new_shape + kdata.shape[1:])
         kdata = torch.moveaxis(kdata, (0,1,2,3,4), (0,2,3,1,4))
-        
+
         ktraj = torch.reshape(ktraj[sort_idx,:,:], new_shape + ktraj.shape[1:])
         ktraj = torch.moveaxis(ktraj, (0,1,2,3,4), (0,2,3,1,4))
-        
+
         for slot in kheader.acq_info.__slots__:
             curr_attr = getattr(kheader.acq_info, slot)
             curr_shape = new_shape
