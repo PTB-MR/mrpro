5c65902dbf7148a50e074b948c643f486cbb5298
Felix Zimmermann <fzimmermann89@gmail.com>
2023-07-20 18:08:24 +0200

    Refactoring of KHeader / KData - Switch to dataclasses for AcqInfo and Header - make ACQ_FLAGS an enum.Flag - make methods classmethods - streamline and fix the data reading

--- src/mrpro/data/_KHeader.py
+++ src/mrpro/data/_KHeader.py
@@ -14,169 +14,146 @@
 
 from __future__ import annotations
 
+from dataclasses import dataclass
+
 import ismrmrd.xsd.ismrmrdschema.ismrmrd as ismrmrdschema
+import torch
 
 from mrpro.data import AcqInfo
-from mrpro.data.raw import ELimits
+from mrpro.data import EncodingLimits
 from mrpro.data.raw import return_acc_factor_tensor
 from mrpro.data.raw import return_coil_label_dict
-from mrpro.data.raw import return_par_enc_limits_tensor
 from mrpro.data.raw import return_par_matrix_tensor
 
 
+@dataclass(slots=True)
 class KHeader:
-    __slots__ = (
-        'protocol_name',
-        'pat_pos',
-        'meas_id',
-        'institution',
-        'receiver_noise_bwdth',
-        'b0',
-        'model',
-        'vendor',
-        'elimits',
-        'acc_factor',
-        'rec_matrix',
-        'rec_fov',
-        'enc_matrix',
-        'enc_fov',
-        'etl',
-        'num_coils',
-        'acq_info',
-        'calib_mode',
-        'interleave_dim',
-        'multiband',
-        'traj_type',
-        'traj_description',
-        'device_id',
-        'device_sn',
-        'coil_label',
-        'station_name',
-        'h1freq',
-        'ref_frame_uid',
-        'series_num',
-        'meas_depend',
-        'ref_im_seq',
-        'rel_table_pos',
-        'seq_name',
-        'series_date',
-        'series_description',
-        'series_uid',
-        'series_time',
-        'te',
-        'ti',
-        'tr_siemens',
-        'fa',
-        'tr',
-        'seq_type',
-        'diffusion',
-        'diffusion_dim',
-        'diffusion_scheme',
-        'accession_number',
-        'body_part',
-        'ref_physician',
-        'study_date',
-        'study_description',
-        'study_id',
-        'study_uid',
-        'study_time',
-    )
+    protocol_name: str
+    pat_pos: str
+    meas_id: int
+    institution: str
+    receiver_noise_bwdth: float
+    b0: float
+    model: str
+    vendor: str
+    encoding_limits: EncodingLimits
+    acc_factor: torch.Tensor | None
+    recon_matrix: torch.Tensor | None
+    recon_fov: torch.Tensor | None
+    encoding_matrix: torch.Tensor | None
+    encoding_fov: torch.Tensor | None
+    echo_train_length: int
+    num_coils: int
+    acq_info: AcqInfo
+    calib_mode: str
+    interleave_dim: str
+    multiband: bool
+    traj_type: str
+    traj_description: str
+    device_id: str
+    device_sn: str
+    coil_label: dict[str, str]
+    station_name: str
+    h1freq: float
+    ref_frame_uid: str
+    series_num: int
+    meas_depend: str
+    ref_im_seq: str
+    rel_table_pos: float
+    seq_name: str
+    series_date: str
+    series_description: str
+    series_uid: str
+    series_time: str
+    te: float
+    ti: float
+    tr_siemens: float
+    fa: float
+    tr: float
+    seq_type: str
+    diffusion: bool
+    diffusion_dim: str
+    diffusion_scheme: str
+    accession_number: str
+    body_part: str
+    ref_physician: str | None = None
+    study_date: str | None = None
+    study_description: str | None = None
+    study_id: str | None = None
+    study_uid: str | None = None
+    study_time: str | None = None
 
-    def __init__(self) -> None:
-        pass
+    @classmethod
+    def from_ismrmrd(
+        cls,
+        header: ismrmrdschema.ismrmrdHeader,
+        acq_info: AcqInfo,
+    ) -> KHeader:
+        if len(header.encoding) != 1:
+            raise NotImplementedError('Multiple encodings not supported')
 
-    def from_ismrmrd_header(
-        self, header: ismrmrdschema.ismrmrdHeader, num_acq: int
-    ) -> None:
-        # Encoding
-        assert (
-            len(header.encoding) == 1
-        ), 'Multiple encodings are not supported.'
-        enc = header.encoding[0]
-        self.etl = enc.echoTrainLength
-        self.enc_fov = return_par_matrix_tensor(
-            enc.encodedSpace.fieldOfView_mm
+        enc: ismrmrdschema.encodingType = header.encoding[0]
+        encoding_limits = EncodingLimits.from_ismrmrd_encodingLimitsType(enc.encodingLimits)
+
+        parameters = dict(
+            echo_train_length=enc.echoTrainLength,
+            recon_fov=return_par_matrix_tensor(enc.reconSpace.fieldOfView_mm),
+            recon_matrix=return_par_matrix_tensor(enc.reconSpace.matrixSize),
+            acc_factor=return_acc_factor_tensor(enc.parallelImaging.accelerationFactor),
+            calib_mode=enc.parallelImaging.calibrationMode.value,
+            interleave_dim=enc.parallelImaging.interleavingDimension.value,
+            multiband=enc.parallelImaging.multiband,
+            traj_type=enc.trajectory.value,
+            traj_description=enc.trajectoryDescription,
+            encoding_fov=return_par_matrix_tensor(enc.encodedSpace.fieldOfView_mm),
+            encoding_matrix=return_par_matrix_tensor(enc.encodedSpace.matrixSize),
+            encoding_limits=encoding_limits,
+            vendor=header.acquisitionSystemInformation.systemVendor,
+            model=header.acquisitionSystemInformation.systemModel,
+            station_name=header.acquisitionSystemInformation.stationName,
+            device_id=header.acquisitionSystemInformation.deviceID,
+            device_sn=header.acquisitionSystemInformation.deviceSerialNumber,
+            coil_label=return_coil_label_dict(header.acquisitionSystemInformation.coilLabel),
+            b0=header.acquisitionSystemInformation.systemFieldStrength_T,
+            receiver_noise_bwdth=header.acquisitionSystemInformation.relativeReceiverNoiseBandwidth,
+            institution=header.acquisitionSystemInformation.institutionName,
+            num_coils=header.acquisitionSystemInformation.receiverChannels,
+            h1freq=header.experimentalConditions.H1resonanceFrequency_Hz,
+            ref_frame_uid=header.measurementInformation.frameOfReferenceUID,
+            series_num=header.measurementInformation.initialSeriesNumber,
+            meas_depend=header.measurementInformation.measurementDependency,
+            meas_id=header.measurementInformation.measurementID,
+            pat_pos=header.measurementInformation.patientPosition.value,
+            protocol_name=header.measurementInformation.protocolName,
+            ref_im_seq=header.measurementInformation.referencedImageSequence,
+            rel_table_pos=header.measurementInformation.relativeTablePosition,
+            seq_name=header.measurementInformation.sequenceName,
+            series_date=header.measurementInformation.seriesDate,
+            series_description=header.measurementInformation.seriesDescription,
+            series_uid=header.measurementInformation.seriesInstanceUIDRoot,
+            series_time=header.measurementInformation.seriesTime,
+            te=header.sequenceParameters.TE,
+            ti=header.sequenceParameters.TI,
+            tr_siemens=header.sequenceParameters.TR,
+            fa=header.sequenceParameters.flipAngle_deg,
+            tr=header.sequenceParameters.echo_spacing,
+            seq_type=header.sequenceParameters.sequence_type,
+            diffusion=header.sequenceParameters.diffusion,
+            diffusion_dim=header.sequenceParameters.diffusionDimension,
+            diffusion_scheme=header.sequenceParameters.diffusionScheme,
+            acq_info=acq_info,
         )
-        self.enc_matrix = return_par_matrix_tensor(enc.encodedSpace.matrixSize)
-        self.rec_fov = return_par_matrix_tensor(enc.reconSpace.fieldOfView_mm)
-        self.rec_matrix = return_par_matrix_tensor(enc.reconSpace.matrixSize)
-        self.acc_factor = return_acc_factor_tensor(
-            enc.parallelImaging.accelerationFactor
-        )
-        self.calib_mode = enc.parallelImaging.calibrationMode.value
-        self.interleave_dim = enc.parallelImaging.interleavingDimension.value
-        self.multiband = enc.parallelImaging.multiband
-        self.traj_type = enc.trajectory.value
-        self.traj_description = enc.trajectoryDescription
-
-        self.elimits = ELimits()
-        for climit in self.elimits.__slots__:
-            setattr(
-                self.elimits,
-                climit,
-                return_par_enc_limits_tensor(
-                    getattr(enc.encodingLimits, climit)
-                ),
+        if header.studyInformation is not None:
+            study_parameters = dict(
+                accession_number=header.studyInformation.accessionNumber,
+                body_part=header.studyInformation.bodyPartExamined,
+                ref_physician=header.studyInformation.referringPhysicianName,
+                study_date=header.studyInformation.studyDate,
+                study_description=header.studyInformation.studyDescription,
+                study_id=header.studyInformation.studyID,
+                study_uid=header.studyInformation.studyInstanceUID,
+                study_time=header.studyInformation.studyTime,
             )
+            parameters.update(study_parameters)
 
-        # AcquisitionSystemInformation
-        self.vendor = header.acquisitionSystemInformation.systemVendor
-        self.model = header.acquisitionSystemInformation.systemModel
-        self.station_name = header.acquisitionSystemInformation.stationName
-        self.device_id = header.acquisitionSystemInformation.deviceID
-        self.device_sn = header.acquisitionSystemInformation.deviceSerialNumber
-        self.coil_label = return_coil_label_dict(
-            header.acquisitionSystemInformation.coilLabel
-        )
-        self.b0 = header.acquisitionSystemInformation.systemFieldStrength_T
-        self.receiver_noise_bwdth = (
-            header.acquisitionSystemInformation.relativeReceiverNoiseBandwidth
-        )
-        self.institution = header.acquisitionSystemInformation.institutionName
-        self.num_coils = header.acquisitionSystemInformation.receiverChannels
-
-        # ExperimentalConditions
-        self.h1freq = header.experimentalConditions.H1resonanceFrequency_Hz
-
-        # MeasurementInformation
-        self.ref_frame_uid = header.measurementInformation.frameOfReferenceUID
-        self.series_num = header.measurementInformation.initialSeriesNumber
-        self.meas_depend = header.measurementInformation.measurementDependency
-        self.meas_id = header.measurementInformation.measurementID
-        self.pat_pos = header.measurementInformation.patientPosition.value
-        self.protocol_name = header.measurementInformation.protocolName
-        self.ref_im_seq = header.measurementInformation.referencedImageSequence
-        self.rel_table_pos = (
-            header.measurementInformation.relativeTablePosition
-        )
-        self.seq_name = header.measurementInformation.sequenceName
-        self.series_date = header.measurementInformation.seriesDate
-        self.series_description = (
-            header.measurementInformation.seriesDescription
-        )
-        self.series_uid = header.measurementInformation.seriesInstanceUIDRoot
-        self.series_time = header.measurementInformation.seriesTime
-
-        # SequenceParameters
-        self.te = header.sequenceParameters.TE
-        self.ti = header.sequenceParameters.TI
-        self.tr_siemens = header.sequenceParameters.TR
-        self.fa = header.sequenceParameters.flipAngle_deg
-        self.tr = header.sequenceParameters.echo_spacing
-        self.seq_type = header.sequenceParameters.sequence_type
-        self.diffusion = header.sequenceParameters.diffusion
-        self.diffusion_dim = header.sequenceParameters.diffusionDimension
-        self.diffusion_scheme = header.sequenceParameters.diffusionScheme
-
-        # StudyInformation
-        self.accession_number = header.studyInformation.accessionNumber
-        self.body_part = header.studyInformation.bodyPartExamined
-        self.ref_physician = header.studyInformation.referringPhysicianName
-        self.study_date = header.studyInformation.studyDate
-        self.study_description = header.studyInformation.studyDescription
-        self.study_id = header.studyInformation.studyID
-        self.study_uid = header.studyInformation.studyInstanceUID
-        self.study_time = header.studyInformation.studyTime
-
-        # Acquisition info will be filled during reading in each readout
-        self.acq_info: AcqInfo = AcqInfo(num_acq)
+        return cls(**parameters)
