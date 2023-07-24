0ec2b01cf329fd084cbf3bff8370b7396e24866a
Felix Zimmermann <fzimmermann89@gmail.com>
2023-07-24 14:59:37 +0200

    Add more KHeader dataclasses
    - EncodingLimits
    - SpatialDimension
    - ISMRMRD Header Enums
    - fix ACQ_NO_FLAG flag

---
+++ src/mrpro/data/_AccelerationFactor.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from ismrmrd.xsd.ismrmrdschema.ismrmrd import accelerationFactorType
+
+
+@dataclass(slots=True)
+class AccelerationFactor:
+    """Acceleration Factor."""
+
+    kspace_encoding_step_1: float
+    kspace_encoding_step_2: float
+
+    @property
+    def overall(self) -> float:
+        return self.kspace_encoding_step_1 * self.kspace_encoding_step_2
+
+    @classmethod
+    def from_ismrmrd(cls, data: accelerationFactorType) -> AccelerationFactor:
+        """
+        Create a AccelerationFactor from ismrmrd accelerationFactorType
+        """
+        return cls(data.kspace_encoding_step_1, data.kspace_encoding_step_1)
