0a75fe98f0fff5c95190fcc2b8b316f97ee0c0e7
Felix Zimmermann <fzimmermann89@gmail.com>
2023-07-26 21:55:20 +0200

    Add KData test using pytest
    A really simple random data generation and KData test using pytest.

---
+++ tests/data/RandomGenerator.py
@@ -0,0 +1,197 @@
+import torch
+
+
+class RandomGenerator:
+    """Generate random numbers for testing purposes. Uses a fixed seed to
+    ensure reproducibility.
+
+    provides:
+        scalar uniform random numbers:
+            int8, int16, int32, int64, uint8, uint16, uint32, uint64
+            float32, float64
+            complex64, complex128
+        tensor of uniform random numbers:
+            int8_tensor, int16_tensor, int32_tensor, int64_tensor, uint8_tensor
+            Note: uint16, uint32 and uint64 are not yet supported by pytorch
+            float32_tensor, float64_tensor
+            complex64_tensor, complex128_tensor
+        tuple of uniform random numbers:
+            int8_tuple, int16_tuple, int32_tuple, int64_tuple
+            uint8_tuple, uint16_tuple, uint32_tuple, uint64_tuple
+            float32_tuple, float64_tuple
+            complex64_tuple, complex128_tuple
+    """
+
+    def __init__(self, seed):
+        self.generator = torch.Generator().manual_seed(seed)
+
+    @staticmethod
+    def _clip_bounds(low, high, lowest, highest):
+        """Clips the bounds (low, high) to the given range (lowest, highest)"""
+        if low > high:
+            raise ValueError('low should be lower than high')
+        low = max(low, lowest)
+        high = max(low, min(high, highest))
+        return low, high
+
+    @staticmethod
+    def _dtype_bounds(dtype):
+        """Returns the bounds of a given dtype."""
+        info: torch.finfo | torch.iinfo
+        if dtype in (torch.uint8, torch.int8, torch.int16, torch.int32, torch.int64):
+            # Integer types
+            info = torch.iinfo(dtype)
+        elif dtype is None:
+            # Defaults to pytorch default dtype
+            info = torch.finfo()
+        else:
+            # Float types
+            info = torch.finfo(dtype)
+        return (info.min, info.max)
+
+    def _randint(self, size, low, high, dtype=torch.int64):
+        """Generates uniform random integers in the range [low, high) with the
+        given dtype."""
+        low, high = self._clip_bounds(low, high, *self._dtype_bounds(dtype))
+        return torch.randint(low, high, size, generator=self.generator, dtype=dtype)
+
+    def _rand(self, size, low, high, dtype=torch.float32):
+        """Generates uniform random floats in the range [low, high) with the
+        given dtype."""
+        low, high = self._clip_bounds(low, high, *self._dtype_bounds(dtype))
+        return (torch.rand(size, generator=self.generator, dtype=dtype) * (high - low)) + low
+
+    def float32_tensor(self, size: tuple[int, ...] = (1,), low: float = 0.0, high: float = 1.0):
+        return self._rand(size, low, high, torch.float32)
+
+    def float64_tensor(self, size: tuple[int, ...] = (1,), low: float = 0.0, high: float = 1.0):
+        return self._rand(size, low, high, torch.float64)
+
+    def complex64_tensor(self, size: tuple[int, ...] = (1,), low: float = 0.0, high: float = 1.0):
+        if low < 0:
+            raise ValueError('low/high refer to the amplitude and must be positive')
+        amp = self.float32_tensor(size, low, high)
+        phase = self.float32_tensor(size, -torch.pi, torch.pi)
+        return (amp * torch.exp(1j * phase)).to(dtype=torch.complex64)
+
+    def complex128_tensor(self, size: tuple[int, ...] = (1,), low: float = 0.0, high: float = 1.0):
+        if low < 0:
+            raise ValueError('low/high refer to the amplitude and must be positive')
+        amp = self.float64_tensor(size, low, high)
+        phase = self.float64_tensor(size, -torch.pi, torch.pi)
+        return (amp * torch.exp(1j * phase)).to(dtype=torch.complex128)
+
+    def int8_tensor(self, size: tuple[int, ...] = (1,), low: int = -1 << 7, high: int = 1 << 7):
+        return self._randint(size, low, high, dtype=torch.int8)
+
+    def int16_tensor(self, size: tuple[int, ...] = (1,), low: int = -1 << 15, high: int = 1 << 15):
+        return self._randint(size, low, high, dtype=torch.int16)
+
+    def int32_tensor(self, size: tuple[int, ...] = (1,), low: int = -1 << 31, high: int = 1 << 31):
+        return self._randint(size, low, high, dtype=torch.int32)
+
+    def int64_tensor(self, size: tuple[int, ...] = (1,), low: int = -1 << 63, high: int = 1 << 63):
+        return self._randint(size, low, high, dtype=torch.int64)
+
+    # There is no uint32 in pytorch yet
+    # def uint32_tensor(self, size: tuple[int, ...] = (1,), low: int = 0, high: int = 1 << 32):
+    #     return self._randint(size, low, high, dtype=torch.int32)
+
+    # There is no uint64 in pytorch yet
+    # def uint64_tensor(self, size: tuple[int, ...] = (1,), low: int = 0, high: int = 1 << 64):
+    #    return self._randint(size, low, high, dtype=torch.uint64)
+
+    def uint8_tensor(self, size: tuple[int, ...] = (1,), low: int = 0, high: int = 1 << 8):
+        return self._randint(size, low, high, dtype=torch.uint8)
+
+    def bool(self) -> bool:
+        return self.uint8(0, 1) == 1
+
+    def float32(self, low: float = 0.0, high: float = 1.0) -> float:
+        return self.float32_tensor((1,), low, high).item()
+
+    def float64(self, low: float = 0.0, high: float = 1.0) -> float:
+        return self.float64_tensor((1,), low, high).item()
+
+    def complex64(self, low: float = 0, high: float = 1.0) -> complex:
+        return self.complex64_tensor((1,), low, high).item()
+
+    def complex128(self, low: float = 0, high: float = 1.0) -> complex:
+        return self.complex128_tensor((1,), low, high).item()
+
+    def uint8(self, low: int = 0, high: int = 1 << 8) -> int:
+        return self.uint8_tensor((1,), low, high).item()
+
+    def uint16(self, low: int = 0, high: int = 1 << 16) -> int:
+        return self.int32_tensor((1,), low, high).item()
+
+    def uint32(self, low: int = 0, high: int = 1 << 32) -> int:
+        # using int64 to avoid overflow
+        return self.int64_tensor((1,), low, high).item()
+
+    def int8(self, low: int = -1 << 7, high: int = 1 << 7 - 1) -> int:
+        return self.int8_tensor((1,), low, high).item()
+
+    def int16(self, low: int = -1 << 15, high: int = 1 << 15 - 1) -> int:
+        return self.int16_tensor((1,), low, high).item()
+
+    def int32(self, low: int = -1 << 31, high: int = 1 << 31 - 1) -> int:
+        return self.int32_tensor((1,), low, high).item()
+
+    def int64(self, low: int = -1 << 63, high: int = 1 << 63 - 1) -> int:
+        return self.int64_tensor((1,), low, high).item()
+
+    def uint64(self, low: int = 0, high: int = 1 << 64) -> int:
+        # pytorch does not support uint64, so we use int64 instead
+        # and then convert to uint64
+        range = high - low
+        if low < 0:
+            raise ValueError('Low must be positive')
+        if range > 1 << 64:
+            raise ValueError('Range too large')
+        new_low = -1 << 63
+        new_high = new_low + range
+        value = self.int64(new_low, new_high) - new_low + low
+        return value
+
+    def float32_tuple(self, size: int, low: float = 0, high: float = 1) -> tuple[float, ...]:
+        return tuple(self.float32_tensor((size,), low, high))
+
+    def float64_tuple(self, size: int, low: float = 0, high: float = 1) -> tuple[float, ...]:
+        return tuple(self.float64_tensor((size,), low, high))
+
+    def complex64_tuple(self, size: int, low: float = 0, high: float = 1) -> tuple[complex, ...]:
+        return tuple(self.complex64_tensor((size,), low, high))
+
+    def complex128_tuple(self, size: int, low: float = 0, high: float = 1) -> tuple[complex, ...]:
+        return tuple(self.complex128_tensor((size,), low, high))
+
+    def uint8_tuple(self, size: int, low: int = 0, high: int = 1 << 8) -> tuple[int, ...]:
+        return tuple(self.uint8_tensor((size,), low, high))
+
+    def uint16_tuple(self, size: int, low: int = 0, high: int = 1 << 16) -> tuple[int, ...]:
+        # no uint16_tensor, so we use uint16 instead
+        return tuple([self.uint16(low, high) for _ in range(size)])
+
+    def uint32_tuple(self, size: int, low: int = 0, high: int = 1 << 32) -> tuple[int, ...]:
+        # no uint32_tensor, so we use uint32 instead
+        return tuple([self.uint32(low, high) for _ in range(size)])
+
+    def uint64_tuple(self, size: int, low: int = 0, high: int = 1 << 64) -> tuple[int, ...]:
+        # no uint64_tensor, so we use uint64 instead
+        return tuple([self.uint64(low, high) for _ in range(size)])
+
+    def int8_tuple(self, size: int, low: int = -1 << 7, high: int = 1 << 7) -> tuple[int, ...]:
+        return tuple(self.int8_tensor((size,), low, high))
+
+    def int16_tuple(self, size: int, low: int = -1 << 15, high: int = 1 << 15) -> tuple[int, ...]:
+        return tuple(self.int16_tensor((size,), low, high))
+
+    def int32_tuple(self, size: int, low: int = -1 << 31, high: int = 1 << 31) -> tuple[int, ...]:
+        return tuple(self.int32_tensor((size,), low, high))
+
+    def int64_tuple(self, size: int, low: int = -1 << 63, high: int = 1 << 63) -> tuple[int, ...]:
+        return tuple(self.int64_tensor((size,), low, high))
+
+    def ascii(self, size: int) -> str:
+        return ''.join([chr(self.uint8(32, 127)) for _ in range(size)])
---
+++ tests/data/conftest.py
@@ -0,0 +1,156 @@
+import tempfile
+
+import ismrmrd
+import pytest
+from ismrmrd import xsd
+from RandomGenerator import RandomGenerator
+from xsdata.models.datatype import XmlDate
+from xsdata.models.datatype import XmlTime
+
+from mrpro.data.enums import AcqFlags
+
+
+def generate_random_encodingcounter_properties(generator: RandomGenerator):
+    return {
+        'kspace_encode_step_1': generator.uint16(),
+        'kspace_encode_step_2': generator.uint16(),
+        'average': generator.uint16(),
+        'slice': generator.uint16(),
+        'contrast': generator.uint16(),
+        'phase': generator.uint16(),
+        'repetition': generator.uint16(),
+        'set': generator.uint16(),
+        'segment': generator.uint16(),
+        'user': generator.uint16_tuple(8),
+    }
+
+
+def generate_random_acquisition_properties(generator: RandomGenerator):
+    idx_properties = generate_random_encodingcounter_properties(generator)
+    return {
+        'flags': generator.uint64(high=2**38 - 1),
+        'measurement_uid': generator.uint32(),
+        'scan_counter': generator.uint32(),
+        'acquisition_time_stamp': generator.uint32(),
+        'physiology_time_stamp': generator.uint32_tuple(3),
+        'available_channels': generator.uint16(),
+        'channel_mask': generator.uint32_tuple(16),
+        'discard_pre': generator.uint16(),
+        'discard_post': generator.uint16(),
+        'center_sample': generator.uint16(),
+        'encoding_space_ref': generator.uint16(),
+        'sample_time_us': generator.float32(),
+        'position': generator.float32_tuple(3),
+        'read_dir': generator.float32_tuple(3),
+        'phase_dir': generator.float32_tuple(3),
+        'slice_dir': generator.float32_tuple(3),
+        'patient_table_position': generator.float32_tuple(3),
+        'idx': ismrmrd.EncodingCounters(**idx_properties),
+        'user_int': generator.uint32_tuple(8),
+        'user_float': generator.float32_tuple(8),
+    }
+
+
+def generate_random_trajectory(generator: RandomGenerator, shape=(256, 2)):
+    return generator.float32_tensor(shape)
+
+
+def generate_random_kdata(
+    generator: RandomGenerator,
+    shape=(32, 256),
+):
+    return generator.complex64_tensor(shape)
+
+
+@pytest.fixture(params=({'seed': 0, 'Ncoils': 32, 'Nsamples': 256},))
+def random_acquisition(request):
+    seed, Ncoils, Nsamples = request.param['seed'], request.param['Ncoils'], request.param['Nsamples']
+    generator = RandomGenerator(seed)
+    kdata = generate_random_kdata(generator, (Ncoils, Nsamples))
+    traj = generate_random_trajectory(generator, (Nsamples, 2))
+    header = generate_random_acquisition_properties(generator)
+    header['flags'] &= ~AcqFlags.ACQ_IS_NOISE_MEASUREMENT.value
+    return ismrmrd.Acquisition.from_array(kdata, traj, **header)
+
+
+@pytest.fixture(params=({'seed': 1, 'Ncoils': 32, 'Nsamples': 256},))
+def random_noise_acquisition(request):
+    seed, Ncoils, Nsamples = request.param['seed'], request.param['Ncoils'], request.param['Nsamples']
+    generator = RandomGenerator(seed)
+    kdata = generate_random_kdata(generator, (Ncoils, Nsamples))
+    traj = generate_random_trajectory(generator, (Nsamples, 2))
+    header = generate_random_acquisition_properties(generator)
+    header['flags'] |= AcqFlags.ACQ_IS_NOISE_MEASUREMENT.value
+    return ismrmrd.Acquisition.from_array(kdata, traj, **header)
+
+
+@pytest.fixture(params=({'seed': 0},))
+def full_header(request) -> xsd.ismrmrdschema.ismrmrdHeader:
+    """Generate a full header, i.e. all values used in
+    KHeader.from_ismrmrd_header() are set."""
+
+    seed = request.param['seed']
+    generator = RandomGenerator(seed)
+    encoding = xsd.encodingType(
+        trajectory=xsd.trajectoryType('other'),
+        encodedSpace=xsd.encodingSpaceType(
+            matrixSize=xsd.matrixSizeType(x=generator.int16(), y=generator.uint8(), z=generator.uint8()),
+            fieldOfView_mm=xsd.fieldOfViewMm(x=generator.uint8(), y=generator.uint8(), z=generator.uint8()),
+        ),
+        reconSpace=xsd.encodingSpaceType(
+            matrixSize=xsd.matrixSizeType(x=generator.uint8(), y=generator.uint8(), z=generator.uint8()),
+            fieldOfView_mm=xsd.fieldOfViewMm(x=generator.uint8(), y=generator.uint8(), z=generator.uint8()),
+        ),
+        echoTrainLength=generator.uint8(),
+        parallelImaging=xsd.parallelImagingType(
+            accelerationFactor=xsd.accelerationFactorType(generator.uint8(), generator.uint8()),
+            calibrationMode=xsd.calibrationModeType('other'),
+        ),
+        encodingLimits=xsd.encodingLimitsType(
+            kspace_encoding_step_1=xsd.limitType(0, 0, 0),
+            kspace_encoding_step_2=xsd.limitType(0, 0, 0),
+        ),
+    )
+    measurementInformation = xsd.measurementInformationType(
+        measurementID=generator.ascii(10),
+        seriesDate=XmlDate(generator.uint16(1970, 2030), generator.uint8(1, 12), generator.uint8(0, 30)),
+        seriesTime=XmlTime(generator.uint8(0, 23), generator.uint8(0, 59), generator.uint8(0, 59)),
+        sequenceName=generator.ascii(10),
+    )
+
+    acquisitionSystemInformation = xsd.acquisitionSystemInformationType(
+        systemFieldStrength_T=generator.float32(0, 12),
+        systemVendor=generator.ascii(10),
+        systemModel=generator.ascii(10),
+        receiverChannels=generator.uint16(1, 32),
+    )
+
+    sequenceParameters = xsd.sequenceParametersType(
+        TR=[generator.float32()],
+        TE=[generator.float32()],
+        flipAngle_deg=[generator.float32(low=10, high=90)],
+        echo_spacing=[generator.float32()],
+        sequence_type=generator.ascii(10),
+    )
+
+    # TODO: add everything that to the header
+    return xsd.ismrmrdschema.ismrmrdHeader(
+        encoding=[encoding],
+        sequenceParameters=sequenceParameters,
+        version=generator.int16(),
+        experimentalConditions=xsd.experimentalConditionsType(H1resonanceFrequency_Hz=generator.int32()),
+        measurementInformation=measurementInformation,
+        acquisitionSystemInformation=acquisitionSystemInformation,
+    )
+
+
+@pytest.fixture()
+def random_ismrmrd_file(random_acquisition, random_noise_acquisition, full_header):
+    with tempfile.NamedTemporaryFile(suffix='.h5') as file:
+        dataset = ismrmrd.Dataset(file.name)
+        dataset.append_acquisition(random_acquisition)
+        dataset.append_acquisition(random_noise_acquisition)
+        dataset.write_xml_header(full_header.toXML())
+        dataset.close()
+
+        yield file.name
