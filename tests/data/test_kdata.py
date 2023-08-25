6012ac40c6f8fbfff0cdec4940b71c724361a462
ckolbPTB <christoph.kolbitsch@ptb.de>
2023-08-25 21:17:19 +0200

    phantom improved. add ref image. compare recon to ref image

--- tests/data/test_kdata.py
+++ tests/data/test_kdata.py
@@ -12,8 +12,10 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+import numpy as np
 import pytest
 from IsmrmrdRawData import IsmrmrdRawData
+from IsmrmrdRawData import k2i
 
 from mrpro.data import KData
 from mrpro.data._KTrajectory import DummyTrajectory
@@ -23,7 +25,7 @@ from mrpro.data._KTrajectory import DummyTrajectory
 def ismrmrd_cart(tmp_path_factory):
     # Fully sampled cartesian data set
     ismrmrd_filename = tmp_path_factory.mktemp('mrpro') / 'ismrmrd_cart.h5'
-    ismrmrd_kdat = IsmrmrdRawData(filename=ismrmrd_filename)
+    ismrmrd_kdat = IsmrmrdRawData(filename=ismrmrd_filename, noise_level=0.0)
     ismrmrd_kdat.create()
     return (ismrmrd_kdat)
 
@@ -36,10 +38,20 @@ def ismrmrd_cart_us4(tmp_path_factory):
     ismrmrd_kdat.create()
     return (ismrmrd_kdat)
 
+# Read in data from file
+
 
 def test_KData_from_file(ismrmrd_cart):
     k = KData.from_file(ismrmrd_cart.filename, DummyTrajectory())
     assert k is not None
+
+# Read in data and verify k-space by comparing reconstructed image
+
+
+def test_KData_kspace(ismrmrd_cart):
+    k = KData.from_file(ismrmrd_cart.filename, DummyTrajectory())
+    irec = k2i(k.data, axes=(3, 4))
+    np.testing.assert_almost_equal(irec[0, 0, 0, ...], ismrmrd_cart.imref)
 
 # Expected to fail with DummyTrajectory
 
@@ -48,10 +60,11 @@ def test_KData_from_file_undersampled(ismrmrd_cart_us4):
     with pytest.raises(ValueError):
         KData.from_file(ismrmrd_cart_us4.filename, DummyTrajectory())
 
+# Overwrite some parameters in the header
+
 
 @pytest.mark.parametrize('field,value', [('b0', 11.3), ('tr', [24.3,])])
 def test_KData_modify_header(ismrmrd_cart, field, value):
-    # Overwrite some parameters
     par_dict = {field: value}
     k = KData.from_file(ismrmrd_cart.filename, DummyTrajectory(), header_overwrites=par_dict)
     assert getattr(k.header, field) == value
