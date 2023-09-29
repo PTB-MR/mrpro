94f17a33daf4ac86f99dd10001e0f73539cba533
Felix Zimmermann <fzimmermann89@gmail.com>
2023-09-29 14:48:32 +0200

    automatically detect repeats in KTrajectroy

--- src/mrpro/data/_KTrajectory.py
+++ src/mrpro/data/_KTrajectory.py
@@ -20,13 +20,23 @@ import numpy as np
 import torch
 
 
-@dataclass(slots=True)
+@dataclass(slots=True, init=False)
 class KTrajectory:
-    """K-space trajectory."""
+    """K-space trajectory.
 
-    kx: torch.Tensor
-    ky: torch.Tensor
-    kz: torch.Tensor
+    Order of directions is always kz, ky, kx
+    Shape of each of kx,ky,kz is (other,k2,k1,k0)
+
+    Example for 2D-Cartesian Trajectories:
+        kx changes along k0 and is Frequency Encoding
+        ky changes along k2 and is Phase Encoding
+        kz is zero(1,1,1,1)
+
+    """
+
+    kz: torch.Tensor  # (other,k2,k1,k0) #Phase Encoding direction, k2 if Cartesian
+    ky: torch.Tensor  # (other,k2,k1,k0) #Phase Encoding direction, k1 if Cartesian
+    kx: torch.Tensor  # (other,k2,k1,k0) #Frequency Encoding direction, k0 if Cartesian
 
     @property
     def broadcasted_shape(self) -> tuple[int, ...]:
@@ -43,25 +53,87 @@ class KTrajectory:
             The dimension to stack the tensor along.
         """
         shape = self.broadcasted_shape
-        return torch.stack([traj.expand(*shape) for traj in (self.kx, self.ky, self.kz)], dim=stack_dim)
+        return torch.stack([traj.expand(*shape) for traj in (self.kz, self.ky, self.kx)], dim=stack_dim)
 
-    @classmethod
-    def from_tensor(cls, tensor: torch.Tensor, stack_dim=0) -> KTrajectory:
-        """Create a KTrajectory from a tensor representation of the trajectory.
+    def __init__(
+        self, kz: torch.Tensor, ky: torch.Tensor, kx: torch.Tensor, repeat_detection_tolerance: float | None = 1e-8
+    ):
+        """K-Space Trajectory dataclass
+
+        Reduces repeated dimensions to singletons if repeat_detection_tolerance
+        is not set to None.
 
         Parameters
         ----------
-        tensor:
-            The tensor representation of the trajectory.
-        stack_dim:
-            The dimension in the tensor the directions have been stacked along.
+        kz, ky, kx:
+            Trajectory coordinates to set
+        repeat_detection_tolerance:
+            Tolerance for repeat detection. Set to None to disable.
         """
-        return cls(*torch.split(tensor, 1, dim=stack_dim))
+        if repeat_detection_tolerance is not None:
+            kz, ky, kx = (KTrajectory._remove_repeat(tensor, repeat_detection_tolerance) for tensor in (kz, ky, kx))
 
-    def __post_init__(self):
+        self.kz = kz
+        self.ky = ky
+        self.kx = kx
+
         try:
             shape = self.broadcasted_shape
             if len(shape) != 4:
                 raise ValueError('The k-space trajectory tensors should each have 4 dimensions.')
         except ValueError:
             raise ValueError('The k-space trajectory dimensions must be broadcastable.')
+
+    @classmethod
+    def from_tensor(
+        cls, tensor: torch.Tensor, stack_dim: int = 0, repeat_detection_tolerance: float | None = 1e-8
+    ) -> KTrajectory:
+        """Create a KTrajectory from a tensor representation of the trajectory.
+
+        Reduces repeated dimensions to singletons if repeat_detection_tolerance
+        is not set to None.
+
+
+        Parameters
+        ----------
+        tensor:
+            The tensor representation of the trajectory.
+            This should be a 5-dim tensor, with (kz,ky,kx) stacked in this order along stack_dim
+        stack_dim:
+            The dimension in the tensor the directions have been stacked along.
+        repeat_detection_tolerance:
+            detects if broadcasting can be used, i.e. if dimensions are repeated.
+            Set to None to disable.
+
+        """
+
+        split_ks = torch.split(tensor, 1, dim=stack_dim)
+
+        if repeat_detection_tolerance is not None:
+            split_ks = (KTrajectory._remove_repeat(tensor, repeat_detection_tolerance) for tensor in split_ks)
+
+        return cls(*split_ks)
+
+    @staticmethod
+    def _remove_repeat(tensor: torch.Tensor, tol: float):
+        """Replace dimensions with all equal values with singletons
+
+        Parameters
+        ----------
+        tensor:
+            The tensor. Must be real
+        tol:
+            The tolerance
+
+
+        """
+        # TODO: Move to utilities
+
+        def can_be_singleton(dim: int) -> bool:
+            # If the distance between min and max is smaller than the tolerance, all values are the same.
+            return torch.all((tensor.amax(dim=dim) - tensor.amin(dim=dim)) <= tol).item()
+
+        take_first = slice(0, 1)
+        take_all = slice(None)
+        index = tuple(take_first if can_be_singleton(dim) else take_all for dim in range(tensor.ndim))
+        return tensor[index]
