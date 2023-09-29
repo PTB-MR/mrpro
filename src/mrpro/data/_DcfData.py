a660e0afd45fdc2882885a15192e617cab637f10
Felix Zimmermann <fzimmermann89@gmail.com>
2023-09-29 22:23:40 +0200

    handle broadcasted, non-uniform dimensions

--- src/mrpro/data/_DcfData.py
+++ src/mrpro/data/_DcfData.py
@@ -15,7 +15,9 @@
 from __future__ import annotations
 
 import dataclasses
+from collections.abc import Callable
 from concurrent.futures import ThreadPoolExecutor
+from functools import reduce
 from itertools import product
 
 import numpy as np
@@ -25,6 +27,8 @@ from scipy.spatial import Voronoi
 
 from mrpro.data import KTrajectory
 
+UNIQUE_ROUNDING_DECIMALS = 15
+
 
 @dataclasses.dataclass(slots=True, frozen=False)
 class DcfData:
@@ -33,7 +37,38 @@ class DcfData:
     data: torch.Tensor
 
     @staticmethod
-    def _dcf_using_voronoi(traj: torch.Tensor) -> torch.Tensor:
+    def _dcf_1d(traj: torch.Tensor) -> torch.Tensor:
+        """Calculate sample density compensation function for 1D trajectory.
+
+        Parameters
+        ----------
+        traj:
+            k-space positions, 1D tensor
+        """
+
+        traj_sorted, inverse, counts = torch.unique(
+            torch.round(traj, decimals=UNIQUE_ROUNDING_DECIMALS), sorted=True, return_inverse=True, return_counts=True
+        )
+
+        # For a sorted trajectory: x0 x1 x2 ... xN
+        # We assign the point at x0 the area (x1-x0) / 2 * 2
+        # We assign the point at x1 the area (x1-x0) / 2  + (x2-x1) / 2
+        # We assign the pint at xN the area (xN-XN-1) / 2 * 2
+        # This is done by central differences (-1,0,1). As the be complicated,
+        # we just append/prepend the correct values afterwards.
+
+        kernel = torch.tensor([-1 / 2, 0, 1 / 2], dtype=torch.float32, device=traj.device).reshape(1, 1, 3)
+        central_diff = torch.nn.functional.conv1d(traj_sorted[None, None, :], kernel)[0, 0]
+        first = traj_sorted[1] - traj_sorted[0]
+        last = traj_sorted[-1] - traj_sorted[-2]
+        central_diff = torch.cat((first[None], central_diff, last[None]), -1)
+
+        # Repeated points are reduced by the number of repeats
+        dcf = torch.nan_to_num(1 / (central_diff * counts))[inverse]
+        return dcf
+
+    @staticmethod
+    def _dcf_2d3d_voronoi(traj: torch.Tensor) -> torch.Tensor:
         """Calculate sample density compensation function using voronoi method.
 
         Points at the edge of k-space are detected as outliers and assigned the
@@ -48,10 +83,8 @@ class DcfData:
         -------
             density compensation values (1, k2, k1, k0)
         """
-        UNIQUE_ROUNDING_DECIMALS = 15
 
         # 2D and 3D trajectories supported
-
         dim = traj.shape[0]
         if dim not in (2, 3):
             raise ValueError(f'Only 2D or 3D trajectories supported, not {dim}D.')
@@ -74,7 +107,7 @@ class DcfData:
         regions = [vdiagram.regions[r] for r in vdiagram.point_region[: -len(corner_points)]]  # Ignore corner points
         vertices = [vdiagram.vertices[region] for region in regions]
 
-        # Calculate volume/area of voronoi cells
+        # Calculate volume/area of voronoi cells using threads (ConvexHull is thread safe and drops the GIL)
         future = ThreadPoolExecutor(max_workers=torch.get_num_threads()).map(lambda v: ConvexHull(v).volume, vertices)
         dcf = np.array(list(future))
 
@@ -106,19 +139,64 @@ class DcfData:
             torch.Tensor containing k-space points (other, 2 or 3, k2, k1, k0).
         """
 
-        ks = [traj.kz, traj.ky, traj.kx]
-        for i, k in enumerate(ks):
-            if any(all(k.shape[ax] == 1 for ax in two_axes) for two_axes in [(-1, -2), (-1, -3), (-2, -3)]):
-                # Found a direction with at least two singleton dimensions, i.e. we have a 2D trajectory in 3D space
-                # Remove this direction from the list of k-space points and
-                # calculate dcf for the remaining 2D trajectory
-                ks.pop(i)
-                new_traj = torch.stack(torch.broadcast_tensors(*ks), 1)
-                dcf = torch.stack([DcfData._dcf_using_voronoi(t) for t in new_traj])
-                dcf = dcf.expand(traj.broadcasted_shape)
-                break
-        else:
-            # Full 3D trajectory
-            dcf = torch.stack([DcfData._dcf_using_voronoi(t) for t in traj.as_tensor(1)])
-
+        ks_needing_voronoi = []
+        dcfs = []
+        for k in [traj.kz, traj.ky, traj.kx]:
+            non_singleton_dims = tuple(dim for dim in (-1, -2, -3) if k.shape[dim] != 1)
+            if len(non_singleton_dims) == 1:
+                # Found a direction with two singleton dimensions and one non-singleton dimension
+                # We can handle this as a 1D trajectory
+                dcfs.append(smap(DcfData._dcf_1d, k, non_singleton_dims))
+            elif len(non_singleton_dims) == 0:
+                # Found a direction with only singleton dimensions, i.e. constant dcf
+                dcfs.append(torch.ones_like(k))
+            else:
+                # A Full Dimension needing voronoi
+                ks_needing_voronoi.append(k)
+        if ks_needing_voronoi:
+            # Any full dimensions needing voronoi
+            dcfs.append(smap(DcfData._dcf_2d3d_voronoi, torch.stack(ks_needing_voronoi, -4), 4))
+        # Multiply all dcfs together
+        dcf = reduce(torch.mul, dcfs)
         return cls(data=dcf)
+
+
+def smap(
+    fun: Callable[[torch.Tensor], torch.Tensor], tensor: torch.Tensor, fun_dims: tuple[int, ...] | int = (-1,)
+) -> torch.Tensor:
+    """Apply a function to a tensor serially along multiple dimensions.
+
+    The function is applied serially without a batch dimensions.
+    Compared to torch.vmap, it works with arbitrary functions, but is slower.
+
+    Parameters
+    ----------
+    fun
+        Function to apply to the tensor.
+        Should handle len(fun_dims) dimensions and not change the number of dimensions.
+    tensor
+        Tensor to apply the function to.
+    fun_dims
+        Dimensions NOT to be batched / dimensions that are passed to the function
+        tuple of dimension indices (negative indices are supported) or an integer
+        an integer n means the last n dimensions are passed to the function
+    """
+    # TODO: Move to utilities
+    if isinstance(fun_dims, int):
+        # use the last fun_dims dimensions for the function
+        moved = tensor
+        first_fun_dim = -fun_dims
+    else:
+        # Move fun_dims to the end
+        fun_dims_dst = tuple(range(-len(fun_dims), 0))
+        moved = tensor.moveaxis(fun_dims, fun_dims_dst)
+        first_fun_dim = fun_dims_dst[0]
+
+    reshaped = moved.flatten(end_dim=first_fun_dim - 1)  # shape: (prod(batch_dims), fun_dim_1, ..., fun_dim_n)
+    result_reshaped = torch.stack([fun(x) for x in reshaped])
+    result = result_reshaped.reshape(moved.shape[:first_fun_dim] + result_reshaped.shape[1:])
+
+    if not isinstance(fun_dims, int):
+        # Move fun_dims back to their original position if we moved them
+        result = result.moveaxis(fun_dims_dst, fun_dims)
+    return result
