2c16f9490aeb10ba396d1f2847ac89320160f00d
Felix Zimmermann <fzimmermann89@gmail.com>
2023-10-04 20:19:31 +0200

    fix order 3d meshgrid. add non-uniform dcf test.

--- tests/data/test_dcf_data.py
+++ tests/data/test_dcf_data.py
@@ -12,8 +12,9 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+import math
+
 import torch
-from einops import rearrange
 
 from mrpro.data import DcfData
 from mrpro.data import KTrajectory
@@ -63,8 +64,9 @@ def test_dcf_2d_rad_traj_voronoi():
 
 
 def test_dcf_3d_cart_traj_broadcast_voronoi():
-    """Compare voronoi-based dcf calculation for 3D regular Cartesian
-    trajectory to analytical solution which is 1 for each k-space point."""
+    """Compare voronoi-based dcf calculation for broadcasted 3D regular
+    Cartesian trajectory to analytical solution which is 1 for each k-space
+    point."""
     # 3D trajectory with points on Cartesian grid with step size of 1
     nk0 = 20
     nk1 = 16
@@ -83,13 +85,13 @@ def test_dcf_3d_cart_traj_broadcast_voronoi():
 
 
 def test_dcf_3d_cart_full_traj_voronoi():
-    """Compare voronoi-based dcf calculation for 3D regular Cartesian
+    """Compare voronoi-based dcf calculation for full 3D regular Cartesian
     trajectory to analytical solution which is 1 for each k-space point."""
     # 3D trajectory with points on Cartesian grid with step size of 1
     nk0 = 20
     nk1 = 16
     nk2 = 40
-    kx, ky, kz = torch.meshgrid(
+    ky, kz, kx = torch.meshgrid(
         torch.linspace(-nk1 // 2, nk1 // 2 - 1, nk1),
         torch.linspace(-nk2 // 2, nk2 // 2 - 1, nk2),
         torch.linspace(-nk0 // 2, nk0 // 2 - 1, nk0),
@@ -103,6 +105,53 @@ def test_dcf_3d_cart_full_traj_voronoi():
     # Do not test outer points because they have to be approximated and cannot be calculated
     # accurately using voronoi
     torch.testing.assert_close(dcf.data[:, 1:-1, 1:-1, 1:-1], dcf_analytical[:, 1:-1, 1:-1, 1:-1])
+
+
+def test_dcf_3d_cart_nonuniform_traj_voronoi():
+    """Compare voronoi-based dcf calculation for 3D nonunifrm Cartesian
+    trajectory to analytical solution which is 1 for each k-space point."""
+
+    def k_range(N: int, *steps: float):
+        """Create a tensor with N values, steps appart."""
+        r = torch.tensor(steps).repeat(math.ceil(N / len(steps))).ravel()[:N]
+        r = torch.cumsum(r, 0)
+        r -= r.mean()
+        return r
+
+    k0_range = k_range(10, 1.0, 0.5)
+    k1_range = k_range(20, 1.0, 0.5, 0.25)
+    k2_range = k_range(30, 1.0)
+
+    ky_full, kz_full, kx_full = torch.meshgrid(
+        k1_range,
+        k2_range,
+        k0_range,
+        indexing='xy',
+    )
+    ktraj_full = KTrajectory(
+        kz_full[None, ...], ky_full[None, ...], kx_full[None, ...], repeat_detection_tolerance=None
+    )
+
+    kx_broadcast = k0_range[None, None, :]
+    ky_broadcast = k1_range[None, :, None]
+    kz_broadcast = k2_range[:, None, None]
+    ktraj_broadcast = KTrajectory(
+        kz_broadcast[None, ...], ky_broadcast[None, ...], kx_broadcast[None, ...], repeat_detection_tolerance=None
+    )
+
+    # Sanity check inputs
+    torch.testing.assert_close(ktraj_full.as_tensor(), ktraj_broadcast.as_tensor())
+    assert ktraj_full.broadcasted_shape == (1, len(k2_range), len(k1_range), len(k0_range))
+    torch.testing.assert_close(kx_full[0, 0, :], k0_range)  # kx changes along k0
+    torch.testing.assert_close(ky_full[0, :, 0], k1_range)  # ky changes along k1
+    torch.testing.assert_close(kz_full[:, 0, 0], k2_range)  # kz changes along k2
+
+    dcf_full = DcfData.from_traj_voronoi(ktraj_full)
+    dcf_broadcast = DcfData.from_traj_voronoi(ktraj_broadcast)
+
+    # Do not test outer points because they have to be approximated and cannot be calculated
+    # accurately using voronoi
+    torch.testing.assert_close(dcf_full.data[:, 1:-1, 1:-1, 1:-1], dcf_broadcast.data[:, 1:-1, 1:-1, 1:-1])
 
 
 def test_dcf_rpe_traj_voronoi():
