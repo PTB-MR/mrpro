ed9887edaba92b5b2128310c290023c93e767d5b
Felix <fzimmermann89@gmail.com>
2023-10-13 16:27:00 +0200

    Fix DCF logic bugs (#86)
    * parameterize dcf tests. add testcase for #84.
    * fix #84: invert broadcast logic to check for dims
    * handle more edge-cases such as Cartesian trajectory with kx<3, ky<3, kz<3

--- src/mrpro/data/_DcfData.py
+++ src/mrpro/data/_DcfData.py
@@ -58,10 +58,17 @@ class DcfData:
         # we assign the point at xN the area (xN - xN-1) / 2 + (xN - xN-1) / 2.
 
         kernel = torch.tensor([-1 / 2, 0, 1 / 2], dtype=torch.float32, device=traj.device).reshape(1, 1, 3)
-        central_diff = torch.nn.functional.conv1d(traj_sorted[None, None, :], kernel)[0, 0]
-        first = traj_sorted[1] - traj_sorted[0]
-        last = traj_sorted[-1] - traj_sorted[-2]
-        central_diff = torch.cat((first[None], central_diff, last[None]), -1)
+
+        if (elements := len(traj_sorted)) >= 3:
+            central_diff = torch.nn.functional.conv1d(traj_sorted[None, None, :], kernel)[0, 0]
+            first = traj_sorted[1] - traj_sorted[0]
+            last = traj_sorted[-1] - traj_sorted[-2]
+            central_diff = torch.cat((first[None], central_diff, last[None]), -1)
+        elif elements == 2:
+            diff = traj_sorted[1] - traj_sorted[0]
+            central_diff = torch.cat((diff[None], diff[None]), -1)
+        else:
+            central_diff = torch.ones_like(traj_sorted)
 
         # Repeated points are reduced by the number of repeats
         dcf = torch.nan_to_num(central_diff / counts)[inverse]
@@ -121,7 +128,8 @@ class DcfData:
         idx_outliers = np.nonzero(dcf > upper_bound)
         num_outliers = len(idx_outliers[0])
         high_values_start = int(0.99 * (len(dcf_sorted) - num_outliers))
-        fill_value = np.average(dcf_sorted[high_values_start:-num_outliers])
+        high_values_end = len(dcf_sorted) - num_outliers  # this works also for num_outliers==0
+        fill_value = np.average(dcf_sorted[high_values_start:high_values_end])
         dcf[idx_outliers] = fill_value
 
         # Sort dcf values back into the original order (i.e. before calling unique)
@@ -136,26 +144,37 @@ class DcfData:
         Parameters
         ----------
         traj
-            torch.Tensor containing k-space points (other, 2 or 3, k2, k1, k0).
+            Trajectory to calculate the density compensation for. Can be broadcasted or dense.
         """
-
-        ks_needing_voronoi = []
         dcfs = []
-        for k in [traj.kz, traj.ky, traj.kx]:
-            non_singleton_dims = tuple(dim for dim in (-1, -2, -3) if k.shape[dim] != 1)
-            if len(non_singleton_dims) == 1:
-                # Found a direction with two singleton dimensions and one non-singleton dimension
-                # We can handle this as a 1D trajectory
-                dcfs.append(smap(DcfData._dcf_1d, k, non_singleton_dims))
-            elif len(non_singleton_dims) == 0:
-                # Found a direction with only singleton dimensions, i.e. constant dcf
-                dcfs.append(torch.ones_like(k))
+
+        ks = [traj.kz, traj.ky, traj.kx]
+        spatial_dims = (-3, -2, -1)
+        ks_needing_voronoi = set()
+        for dim in spatial_dims:
+            non_singleton_ks = [ax for ax in ks if ax.shape[dim] != 1]
+            if len(non_singleton_ks) == 1:
+                # Found a dimension with only one non-singleton axes in ks
+                # --> Can handle this as a 1D trajectory
+                dcfs.append(smap(DcfData._dcf_1d, non_singleton_ks.pop(), (dim,)))
+            elif len(non_singleton_ks) > 0:
+                # More than one of the ks is non-singleton
+                # --> A full dimension needing voronoi
+                ks_needing_voronoi |= set(non_singleton_ks)
             else:
-                # A Full Dimension needing voronoi
-                ks_needing_voronoi.append(k)
+                # A dimension in which each of ks is singleton
+                # --> Don't need to do anything
+                pass
+
         if ks_needing_voronoi:
-            # Any full dimensions needing voronoi
-            dcfs.append(smap(DcfData._dcf_2d3d_voronoi, torch.stack(ks_needing_voronoi, -4), 4))
-        # Multiply all dcfs together
-        dcf = reduce(torch.mul, dcfs)
+            # Handle full dimensions needing voronoi
+            dcfs.append(smap(DcfData._dcf_2d3d_voronoi, torch.stack(list(ks_needing_voronoi), -4), 4))
+
+        if dcfs:
+            # Multiply all dcfs together
+            dcf = reduce(torch.mul, dcfs)
+        else:
+            # Edgecase: Only singleton spatial dimensions
+            dcf = torch.ones(*traj.broadcasted_shape[-3:], 1, 1, 1, device=traj.kx.device)
+
         return cls(data=dcf)
