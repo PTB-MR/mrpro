a98af2f4d2f1f8749ef3235baa767914637fc726
David Schote <david.schote@web.de>
2023-10-18 15:29:51 +0200

    Fix quotes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ import os
 import sys
 from sphinx_pyproject import SphinxConfig
 
-config = SphinxConfig("../../pyproject.toml", globalns=globals())
+config = SphinxConfig('../../pyproject.toml', globalns=globals())
 sys.path.insert(0, os.path.abspath('../../src'))  # Source code dir relative to this file
 
 # -- Project information -----------------------------------------------------
