d58fa631800ca1db11c48635c781408e98dd9cfe
Felix Zimmermann <fzimmermann89@gmail.com>
2023-10-19 14:09:36 +0200

    operator template

---
+++ src/mrpro/operators/_LinearOperator.py
@@ -0,0 +1,56 @@
+"""Linear Operators."""
+
+# Copyright 2023 Physikalisch-Technische Bundesanstalt
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#       http://www.apache.org/licenses/LICENSE-2.0
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+
+from abc import ABC
+from abc import abstractmethod
+
+import torch
+from torch import nn
+
+
+class LinearOperator(ABC, nn.Module):
+    """General Linear Operator."""
+
+    @abstractmethod
+    def forward(self, x: torch.Tensor):
+        ...
+
+    @abstractmethod
+    def adjoint(self, x: torch.Tensor):
+        ...
+
+    @property
+    def H(self):
+        """Adjoint operator."""
+        return AdjointLinearOperator(self)
+
+
+class AdjointLinearOperator(LinearOperator):
+    def __init__(self, operator: LinearOperator):
+        super().__init__()
+        self._operator = operator
+
+    def forward(self, x: torch.Tensor):
+        """Adjoint of the operator."""
+        return self._operator.adjoint(x)
+
+    def adjoint(self, x: torch.Tensor):
+        """Operator."""
+        return self._operator.forward(x)
+
+    @property
+    def H(self):
+        """Adjoint of adjoint operator."""
+        return self.operator
--- src/mrpro/operators/__init__.py
+++ src/mrpro/operators/__init__.py
@@ -1,1 +1,3 @@
 from mrpro.operators._SensitivityOp import SensitivityOp
+from mrpro.operators._LinearOperator import LinearOperator
+from mrpro.operators._Operator import Operator
