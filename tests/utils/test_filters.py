f3dff88aa918988b0dea16f938ab1f29964282cb
Patrick Schuenke <patrick.schuenke@gmail.com>
2023-10-19 13:41:59 +0200

    add utils test folder

---
+++ tests/utils/test_filters.py
@@ -0,0 +1,28 @@
+"""Tests for filters."""
+
+# Copyright 2023 Physikalisch-Technische Bundesanstalt
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#       http://www.apache.org/licenses/LICENSE-2.0
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import torch
+
+from mrpro.data import SpatialDimension
+from mrpro.utils.filters import spatial_uniform_filter_3d
+
+
+def test_spatial_uniform_filter_3d():
+    """Test spatial_uniform_filter_3d."""
+    data = torch.zeros(1, 1, 5, 5, 5)
+    data[..., 2, 2, 2] = 1.0
+
+    # test with SpatialDimension
+    res = spatial_uniform_filter_3d(data, SpatialDimension(3, 3, 3))
+    assert torch.sum(res) == torch.sum(data)
