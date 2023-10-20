24020dc0a665a555fa6d63786af15f87d6c5375e
guastinimara <mara.guastini@gmail.com>
2023-10-20 17:40:52 +0200

    updated SensitivityOp ad tests from comments

--- src/mrpro/operators/_SensitivityOp.py
+++ src/mrpro/operators/_SensitivityOp.py
@@ -1,5 +1,17 @@
 """Class for Sensitivity Operator."""
 
+# Copyright 2023 Physikalisch-Technische Bundesanstalt
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#       http://www.apache.org/licenses/LICENSE-2.0
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 import torch
 
 from mrpro.data import CsmData
@@ -11,13 +23,28 @@ class SensitivityOp:  # todo: later put (LinearOp) here
     def __init__(self, csm: CsmData):
         self.C = csm.data
 
-    def forward(self, u: torch.Tensor):
-        """Apply the forward operator, thus expand the num_coils dimension."""
-        Cu = self.C * u
-        return Cu
+    def forward(self, img_data: torch.Tensor):
+        """Apply the forward operator, thus expand the coils dimension.
 
-    def adjoint(self, v: torch.Tensor):
-        """Apply the adjoint operator, thus reduce the num_coils dimension."""
-        C_H = torch.conj(self.C)
-        C_Hv = torch.sum(C_H * v, dim=1).unsqueeze(1)
-        return C_Hv
+        Parameters
+        ----------
+        img_data
+            image data tensor with dimensions (all_other, 1, z, y, x).
+            The method returns an image data tensor with dimensions
+            (all_other, coils, z, y, x), where coils is the number of coils
+            given by csm.
+        """
+        return self.C * img_data
+
+    def adjoint(self, img_data: torch.Tensor):
+        """Apply the adjoint operator, thus reduce the coils dimension.
+
+        Parameters
+        ----------
+        img_data
+            image data tensor with dimensions (all_other, coils, z, y, x).
+            The method returns an image data tensor with dimensions
+            (all_other, 1, z, y, x).
+        """
+
+        return (self.C.conj() * img_data).sum(-4, keepdim=True)
