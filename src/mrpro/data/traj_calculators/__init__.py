c3a8b9ce9918291e8fca868c6a5b9b1622bc5eb7
Christoph Kolbitsch <christoph.kolbitsch@ptb.de>
2023-10-26 19:49:04 +0200

    Ismrmrd traj (#113)
    * ismrmrd test data radial traj added
    
    * read out trajectory from ismrmrd
    
    * tests for ismrmrd traj added

--- src/mrpro/data/traj_calculators/__init__.py
+++ src/mrpro/data/traj_calculators/__init__.py
@@ -2,3 +2,4 @@ from mrpro.data.traj_calculators._KTrajectoryCalculator import KTrajectoryCalcul
 from mrpro.data.traj_calculators._KTrajectoryRpe import KTrajectoryRpe
 from mrpro.data.traj_calculators._KTrajectorySunflowerGoldenRpe import KTrajectorySunflowerGoldenRpe
 from mrpro.data.traj_calculators._KTrajectoryRadial2D import KTrajectoryRadial2D
+from mrpro.data.traj_calculators._KTrajectoryIsmrmrd import KTrajectoryIsmrmrd
