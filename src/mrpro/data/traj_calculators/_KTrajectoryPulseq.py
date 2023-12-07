9c99c45427a8a4411b09715c034aa34f4247aa07
Johannes Hammacher <135006694+JoHa0811@users.noreply.github.com>
2023-12-07 10:35:22 +0100

    KTrajectoryPulseq (#111)
    * added pypulseq dependency (need to change GitHub -> PyPi when stable release available)
    
    * added KTrajectoryRawShape class
    
    * added KTrajectoryPulseq class
    
    * added tests for new implementations
    
    * added new contributors
    
    ---------
    
    Co-authored-by: Johannes <johannes.hammacher@ptb.de>
    Co-authored-by: Patrick Schuenke <patrick.schuenke@gmail.com>

---
+++ src/mrpro/data/traj_calculators/_KTrajectoryPulseq.py
@@ -0,0 +1,112 @@
+"""K-space trajectory from .seq file class."""
+
+# Copyright 2023 Physikalisch-Technische Bundesanstalt
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#       http://www.apache.org/licenses/LICENSE-2.0
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+from __future__ import annotations
+
+from pathlib import Path
+
+import pypulseq as pp
+import torch
+from einops import rearrange
+
+from mrpro.data import KHeader
+from mrpro.data import KTrajectoryRawShape
+from mrpro.data.traj_calculators import KTrajectoryCalculator
+
+
+class KTrajectoryPulseq(KTrajectoryCalculator):
+    """Trajectory from .seq file.
+
+    Parameters
+    ----------
+    seq_path
+        absolute path to .seq file
+    """
+
+    def __init__(self, seq_path: str | Path) -> None:
+        super().__init__()
+        self.seq_path = seq_path
+
+    def __call__(self, kheader: KHeader) -> KTrajectoryRawShape:
+        """Calculate trajectory from given .seq file and header information.
+
+        Parameters
+        ----------
+        kheader
+           MR raw data header (KHeader) containing required meta data
+
+        Returns
+        -------
+            trajectory of type KTrajectoryRawShape
+        """
+
+        # create PyPulseq Sequence object and read .seq file
+        seq = pp.Sequence()
+        seq.read(file_path=str(self.seq_path))
+
+        # calculate k-space trajectory using PyPulseq
+        k_traj_adc_numpy, _, _, _, _ = seq.calculate_kspace()
+        k_traj_adc = torch.from_numpy(k_traj_adc_numpy).clone().detach().requires_grad_(True)
+
+        unique_idxs = {label: torch.unique(getattr(kheader.acq_info.idx, label)) for label in ['k1', 'k2']}
+
+        k1 = len(unique_idxs['k1'])
+        k2 = len(unique_idxs['k2'])
+
+        num_samples = kheader.acq_info.number_of_samples
+        if len(torch.unique(num_samples)) > 1:
+            raise ValueError('We currently only support constant number of samples')
+
+        # get number of samples as integer
+        # ToDo: find more pythonic solution compatible with mypy
+        k0 = int(num_samples[0].squeeze().tolist()[0])
+
+        sample_size = num_samples.shape[0]
+
+        with torch.no_grad():
+            k_traj_adc[0] = k_traj_adc[0] / torch.max(torch.abs(k_traj_adc[0])) * torch.pi
+            k_traj_adc[1] = k_traj_adc[1] / torch.max(torch.abs(k_traj_adc[1])) * torch.pi
+            k_traj_adc[2] = k_traj_adc[2] / torch.max(torch.abs(k_traj_adc[2])) * torch.pi
+
+        kx = k_traj_adc[0].view((sample_size, k2, k1, k0))
+        ky = k_traj_adc[1].view((sample_size, k2, k1, k0))
+        kz = k_traj_adc[2].view((sample_size, k2, k1, k0))
+
+        # rearrange k-space trajectory to match MRpro convention
+        kx = rearrange(
+            kx,
+            'other k2 k1 k0 -> (other k2 k1) k0',
+            k0=k0,
+            k2=k2,
+            k1=k1,
+            other=sample_size,
+        )
+        ky = rearrange(
+            ky,
+            'other k2 k1 k0 -> (other k2 k1) k0',
+            k0=k0,
+            k2=k2,
+            k1=k1,
+            other=sample_size,
+        )
+        kz = rearrange(
+            kz,
+            'other k2 k1 k0 -> (other k2 k1) k0',
+            k0=k0,
+            k2=k2,
+            k1=k1,
+            other=sample_size,
+        )
+
+        return KTrajectoryRawShape(kz, ky, kx)
