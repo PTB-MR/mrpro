0b35cdd1eea537f1e6ed90b4a6a65ac7b2fa0482
Christoph Kolbitsch <christoph.kolbitsch@ptb.de>
2023-12-08 16:33:02 +0100

    reading in of all dcm files from folder (#124)
    * reading in of all dcm files from folder
    
    * dcm_pixelarray_to_tensor: staticmethod -> function
    get_unique_slice_pos: return empty list if Tag not available
    add missing type hints
    minor code style changes
    
    ---------
    
    Co-authored-by: Patrick Schuenke <patrick.schuenke@gmail.com>

--- src/mrpro/data/_IData.py
+++ src/mrpro/data/_IData.py
@@ -21,9 +21,36 @@ import numpy as np
 import torch
 from einops import rearrange
 from pydicom import dcmread
+from pydicom.dataset import Dataset
+from pydicom.tag import Tag
 
 from mrpro.data import IHeader
 from mrpro.data import KHeader
+
+
+def _dcm_pixelarray_to_tensor(ds: Dataset) -> torch.Tensor:
+    """Transform pixel array in dicom file to tensor.
+
+    "Rescale intercept, (0028|1052), and rescale slope (0028|1053) are
+    DICOM tags that specify the linear transformation from pixels in
+    their stored on disk representation to their in memory
+    representation.     U = m*SV + b where U is in output units, m is
+    the rescale slope, SV is the stored value, and b is the rescale
+    intercept." (taken from
+    https://www.kitware.com/dicom-rescale-intercept-rescale-slope-and-itk/)
+    """
+    if 'RescaleSlope' in ds:
+        slope = float(ds.data_element('RescaleSlope').value)
+    else:
+        slope = 1.0
+
+    if 'RescaleIntercept' in ds:
+        intercept = float(ds.data_element('RescaleSlope').value)
+    else:
+        intercept = 0.0
+
+    # Image data is 2D np.array of Uint16, which cannot directly be converted to tensor
+    return slope * torch.as_tensor(ds.pixel_array.astype(np.complex64)) + intercept
 
 
 @dataclasses.dataclass(slots=True, frozen=True)
@@ -58,9 +85,55 @@ class IData:
         """
 
         ds = dcmread(filename)
-        # Image data is 2D np.array of Uint16, which cannot directly be converted to tensor
-        idata = torch.as_tensor(ds.pixel_array.astype(np.complex64))
-        idata = rearrange(idata[None, ...], '(other coil z) y x -> other coil z y x', other=1, coil=1, z=1)
+        idata = _dcm_pixelarray_to_tensor(ds)[None, :]
+        idata = rearrange(idata, '(other coil z) y x -> other coil z y x', other=1, coil=1, z=1)
 
-        header = IHeader.from_dicom(ds)
+        header = IHeader.from_dicom_list([ds])
+        return cls(data=idata, header=header)
+
+    @classmethod
+    def from_dicom_folder(cls, foldername: str | Path, suffix: str | None = 'dcm') -> IData:
+        """Read all DICOM files from a folder and return IData object.
+
+        Parameters
+        ----------
+        foldername:
+            Path to folder with DICOM files.
+        suffix:
+            File extension (without period/full stop) to identify the DICOM files.
+            If None, then all files in the folder are read in.
+        """
+
+        # Get files
+        if suffix is None:  # Read all files in the folder
+            file_paths = list(Path(foldername).glob('*'))
+        else:
+            file_paths = list(Path(foldername).glob('*.' + suffix))
+
+        if len(file_paths) == 0:
+            raise ValueError(f'No dicom files with suffix {suffix} found in {foldername}')
+
+        # Read in all files
+        ds_list = [dcmread(filename) for filename in file_paths]
+
+        # Ensure they all have the same orientation (same (0019, 1015) SlicePosition_PCS tag)
+        def get_unique_slice_pos(slice_pos_tag: Tag = 0x00191015):
+            if not ds_list[0].get_item(slice_pos_tag):
+                return []
+            else:
+                sl_pos = [ds_list[0].get_item(slice_pos_tag).value]
+                for ds in ds_list[1:]:
+                    _val = ds.get_item(slice_pos_tag).value
+                    if _val not in sl_pos and not isinstance(_val, bytes):
+                        sl_pos.append(_val)
+                return sl_pos
+
+        if len(ds_list) > 1 and len(get_unique_slice_pos()) > 1:
+            raise ValueError('Only dicoms with the same orientation can be read in.')
+
+        # torch.stack is necessary otherwises mypy does not realize that rearrange yields Tensor from list[Tensor]
+        idata = torch.stack([_dcm_pixelarray_to_tensor(ds) for ds in ds_list])
+        idata = rearrange(idata, '(other coil z) y x -> other coil z y x', other=len(idata), coil=1, z=1)
+
+        header = IHeader.from_dicom_list(ds_list)
         return cls(data=idata, header=header)
