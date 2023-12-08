b00a3ff4f477c8db2c38503818177b7eb7f30a31
Felix <fzimmermann89@gmail.com>
2023-12-08 18:52:12 +0700

    Operator Template and Nufft (#104)
    * operator template
    
    * NUFFT
    
    * created NonLinearOperator template
    
    ---------
    
    Co-authored-by: koflera <andreas.kofler@ptb.de>

--- src/mrpro/operators/__init__.py
+++ src/mrpro/operators/__init__.py
@@ -1,1 +1,5 @@
+from mrpro.operators._Operator import Operator
+from mrpro.operators._LinearOperator import LinearOperator
+from mrpro.operators._NonLinearOperator import NonLinearOperator
 from mrpro.operators._SensitivityOp import SensitivityOp
+from mrpro.operators._FourierOp import FourierOp
