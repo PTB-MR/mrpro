1c9f262009c14c245038d91c153eaecdf2470ffc
Johannes <johannes.hammacher@ptb.de>
2023-12-11 16:56:02 +0100

    Added WASABITI model

--- src/mrpro/operators/models/WASABITI.py
+++ src/mrpro/operators/models/WASABITI.py
@@ -16,33 +16,37 @@ from einops import rearrange
 from mrpro.operators import Operator
 
 
-class WASABI(Operator):
+class WASABITI(Operator):
     def __init__(
         self,
         offsets: torch.Tensor,
+        trec: torch.Tensor,
         tp: float = 0.005,
-        b1_nom: float = 3.70,
+        b1_nom: float = 3.75,
         gamma: float = 42.5764,
         freq: float = 127.7292,
     ) -> None:
-        """WASABI function for simultaneous determination of B1 and B0.
+        """WASABITI function for simultaneous determination of B1, B0 and T1.
 
-        For more details see: https://doi.org/10.1002/mrm.26133
+        For more details see: Proc. Intl. Soc. Mag. Reson. Med. 31 (2023): 0906
 
         Parameters
         ----------
         offsets
             frequency offsets [Hz]
+        trec
+            recovery time between offsets [s]
         tp, optional
             RF pulse duration [s], by default 0.005
         b1_nom, optional
-            nominal B1 amplitude [µT], by default 3.70
+            nominal B1 amplitude [µT], by default 3.75
         gamma, optional
             gyromagnetic ratio [MHz/T], by default 42.5764
         freq, optional
             larmor frequency [MHz], by default 127.7292
         """
         self.offsets = offsets
+        self.trec = trec
         self.tp = tp
         self.b1_nom = b1_nom
         self.gamma = gamma
@@ -51,14 +55,20 @@ class WASABI(Operator):
     def forward(self, qdata: torch.Tensor) -> torch.Tensor:
         b0_shift = qdata[0].unsqueeze(0)
         rb1 = qdata[1].unsqueeze(0)
-        c = qdata[2].unsqueeze(0)
-        d = qdata[3].unsqueeze(0)
+        t1 = qdata[2].unsqueeze(0)
 
         b1 = self.b1_nom * rb1
         offsets = rearrange(self.offsets, 'x -> x 1 1 1 1 1')
+        trec = rearrange(self.trec, 'x -> x 1 1 1 1 1')
 
-        res = c - d * (torch.pi * b1 * self.gamma * self.tp) ** 2 * torch.sinc(
-            self.tp * torch.sqrt((b1 * self.gamma) ** 2 + ((offsets - b0_shift) * self.freq) ** 2) ** 2
+        da = (offsets - b0_shift) * self.freq
+        Mzi = 1.0 - torch.exp(torch.multiply(-1.0 / t1, trec))
+
+        res = Mzi * (
+            1
+            - 2
+            * (torch.pi * b1 * self.gamma * self.tp) ** 2
+            * torch.sinc(self.tp * torch.sqrt((b1 * self.gamma) ** 2 + da**2)) ** 2
         )
 
         return rearrange(res, 'p other c z y x -> (p other) c z y x')
