4deb6dbb72b5061d5a163c5381772577ad1e29a7
koflera <andreas.kofler@ptb.de>
2023-12-12 13:49:07 +0100

    first lbfgs with test

---
+++ src/mrpro/algorithms/_lbfgs.py
@@ -0,0 +1,86 @@
+"""LBFGS for solving non-linear minimization problems."""
+
+# Copyright 2023 Physikalisch-Technische Bundesanstalt
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#       http://www.apache.org/licenses/LICENSE-2.0
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+
+from torch.optim import LBFGS
+
+
+def lbfgs(
+    f,
+    p,
+    lr=1,
+    max_iter=100,
+    max_eval=100,
+    tolerance_grad=1e-07,
+    tolerance_change=1e-09,
+    history_size=10,
+    line_search_fn='strong_wolfe',
+):
+    """LBFGS for non-linear minimization problems.
+
+    Parameters
+    ----------
+    f
+        scalar function to be minimized
+    p
+        initial guess of the solution
+    lr, optional
+        learning rate
+    max_iter, optional
+        maximal number of iterations, by default 100
+    max_eval, optional
+        maximal number of evaluations of f per optimization step,
+        by default 100
+    tolerance_grad, optional
+        termination tolerance on first order optimality,
+        by default 1e-07
+    tolerance_change, optional
+        termination tolerance on function value/parameter changes, by default 1e-09
+    history_size, optional
+        update history size, by default 10
+    line_search_fn, optional
+        line search algorithm, either ‘strong_wolfe’ or None,
+        by default "strong_wolfe"
+
+    Returns
+    -------
+        optimized parameter p
+    """
+
+    # enable gradient calculation
+    p.requires_grad = True
+
+    # define lbfgs routine
+
+    lbfgs_ = LBFGS(
+        [p],
+        lr=lr,
+        history_size=history_size,
+        max_iter=max_iter,
+        max_eval=max_eval,
+        tolerance_grad=tolerance_grad,
+        tolerance_change=tolerance_change,
+        line_search_fn=line_search_fn,
+    )
+
+    def closure():
+        lbfgs_.zero_grad()
+        objective = f(p)
+        objective.backward()
+        return objective
+
+    # run lbfgs
+    lbfgs_.step(closure)
+
+    return p
