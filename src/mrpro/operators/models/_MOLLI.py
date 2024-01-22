e0ede905601bb5550d5466fa0128d1b19267d8ae
Stefan Martin <67423672+Stef-Martin@users.noreply.github.com>
2024-01-22 15:57:16 +0100

    Signal models added: Inversion and saturation recovery, MOLLI (#129)
    * Signal models added: Inversion and saturation recovery, MOLLI
    
    * Corrections and Testcases added for all three models
    
    * Parametrized tests, corrected TI in MOLLI, other small changes
    
    * eps-addition only when needed
    
    * Individual tensors as input in signal models
    
    * Operator without abstractmethod
    
    ---------
    
    Co-authored-by: stefan.martin <stefan.martin@ptb.de>
    Co-authored-by: JoHa0811 <johanneshammacher1994@gmail.com>

---
+++ src/mrpro/operators/models/_MOLLI.py
@@ -0,0 +1,47 @@
+# Copyright 2023 Physikalisch-Technische Bundesanstalt
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#       http://www.apache.org/licenses/LICENSE-2.0
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+import torch
+from einops import rearrange
+
+from mrpro.operators import Operator
+
+
+class MOLLI(Operator):
+    def __init__(self, ti: torch.Tensor):
+        """Parameters needed for MOLLI.
+
+        Parameters
+        ----------
+        ti
+            inversion times
+        """
+        super().__init__()
+        self.ti = torch.nn.Parameter(ti, requires_grad=ti.requires_grad)
+
+    def forward(self, a: torch.Tensor, b: torch.Tensor, t1: torch.Tensor) -> torch.Tensor:
+        """Apply the forward model.
+
+        Parameters
+        ----------
+            Quantitative parameters m0, t1 with dimensions (other, c, z, y, x)
+
+        Returns
+        -------
+            Image data tensor (other, c, z, y, x)
+        """
+        t1 = torch.where(t1 == 0, 1e-10, t1)
+        a = torch.where(a == 0 | torch.equal((b / a), torch.ones_like(a)), a + 1e-10, a)
+        ti = self.ti[(...,) + (None,) * (a.ndim)]
+        t1_star = t1 / ((b / a) - 1)
+        y = a - b * torch.exp(-(ti / (t1_star)))
+        res = rearrange(y, 't ... c z y x -> (... t) c z y x')
+        return res
