bb4a0a422a96c410d22da0de66df1b175ba99113
JoHa0811 <johanneshammacher1994@gmail.com>
2024-01-22 13:58:17 +0100

    trying to fix tests

--- tests/operators/test_WASABI.py
+++ tests/operators/test_WASABI.py
@@ -6,13 +6,12 @@ from mrpro.operators.models._WASABI import WASABI
 
 def create_data(offset_max=250, offset_nr=101, b0_shift=0, rb1=1.0, c=1.0, d=2.0, p=4, other=1, coils=1, z=1, y=1, x=1):
     offsets = torch.linspace(-offset_max, offset_max, offset_nr)
-    qdata = torch.ones(p, other, coils, z, y, x)
-    qdata[0, ...] = b0_shift
-    qdata[1, ...] = rb1
-    qdata[2, ...] = c
-    qdata[3, ...] = d
+    b0_shift = torch.zeros([1, 1, 1, 1, 1])  # b0_shift
+    rb1 = torch.Tensor([rb1])
+    c = torch.Tensor([c])
+    d = torch.Tensor([d])
 
-    return offsets, qdata
+    return offsets, b0_shift, rb1, c, d
 
 
 @pytest.mark.parametrize(
@@ -26,9 +25,9 @@ def create_data(offset_max=250, offset_nr=101, b0_shift=0, rb1=1.0, c=1.0, d=2.0
 )
 def test_WASABI_signal_model_shape(offset_max, offset_nr, b0_shift, rb1, c, d, p, other, coils, z, y, x):
     """Test for correct output shape."""
-    offsets, qdata = create_data(offset_max, offset_nr, b0_shift, rb1, c, d, p, other, coils, z, y, x)
+    offsets, b0_shift, rb1, c, d = create_data(offset_max, offset_nr, b0_shift, rb1, c, d, p, other, coils, z, y, x)
     wasabi_model = WASABI(offsets=offsets)
-    sig = wasabi_model.forward(qdata)
+    sig = wasabi_model.forward(b0_shift, rb1, c, d)
 
     signal_shape = torch.Tensor(offset_nr, coils, z, y, x)
     assert sig.shape == signal_shape.shape
@@ -36,13 +35,13 @@ def test_WASABI_signal_model_shape(offset_max, offset_nr, b0_shift, rb1, c, d, p
 
 def test_WASABI_shift():
     """Test symmetry property of shifted and unshifted WASABITI spectra."""
-    offsets_unshifted, qdata_unshifted = create_data(offset_max=500, offset_nr=100, b0_shift=0)
+    offsets_unshifted, b0_shift, rb1, c, d = create_data(offset_max=500, offset_nr=100, b0_shift=0)
     wasabi_model = WASABI(offsets=offsets_unshifted)
-    sig_unshifted = wasabi_model.forward(qdata_unshifted)
+    sig_unshifted = wasabi_model.forward(b0_shift, rb1, c, d)
 
-    offsets_shifted, qdata_shifted = create_data(offset_max=500, offset_nr=101, b0_shift=100)
+    offsets_shifted, b0_shift, rb1, c, d = create_data(offset_max=500, offset_nr=101, b0_shift=100)
     wasabi_model = WASABI(offsets=offsets_shifted)
-    sig_shifted = wasabi_model.forward(qdata_shifted)
+    sig_shifted = wasabi_model.forward(b0_shift, rb1, c, d)
 
     lower_index = (offsets_shifted == -300).nonzero()[0][0].item()
     upper_index = (offsets_shifted == 500).nonzero()[0][0].item()
@@ -52,9 +51,9 @@ def test_WASABI_shift():
 
 
 def test_WASABI_symmetry():
-    offsets, qdata = create_data(offset_max=300, offset_nr=3, b0_shift=0)
+    offsets, b0_shift, rb1, c, d = create_data(offset_max=300, offset_nr=3, b0_shift=0)
     offsets_new = torch.FloatTensor([30000, 500, 0, 200, 5000])
     wasabi_model = WASABI(offsets=offsets_new)
-    sig = wasabi_model.forward(qdata)
+    sig = wasabi_model.forward(b0_shift, rb1, c, d)
 
     assert torch.isclose(sig[0], torch.FloatTensor([1]), rtol=1e-8)
