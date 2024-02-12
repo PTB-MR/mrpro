b83ad273debe183bb2b5a6c6499bf11612a11673
Felix Zimmermann <fzimmermann89@gmail.com>
2024-02-12 02:27:30 +0100

    Add separable nd-filters and sliding window

---
+++ tests/utils/test_window.py
@@ -0,0 +1,109 @@
+"""Tests for sliding windows."""
+
+# Copyright 2024 Physikalisch-Technische Bundesanstalt
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#       http://www.apache.org/licenses/LICENSE-2.0
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import pytest
+import torch
+from mrpro.utils.window import sliding_window
+
+from tests import RandomGenerator
+
+
+def alternative_implementation(data, window_shape, axis, stride):
+    """Alternative implementation of sliding window for testing purposes.
+
+    no error handling, expanded parameters, no fancy indexing, copies
+    """
+    for ax, ws, st in zip(axis, window_shape, stride, strict=False):
+        data = data.unfold(ax, ws, st)
+    return data
+
+
+@pytest.mark.filterwarnings('ignore:strides other than 1 are not fully supported')
+@pytest.mark.parametrize(
+    ('shape', 'window_shape', 'axis', 'stride'),
+    [
+        ((5, 6, 7), (3, 3, 3), (0, 1, 2), (1, 1, 1)),
+        ((5, 6), (2, 2), (1, 0), (1, 1)),
+        ((5, 6), (1, 1), (0, 1), (2, 3)),
+    ],
+)
+def test_window_contents(shape, window_shape, axis, stride):
+    """Test that the contents of the window are correct."""
+    # we use this pattern for easier debugging than random inputs
+    data = torch.arange(int(torch.tensor(shape).prod())).reshape(shape)
+    window = sliding_window(data, window_shape, axis, stride)
+    alt_window = alternative_implementation(data, window_shape, axis, stride)
+    torch.testing.assert_close(window, alt_window)
+
+
+def test_repeated_axes():
+    """Test that repeated axes raise an error."""
+    data = torch.zeros(2, 3, 4)
+    with pytest.raises(ValueError, match='duplicate value'):
+        _ = sliding_window(data, window_shape=(2, 2), axis=(0, 0))
+    with pytest.raises(ValueError, match='duplicate value'):
+        _ = sliding_window(data, window_shape=(2, 2), axis=(0, -3))
+
+
+def test_negative_window_shape():
+    """Test that negative window shapes raise an error."""
+    data = torch.zeros(2, 3, 4)
+    with pytest.raises(ValueError, match='negative values'):
+        _ = sliding_window(data, window_shape=(2, -2), axis=(0, 1))
+    with pytest.raises(ValueError, match='negative values'):
+        _ = sliding_window(data, window_shape=-2, axis=(0, 1))
+
+
+@pytest.mark.filterwarnings('ignore:strides other than 1 are not fully supported')
+def test_negative_strides():
+    """Test that negative strides raise an error."""
+    data = torch.zeros(2, 3, 4)
+    with pytest.raises(ValueError, match='negative values'):
+        _ = sliding_window(data, window_shape=(2, 2), axis=(0, 1), strides=(1, -1))
+    with pytest.raises(ValueError, match='negative values'):
+        _ = sliding_window(data, window_shape=(2, 2), axis=(0, 1), strides=-1)
+
+
+def test_length_mismatch():
+    """Test that length mismatches raise an error."""
+    data = torch.zeros(2, 3, 4)
+    with pytest.raises(ValueError, match='matching length'):
+        _ = sliding_window(data, window_shape=(2, 2), axis=(0, 1, 2), strides=(1, 1))
+    with pytest.raises(ValueError, match='matching length'):
+        _ = sliding_window(data, window_shape=(2, 2), axis=(0, 1), strides=(1, 1, 1))
+
+
+def test_window_too_large():
+    """Test that too large windows raise an error."""
+    data = torch.zeros(2, 3, 4)
+    with pytest.raises(ValueError, match='too large'):
+        _ = sliding_window(data, window_shape=3, axis=0, strides=1)
+
+
+@pytest.mark.filterwarnings('ignore:strides other than 1 are not fully supported')
+def test_scalar_arguments():
+    """Test that scalar arguments are correctly interpreted."""
+    data = RandomGenerator(42).float32_tensor((5, 6, 7))
+    tuple_result = sliding_window(data, window_shape=(2,), axis=(1,), strides=(2,))
+    int_result = sliding_window(data, window_shape=2, axis=1, strides=2)
+    torch.testing.assert_close(tuple_result, int_result)
+
+
+def test_isview():
+    """Test that the window is a view of the original data."""
+    data = RandomGenerator(42).float32_tensor((5, 6, 7))
+    window = sliding_window(data, window_shape=(2, 2, 2), axis=(0, 1, 2), strides=(1, 1, 1))
+    assert not window.is_contiguous()
+    assert window.data_ptr() == data.data_ptr()
+    assert window.shape == (4, 5, 6, 2, 2, 2)
