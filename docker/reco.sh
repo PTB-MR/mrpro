b1cca27720bd96bf6d4f01c2c15e27180d50b3df
ckolbPTB <christoph.kolbitsch@ptb.de>
2024-02-22 21:33:44 +0100

    py312 and build from pyproject.toml

--- docker/reco.sh
+++ docker/reco.sh
@@ -13,22 +13,23 @@ ${APT_GET_INSTALL} git software-properties-common gpg-agent
 # add repo for python installation
 add-apt-repository ppa:deadsnakes/ppa
 apt update -qq
-${APT_GET_INSTALL} python3.11-full
+${APT_GET_INSTALL} $PYTHON-full
 
-# create alias for python3.11
-echo 'alias python="/usr/bin/python3.11"' >> /root/.bashrc
-source /root/.bashrc
-ln -s /usr/bin/python3.11 /usr/local/bin/python
+# create alias for installed python version
+ln -s /usr/bin/$PYTHON /usr/local/bin/python
 
-# clone repo to get requirements.txt
+# clone repo to get requirements
 git clone https://github.com/PTB-MR/mrpro --depth 1 /opt/mrpro
+cd /opt/mrpro
 python -m ensurepip --upgrade
 python -m pip install --upgrade pip
-# pre-install cpu-version of torch to avoid installation of cuda-version via requirements.txt
-python -m pip install --no-cache-dir torch==2.2.0 torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
-# exclude any nvidia packages and triton to save space
-grep -v '^ *#\|^nvidia*\|^triton*' /opt/mrpro/binder/requirements.txt | grep . > /opt/mrpro/binder/requirements_slim.txt
-python -m pip install --no-cache-dir -r /opt/mrpro/binder/requirements_slim.txt
-rm -r /opt/mrpro
 
+# pre-install cpu-version of torch to avoid installation of cuda-version via requirements.txt
+python -m pip install --no-cache-dir torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+
+# install mrpro + dependencies
+python -m pip install .[notebook,test]
+
+# clean up
+rm -r /opt/mrpro
 apt-get clean && rm -rf /var/lib/apt/lists/*
