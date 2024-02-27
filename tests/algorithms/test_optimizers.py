88b59d27e6cb90df2b1221dedc9a701bc97afb6e
Patrick Schuenke <37338697+schuenke@users.noreply.github.com>
2024-02-27 16:59:23 +0100

    Lbfgs box (#131)
    * NUFFT
    
    * created NonLinearOperator template
    
    * first lbfgs with test
    
    * changed input of lbfgs to list of params
    
    * lbfgs raises error for complex-valued tensors
    
    * fixed lbfgs; added adam with sat-recov example for ellipse phantom
    
    * fixed tests; removed warning
    
    * addressed review points
    
    * fixed docstring in mse class
    
    * test
    
    * fertig
    
    * addressed final reviews
    
    * fixed mypy complain
    
    * removed pytestwarning comment
    
    * deleted phantom and commented pytest warning
    
    * fixed mse type hint; updated adam and lbfgs docstrings
    
    * add test for invalid bounds and address review commnts
    
    * rename contraintop test file
    
    * fix error introduced by me...
    
    * fix typo in comment
    
    * allow neginf/posinf as bounds
    
    ---------
    
    Co-authored-by: koflera <andreas.kofler@ptb.de>
    Co-authored-by: koflera <andreas_kofler@gmx.net>
    Co-authored-by: Felix <fzimmermann89@gmail.com>

---
+++ tests/algorithms/test_optimizers.py
@@ -0,0 +1,82 @@
+"""Tests for non-linear optimization algorithms."""
+
+# Copyright 2024 Physikalisch-Technische Bundesanstalt
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#       http://www.apache.org/licenses/LICENSE-2.0
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import pytest
+import torch
+
+from mrpro.algorithms import adam
+from mrpro.algorithms import lbfgs
+from mrpro.operators import ConstraintsOp
+from tests import RandomGenerator
+from tests.operators._OptimizationTestFunctions import Rosenbrock
+
+
+@pytest.mark.parametrize('enforce_bounds_on_x1', [True, False])
+@pytest.mark.parametrize(
+    'optimizer',
+    [
+        adam,
+        lbfgs,
+    ],
+)
+def test_optimizers_rosenbrock(optimizer, enforce_bounds_on_x1):
+
+    # TODO: remove once fixed in pytorch. see also issue #132 on GitHub
+    with pytest.raises(ImportWarning):
+
+        random_generator = RandomGenerator(seed=0)
+
+        # use Rosenbrock function as test case with 2D test data
+        a, b = 1, 100
+        rosen_brock = Rosenbrock(a, b)
+
+        # initial point of optimization
+        x1 = torch.tensor([42.0])
+        x2 = torch.tensor([3.14])
+        x1.grad = torch.tensor([2.7])
+        x2.grad = torch.tensor([-1.0])
+        params_init = [x1, x2]
+
+        # save to compare with later as optimization should not change the initial points
+        params_init_before = [i.detach().clone for i in params_init]
+        params_init_grad_before = [i.grad.clone() for i in params_init]
+
+        if enforce_bounds_on_x1:
+            # the analytical solution for x_1 will be a, thus we can limit it into [0,2a]
+            constrain_op = ConstraintsOp(bounds=((0, 2 * a),))
+            functional = rosen_brock @ constrain_op
+        else:
+            functional = rosen_brock
+
+        # hyperparams for optimizer
+        lr = 1e-2
+        max_iter = 250
+
+        # minimizer of Rosenbrock function
+        analytical_solution = torch.tensor([a, a**2])
+
+        # estimate minimizer
+        params_result = optimizer(
+            functional,
+            params_init,
+            max_iter=max_iter,
+            lr=lr,
+        )
+
+        # obtained solution should match analytical
+        torch.testing.assert_close(torch.tensor(params_result), analytical_solution)
+
+        for p, before, grad_before in zip(params_init, params_init_before, params_init_grad_before):
+            assert p == before, 'the initial parameter should not have changed during optimization'
+            assert p.grad == before, 'the inital paramters gradient should not have changed during optimization'
