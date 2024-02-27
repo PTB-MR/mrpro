19b264bfb5afe6caccd29c974cba858b56c5ee02
Patrick Schuenke <37338697+schuenke@users.noreply.github.com>
2024-02-27 16:59:23 +0100

    Lbfgs box (#131)
    * NUFFT
    
    * created NonLinearOperator template
    
    * first lbfgs with test
    
    * changed input of lbfgs to list of params
    
    * lbfgs raises error for complex-valued tensors
    
    * fixed lbfgs; added adam with sat-recov example for ellipse phantom
    
    * fixed tests; removed warning
    
    * addressed review points
    
    * fixed docstring in mse class
    
    * test
    
    * fertig
    
    * addressed final reviews
    
    * fixed mypy complain
    
    * removed pytestwarning comment
    
    * deleted phantom and commented pytest warning
    
    * fixed mse type hint; updated adam and lbfgs docstrings
    
    * add test for invalid bounds and address review commnts
    
    * rename contraintop test file
    
    * fix error introduced by me...
    
    * fix typo in comment
    
    * allow neginf/posinf as bounds
    
    ---------
    
    Co-authored-by: koflera <andreas.kofler@ptb.de>
    Co-authored-by: koflera <andreas_kofler@gmx.net>
    Co-authored-by: Felix <fzimmermann89@gmail.com>

--- src/mrpro/algorithms/__init__.py
+++ src/mrpro/algorithms/__init__.py
@@ -1,2 +1,4 @@
 from mrpro.algorithms._prewhiten_kspace import prewhiten_kspace
 from mrpro.algorithms._remove_readout_os import remove_readout_os
+from mrpro.algorithms._lbfgs import lbfgs
+from mrpro.algorithms._adam import adam
