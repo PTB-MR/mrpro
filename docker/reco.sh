9a6d19d4e208e57f6a9387cd2cc375ef90024a6e
ckolbPTB <christoph.kolbitsch@ptb.de>
2024-02-29 20:45:43 +0100

    create matrix

--- docker/reco.sh
+++ docker/reco.sh
@@ -19,11 +19,16 @@ ${APT_GET_INSTALL} $PYTHON-full
 ln -s /usr/bin/$PYTHON /usr/local/bin/python
 ln -s /usr/bin/$PYTHON /usr/local/bin/python3
 
+pip install matplotlib
+
 # clone repo to get requirements
 git clone https://github.com/PTB-MR/mrpro --depth 1 /opt/mrpro
 cd /opt/mrpro
 python -m ensurepip --upgrade
 python -m pip install --upgrade pip
+
+# create alias to ensure pip works in the same way as pip3
+ln -s /usr/local/bin/pip3 /usr/local/bin/pip
 
 # pre-install cpu-version of torch to avoid installation of cuda-version via requirements.txt
 python -m pip install --no-cache-dir torch --index-url https://download.pytorch.org/whl/cpu
