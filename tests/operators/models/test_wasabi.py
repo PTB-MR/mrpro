199dfc0cc9a4d63ff96cee75d359bf97267863b0
Felix F Zimmermann <fzimmermann89@gmail.com>
2024-03-06 14:20:14 +0000

    fix some issues of the wasabi tests to pass pytest again

--- tests/operators/models/test_wasabi.py
+++ tests/operators/models/test_wasabi.py
@@ -26,37 +26,35 @@ def test_WASABI_signal_model_shape(offset_max, offset_nr, b0_shift, rb1, c, d, c
     """Test for correct output shape."""
     offsets, b0_shift, rb1, c, d = create_data(offset_max, offset_nr, b0_shift, rb1, c, d)
     wasabi_model = WASABI(offsets=offsets)
-    sig = wasabi_model.forward(b0_shift, rb1, c, d)
+    signal, = wasabi_model.forward(b0_shift, rb1, c, d)
 
-    signal_shape = torch.Tensor(offset_nr, coils, z, y, x)
-    assert sig.shape == signal_shape.shape
+    assert signal.shape == (offset_nr, coils, z, y, x)
 
 
 def test_WASABI_shift():
-    """Test symmetry property of shifted and unshifted WASABITI spectra."""
+    """Test symmetry property of shifted and unshifted WASABI spectra."""
     offsets_unshifted, b0_shift_unshifted, rb1_unshifted, c_unshifted, d_unshifted = create_data(
         offset_max=500,
         offset_nr=100,
         b0_shift_in=0,
     )
     wasabi_model = WASABI(offsets=offsets_unshifted)
-    sig_unshifted = wasabi_model.forward(b0_shift_unshifted, rb1_unshifted, c_unshifted, d_unshifted)
+    signal, = wasabi_model.forward(b0_shift_unshifted, rb1_unshifted, c_unshifted, d_unshifted)
 
     offsets_shifted, b0_shift, rb1, c, d = create_data(offset_max=500, offset_nr=101, b0_shift_in=100)
     wasabi_model = WASABI(offsets=offsets_shifted)
-    sig_shifted = wasabi_model.forward(b0_shift, rb1, c, d)
+    signal_shifted, = wasabi_model.forward(b0_shift, rb1, c, d)
 
     lower_index = (offsets_shifted == -300).nonzero()[0][0].item()
     upper_index = (offsets_shifted == 500).nonzero()[0][0].item()
 
-    assert sig_unshifted[0] == sig_unshifted[-1]
-    assert sig_shifted[lower_index] == sig_shifted[upper_index]
+    assert signal[0] == signal[-1], "Result should be symmetric around center"
+    assert signal_shifted[lower_index] == signal_shifted[upper_index], "Result should be symmetric around shift"
 
 
-def test_WASABI_symmetry():
-    _, b0_shift, rb1, c, d = create_data(offset_max=300, offset_nr=3, b0_shift_in=0)
-    offsets_new = torch.FloatTensor([30000, 500, 0, 200, 5000])
-    wasabi_model = WASABI(offsets=offsets_new)
-    sig = wasabi_model.forward(b0_shift, rb1, c, d)
+def test_WASABI_extreme_offset():
+    offset, b0_shift, rb1, c, d = create_data(offset_max=30000, offset_nr=1, b0_shift_in=0)
+    wasabi_model = WASABI(offsets=offset)
+    signal, = wasabi_model.forward(b0_shift, rb1, c, d)
 
-    assert torch.isclose(sig[0], torch.FloatTensor([1]), rtol=1e-8)
+    assert torch.isclose(signal, torch.tensor([1.])), "For an extreme offset, the signal should be unattenuated"
