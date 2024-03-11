718c354e6fabd8e086cf55ad248b06f47a0ac326
Patrick Schuenke <patrick.schuenke@gmail.com>
2024-03-11 09:20:01 +0100

    add test to ensure equality of csm from walsh using SpatialDimension and int for smoothing width

--- tests/data/test_csm_data.py
+++ tests/data/test_csm_data.py
@@ -58,6 +58,22 @@ def test_CsmData_iterative_Walsh(ellipse_phantom, random_kheader):
     assert relative_image_difference(torch.abs(csm.data), torch.abs(csm_ref)) <= 0.01
 
 
+def test_CsmData_interactive_Walsh_smoothing_width(ellipse_phantom, random_kheader):
+    """CsmData from iterative Walsh method using SpatialDimension and int for
+    smoothing width."""
+    idata, csm_ref = multi_coil_image(n_coils=4, ph_ellipse=ellipse_phantom, random_kheader=random_kheader)
+
+    # Estimate coil sensitivity maps using SpatialDimension for smoothing width
+    smoothing_width = SpatialDimension(z=5, y=5, x=5)
+    csm_using_spatial_dimension = CsmData.from_idata_walsh(idata, smoothing_width)
+
+    # Estimate coil sensitivity maps using int for smoothing width
+    csm_using_int = CsmData.from_idata_walsh(idata, smoothing_width=5)
+
+    # assert that both coil sensitivity maps are equal, not just close
+    assert torch.equal(csm_using_spatial_dimension.data, csm_using_int.data)
+
+
 @pytest.mark.cuda()
 def test_CsmData_iterative_Walsh_cuda(ellipse_phantom, random_kheader):
     """CsmData obtained with the iterative Walsh method in CUDA memory."""
