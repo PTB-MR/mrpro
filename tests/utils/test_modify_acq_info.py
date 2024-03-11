8347778c024f29df073098f4400cc0d2efa3ab3f
Patrick Schuenke <patrick.schuenke@gmail.com>
2024-03-11 09:01:22 +0100

    adjust variable names and docstring

--- tests/utils/test_modify_acq_info.py
+++ tests/utils/test_modify_acq_info.py
@@ -1,4 +1,4 @@
-"""Tests of utilities for kdata objects."""
+"""Tests for modification of acquisition infos."""
 
 # Copyright 2023 Physikalisch-Technische Bundesanstalt
 #
@@ -18,13 +18,13 @@ from mrpro.utils import modify_acq_info
 
 def test_modify_acq_info(random_kheader_shape):
     """Test the modification of the acquisition info."""
-    # Create random header where AcqInfo fields are of shape [nk1*nk2] and reshape to [other, nk2, nk1]
-    kheader, num_other, _, num_k2, num_k1, _ = random_kheader_shape
+    # Create random header where AcqInfo fields are of shape [n_k1*n_k2] and reshape to [n_other, n_k2, n_k1]
+    kheader, n_other, _, n_k2, n_k1, _ = random_kheader_shape
 
     def reshape_acq_data(data):
-        return rearrange(data, '(other k2 k1) ... -> other k2 k1 ...', other=num_other, k2=num_k2, k1=num_k1)
+        return rearrange(data, '(other k2 k1) ... -> other k2 k1 ...', other=n_other, k2=n_k2, k1=n_k1)
 
     kheader.acq_info = modify_acq_info(reshape_acq_data, kheader.acq_info)
 
     # Verify shape
-    assert kheader.acq_info.center_sample.shape == (num_other, num_k2, num_k1, 1)
+    assert kheader.acq_info.center_sample.shape == (n_other, n_k2, n_k1, 1)
