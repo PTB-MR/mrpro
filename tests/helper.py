5dc2fa8fdd431cbc23bee16163367ab6406c77e9
Felix Zimmermann <fzimmermann89@gmail.com>
2024-03-14 14:22:46 +0100

    check shapes in dotproduct test

--- tests/helper.py
+++ tests/helper.py
@@ -36,12 +36,49 @@ def relative_image_difference(img1, img2):
     return image_difference / image_mean
 
 
-def dotproduct_adjointness_test(operator, u, v):
-    """Test if <Operator(u),v> == <u, Operator^H(v)> for u ∈ domain and v ∈
-    range of Operator."""
+def dotproduct_adjointness_test(
+    operator, u: torch.Tensor, v: torch.Tensor, relative_tolerance: float = 1e-3, absolute_tolerance=1e-5
+):
+    """Test the adjointness of operator and operator.H
+
+    Test if
+         <Operator(u),v> == <u, Operator^H(v)>
+         for one u ∈ domain and one v ∈ range of Operator.
+    and if the shapes match.
+
+    Note: This property should hold for all u and v.
+    Commonly, this function is called with two random vectors u and v.
+
+
+    Parameters
+    ----------
+    operator
+        operator
+    u
+        element of the domain of the operator
+    v
+        element of the range of the operator
+    relative_tolerance
+        default is pytorch's default for float16
+    absolute_tolerance
+        default is pytorch's default for float16
+
+    Raises
+    ------
+    AssertionError
+        if the adjointness property does not hold
+    AssertionError
+        if the shape of operator(u) and v does not match
+        if the shape of u and operator.H(v) does not match
+
+    """
     (forward_u,) = operator(u)
     (adjoint_v,) = operator.adjoint(v)
+
+    # explicitly check the shapes, as flatten makes the dot product insensitive to wrong shapes
+    assert forward_u.shape == v.shape
+    assert adjoint_v.shape == u.shape
+
     dotproduct_range = torch.vdot(forward_u.flatten(), v.flatten())
     dotproduct_domain = torch.vdot(u.flatten().flatten(), adjoint_v.flatten())
-    # tolerances relaxed to torch.float16 defaults.
-    torch.testing.assert_close(dotproduct_range, dotproduct_domain, rtol=1e-3, atol=1e-5)
+    torch.testing.assert_close(dotproduct_range, dotproduct_domain, rtol=relative_tolerance, atol=absolute_tolerance)
