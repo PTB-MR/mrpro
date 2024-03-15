050699662c88d9f6aa510351a93d831b895e39d6
Felix F Zimmermann <fzimmermann89@gmail.com>
2024-03-15 19:29:49 +0100

    Add separable nd-filters and sliding window (#161)
    * add separable nd-filters, gaussian filter, uniform filter
    * add sliding window view

--- src/mrpro/utils/filters.py
+++ src/mrpro/utils/filters.py
@@ -14,41 +14,159 @@
 
 from __future__ import annotations
 
+import warnings
+from collections.abc import Sequence
+from math import ceil
+
+import numpy as np
 import torch
-from einops import repeat
 
 from mrpro.data import SpatialDimension
 
 
-def spatial_uniform_filter_3d(
+def uniform_filter_3d(
     data: torch.Tensor,
-    filter_width: SpatialDimension[int] | tuple[int, int, int],
+    filter_width: SpatialDimension[int] | tuple[int, int, int] | int,
 ) -> torch.Tensor:
     """Spatial smoothing using convolution with box function.
+
+    Filters along the last three dimensions of the data tensor.
 
     Parameters
     ----------
     data
         Data to be smoothed in the shape (... z y x)
     filter_width
-        Width of 3D the filter as SpatialDimension(z, y, x) or tuple(z, y, x).
+        Width of the filter as SpatialDimension(z, y, x) or tuple(z, y, x).
+        If a single integer is supplied, it is used as the width along z, y, and x.
         The filter width is clipped to the data shape.
     """
-    # Create a box-shaped filter kernel
     match filter_width:
+        case int(width):
+            z = min(data.shape[-3], width)
+            y = min(data.shape[-2], width)
+            x = min(data.shape[-1], width)
         case SpatialDimension(z, y, x) | (z, y, x):
             z = min(data.shape[-3], z)
             y = min(data.shape[-2], y)
             x = min(data.shape[-1], x)
-            kernel = torch.ones(1, 1, z, y, x)
         case _:
             raise ValueError(f'Invalid filter width: {filter_width}')
+    return uniform_filter(data, (z, y, x), axis=(-3, -2, -1))
 
-    kernel /= kernel.sum()  # normalize
-    kernel = kernel.to(dtype=data.dtype, device=data.device)
 
-    # filter by 3D convolution
-    # TODO: consider replacing by 3 1D convolutions for larger kernel sizes.
-    reshaped = repeat(data, '... z y x -> (...) channel z y x', channel=1)  # channel dim required for conv3d
-    output = torch.nn.functional.conv3d(reshaped, weight=kernel, padding='same').view_as(data)
-    return output
+def _filter_separable(x: torch.Tensor, kernels: Sequence[torch.Tensor], axis: Sequence[int]) -> torch.Tensor:
+    """Apply the separable filter kernels to the tensor x along the axes axis.
+
+    Does zero-padding to keep the output the same size as the input.
+
+    Parameters
+    ----------
+    x
+        Tensor to filter
+    kernels
+        List of 1D kernels to apply to the tensor x.
+    axis
+        axes to filter over. Must have the same length as kernels.
+    """
+    if len(axis) != len(kernels):
+        raise ValueError('Must provide matching length kernels and axis arguments. ')
+
+    # normalize axis to allow negative indexing in input
+    axis = tuple([a % x.ndim for a in axis])
+    if len(axis) != len(set(axis)):
+        raise ValueError(f'Axis must be unique. Normalized axis are {axis}')
+
+    for kernel, ax in zip(kernels, axis, strict=False):
+        # either both are complex or both are real
+        if x.is_complex() and not kernel.is_complex():
+            kernel = kernel + 0.0j
+        elif kernel.is_complex() and not x.is_complex():
+            x = x + 0.0j
+        # moveaxis is not implemented for batched tensors, so vmap would fail.
+        # thus we use permute.
+        idx = list(range(x.ndim))
+        # swapping the last axis and the axis to filter over
+        idx[ax], idx[-1] = idx[-1], idx[ax]
+        x = x.permute(idx)
+        x = torch.nn.functional.conv1d(
+            x.flatten(end_dim=-2)[:, None, :], kernel[None, None, :], padding='same'
+        ).reshape(x.shape)
+        # for a single permutation, this undoes the permutation
+        x = x.permute(idx)
+    return x
+
+
+def gaussian_filter(
+    x: torch.Tensor,
+    sigmas: float | Sequence[float] | torch.Tensor,
+    axis: int | Sequence[int] | None = None,
+    truncate: int = 3,
+) -> torch.Tensor:
+    """Apply a and-Gaussian filter.
+
+    Parameters
+    ----------
+    x
+        Tensor to filter
+    sigmas
+        Standard deviation for Gaussian kernel. If iterable, must have length equal to the number of axes.
+    axis
+        Axis or axes to filter over. If None, filters over all axes.
+    truncate
+        Truncate the filter at this many standard deviations.
+    """
+    if axis is None:
+        axis = tuple(range(x.ndim))
+    elif isinstance(axis, int):
+        axis = (axis,)
+    sigmas = torch.as_tensor(sigmas) if np.iterable(sigmas) else torch.tensor([sigmas] * len(axis))
+    if not torch.all(sigmas > 0):
+        raise ValueError('`sigmas` must be positive')
+
+    if len(sigmas) != len(axis):
+        raise ValueError('Must provide matching length sigmas and axis arguments. ')
+
+    kernels = tuple(
+        [
+            torch.exp(-0.5 * (torch.arange(-ceil(truncate * sigma), ceil(truncate * sigma) + 1) / sigma) ** 2)
+            for sigma in sigmas
+        ]
+    )
+    kernels = tuple([k / k.sum() for k in kernels])
+    x_filtered = _filter_separable(x, kernels, axis)
+    return x_filtered
+
+
+def uniform_filter(
+    x: torch.Tensor,
+    width: int | Sequence[int] | torch.Tensor,
+    axis: int | Sequence[int] | None = None,
+) -> torch.Tensor:
+    """Apply a and-uniform filter.
+
+    Parameters
+    ----------
+    x
+        Tensor to filter
+    width
+        Width of uniform kernel. If iterable, must have length equal to the number of axes.
+    axis
+        Axis or axes to filter over. If None, filters over all axes.
+    """
+    if axis is None:
+        axis = tuple(range(x.ndim))
+    elif isinstance(axis, int):
+        axis = (axis,)
+    width = torch.as_tensor(width) if np.iterable(width) else torch.tensor([width] * len(axis))
+    if not torch.all(width > 0):
+        raise ValueError('width must be positive.')
+    if torch.any(width % 2 != 1):
+        warnings.warn('width should be odd.', stacklevel=2)
+    if len(width) != len(axis):
+        raise ValueError('Must provide matching length width and axis arguments. ')
+    width = torch.minimum(width, torch.tensor(x.shape)[(axis), ...])
+
+    kernels = tuple([torch.ones(width) / width for width in width])
+    x_filtered = _filter_separable(x, kernels, axis)
+    return x_filtered
---
+++ src/mrpro/utils/sliding_window.py
@@ -0,0 +1,84 @@
+"""Sliding window view."""
+
+# Copyright 2023 Physikalisch-Technische Bundesanstalt
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#       http://www.apache.org/licenses/LICENSE-2.0
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+from __future__ import annotations
+
+import warnings
+from collections.abc import Sequence
+
+import torch
+
+
+def sliding_window(
+    x: torch.Tensor,
+    window_shape: int | Sequence[int],
+    axis: int | Sequence[int] | None = None,
+    strides: int | Sequence[int] = 1,
+) -> torch.Tensor:
+    """Sliding window view into the tensor x.
+
+    Returns a view into the tensor x that represents a sliding window.
+    The window-axes will be at the end in the order of the axis argument.
+    Non-overlapping windows can be achieved by setting the strides to the window_shape.
+    Note that the stride argument is **experimental** and not fully supported.
+
+    Parameters
+    ----------
+    x
+        Tensor to slide over
+    window_shape
+        Size of window over each axis that takes part in the sliding window.
+    axis
+        Axis or axes to slide over. If None, slides over all axes.
+    strides
+        Stride of the sliding window. **Experimental**.
+    """
+    if axis is None:
+        axis = tuple(range(x.ndim))
+    elif isinstance(axis, int):
+        axis = (axis,)
+    else:
+        axis = tuple([ax % x.ndim for ax in axis])
+        if len(set(axis)) != len(axis):
+            raise ValueError('duplicate values in axis are not allowed')
+
+    window_shape = tuple(window_shape) if isinstance(window_shape, Sequence) else (window_shape,) * len(axis)
+    strides = tuple(strides) if isinstance(strides, Sequence) else (strides,) * len(axis)
+    # we want to use fancy indexing, so we need these as tensors
+    window_shape_tensor = torch.tensor(window_shape)
+    strides_tensor = torch.tensor(strides)
+    x_shape_tensor = torch.tensor(x.shape)
+
+    if torch.any(strides_tensor != 1):
+        warnings.warn('strides other than 1 are not fully supported', stacklevel=2)
+    if torch.any(window_shape_tensor < 0):
+        raise ValueError('window_shape cannot contain negative values')
+    if torch.any(strides_tensor < 0):
+        # this is a pytorch limitation. python api standard should allow negative strides
+        raise ValueError('strides cannot contain negative values')
+    if len(window_shape) != len(axis):
+        raise ValueError('Must provide matching length window_shape and axis arguments. ')
+    if len(strides) != len(axis):
+        raise ValueError('Must provide matching length strides and axis arguments.')
+    # out_strides should be the original strides, but for sliding windows axis the stride should be increased
+    # and a new dimension should be added
+    out_strides = torch.tensor([x.stride(i) for i in range(x.ndim)] + [x.stride(ax) for ax in axis])
+    out_strides[axis,] = out_strides[axis,] * strides_tensor
+    # remove boundaries, similar to convolution with padding="valid".
+    x_shape_tensor[axis,] = (x_shape_tensor[axis,] + strides_tensor - window_shape_tensor) // strides_tensor
+    if torch.any(x_shape_tensor <= 0):
+        # only partial views
+        raise ValueError('strides or windows too large')
+    out_shape = tuple(x_shape_tensor) + window_shape
+    view = x.as_strided(size=out_shape, stride=tuple(out_strides))
+    return view
