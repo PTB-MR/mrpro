a1a73fec883cd6b419650578110eb5e6d464443c
ckolbPTB <christoph.kolbitsch@ptb.de>
2024-03-21 18:03:30 +0100

    variable shapes for parameters and ti

--- tests/operators/models/test_t1_models.py
+++ tests/operators/models/test_t1_models.py
@@ -5,12 +5,43 @@ from mrpro.operators.models import InversionRecovery
 from mrpro.operators.models import SaturationRecovery
 from tests import RandomGenerator
 
+SHAPE_VARIATIONS = pytest.mark.parametrize(
+    ('parameter_shape', 'ti_shape', 'signal_shape'),
+    [
+        ((1, 1, 10, 20, 30), (5), (5, 1, 1, 10, 20, 30)),  # single map with different inversion times
+        ((1, 1, 10, 20, 30), (5, 1), (5, 1, 1, 10, 20, 30)),
+        ((4, 1, 1, 10, 20, 30), (5, 1), (5, 4, 1, 1, 10, 20, 30)),  # multiple maps along additional batch dimension
+        ((4, 1, 1, 10, 20, 30), (5), (5, 4, 1, 1, 10, 20, 30)),
+        ((4, 1, 1, 10, 20, 30), (5, 4), (5, 4, 1, 1, 10, 20, 30)),
+        ((3, 1, 10, 20, 30), (5), (5, 3, 1, 10, 20, 30)),  # multiple maps along other dimension
+        ((3, 1, 10, 20, 30), (5, 1), (5, 3, 1, 10, 20, 30)),
+        ((3, 1, 10, 20, 30), (5, 3), (5, 3, 1, 10, 20, 30)),
+        ((4, 3, 1, 10, 20, 30), (5), (5, 4, 3, 1, 10, 20, 30)),  # multiple maps along other and batch dimension
+        ((4, 3, 1, 10, 20, 30), (5, 4), (5, 4, 3, 1, 10, 20, 30)),
+        ((4, 3, 1, 10, 20, 30), (5, 4, 1), (5, 4, 3, 1, 10, 20, 30)),
+        ((4, 3, 1, 10, 20, 30), (5, 1, 3), (5, 4, 3, 1, 10, 20, 30)),
+        ((4, 3, 1, 10, 20, 30), (5, 4, 3), (5, 4, 3, 1, 10, 20, 30)),
+        ((1,), (5), (5, 1)),  # single voxel
+        ((4, 3, 1), (5, 4, 3), (5, 4, 3, 1)),
+    ],
+)
 
-def create_data(other=10, coils=5, z=100, y=100, x=100):
+
+def create_parameter_tensors(data_shape=(10, 5, 100, 100, 100), number_of_tensors=2):
     random_generator = RandomGenerator(seed=0)
-    m0 = random_generator.float32_tensor(size=(other, coils, z, y, x), low=1e-10)
-    t1 = random_generator.float32_tensor(size=(other, coils, z, y, x), low=1e-10)
-    return m0, t1
+    parameter_tensors = random_generator.float32_tensor(size=(number_of_tensors, *data_shape), low=1e-10)
+    return torch.unbind(parameter_tensors)
+
+
+@SHAPE_VARIATIONS
+def test_saturation_recovery_shape(parameter_shape, ti_shape, signal_shape):
+    """Test correct signal shapes."""
+    random_generator = RandomGenerator(seed=0)
+    ti = random_generator.float32_tensor(size=ti_shape, low=0)
+    model = SaturationRecovery(ti)
+    m0, t1 = create_parameter_tensors(parameter_shape)
+    (signal,) = model.forward(m0, t1)
+    assert signal.shape == signal_shape
 
 
 @pytest.mark.parametrize(
@@ -31,17 +62,28 @@ def test_saturation_recovery(t, result):
 
     # Generate signal model and torch tensor for comparison
     model = SaturationRecovery(ti)
-    m0, t1 = create_data()
+    m0, t1 = create_parameter_tensors()
     (image,) = model.forward(m0, t1)
 
     zeros = torch.zeros_like(m0)
 
     # Assert closeness to zero for t=0
     if result == '0':
-        torch.testing.assert_close(image, zeros)
+        torch.testing.assert_close(image[0, ...], zeros)
     # Assert closeness to m0 for large t
     elif result == 'm0':
-        torch.testing.assert_close(image, m0)
+        torch.testing.assert_close(image[0, ...], m0)
+
+
+@SHAPE_VARIATIONS
+def test_inversion_recovery_shape(parameter_shape, ti_shape, signal_shape):
+    """Test correct signal shapes."""
+    random_generator = RandomGenerator(seed=0)
+    ti = random_generator.float32_tensor(size=ti_shape, low=0)
+    model = InversionRecovery(ti)
+    m0, t1 = create_parameter_tensors(parameter_shape)
+    (signal,) = model.forward(m0, t1)
+    assert signal.shape == signal_shape
 
 
 @pytest.mark.parametrize(
@@ -63,15 +105,26 @@ def test_inversion_recovery(t, result):
 
     # Generate signal model and torch tensor for comparison
     model = InversionRecovery(ti)
-    m0, t1 = create_data()
+    m0, t1 = create_parameter_tensors()
     (image,) = model.forward(m0, t1)
 
     # Assert closeness to -m0 for t=0
     if result == '-m0':
-        torch.testing.assert_close(image, -m0)
+        torch.testing.assert_close(image[0, ...], -m0)
     # Assert closeness to m0 for large t
     elif result == 'm0':
-        torch.testing.assert_close(image, m0)
+        torch.testing.assert_close(image[0, ...], m0)
+
+
+@SHAPE_VARIATIONS
+def test_molli_shape(parameter_shape, ti_shape, signal_shape):
+    """Test correct signal shapes."""
+    random_generator = RandomGenerator(seed=0)
+    ti = random_generator.float32_tensor(size=ti_shape, low=0)
+    model = MOLLI(ti)
+    a, b, t1 = create_parameter_tensors(parameter_shape, number_of_tensors=3)
+    (signal,) = model.forward(a, b, t1)
+    assert signal.shape == signal_shape
 
 
 @pytest.mark.parametrize(
@@ -102,7 +155,7 @@ def test_molli(t, result):
 
     # Assert closeness to a-b for large t
     if result == 'a-b':
-        torch.testing.assert_close(image, a - b)
+        torch.testing.assert_close(image[0, ...], a - b)
     # Assert closeness to a for t=0
     elif result == 'a':
-        torch.testing.assert_close(image, a)
+        torch.testing.assert_close(image[0, ...], a)
