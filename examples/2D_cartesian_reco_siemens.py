68070bf530414228dfc61a4e5fd16bd1cda7f456
Hannes Dillinger <hannes.dillinger@ptb.de>
2024-03-22 14:56:42 +0100

    incorporated imshow example to 2D cartesian notebook

--- examples/2D_cartesian_reco_siemens.py
+++ examples/2D_cartesian_reco_siemens.py
@@ -24,7 +24,6 @@
 
 # %%
 # Imports
-import shutil
 from pathlib import Path
 
 import matplotlib.pyplot as plt
@@ -33,13 +32,13 @@ from mrpro.data import CsmData
 from mrpro.data import IData
 from mrpro.data import KData
 from mrpro.data.traj_calculators import KTrajectoryCartesian
+from mrpro.operators import CartesianSamplingOp
 from mrpro.operators import FourierOp
 from mrpro.operators import SensitivityOp
 
 # %%
-data_folder = Path(R'Z:\_allgemein\projects\8_13\MRPro\2024_03_19\siemens_data\2D_cartesian')
+data_folder = Path(R'/echo/_allgemein/projects/8_13/MRPro/2024_03_19/siemens_data/2D_cartesian')
 fnames = list(data_folder.glob('*.h5'))
-
 # %% [markdown]
 # ## Image reconstruction
 # Image reconstruction involves the following steps:
@@ -51,15 +50,18 @@ fnames = list(data_folder.glob('*.h5'))
 # Read raw data and trajectory
 image_list = []
 title_list = []
+header_list = []
 
 for fname in fnames:
     kdata = KData.from_file(data_folder / fname, KTrajectoryCartesian())
 
     # Reconstruct average image for coil map estimation
+    Sop = CartesianSamplingOp(kdata.header.encoding_matrix, kdata.traj)
     fourier_op = FourierOp(
         recon_matrix=kdata.header.recon_matrix, encoding_matrix=kdata.header.encoding_matrix, traj=kdata.traj
     )
-    (img,) = fourier_op.adjoint(kdata.data)
+    (img,) = fourier_op.adjoint(Sop.adjoint(kdata.data)[0])
+    print(img.shape)
 
     # Calculate coilmaps
     idata = IData.from_tensor_and_kheader(img, kdata.header)
@@ -71,23 +73,71 @@ for fname in fnames:
 
     title_list.append(fname.name.replace('2D_cart_', ''))
     image_list.append(torch.squeeze(img))
+    header_list.append(idata.header)
 
-
+# %% [markdown]
+# ## Data in pixel coordinates
 # %%
-# Visualize results
-# Create figure with suitable number of subplots
 n_images = len(image_list)
 n_cols = 2
-n_rows = (n_images) // n_cols
+n_rows = int(torch.ceil(torch.tensor((n_images) / n_cols)))
 plt.subplots(n_rows, n_cols)
 for i, img in enumerate(image_list):
     plt.subplot(n_rows, n_cols, i + 1)
     plt.title(title_list[i])
+
     if img.dim() == 3:
         img = img[0]
-    plt.imshow(torch.abs(img))
-    plt.axis('off')
+
+    res_x = header_list[i].fov.x / img.shape[0]
+    res_y = header_list[i].fov.y / img.shape[1]
+
+    n_rows_img = torch.tensor(img.shape[0])
+    n_cols_img = torch.tensor(img.shape[1])
+
+    plt.imshow(
+        torch.abs(img),
+        interpolation=None,
+        extent=(
+            -float(torch.floor(n_cols_img / 2) - 0.5),
+            float(torch.ceil(n_cols_img / 2) - 0.5),
+            -float(torch.floor(n_rows_img / 2) - 0.5),
+            float(torch.ceil(n_rows_img / 2) - 0.5),
+        ),
+    )
+    plt.gca().set_aspect(res_x / res_y)
+
+# %% [markdown]
+# ## Data in Field of view coordinates
+# %%
+n_images = len(image_list)
+n_cols = 2
+n_rows = int(torch.ceil(torch.tensor((n_images) / n_cols)))
+plt.subplots(n_rows, n_cols)
+for i, img in enumerate(image_list):
+    plt.subplot(n_rows, n_cols, i + 1)
+    plt.title(title_list[i])
+
+    if img.dim() == 3:
+        img = img[0]
+
+    res_x = header_list[i].fov.x / img.shape[0]
+    res_y = header_list[i].fov.y / img.shape[1]
+
+    n_rows_img = torch.tensor(img.shape[0])
+    n_cols_img = torch.tensor(img.shape[1])
+
+    plt.imshow(
+        torch.abs(img),
+        interpolation=None,
+        extent=(
+            -header_list[i].fov.x / 2 - res_y / 2,
+            header_list[i].fov.x / 2 + res_y / 2,
+            -header_list[i].fov.y / 2 - res_x / 2,
+            header_list[i].fov.y / 2 + res_x / 2,
+        ),
+    )
+    plt.gca().set_xticks((-header_list[i].fov.x / 2, 0, header_list[i].fov.x / 2))
+    plt.gca().set_yticks((-header_list[i].fov.y / 2, 0, header_list[i].fov.y / 2))
 
 # %%
-# Clean-up by removing temporary directory
-shutil.rmtree(data_folder)
