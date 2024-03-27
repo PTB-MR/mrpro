3f0429b5402a6d1be77b804d3d2108d5e1b1c2c2
Patrick Schuenke <patrick.schuenke@gmail.com>
2024-03-27 10:55:21 +0100

    clean up wasabi and wasabiti tests

--- tests/operators/models/test_wasabiti.py
+++ tests/operators/models/test_wasabiti.py
@@ -3,24 +3,20 @@ import torch
 from mrpro.operators.models import WASABITI
 
 
-def create_data(offset_max=250, offset_nr=101, b0_shift_in=0, rb1=1.0, t1=1.0):
-    offsets = torch.linspace(-offset_max, offset_max, offset_nr)
-    b0_shift = torch.zeros([1, 1, 1, 1, 1])
-    b0_shift[0] = b0_shift_in
-    rb1 = torch.Tensor([rb1])
-    t1 = torch.Tensor([t1])
-
-    return offsets, b0_shift, rb1, t1
+def create_data(offset_max=500, n_offsets=101, b0_shift=0, rb1=1.0, t1=1.0):
+    offsets = torch.linspace(-offset_max, offset_max, n_offsets)
+    return offsets, torch.Tensor([b0_shift]), torch.Tensor([rb1]), torch.Tensor([t1])
 
 
 def test_WASABITI_shift_and_symmetry():
     """Test symmetry property of shifted and unshifted WASABITI spectra."""
-    offsets_unshifted, b0_shift, rb1, t1 = create_data(offset_max=500, offset_nr=100, b0_shift_in=0)
+    offsets_unshifted, b0_shift, rb1, t1 = create_data()
+    # note that the symmetry is only guaranteed for identical trec values
     trec = torch.ones_like(offsets_unshifted)
     wasabiti_model = WASABITI(offsets=offsets_unshifted, trec=trec)
     (signal,) = wasabiti_model.forward(b0_shift, rb1, t1)
 
-    offsets_shifted, b0_shift, rb1, t1 = create_data(offset_max=500, offset_nr=101, b0_shift_in=100)
+    offsets_shifted, b0_shift, rb1, t1 = create_data(b0_shift=100)
     trec = torch.ones_like(offsets_shifted)
     wasabiti_model = WASABITI(offsets=offsets_shifted, trec=trec)
     (signal_shifted,) = wasabiti_model.forward(b0_shift, rb1, t1)
@@ -35,10 +31,9 @@ def test_WASABITI_shift_and_symmetry():
 @pytest.mark.parametrize('t1', [(1), (2), (3)])
 def test_WASABITI_relaxation_term(t1):
     """Test relaxation term (Mzi) of WASABITI model."""
-    _, b0_shift, rb1, t1 = create_data(offset_max=300, offset_nr=3, b0_shift_in=0, t1=t1)
-    offsets_new = torch.FloatTensor([30000])
-    trec = torch.ones_like(offsets_new) * t1
-    wasabiti_model = WASABITI(offsets=offsets_new, trec=trec)
+    offset, b0_shift, rb1, t1 = create_data(offset_max=50000, n_offsets=1, t1=t1)
+    trec = torch.ones_like(offset) * t1
+    wasabiti_model = WASABITI(offsets=offset, trec=trec)
     sig = wasabiti_model.forward(b0_shift, rb1, t1)
 
     assert torch.isclose(sig[0], torch.FloatTensor([1 - torch.exp(torch.FloatTensor([-1]))]), rtol=1e-8)
