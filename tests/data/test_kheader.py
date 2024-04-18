4909840ac9bb2097c02bc2e0641beb650e72c9c1
ckolbPTB <christoph.kolbitsch@ptb.de>
2024-04-18 20:04:10 +0200

    tests added

--- tests/data/test_kheader.py
+++ tests/data/test_kheader.py
@@ -12,48 +12,41 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-import datetime
-
 import pytest
 import torch
-from ismrmrd import xsd
 from mrpro.data import KHeader
-
-
-def test_kheader_from_mandatory_ismrmrd_header(random_mandatory_ismrmrd_header, random_acq_info):
-    KHeader.from_ismrmrd(random_mandatory_ismrmrd_header, random_acq_info)
+from mrpro.data.traj_calculators._KTrajectoryCalculator import DummyTrajectory
 
 
 def test_kheader_fail_from_mandatory_ismrmrd_header(random_mandatory_ismrmrd_header, random_acq_info):
+    """KHeader cannot be created only from ismrmrd header because trajectory is missing."""
     with pytest.raises(ValueError, match='Could not create Header'):
         _ = KHeader.from_ismrmrd(random_mandatory_ismrmrd_header, random_acq_info)
 
 
 def test_kheader_overwrite_missing_parameter(random_mandatory_ismrmrd_header, random_acq_info):
-    overwrite = {
-        'trajectory': xsd.trajectoryType('other'),
-        'n_coils': 1,
-        'datetime': datetime.datetime.now(),
-        'te': torch.tensor([0.01]),
-        'ti': torch.tensor([1.0]),
-        'fa': torch.tensor([10.0]),
-        'tr': torch.tensor([0.1]),
-        'echo_spacing': torch.tensor([0.001]),
-    }
+    """KHeader can be created if trajectory is provided."""
+    overwrite = {'trajectory': DummyTrajectory()}
     kheader = KHeader.from_ismrmrd(random_mandatory_ismrmrd_header, random_acq_info, overwrite=overwrite)
     assert kheader is not None
 
 
 def test_kheader_set_missing_defaults(random_mandatory_ismrmrd_header, random_acq_info):
-    defaults = {
-        'trajectory': xsd.trajectoryType('other'),
-        'n_coils': 1,
-        'datetime': datetime.datetime.now(),
-        'te': torch.tensor([1]),
-        'ti': torch.tensor([1]),
-        'fa': torch.tensor([1]),
-        'tr': torch.tensor([1]),
-        'echo_spacing': torch.tensor([1]),
-    }
+    """KHeader can be created if default trajectory is defined."""
+    defaults = {'trajectory': DummyTrajectory()}
     kheader = KHeader.from_ismrmrd(random_mandatory_ismrmrd_header, random_acq_info, defaults=defaults)
     assert kheader is not None
+
+
+def test_kheader_verify_None(random_mandatory_ismrmrd_header, random_acq_info):
+    """Correct handling of None and missing values in KHeader creation."""
+    tr_default = None
+    fa_default = torch.as_tensor([0.1])
+    defaults = {'trajectory': DummyTrajectory(), 'tr': tr_default, 'fa': fa_default}
+    kheader = KHeader.from_ismrmrd(random_mandatory_ismrmrd_header, random_acq_info, defaults=defaults)
+    # ti is not mandatory
+    assert kheader.ti is None
+    # fa is not mandatory but overwriting with value
+    assert torch.allclose(kheader.fa, fa_default)
+    # tr is not mandatory but overwritten with None
+    assert kheader.tr is tr_default
