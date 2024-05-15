a52c99cd7a98600c8ca45d2dad798d14201b1fba
Christoph Kolbitsch <christoph.kolbitsch@ptb.de>
2024-05-15 21:35:22 +0200

    copyright

--- src/mrpro/operators/models/_EPG.py
+++ src/mrpro/operators/models/_EPG.py
@@ -240,3 +240,18 @@ class EpgTse(SignalModel[torch.Tensor, torch.Tensor, torch.Tensor]):
             epg_configuration_states = tr_relaxation.forward(epg_configuration_states)
                 
         return (signal,)
+    
+    
+# Parts of this code were adapted from https://github.com/fzimmermann89/epgtorch/tree/master
+#
+# Copyright (c) 2022 Felix
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
