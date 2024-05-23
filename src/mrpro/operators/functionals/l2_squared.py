1bca5f24a892449f005ddafb5eb80d0dcbfd58c6
JoHa0811 <johanneshammacher1994@gmail.com>
2024-05-23 14:27:31 +0200

    l2_squared functional

--- src/mrpro/operators/functionals/l2_squared.py
+++ src/mrpro/operators/functionals/l2_squared.py
@@ -3,12 +3,14 @@
 import torch
 import torch.nn.functional as F  # noqa: N812
 
-from mrpro.operators._Operator import Operator
+from mrpro.operators._Functional import Functional
 
-class L2NormSquared(Operator):
+class L2NormSquared(Functional):
     
-    def __init__(self):
-        super().__init__()
+    def __init__(self, lam=1, g:torch.Tensor=None):
+        super().__init__(lam=1)
+        self.g = g
+        self.lam = lam
 
     def forward(self, x:torch.Tensor) -> torch.Tensor:
         return x.inner(x)
@@ -16,8 +18,32 @@ class L2NormSquared(Operator):
     def convex_conj(self):
         return 1/4 * L2NormSquared()
     
-    def prox(self):
-        return None
+    def prox(self, x:torch.Tensor, sigma:torch.Tensor):
+        if sigma is torch.scalar_tensor:
+            if self.g is None:
+                out = x/(1+2*sigma/self.lam)
+            else:
+                out = x/(1+2*sigma/self.lam)-sigma*self.g/(1+2*sigma/self.lam)
+        else:
+            if self.g is None:
+                out = x/(1+2/self.lam*sigma)
+            else:
+                out = (x-sigma*self.g)/(1+2/self.lam*sigma)
+        
+        return out
+                
     
-    def prox_convex_conj(self):
-        return self.prox(self.convex_conj())
+    def prox_convex_conj(self, x:torch.Tensor, sigma:torch.Tensor):
+        if sigma is torch.scalar_tensor:
+            if self.g is None:
+                out = x/(1+0.5*sigma/self.lam)
+            else:
+                out = x/(1+0.5*sigma/self.lam)-sigma*self.g/(1+0.5*sigma/self.lam)
+        else:
+            if self.g is None:
+                out = x/(1+0.5 / self.lam*sigma)
+            else:
+                out = (x-sigma*self.g)/(1+0.5 / self.lam*sigma)
+        
+        return out
+                
