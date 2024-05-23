f00e8441bb4973fb8317c6b26e8692898956b4ab
Stef-Martin <stefan.martin.93@gmx.de>
2024-05-23 17:26:20 +0200

    Changed L1 and L2 squared to accomodate complex

--- src/mrpro/operators/functionals/l2_squared.py
+++ src/mrpro/operators/functionals/l2_squared.py
@@ -7,42 +7,37 @@ from mrpro.operators._Functional import ProximableFunctional
 
 class L2NormSquared(ProximableFunctional):
     
-    def __init__(self, lam=1, g:torch.Tensor=None):
-        super().__init__(lam=1)
+    def __init__(self, lam:float=1.0, g:torch.Tensor=torch.tensor([0])):
+        super().__init__(lam=lam)
         self.g = g
 
-    def forward(self, x:torch.Tensor) -> torch.Tensor:
-        return x.inner(x) # does this work for complex? or should it be vdot
+    def forward(self, x:torch.Tensor) -> tuple[torch.Tensor]:
+        return (x.inner(x),) # does this work for complex? or should it be vdot
     
     def convex_conj(self):
         return 1/4 * L2NormSquared()
     
-    def prox(self, x:torch.Tensor, sigma:torch.Tensor):
-        if sigma is torch.scalar_tensor:
-            if self.g is None:
-                out = x/(1+2*sigma/self.lam)
-            else:
-                out = x/(1+2*sigma/self.lam)-sigma*self.g/(1+2*sigma/self.lam)
-        else:
-            if self.g is None:
-                out = x/(1+2/self.lam*sigma)
-            else:
-                out = (x-sigma*self.g)/(1+2/self.lam*sigma)
-        
-        return out
+    def prox(self, x:torch.Tensor, sigma:torch.Tensor) -> tuple[torch.Tensor]:
+        is_complex = x.is_complex()
+        if is_complex:
+            x = torch.view_as_real(x)
+            self.lam = torch.tensor([self.lam]).unsqueeze(-1)
+            sigma = torch.tensor([sigma]).unsqueeze(-1)
+            self.g = torch.tensor([self.g]).unsqueeze(-1)
+        x = (x+sigma*self.g)/(1+2*self.lam*sigma)
+        if is_complex:
+            x = torch.view_as_complex(x)
+        return (x,)
                 
-    
-    def prox_convex_conj(self, x:torch.Tensor, sigma:torch.Tensor):
-        if sigma is torch.scalar_tensor:
-            if self.g is None:
-                out = x/(1+0.5*sigma/self.lam)
-            else:
-                out = x/(1+0.5*sigma/self.lam)-sigma*self.g/(1+0.5*sigma/self.lam)
-        else:
-            if self.g is None:
-                out = x/(1+0.5 / self.lam*sigma)
-            else:
-                out = (x-sigma*self.g)/(1+0.5 / self.lam*sigma)
-        
-        return out
+    def prox_convex_conj(self, x:torch.Tensor, sigma:torch.Tensor) -> tuple[torch.Tensor]:
+        is_complex = x.is_complex()
+        if is_complex:
+            x = torch.view_as_real(x)
+            self.lam = torch.tensor([self.lam]).unsqueeze(-1)
+            sigma = torch.tensor([sigma]).unsqueeze(-1)
+            self.g = torch.tensor([self.g]).unsqueeze(-1)
+        x = (x-sigma*self.g)/(1+0.5 / self.lam*sigma)
+        if is_complex:
+            x = torch.view_as_complex(x)
+        return (x,)
                 
