e3d7ec6fd519c6b4b58f7792974f359e0a9c9a08
JoHa0811 <johanneshammacher1994@gmail.com>
2024-05-24 13:33:25 +0200

    added first tests for l2

--- src/mrpro/operators/functionals/examples.py
+++ src/mrpro/operators/functionals/examples.py
@@ -5,13 +5,30 @@ from mrpro.operators.functionals.l2_squared import L2NormSquared
 
 l1 = L1Norm(lam=1) 
 l2_squared = L2NormSquared(lam=1)
+
+test = torch.ones((3,3,3), dtype=torch.complex64)
 # %%
-print(l1.forward(torch.tensor([1,2,3])))
-print(l2_squared.forward(torch.tensor([1,2,3])))
+print(l1.forward(test))
+print(l1.prox(test,sigma=1))
+#%%
+print(l2_squared.forward(test))
+print(l2_squared.prox(test,sigma=0.5))
+print(l2_squared.prox_convex_conj(test,sigma=0.5))
 # %%
-print(l1.prox(torch.tensor([1,2,3]),sigma=1))
-print(l2_squared.prox(torch.tensor([1,2,3]),sigma=1))
+#print(l1.prox(torch.tensor([1,2,3]),sigma=1))
+#print(l2_squared.prox(torch.tensor([1,2,3]),sigma=1))
+l2_squared = L2NormSquared(lam=1, dim=[0,1])
+print(l2_squared.prox(torch.real(test),sigma=1))
+print(l2_squared.prox(torch.imag(test),sigma=1))
+print(l2_squared.prox(test,sigma=1))
+#%%
+print(l2_squared.prox_convex_conj(torch.real(test),sigma=1))
+print(l2_squared.prox_convex_conj(torch.imag(test),sigma=1))
+print(l2_squared.prox_convex_conj(test,sigma=1))
 #%%
 print(l1.prox_convex_conj(torch.tensor([1,2,3]),sigma=1))
 print(l2_squared.prox_convex_conj(torch.tensor([1,2,3]),sigma=1))
 #%%
+
+ex_tensor = torch.rand([3,4,126,126])
+# %%
--- src/mrpro/operators/functionals/l2_squared.py
+++ src/mrpro/operators/functionals/l2_squared.py
@@ -1,5 +1,3 @@
-
-
 import torch
 import torch.nn.functional as F  # noqa: N812
 
@@ -7,37 +5,21 @@ from mrpro.operators._Functional import ProximableFunctional
 
 class L2NormSquared(ProximableFunctional):
     
-    def __init__(self, lam:float=1.0, g:torch.Tensor=torch.tensor([0])):
+    def __init__(self, lam:float=1.0, g:torch.Tensor=torch.tensor([0]), dim:list=(None)):
         super().__init__(lam=lam)
         self.g = g
+        self.dim = dim
 
     def forward(self, x:torch.Tensor) -> tuple[torch.Tensor]:
-        return (x.inner(x),) # does this work for complex? or should it be vdot
+        return (torch.pow(torch.linalg.norm(x.flatten(), ord=2, dim=self.dim, keepdim=True),2),)
     
     def convex_conj(self):
         return 1/4 * L2NormSquared()
     
     def prox(self, x:torch.Tensor, sigma:torch.Tensor) -> tuple[torch.Tensor]:
-        is_complex = x.is_complex()
-        if is_complex:
-            x = torch.view_as_real(x)
-            self.lam = torch.tensor([self.lam]).unsqueeze(-1)
-            sigma = torch.tensor([sigma]).unsqueeze(-1)
-            self.g = torch.tensor([self.g]).unsqueeze(-1)
-        x = (x+sigma*self.g)/(1+2*self.lam*sigma)
-        if is_complex:
-            x = torch.view_as_complex(x)
-        return (x,)
+        x_out = ((x+sigma*self.g)/(1+2*self.lam*sigma))
+        return (x_out,)
                 
     def prox_convex_conj(self, x:torch.Tensor, sigma:torch.Tensor) -> tuple[torch.Tensor]:
-        is_complex = x.is_complex()
-        if is_complex:
-            x = torch.view_as_real(x)
-            self.lam = torch.tensor([self.lam]).unsqueeze(-1)
-            sigma = torch.tensor([sigma]).unsqueeze(-1)
-            self.g = torch.tensor([self.g]).unsqueeze(-1)
-        x = (x-sigma*self.g)/(1+0.5 / self.lam*sigma)
-        if is_complex:
-            x = torch.view_as_complex(x)
-        return (x,)
-                
+        x_out = (x-sigma*self.g)/(1+0.5 / self.lam*sigma)
+        return (x_out,)
