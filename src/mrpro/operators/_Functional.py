6ef59690293f15624a5f280447dd42cfec460f7c
JoHa0811 <johanneshammacher1994@gmail.com>
2024-05-24 16:41:48 +0200

    fixed pre-commits

--- src/mrpro/operators/_Functional.py
+++ src/mrpro/operators/_Functional.py
@@ -1,5 +1,9 @@
 """Base Class Functional."""
 
+from abc import ABC
+from abc import abstractmethod
+from collections.abc import Sequence
+
 import torch
 
 from mrpro.operators._Operator import Operator
@@ -12,27 +16,33 @@ class Functional(Operator[torch.Tensor, tuple[torch.Tensor]]):
         Operator (_type_): _description_
     """
 
-    def __init__(self, lam: float = 1.0):
+    def __init__(
+        self, lam: torch.Tensor | float = 1.0, g: torch.Tensor | None = None, dim: Sequence[int] | None = None
+    ):
         """init.
 
         Args:
             lam (float, optional): _description_. Defaults to 1.0.
         """
         super().__init__()
-        self.lam = lam
+        self.register_buffer('lam', torch.as_tensor(lam))
+        if g is None:
+            g = torch.tensor(0.0)
+        self.register_buffer('g', g)
+        self.dim = dim
 
 
-class ProximableFunctional(Functional):
+class ProximableFunctional(Functional, ABC):
     """ProximableFunction Base Class.
 
     Args:
         Functional (_type_): _description_
     """
 
+    @abstractmethod
     def prox(self, x: torch.Tensor, sigma: torch.Tensor) -> tuple[torch.Tensor]:
         """Apply forward operator."""
-        return (x - self.prox_convex_conj(x, sigma),)
 
     def prox_convex_conj(self, x: torch.Tensor, sigma: torch.Tensor) -> tuple[torch.Tensor]:
         """Apply forward operator."""
-        return (x - self.prox(x, sigma),)
+        return (x - sigma * self.prox(x * 1 / sigma, 1 / sigma),)
