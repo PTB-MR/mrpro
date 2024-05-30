62702c394a60639a16dae8625a9bc586c1446042
ckolbPTB <christoph.kolbitsch@ptb.de>
2024-05-30 20:56:49 +0200

    doc string added

--- src/mrpro/operators/_WaveletOp.py
+++ src/mrpro/operators/_WaveletOp.py
@@ -1,3 +1,19 @@
+"""Wavelet operator."""
+
+# Copyright 2024 Physikalisch-Technische Bundesanstalt
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from __future__ import annotations
 
 from collections.abc import Sequence
@@ -26,7 +42,7 @@ class WaveletOp(LinearOperator):
         wavelet_name: str = 'db4',
         level: int | None = None,
     ):
-        """_summary_
+        """Wavelet operator.
 
         Parameters
         ----------
