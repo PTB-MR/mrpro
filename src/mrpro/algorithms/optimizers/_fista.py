7d1106c1a0ec464b31cb8783a297904f6d0329e5
guastinimara <mara.guastini@gmail.com>
2024-06-06 09:25:49 +0200

    added fista algorithm and commented ideas on examples

--- src/mrpro/algorithms/optimizers/_fista.py
+++ src/mrpro/algorithms/optimizers/_fista.py
@@ -11,24 +11,41 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-import torch
 import math
 
-from mrpro.operators._LinearOperator import LinearOperator
+import torch
+
+from mrpro.operators._Functional import Functional
+
+
+def grad(f: Functional, x: torch.Tensor):
+    """Compute the gradient of f(x).
+
+    Parameters
+    ----------
+    f
+        differentiable function
+    x
+        point in the domain of f
+    """
+    x = x.clone().detach().requires_grad_(True)
+    y = f(x)
+    return torch.autograd.grad(y, x)[0]
 
 
 def fista(
     f: Functional,
     g: Functional,
     initial_value: torch.Tensor,
-    stepsize: float = 1., 
+    stepsize: float = 1.0,
     reg_parameter: float = 0.01,
     max_iterations: int = 128,
+    backtrack_factor: float = 1.0,
 ) -> torch.Tensor:
     """FISTA algorithm for solving problem min_x f(x) + g(x).
 
     It relies on the implementation of the proximal map of g.
-    
+
     Parameters
     ----------
     f
@@ -38,121 +55,69 @@ def fista(
     initial_value
         initial value for the solution of the algorithm
     stepsize
-        stepsize needed in the gradient step, is constant throughout all 
+        stepsize needed in the gradient step, is constant throughout all
         iterations
+    reg_parameter
+        regularization parameter that multiplies g
     max_iterations, optional
         number of iterations
-    
+    backtrack_factor
+        must be <=1. if <1., Backtracking rule for stepsize following https://www.ceremade.dauphine.fr/~carlier/FISTA
+        is used
+
     Returns
     -------
         an approximate solution of the minimization problem
     """
-    x = initial_value
-    y = x
-    t = 1.
-    
-    for iteration in range(max_iterations):
-        # calculate the proximal step
-        x_update = g.prox(y - stepsize * torch.autograd.grad(f(y), y), 
-                          reg_parameter) 
-        
-        # update the stepsize
-        t_update = (1 + math.sqrt(1 + 4*t**2))/2
-        
+    backtracking = not math.isclose(backtrack_factor, 1)
+    complex_input = torch.is_complex(initial_value)
+    x_old = initial_value
+    y = initial_value
+    t_old = 1.0
+
+    for _ in range(max_iterations):
+        while stepsize > 1e-30:
+            f_y = f(y)
+            # calculate the proximal gradient step
+            gradient = grad(f_y, y)
+            x = g.prox(y - stepsize * gradient, reg_parameter * stepsize)
+
+            if not backtracking:
+                # no need to check stepsize, continue to next iteration
+                break
+            difference = x - y
+            Q = f_y + 1 / (2 * stepsize) * difference.abs().square().sum()
+            if complex_input:
+                Q += torch.vdot(gradient.flatten(), difference.flatten()).real
+            else:
+                Q += torch.vdot(gradient.flatten(), difference.flatten())
+
+            if f(x) <= Q:
+                # stepsize is ok, continue to next iteration
+                break
+            stepsize *= backtrack_factor
+
+        else:
+            if backtracking:
+                raise RuntimeError('After backtracking, the stepsize became to small.')
+            else:
+                raise RuntimeError('Stepsize to small.')
+
+        # update fista timestep t
+        t = (1 + math.sqrt(1 + 4 * t_old**2)) / 2
+
         # update the solution
-        y = x_update + (t - 1.)/t_update * (x_update - x)
-        
-        # update x and stepsize t
-        x = x_update
-        t = t_update
+        y = x + (t - 1.0) / t * (x - x_old)
+
+        # update x and  t
+        x_old = x
+        t_old = t
 
     return y
-
-
-def backtracking_stepsize_rule(f,g,stepsize_old, y, reg_parameter, eta=1.2):
-    """Backtracking rule for stepsize following https://www.ceremade.dauphine.fr/~carlier/FISTA."""
-    stepsize_new = stepsize_old
-    prox_y = g.prox(y - stepsize_new * torch.autograd.grad(f(y), y), 
-                    reg_parameter)
-    difference = prox_y - y
-    Q = torch.vdot(difference.flatten(),torch.autograd.grad(f(y),y).flatten()) + 0.5 * stepsize_new * torch.norm(difference, 2)**2
-    f_prox_y = f(prox_y) 
-    
-    i = 0
-    while f_prox_y - f(y) > Q:
-        i += 1
-        stepsize_new= eta**i * stepsize_old
-        
-        prox_y = g.prox(y - stepsize_new * torch.autograd.grad(f(y), y), reg_parameter)
-        difference = prox_y - y
-        Q = torch.vdot(difference.flatten(),torch.autograd.grad(f(y),y).flatten()) + 0.5 * stepsize_new * torch.norm(difference, 2)**2
-        f_prox_y = f(prox_y) 
-    
-    return stepsize_new
-
-
-def fista_with_backtracking(
-    f: Functional,
-    g: Functional,
-    initial_value: torch.Tensor, 
-    stepsize_initial: float = 1., 
-    reg_parameter: float = 0.01,
-    max_iterations: int = 128,
-) -> torch.Tensor:
-    """FISTA algorithm for solving problem min_x f(x) + g(x).
-
-    This relies on the implementation of the proximal map of g.
-    
-    stepsize is updated in each iteration.
-    
-    Parameters
-    ----------
-    f
-        convex, differentiable functional
-    g
-        convex, non-smooth functional with computable proximal map
-    initial_value
-        initial value for the solution of the algorithm
-    stepsize_initial
-        initial stepsize for gradient step
-    max_iterations, optional
-        maximal number of iterations
-    
-    Returns
-    -------
-        an approximate solution of the minimization problem
-    """
-    x = initial_value
-    y = x
-    t = 1.
-    stepsize_old = stepsize_initial
-    
-    for iteration in range(max_iterations):
-        # updte the stepsize 
-        stepsize_prox = backtracking_stepsize_rule(f,g,stepsize_old, y, 
-                                                   reg_parameter)
-        
-        # calculate the proximal step
-        x_update = g.prox(y - stepsize_prox * torch.autograd.grad(f(y), y), 
-                          reg_parameter)
-        
-        # update the stepsize
-        t_update = (1 + math.sqrt(1 + 4*t**2))/2
-        
-        # update the solution
-        y = x_update + (t - 1.)/t_update * (x_update - x)
-        
-        # update x and stepsize t
-        x = x_update
-        t = t_update
-        stepsize_old = stepsize_prox
-
-    return y
-
 
 
 ##  ideas for examples
-# example deblurring ? 
+# example deblurring ?
 
 # from mrpro.operators._LinearOperator import LinearOperator
 # import torch.nn as nn
@@ -168,11 +133,11 @@ def fista_with_backtracking(
 
 #     def forward(self,x: torch.Tensor, padding=4) -> torch.Tensor:
 #         return F.conv2d(x, self.kernel, padding)
-    
+
 #     def adjoint(self, y, padding=4):
 #         return F.conv_transpose2d(y,self.kernel,padding=4)
-    
-    
+
+
 # def gaussian_kernel(size: int, sigma: float) -> torch.Tensor:
 #     """Generates a Gaussian kernel."""
 #     x = torch.arange(-size // 2 + 1, size // 2 + 1, dtype=torch.float32)
@@ -198,20 +163,19 @@ def fista_with_backtracking(
 # s_solution = fista(f=f,
 #                  g=g,
 #                 initial_value=s_initial,
-#                 stepsize=stepsize, 
+#                 stepsize=stepsize,
 #                 reg_parameter=0.01)
 
 # s_solution = fista_with_backtracking(f=f,
 #                  g=g,
 #                 initial_value=s_initial,
-#                 stepsize_initial=0.5, 
+#                 stepsize_initial=0.5,
 #                 reg_parameter=0.01)
 
 # x_solution = W.inverse(s_solution)
 
 
-
-# example wavelet 
+# example wavelet
 
 # W = WaveletOperator(...)
 # F = FourierOperator(...)
@@ -228,14 +192,13 @@ def fista_with_backtracking(
 # s_solution = fista(f=f,
 #                  g=g,
 #                 initial_value=s_initial,
-#                 stepsize=stepsize, 
+#                 stepsize=stepsize,
 #                 reg_parameter=0.01)
 
 # s_solution = fista_with_backtracking(f=f,
 #                  g=g,
 #                 initial_value=s_initial,
-#                 stepsize_initial=0.5, 
+#                 stepsize_initial=0.5,
 #                 reg_parameter=0.01)
 
 # x_solution = W.inverse(s_solution)
-
