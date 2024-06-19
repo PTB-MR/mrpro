61dadd546ff9e06be008ab41c150db30dff634de
guastinimara <mara.guastini@gmail.com>
2024-06-19 10:21:54 +0200

    corrected with l2 norm

--- tests/algorithms/test_pgd.py
+++ tests/algorithms/test_pgd.py
@@ -17,20 +17,20 @@ from mrpro.algorithms.optimizers import pgd
 from mrpro.data.SpatialDimension import SpatialDimension
 from mrpro.operators import FastFourierOp
 from mrpro.operators.functionals import L1Norm
-from mrpro.operators.functionals import MSEDataDiscrepancy
+from mrpro.operators.functionals import L2NormSquared
 from mrpro.phantoms import EllipsePhantom
 
 
 def test_pgd_convergence_fft_example():
-    dim = SpatialDimension.from_array_xyz((100, 100, 1))
+    dim = SpatialDimension(x=100, y=100, z=1)
     ellipse = EllipsePhantom()
     image = ellipse.image_space(dim)
     fft = FastFourierOp()
     (kspace,) = fft(image)
 
-    mse_discr = MSEDataDiscrepancy(data=kspace)
-    f = mse_discr @ fft
-    g = L1Norm(weight=1 / (100**2))
+    l2 = L2NormSquared(target=kspace)
+    f = l2 @ fft
+    g = L1Norm()
 
     initial_value = torch.ones_like(image)
     pgd_solution = pgd(
@@ -47,22 +47,22 @@ def test_pgd_convergence_fft_example():
 
 
 def test_pgd_convergence_backtracking_fft_example():
-    dim = SpatialDimension.from_array_xyz((100, 100, 1))
+    dim = SpatialDimension(x=100, y=100, z=1)
     ellipse = EllipsePhantom()
     image = ellipse.image_space(dim)
     fft = FastFourierOp()
     (kspace,) = fft(image)
 
-    mse_discr = MSEDataDiscrepancy(data=kspace)
-    f = mse_discr @ fft
-    g = L1Norm(weight=1 / (100**2))
+    l2 = L2NormSquared(target=kspace)
+    f = l2 @ fft
+    g = L1Norm()
 
     initial_value = torch.ones_like(image)
     pgd_solution = pgd(
         f=f,
         g=g,
         initial_value=initial_value,
-        stepsize=1.0,
+        stepsize=2.0,
         reg_parameter=0.01,
         max_iterations=200,
         backtrack_factor=0.75,
@@ -71,48 +71,24 @@ def test_pgd_convergence_backtracking_fft_example():
     assert f(pgd_solution)[0] + g(pgd_solution)[0] < f(initial_value)[0] + g(initial_value)[0]
 
 
-def test_pgd_convergence_denoising_example():
-    dim = SpatialDimension.from_array_xyz((100, 100, 1))
-    ellipse = EllipsePhantom()
-    image = ellipse.image_space(dim)
-    noise = torch.randn_like(image)
-
-    mse_discr = MSEDataDiscrepancy(data=image)
-    f = mse_discr
-    g = L1Norm(weight=1 / (100**2))
-
-    initial_value = image + noise
-    pgd_solution = pgd(
-        f=f,
-        g=g,
-        initial_value=initial_value,
-        stepsize=0.5,
-        reg_parameter=0.01,
-        max_iterations=100,
-        backtrack_factor=1.0,
-    )
-
-    assert f(pgd_solution)[0] + g(pgd_solution)[0] < f(initial_value)[0] + g(initial_value)[0]
-
-
 def test_pgd_convergence_backtracking_denoising_example():
-    dim = SpatialDimension.from_array_xyz((100, 100, 1))
+    dim = SpatialDimension(x=100, y=100, z=1)
     ellipse = EllipsePhantom()
     image = ellipse.image_space(dim)
     noise = torch.randn_like(image)
+    noisy_image = image + noise
 
-    mse_discr = MSEDataDiscrepancy(data=image)
-    f = mse_discr
-    g = L1Norm(weight=1 / (100**2))
+    f = L2NormSquared(target=noisy_image)
+    g = L1Norm()
 
-    initial_value = image + noise
+    initial_value = torch.ones_like(image)
     pgd_solution = pgd(
         f=f,
         g=g,
         initial_value=initial_value,
-        stepsize=1.0,
+        stepsize=2.0,
         reg_parameter=0.01,
-        max_iterations=100,
+        max_iterations=200,
         backtrack_factor=0.75,
     )
 
