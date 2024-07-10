7dbfe3c9be366480f6d4b72fb87e174102b54aa5
Pierrick Bouilloux <148056480+Pierrickkk@users.noreply.github.com>
2024-07-10 11:13:44 +0200

    Remove duplicate KDataProtocol (#356)
    Co-authored-by: Patrick Schuenke <37338697+schuenke@users.noreply.github.com>

--- src/mrpro/data/_kdata/KData.py
+++ src/mrpro/data/_kdata/KData.py
@@ -19,7 +19,7 @@ import datetime
 import warnings
 from collections.abc import Callable
 from pathlib import Path
-from typing import Protocol, Self
+from typing import Self
 
 import h5py
 import ismrmrd
@@ -199,29 +199,3 @@ class KData(KDataSplitMixin, KDataRearrangeMixin, KDataSelectMixin, KDataRemoveO
             ) from None
 
         return cls(kheader, kdata, ktrajectory_final)
-
-
-class _KDataProtocol(Protocol):
-    """Protocol for KData used for type hinting in KData mixins.
-
-    Note that the actual KData class can have more properties and methods than those defined here.
-
-    If you want to use a property or method of KData in a new KDataMixin class,
-    you must add it to this Protocol to make sure that the type hinting works.
-
-    For more information about Protocols see:
-    https://typing.readthedocs.io/en/latest/spec/protocol.html#protocols
-    """
-
-    @property
-    def header(self) -> KHeader: ...
-
-    @property
-    def data(self) -> torch.Tensor: ...
-
-    @property
-    def traj(self) -> KTrajectory: ...
-
-    def __init__(self, header: KHeader, data: torch.Tensor, traj: KTrajectory): ...
-
-    def _split_k2_or_k1_into_other(self, split_idx: torch.Tensor, other_label: str, split_dir: str) -> Self: ...
