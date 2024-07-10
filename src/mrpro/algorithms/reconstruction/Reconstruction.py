865a2cf4068038fa2e8c13187c2056217a6d38c4
ckolbPTB <christoph.kolbitsch@ptb.de>
2024-07-10 17:02:30 +0200

    precomit

--- src/mrpro/algorithms/reconstruction/Reconstruction.py
+++ src/mrpro/algorithms/reconstruction/Reconstruction.py
@@ -14,10 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from abc import ABC
-from abc import abstractmethod
-from typing import Literal
-from typing import Self
+from abc import ABC, abstractmethod
+from typing import Literal, Self
 
 import torch
 
