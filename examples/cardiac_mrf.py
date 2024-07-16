1ac615a0b85c2b7034aef6d30d40627dd4cc489e
Christoph Kolbitsch <christoph.kolbitsch@ptb.de>
2024-07-16 17:56:35 +0200

    radial mrf

--- examples/cardiac_mrf.py
+++ examples/cardiac_mrf.py
@@ -19,9 +19,10 @@ from mrpro.algorithms.optimizers import adam
 from mrpro.data import DcfData
 from mrpro.operators import MagnitudeOp
 from mrpro.operators import FourierOp
+from mrpro.utils import split_idx
 from mrpro.operators.functionals import MSEDataDiscrepancy
 from mrpro.operators.models import InversionRecovery
-from mrpro.data import KData
+from mrpro.data import KData, DcfData
 from mrpro.data import KTrajectory
 from mrpro.data.traj_calculators import KTrajectoryIsmrmrd
 from mrpro.data.traj_calculators import KTrajectoryRadial2D
@@ -37,14 +38,30 @@ scan_name = Path('meas_MID00027_FID05469_20240624_spiral_cMRF_705rep_trig_delay8
 #scan_name = Path('meas_MID00026_FID05468_20240624_spiral_cMRF_705rep_trig_delay800ms/meas_MID00026_FID05468_20240624_spiral_cMRF_705rep_trig_delay800ms_with_traj.h5')
 scan_name = Path('meas_MID00019_FID05461_20240624_spiral_cMRF_705rep_trig_delay800ms/meas_MID00019_FID05461_20240624_spiral_cMRF_705rep_trig_delay800ms_with_traj.h5')
 
-
 pname = Path('/echo/_allgemein/projects/pulseq/measurements/2024-06-27_cMRF_tests/')
 scan_name = Path('meas_MID00143_FID05777_20240627_spiral_cMRF_705rep_only_t1prep/meas_MID00143_FID05777_20240627_spiral_cMRF_705rep_only_t1prep_with_traj.h5')
 
 
-pname_new = Path('/echo/_allgemein/projects/pulseq/measurements/2024-06-27_cMRF_tests/')
-scan_name_new = Path('meas_MID00143_FID05777_20240627_spiral_cMRF_705rep_only_t1prep/meas_MID00143_FID05777_20240627_spiral_cMRF_705rep_only_t1prep_with_traj.h5')
+pname = Path('/echo/_allgemein/projects/pulseq/measurements/2024-07-10_t2prep_tests/meas_MID00018_FID06326_20240710_spiral_cMRF_705rep_trig_delay800ms/')
+scan_name = Path('meas_MID00018_FID06326_20240710_spiral_cMRF_705rep_trig_delay800ms_with_traj.h5')
 
+
+pname = Path('/echo/_allgemein/projects/pulseq/measurements/2024-10-16_T1MES_cMRF/meas_MID00069_FID06447_20240716_spiral_cMRF_no_vds_705rep_trig_delay800ms/')
+scan_name = Path('meas_MID00069_FID06447_20240716_spiral_cMRF_no_vds_705rep_trig_delay800ms_with_traj.h5')
+rr_duration = 1200 
+
+#pname = Path('/echo/_allgemein/projects/pulseq/measurements/2024-10-16_T1MES_cMRF/meas_MID00070_FID06448_20240716_spiral_cMRF_no_vds_705rep_trig_delay800ms/')
+#scan_name = Path('meas_MID00070_FID06448_20240716_spiral_cMRF_no_vds_705rep_trig_delay800ms_with_traj.h5')
+#rr_duration = 1200 
+   
+#pname = Path('/echo/_allgemein/projects/pulseq/measurements/2024-10-16_T1MES_cMRF/meas_MID00021_FID06399_20240710_spiral_cMRF_705rep_trig_delay800ms/')
+#scan_name = Path('meas_MID00021_FID06399_20240710_spiral_cMRF_705rep_trig_delay800ms_with_traj.h5')
+#rr_duration = 1000  
+
+pname = Path('/echo/_allgemein/projects/pulseq/measurements/2024-10-16_T1MES_cMRF/meas_MID00074_FID06452_20240716_radial_cMRF_with_prep_705rep_trig_delay800ms/')
+scan_name = Path('meas_MID00074_FID06452_20240716_radial_cMRF_with_prep_705rep_trig_delay800ms_with_traj.h5')
+rr_duration = 1200 
+     
 fname_angle = Path('/echo/_allgemein/projects/pulseq/mrf/cMRF_fa_705rep.txt')
 with open(fname_angle, "r") as file:
     fa = torch.as_tensor([float(line) for line in file.readlines()])/180 * torch.pi
@@ -52,52 +69,76 @@ with open(fname_angle, "r") as file:
 # %%
 # Image reconstruction
 trajectory = KTrajectoryIsmrmrd()
+#trajectory = KTrajectoryPulseq(pname / '20240716_radial_cMRF_lin_label_705rep_trig-delay800ms.seq')
 kdata = KData.from_file(pname / scan_name, trajectory)
 kdata.header.recon_matrix.x = 192
 kdata.header.recon_matrix.y = 192
 
-kdata_new = KData.from_file(pname_new / scan_name_new, KTrajectoryRadial2D())
-kdata = KData(header=kdata_new.header, data=kdata_new.data, traj=kdata.traj)
+#traj_minus_first_point = KTrajectory(kz=kdata.traj.kz, ky=kdata.traj.ky-kdata.traj.ky[:,:,:,0,None], kx=kdata.traj.kx-kdata.traj.kx[:,:,:,0,None])
+#kdata = KData(header=kdata.header, data=kdata.data, traj=traj_minus_first_point)
 
 # Voronoi dcf
-ktraj_all_rep = KTrajectory(kz=kdata.traj.kz, ky=rearrange(kdata.traj.ky, 'other k2 k1 k0->k2 k1 other k0'), kx=rearrange(kdata.traj.kx, 'other k2 k1 k0->k2 k1 other k0'))
-dcf = DcfData.from_traj_voronoi(ktraj_all_rep)
-dcf_data_voronoi = rearrange(dcf.data, 'k2 k1 other k0->other k2 k1 k0')
-dcf_data_voronoi[dcf_data_voronoi < 1e-2] = 1e-2
+#ktraj_all_rep = KTrajectory(kz=kdata.traj.kz, ky=rearrange(kdata.traj.ky, 'other k2 k1 k0->k2 k1 other k0'), kx=rearrange(kdata.traj.kx, 'other k2 k1 k0->k2 k1 other k0'))
+#dcf = DcfData.from_traj_voronoi(ktraj_all_rep)
+#dcf_data_voronoi = rearrange(dcf.data, 'k2 k1 other k0->other k2 k1 k0')
+#dcf_data_voronoi[dcf_data_voronoi < 1e-2] = 1e-2
 
-# Analytical dcf calculation following 
-# Hoge, R.D., Kwan, R.K.S. and Bruce Pike, G. (1997), Density compensation functions for spiral MRI. Magn. Reson. Med., 
-# 38: 117-128. https://doi.org/10.1002/mrm.1910380117
-ktraj_tensor = kdata.traj.as_tensor()
-ktraj_tensor_norm = torch.linalg.norm(ktraj_tensor[-2:,...], dim=0)
-ktraj_tensor_angle = torch.atan2(ktraj_tensor[-2,...], ktraj_tensor[-1,...])
-gradient_tensor = torch.diff(ktraj_tensor, dim=-1)
-gradient_tensor = torch.cat([gradient_tensor, gradient_tensor[...,-1,None]], dim=-1)
-gradient_tensor_norm = torch.linalg.norm(gradient_tensor[-2:,...], dim=0)
-gradient_tensor_angle = torch.atan2(gradient_tensor[-2,...], gradient_tensor[-1,...])
-dcf_data = gradient_tensor_norm * ktraj_tensor_norm * torch.abs(torch.cos(gradient_tensor_angle - ktraj_tensor_angle))
-dcf_data /= dcf_data.max()
-dcf_data[dcf_data < 1e-2] = 1e-2
+dcf = DcfData.from_traj_voronoi(kdata.traj)
+dcf_data_voronoi = dcf.data
 
-dcf_data2 = gradient_tensor_norm * torch.abs(torch.sin(gradient_tensor_angle - ktraj_tensor_angle))
+dyn_idx = split_idx(torch.arange(0,47), 10, 8)
+dyn_idx = torch.cat([dyn_idx + ind*47 for ind in range(15)], dim=0)
 
-plt.figure()
-plt.plot(dcf_data_voronoi[0,0,0,:], '-r')
-plt.plot(dcf_data_voronoi[100,0,0,:], '-b')
-plt.plot(dcf_data[0,0,0,:], '--r')
-plt.plot(dcf_data[100,0,0,:], '--b')
+kdata = kdata.split_k1_into_other(dyn_idx, other_label='repetition')
+dcf_data_voronoi = rearrange(dcf_data_voronoi, 'k2 k1 other k0->other k2 k1 k0')
+dcf_data_voronoi = rearrange(dcf_data_voronoi[dyn_idx.flatten(),...], '(other k1) 1 k2 k0->other k2 k1 k0', k1=dyn_idx.shape[-1])
+if False:
+    # Analytical dcf calculation following 
+    # Hoge, R.D., Kwan, R.K.S. and Bruce Pike, G. (1997), Density compensation functions for spiral MRI. Magn. Reson. Med., 
+    # 38: 117-128. https://doi.org/10.1002/mrm.1910380117
+    ktraj_tensor = kdata.traj.as_tensor()
+    ktraj_tensor_norm = torch.linalg.norm(ktraj_tensor[-2:,...], dim=0)
+    ktraj_tensor_angle = torch.atan2(ktraj_tensor[-2,...], ktraj_tensor[-1,...])
+    gradient_tensor = torch.diff(ktraj_tensor, dim=-1)
+    gradient_tensor = torch.cat([gradient_tensor, gradient_tensor[...,-1,None]], dim=-1)
+    gradient_tensor_norm = torch.linalg.norm(gradient_tensor[-2:,...], dim=0)
+    gradient_tensor_angle = torch.atan2(gradient_tensor[-2,...], gradient_tensor[-1,...])
+    dcf_data = gradient_tensor_norm * ktraj_tensor_norm * torch.abs(torch.cos(gradient_tensor_angle - ktraj_tensor_angle))
+    dcf_data /= dcf_data.max()
+    dcf_data[dcf_data < 1e-2] = 1e-2
+
+
+    kdata_all_rep = rearrange(kdata.data, 'other coils k2 k1 k0->k2 coils k1 other k0')
+    kdata_pipe = KData(header=kdata.header, data=kdata_all_rep, traj=ktraj_all_rep)
+    dcf_data_pipe=torch.ones(*kdata_pipe.data.shape[:-4],1,*kdata_pipe.data.shape[-3:])
+    F=FourierOp.from_kdata(kdata_pipe)
+    for i in range(10):
+        new = (F@F.H)(dcf_data_pipe+0j)[0]
+        print("loss",(new-1).abs().mean())
+        dcf_data_pipe/=new.abs().sqrt()
+    #dcf_data_pipe = DcfData(dcf_pipe.squeeze(-4))
+    dcf_data_pipe = dcf_data_pipe[:,0,...]
+    dcf_data_pipe = rearrange(dcf_data_pipe, 'k2 k1 other k0->other k2 k1 k0')
+
+    plt.figure()
+    plt.plot(dcf_data_voronoi[0,0,0,:], '-r')
+    plt.plot(dcf_data_voronoi[100,0,0,:], '-b')
+    plt.plot(dcf_data[0,0,0,:], '--r')
+    plt.plot(dcf_data[100,0,0,:], '--b')
+    plt.plot(dcf_data_pipe[0,0,0,:], ':r')
+    plt.plot(dcf_data_pipe[100,0,0,:], ':b')
 
 fourier_op = FourierOp.from_kdata(kdata)
-(img,) = fourier_op.adjoint(kdata.data * dcf_data[:,None,...])
-img = torch.sum(img**2,dim=1)
+(img,) = fourier_op.adjoint(kdata.data * dcf_data_voronoi[:,None,...])
+img = torch.sum(img,dim=1)
 img = torch.squeeze(img)
 
 fig, ax = plt.subplots(1,3, figsize=(12,4))
 ax[0].imshow(img[0,:,:].abs())
 ax[0].set_title('First image')
-ax[1].imshow(torch.mean(img[:47,:,:].abs(),dim=0))
-ax[1].set_title('Average over first 44 images')
-ax[2].imshow(torch.mean(img[:,:,:].abs(),dim=0))
+ax[1].imshow(torch.abs(torch.mean(img[:47,:,:],dim=0)))
+ax[1].set_title('Average over first 47 images')
+ax[2].imshow(torch.abs(torch.mean(img[:,:,:],dim=0)))
 ax[2].set_title('Average over all images')
 
 # %%
@@ -115,14 +156,17 @@ flip_angles = fa
 rf_phases = 0
 te = 0.845
 tr = kdata.header.tr * 1000
-inv_prep_ti = [20,None,None,None,None]*3 # 20 ms delay after inversion pulse in block 0
+inv_prep_ti = [21,None,None,None,None]*3 # 20 ms delay after inversion pulse in block 0
 t2_prep_te = [None,None,30,50,100]*3 # T2-preparation pulse with TE = 30, 50, 100
-t2_prep_te = None
 n_rf_pulses_per_block = 47 # 47 RF pulses in each block
-delay_after_block = [1000, 970, 950, 900, 980]*3
+delay_after_block = [0, 30, 50, 100, 21]*3
+delay_after_block = [rr_duration-delay for delay in delay_after_block]
 epg_mrf_fisp = EpgMrfFispWithPreparation(flip_angles, rf_phases, te, tr, inv_prep_ti, t2_prep_te, n_rf_pulses_per_block, delay_after_block)
 (signal_dictionary,) = epg_mrf_fisp.forward(m0, t1, t2)
 
+
+signal_dictionary = rearrange(signal_dictionary[dyn_idx.flatten(),...], '(other k1) t->other t k1', k1=dyn_idx.shape[-1])
+signal_dictionary = torch.mean(signal_dictionary, dim=-1)
 signal_dictionary = signal_dictionary.abs()
 
 # %%
@@ -159,15 +203,27 @@ t2_match = rearrange(t2[idx_best_match], '(y x)->y x', y=n_y, x=n_x)
 # %%
 # Read in reference scans
 dcm = pydicom.dcmread(fname_t1_ref)
-t1_ref = np.asarray(dcm.pixel_array.astype(np.float32))
+t1_ref = np.asarray(dcm.pixel_array.astype(np.float32))[:,::-1]
 
 dcm = pydicom.dcmread(fname_t2_ref)
-t2_ref = np.asarray(dcm.pixel_array.astype(np.float32))/10 # t2 maps are always scaled
+t2_ref = np.asarray(dcm.pixel_array.astype(np.float32))[:,::-1]/10 # t2 maps are always scaled
 
 # %%
-fig, ax = plt.subplots(2,2, figsize=(8,4))
-ax[0,0].imshow(t1_ref, vmin=0, vmax=2000)
-ax[1,0].imshow(t2_ref, vmin=0, vmax=200)
-ax[0,1].imshow(t1_match, vmin=0, vmax=2000)
-ax[1,1].imshow(t2_match, vmin=0, vmax=200)
+fig, ax = plt.subplots(2,2, figsize=(6,4))
+im = ax[0,0].imshow(t1_ref, vmin=0, vmax=2000)
+ax[0,0].set_title('MOLLI (ms)')
+fig.colorbar(im, extend='both', shrink=0.9, ax=ax[0,0])
+
+im = ax[1,0].imshow(t2_ref, vmin=0, vmax=200)
+ax[1,0].set_title('T2prep (ms)')
+fig.colorbar(im, extend='both', shrink=0.9, ax=ax[1,0])
+
+im = ax[0,1].imshow(t1_match[40:160,40:160], vmin=0, vmax=2000)
+ax[0,1].set_title('MRF T1 (ms)')
+fig.colorbar(im, extend='both', shrink=0.9, ax=ax[0,1])
+
+im = ax[1,1].imshow(t2_match[40:160,40:160], vmin=0, vmax=200)
+ax[1,1].set_title('MRF T2 (ms)')
+fig.colorbar(im, extend='both', shrink=0.9, ax=ax[1,1])
+plt.tight_layout()
 # %%
---
+++ examples/cardiac_mrf_proj.py
@@ -0,0 +1,119 @@
+#%%
+import numpy as np
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from mrpro.data import KData
+from mrpro.data.traj_calculators import KTrajectoryCartesian
+from mrpro.operators import FastFourierOp
+from einops import rearrange
+from pathlib import Path
+import torch
+from mrpro.operators.models import EpgMrfFispWithPreparation
+
+#%%
+pname = Path('/echo/_allgemein/projects/pulseq/measurements/2024-10-16_T1MES_cMRF/meas_MID00043_FID06421_20240716_cMRF_1D_705rep_trig_delay800ms/')
+scan_name = Path('meas_MID00043_FID06421_20240716_cMRF_1D_705rep_trig_delay800ms.h5')
+rr_duration = 1200    
+     
+fname_angle = Path('/echo/_allgemein/projects/pulseq/mrf/cMRF_fa_705rep.txt')
+with open(fname_angle, "r") as file:
+    fa = torch.as_tensor([float(line) for line in file.readlines()])/180 * torch.pi
+
+
+trajectory = KTrajectoryCartesian()
+kdata = KData.from_file(pname / scan_name, trajectory)
+
+fft_1d_op = FastFourierOp(dim = (-1,))
+(idat,) = fft_1d_op(kdata.data)
+idat = torch.abs(torch.squeeze(idat[:,0,...]))
+
+#%%
+plt.figure(figsize=(12,16))
+plt.imshow(idat)
+
+#%%
+
+t1_ref = [850, 1718, 1100, 500]
+t2_ref = [155, 270, 240, 80]
+
+# Select tubes
+tube1 = torch.mean(idat[:,70:75], dim=1)
+tube2 = torch.mean(idat[:, 85:90], dim=1)
+tube3 = torch.mean(idat[:, 105:110], dim=1)
+tube4 = torch.mean(idat[:, 120:125], dim=1)
+signal_curves = torch.stack((tube1, tube2, tube3, tube4), dim=0)
+signal_curves = rearrange(signal_curves, 'x t->t x')
+
+plt.figure()
+plt.plot(signal_curves)
+
+
+# %%
+# Dictionary settings
+t1 = torch.linspace(100, 3000, 100)[:,None]
+t2 = torch.linspace(10, 400, 100)[None,:]
+t1, t2 = torch.broadcast_tensors(t1, t2)
+t1 = t1.flatten()
+t2 = t2.flatten()
+m0 = torch.ones_like(t1)
+
+#%%
+flip_angles = fa
+rf_phases = 0
+te = 2.2
+tr = 5.4 #kdata.header.tr * 1000
+inv_prep_ti = [21,None,None,None,None]*3 # 20 ms delay after inversion pulse in block 0
+t2_prep_te = [None,None,30,50,100]*3 # T2-preparation pulse with TE = 30, 50, 100
+n_rf_pulses_per_block = 47 # 47 RF pulses in each block
+delay_after_block = [0, 30, 50, 100, 21]*3
+delay_after_block = [rr_duration-delay for delay in delay_after_block]
+epg_mrf_fisp = EpgMrfFispWithPreparation(flip_angles, rf_phases, te, tr, inv_prep_ti, t2_prep_te, n_rf_pulses_per_block, delay_after_block)
+(signal_dictionary,) = epg_mrf_fisp.forward(m0, t1, t2)
+
+signal_dictionary = signal_dictionary.abs()
+
+# Normalise dictionary entries
+vector_norm = torch.linalg.vector_norm(signal_dictionary, dim=0)
+signal_dictionary /= vector_norm
+
+# %%
+t1_eval = torch.as_tensor([100, 500, 1000, 100, 500, 1000])
+t2_eval = torch.as_tensor([40, 40, 40, 100, 100, 100])
+m0_eval = torch.ones_like(t1_eval)
+(signal_eval,) = epg_mrf_fisp.forward(m0_eval, t1_eval, t2_eval)
+
+fig, ax = plt.subplots(2,1, figsize=(12,6))
+ax[0].plot(fa / torch.pi * 180)
+ax[1].plot(signal_eval.abs(), label=['100|40', '500|40', '1000|40', '100|100', '500|100', '1000|100'])
+ax[1].legend()
+
+# %%
+# Dictionary matching
+dot_product = torch.mm(rearrange(signal_curves.abs(), 'other x->x other'), signal_dictionary)
+idx_best_match = torch.argmax(torch.abs(dot_product), dim=1)
+t1_match = t1[idx_best_match]
+t2_match = t2[idx_best_match]
+signal_best_match = signal_dictionary[:, idx_best_match]
+
+#%% 
+# Calc ref signals
+if t1_ref is not None and t2_ref is not None:
+    m0_ref = [1,] * len(t1_ref)
+    (signal_ref,) = epg_mrf_fisp.forward(torch.as_tensor(m0_ref), 
+                                         torch.as_tensor(t1_ref), 
+                                         torch.as_tensor(t2_ref))
+    signal_ref = torch.abs(signal_ref)
+
+
+# %%
+sig_max = signal_curves.max() * 1.2
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots(len(t2_match),1, figsize=(12,12))
+for ind in range(len(t2_match)):
+    ax[ind].set_title(f'T2: {int(t2_match[ind])} | T1: {int(t1_match[ind])} (GT: T2: {int(t2_ref[ind])} | T1: {int(t1_ref[ind])})')
+    p = ax[ind].plot(signal_curves[:,ind]/signal_curves[:,ind].max(), '-o')
+    ax[ind].plot(signal_best_match[:, ind]/signal_best_match[:, ind].max(), '--', color=p[0].get_color())
+    ax[ind].plot(signal_ref[:,ind]/signal_ref[:,ind].max(), '--k')
+    ax[ind].set_ylabel('Signal')
+# %%
