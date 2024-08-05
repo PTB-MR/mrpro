5ebe5f6d141d050086dcb428787fb04b5722d7d7
ckolbPTB <christoph.kolbitsch@ptb.de>
2024-08-05 20:50:48 +0200

    bug fixed and tests added

--- src/mrpro/algorithms/csm/inati.py
+++ src/mrpro/algorithms/csm/inati.py
@@ -8,52 +8,60 @@ from mrpro.utils.sliding_window import sliding_window
 
 def inati(
     coil_images: torch.Tensor,
-    kernel_size: SpatialDimension[int] | int,
-    n_power_iterations: int,
+    smoothing_width: SpatialDimension[int] | int,
 ) -> torch.Tensor:
     """Calculate a coil sensitivity map (csm) using an the Inati method [INA2013]_ [INA2014]_.
 
     This is for a single set of coil images. The input should be a tensor with dimensions (coils, z, y, x). The output
     will have the same dimensions. Either apply this function individually to each set of coil images, or see
-    CsmData.from_idata_walsh which performs this operation on a whole dataset.
+    CsmData.from_idata_inati which performs this operation on a whole dataset.
 
     .. [INA2013] Inati S, Hansen M, Kellman P (2013) A solution to the phase problem in adaptvie coil combination.
        in Proceedings of the 21st Annual Meeting of ISMRM, Salt Lake City, USA, 2672.
 
-    .. [INA2014] Inati S, Hansen M (2014) A Fast Optimal Method for Coil Sensitivity Estimation and Adaptive Coil Combination
-       for Complex Images. in Proceedings of Joint Annual Meeting ISMRM-ESMRMB, Milan, Italy, 7115.
+    .. [INA2014] Inati S, Hansen M (2014) A Fast Optimal Method for Coil Sensitivity Estimation and Adaptive Coil
+       Combination for Complex Images. in Proceedings of Joint Annual Meeting ISMRM-ESMRMB, Milan, Italy, 7115.
 
     Parameters
     ----------
     coil_images
-        images for each coil element
-    kernel_size
-        kernel size
-    n_power_iterations
-        number of iterations used to determine dominant eigenvector
+        Images for each coil element
+    smoothing_width
+        Size of the smoothing kernel
     """
-    padding_mode = 'circular'
-    if isinstance(kernel_size, int):
-        kernel_size = SpatialDimension(kernel_size, kernel_size, kernel_size)
+    # Padding at the edge of the images
+    padding_mode = 'replicate'
 
-    if any([ks % 2 != 1 for ks in [kernel_size.z, kernel_size.y, kernel_size.x]]):
+    # Maximum number of power iterations. Stop if relative change < 0.1
+    max_power_iterations = 100
+
+    if isinstance(smoothing_width, int):
+        smoothing_width = SpatialDimension(smoothing_width, smoothing_width, smoothing_width)
+
+    if any(ks % 2 != 1 for ks in [smoothing_width.z, smoothing_width.y, smoothing_width.x]):
         raise ValueError('kernel_size must be odd')
-    if n_power_iterations < 1:
-        raise ValueError('power must be at least 1')
 
-    halfKs = [ks // 2 for ks in kernel_size.zyx]
-    padded = torch.nn.functional.pad(
-        coil_images, (halfKs[-1], halfKs[-1], halfKs[-2], halfKs[-2], halfKs[-3], halfKs[-3]), mode=padding_mode
+    ks_halved = [ks // 2 for ks in smoothing_width.zyx]
+    padded_coil_images = torch.nn.functional.pad(
+        coil_images,
+        (ks_halved[-1], ks_halved[-1], ks_halved[-2], ks_halved[-2], ks_halved[-3], ks_halved[-3]),
+        mode=padding_mode,
     )
-    D = sliding_window(padded, kernel_size.zyx, axis=(-3, -2, -1)).flatten(-3)  # coil E1, E0, ks*ks
-    DH_D = torch.einsum('i...j,k...j->...ik', D, D.conj())  # E1,E0,coil,coil
-    singular_vector = torch.sum(D, dim=-1)  # coil, E1, E0
+    # Get the voxels in an ROI defined by the smoothing_width around each voxel leading to shape (coils z y x ks*ks)
+    coil_images_roi = sliding_window(padded_coil_images, smoothing_width.zyx, axis=(-3, -2, -1)).flatten(-3)
+    # Covariance with shape (z y x coils coils)
+    coil_images_covariance = torch.einsum('i...j,k...j->...ik', coil_images_roi.conj(), coil_images_roi)
+    singular_vector = torch.sum(coil_images_roi, dim=-1)  # coils z y x
     singular_vector /= singular_vector.abs().square().sum(0, keepdim=True).sqrt()
-    for _ in range(n_power_iterations):
-        singular_vector = torch.einsum('...ij,j...->i...', DH_D, singular_vector)  # coil, E1, E0
+    for _ in range(max_power_iterations):
+        singular_vector_old = singular_vector
+        singular_vector = torch.einsum('...ij,j...->i...', coil_images_covariance, singular_vector)  # coils z y x
         singular_vector /= singular_vector.abs().square().sum(0, keepdim=True).sqrt()
-    singular_value = torch.einsum('i...j,i...->...j', D, singular_vector)  # E1, E0, ks*ks
+        if torch.isclose(singular_vector, singular_vector_old, rtol=1e-1).all():
+            break
+
+    singular_value = torch.einsum('i...j,i...->...j', coil_images_roi, singular_vector)  # z y x ks*ks
     phase = singular_value.sum(-1)
-    phase /= phase.abs()  # E1, E0
+    phase /= phase.abs()  # z y x
     csm = singular_vector.conj() * phase[None, ...]
     return csm
--- tests/algorithms/csm/test_iterative_walsh.py
+++ tests/algorithms/csm/test_iterative_walsh.py
@@ -2,24 +2,9 @@
 
 import torch
 from mrpro.algorithms.csm import iterative_walsh
-from mrpro.data import IData, SpatialDimension
-from mrpro.phantoms.coils import birdcage_2d
+from mrpro.data import SpatialDimension
+from tests.algorithms.csm.conftest import multi_coil_image
 from tests.helper import relative_image_difference
-
-
-def multi_coil_image(n_coils, ph_ellipse, random_kheader):
-    """Create multi-coil image."""
-    image_dimensions = SpatialDimension(z=1, y=ph_ellipse.n_y, x=ph_ellipse.n_x)
-
-    # Create reference coil sensitivities
-    csm_ref = birdcage_2d(n_coils, image_dimensions)
-
-    # Create multi-coil phantom image data
-    img = ph_ellipse.phantom.image_space(image_dimensions)
-    # +1 to ensure that there is signal everywhere, for voxel == 0 csm cannot be determined.
-    img_multi_coil = (img + 1) * csm_ref
-    idata = IData.from_tensor_and_kheader(data=img_multi_coil, kheader=random_kheader)
-    return (idata, csm_ref)
 
 
 def test_iterative_Walsh(ellipse_phantom, random_kheader):
