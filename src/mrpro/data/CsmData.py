27128024f93f38c8f4d13caf1a3d4ad67f9c8153
Christoph Kolbitsch <christoph.kolbitsch@ptb.de>
2024-09-03 16:55:18 +0200

    Add Csm Inati 2D and 3D (#345)
    Co-authored-by: Felix F Zimmermann <fzimmermann89@gmail.com>

--- src/mrpro/data/CsmData.py
+++ src/mrpro/data/CsmData.py
@@ -53,6 +53,32 @@ class CsmData(QData):
         csm = cls(header=idata.header, data=csm_tensor)
         return csm
 
+    @classmethod
+    def from_idata_inati(
+        cls,
+        idata: IData,
+        smoothing_width: int | SpatialDimension[int] = 5,
+        chunk_size_otherdim: int | None = None,
+    ) -> Self:
+        """Create csm object from image data using Inati method.
+
+        Parameters
+        ----------
+        idata
+            IData object containing the images for each coil element.
+        smoothing_width
+            Size of the smoothing kernel.
+        chunk_size_otherdim:
+            How many elements of the other dimensions should be processed at once.
+            Default is None, which means that all elements are processed at once.
+        """
+        from mrpro.algorithms.csm.inati import inati
+
+        csm_fun = torch.vmap(lambda img: inati(img, smoothing_width), chunk_size=chunk_size_otherdim)
+        csm_tensor = csm_fun(idata.data)
+        csm = cls(header=idata.header, data=csm_tensor)
+        return csm
+
     def as_operator(self) -> SensitivityOp:
         """Create SensitivityOp using a copy of the CSMs."""
         from mrpro.operators.SensitivityOp import SensitivityOp
