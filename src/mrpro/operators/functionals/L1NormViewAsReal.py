18beefd962bbd3be06514b156e565337e24a75e3
Felix F Zimmermann <fzimmermann89@gmail.com>
2024-09-14 14:49:56 +0000

    change l1normviewasreal complex weight meaning. passes tests.

--- src/mrpro/operators/functionals/L1NormViewAsReal.py
+++ src/mrpro/operators/functionals/L1NormViewAsReal.py
@@ -1,7 +1,5 @@
 """L1 Norm."""
 
-from collections.abc import Sequence
-
 import torch
 
 from mrpro.operators.Functional import ProximableFunctional
@@ -11,154 +9,77 @@ class L1NormViewAsReal(ProximableFunctional):
     r"""Functional class for the L1 Norm, where C is identified with R^2.
 
     This implements the functional given by
-        f: C^N --> [0, \infty), x ->  \| W*(x-b)\|_1 := \|Re( W*(x-b) )\|_1 + \|Im( W*(x-b) )\|_1
-    where W is a either a scalar or tensor that corresponds to a (block-) diagonal operator
-    that is applied to the input.
+        f: C^N --> [0, \infty), x ->  \|Wr.*Re(x-b) )\|_1 + \|( Wi.*Im(x-b) )\|_1
+    where Wr and Wi are a either scalars or tensors and .* denotes element-wise multiplication.
+
+    If the weight parameter W is real-valued, Wr and Wi are both set to W.
+    If it is complex-valued, Wr and Wi are set to the real and imaginary part of W, respectively.
 
     The norm of the vector is computed along the dimensions given by "dim".
-
     Further, it is possible to scale the functional by N, i.e. by the number voxels of
     the elements of the vector space that is spanned by the dimensions indexed by "dim".
     If "dim" is set to None and "keepdim" to False, the result is a single number, which
     is typically of interest for computing loss functions.
 
-    Further, the proximal mapping and the proximal mapping of the convex conjugate are given.
-
-    Note that here, the proximal mapping and the convex conjugate of the proximal mapping are
-    derived assuming the weight vector defines an invertible (block-) diagonal
-    operator with all entries being strictly greater than zero, i.e. only real-valued
-    weight-tensors are allowed.
     """
 
     def forward(
         self,
         x: torch.Tensor,
-        dim: Sequence[int] | None = None,
-        keepdim: bool | None = None,
-        divide_by_n: bool | None = None,
     ) -> tuple[torch.Tensor]:
         """Forward method.
 
-        Compute the l1-norm of the input with C identified as R^2.
+        Compute the L1-norm of the input with C identified as R^2.
 
         Parameters
         ----------
-            x
-                input tensor
-            dim
-                dimensions that span the considered vector space
-            divide_by_n
-                divide by the number of voxels or not
-            keepdim
-                whether or not to maintain the dimensions of the input
+        x
+            input tensor
 
         Returns
         -------
-            l1 norm of the input tensor
+            L1 norm of the input tensor, where C is identified as R^2
         """
-        if dim is None:
-            dim = self.dim
-        if keepdim is None:
-            keepdim = self.keepdim
-        if divide_by_n is None:
-            divide_by_n = self.divide_by_n
-
         dtype = torch.promote_types(self.target.dtype, x.dtype)
         x = x.to(dtype)
         target = self.target.to(dtype)
         diff = x - target
-        is_complex = diff.is_complex()
-
-        l1norm = L1Norm(weight=self.weight, target=self.target, divide_by_n=divide_by_n)
-
-        if is_complex:
-            l1norm_real = L1Norm(weight=self.weight, target=self.target.real, divide_by_n=divide_by_n)
-            l1norm_imag = L1Norm(weight=self.weight, target=self.target.imag, divide_by_n=divide_by_n)
-
-            return (
-                l1norm_real.forward(diff.real, dim, keepdim, divide_by_n)[0]
-                + l1norm_imag.forward(diff.imag, dim, keepdim, divide_by_n)[0],
-            )
+        if diff.is_complex() and self.weight.is_complex():
+            value = (self.weight.real * diff.real).abs() + (self.weight.imag * diff.imag).abs()
+        elif diff.is_complex():
+            value = (self.weight * diff.real).abs() + (self.weight * diff.imag).abs()
         else:
-            l1norm = L1Norm(weight=self.weight, target=self.target, divide_by_n=divide_by_n)
-            return (l1norm.forward(diff, dim, keepdim, divide_by_n)[0],)
+            value = (self.weight * diff).abs()
 
-    def prox(
-        self, x: torch.Tensor, sigma: torch.Tensor, dim: Sequence[int] | None = None, divide_by_n: bool | None = None
-    ) -> tuple[torch.Tensor]:
+        if self.divide_by_n:
+            return (torch.mean(value, dim=self.dim, keepdim=self.keepdim),)
+        else:
+            return (torch.sum(value, dim=self.dim, keepdim=self.keepdim),)
+
+    def prox(self, x: torch.Tensor, sigma: torch.Tensor | float) -> tuple[torch.Tensor]:
         """Proximal Mapping of the L1 Norm.
 
         Compute the proximal mapping of the L1-norm with C identified as R^2.
 
         Parameters
         ----------
-            x
-                input tensor
-            sigma
-                scaling factor
-            dim
-                dimensions that span the considered vector space
-            divide_by_n
-                divide by the number of voxels or not
+        x
+            input tensor
+        sigma
+            real valued scaling factor
 
         Returns
         -------
             Proximal mapping applied to the input tensor
         """
-        if dim is None:
-            dim = self.dim
-        if divide_by_n is None:
-            divide_by_n = self.divide_by_n
-
-        if x.is_complex():
-            l1norm_real = L1Norm(weight=self.weight, target=self.target.real, divide_by_n=divide_by_n)
-            l1norm_imag = L1Norm(weight=self.weight, target=self.target.imag, divide_by_n=divide_by_n)
-            return (
-                torch.complex(
-                    l1norm_real.prox(x.real, sigma=sigma, dim=dim, divide_by_n=divide_by_n)[0],
-                    l1norm_imag.prox(x.imag, sigma=sigma, dim=dim, divide_by_n=divide_by_n)[0],
-                ),
-            )
+        diff = x - self.target
+        threshold = self._divide_by_n(self.weight * sigma, torch.broadcast_shapes(x.shape, self.weight.shape))
+        if diff.is_complex():
+            real = x.real - diff.real / torch.clamp_min((diff.real / threshold.real).abs(), 1)
+            threshold_imag = threshold.imag if self.weight.is_complex() else threshold
+            imag = x.imag - diff.imag / torch.clamp_min((diff.imag / threshold_imag).abs(), 1)
+            out = torch.complex(real, imag)
         else:
-            l1norm = L1Norm(weight=self.weight, target=self.target, divide_by_n=divide_by_n)
-            return ((l1norm.prox(x, sigma=sigma, dim=dim, divide_by_n=divide_by_n)[0]),)
-
-    def prox_convex_conj(
-        self, x: torch.Tensor, sigma: torch.Tensor, dim: Sequence[int] | None = None, divide_by_n: bool | None = None
-    ) -> tuple[torch.Tensor]:
-        """Convex conjugate of the L1 Norm with C identified as R^2.
-
-        Compute the proximal mapping of the convex conjugate of the squared l2-norm.
-
-        Parameters
-        ----------
-            x
-                data tensor
-            sigma
-                scaling factor
-            dim
-                dimensions that span the considered vector space
-            divide_by_n
-                divide by the number of voxels or not
-
-        Returns
-        -------
-            Proximal of the convex conjugate applied to the input tensor
-        """
-        if dim is None:
-            dim = self.dim
-        if divide_by_n is None:
-            divide_by_n = self.divide_by_n
-
-        if x.is_complex():
-            l1norm_real = L1Norm(weight=self.weight, target=self.target.real, divide_by_n=divide_by_n)
-            l1norm_imag = L1Norm(weight=self.weight, target=self.target.imag, divide_by_n=divide_by_n)
-            return (
-                torch.complex(
-                    l1norm_real.prox_convex_conj(x.real, sigma=sigma, dim=dim, divide_by_n=divide_by_n)[0],
-                    l1norm_imag.prox_convex_conj(x.imag, sigma=sigma, dim=dim, divide_by_n=divide_by_n)[0],
-                ),
-            )
-        else:
-            l1norm = L1Norm(weight=self.weight, target=self.target, divide_by_n=divide_by_n)
-            return (l1norm.prox_convex_conj(x, sigma=sigma, dim=dim, divide_by_n=divide_by_n)[0],)
+            out = x - diff / torch.clamp_min((diff / threshold.real).abs(), 1)
+        out = out.to(torch.result_type(threshold, out))
+        return (out,)
