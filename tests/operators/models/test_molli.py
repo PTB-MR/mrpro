19299a771b62829e2a6448ccf7ac0531d3ca81dc
ckolbPTB <christoph.kolbitsch@ptb.de>
2024-09-18 21:27:45 +0200

    use c = b/a

--- tests/operators/models/test_molli.py
+++ tests/operators/models/test_molli.py
@@ -3,35 +3,35 @@
 import pytest
 import torch
 from mrpro.operators.models import MOLLI
+from tests import RandomGenerator
 from tests.operators.models.conftest import SHAPE_VARIATIONS_SIGNAL_MODELS, create_parameter_tensor_tuples
 
 
 @pytest.mark.parametrize(
     ('ti', 'result'),
     [
-        (0, 'a-b'),  # short ti
-        (20, 'a'),  # long ti
+        (0, 'a(1-c)'),  # short ti
+        (200, 'a'),  # long ti
     ],
 )
 def test_molli(ti, result):
     """Test for MOLLI.
 
     Checking that idata output tensor at ti=0 is close to a. Checking
-    that idata output tensor at large ti is close to a-b.
+    that idata output tensor at large ti is close to a(1-c).
     """
-    # Generate qdata tensor, not random as a<b is necessary for t1_star to be >= 0
-    other, coils, z, y, x = 10, 5, 100, 100, 100
-    a = torch.ones((other, coils, z, y, x)) * 2
-    b = torch.ones((other, coils, z, y, x)) * 5
-    t1 = torch.ones((other, coils, z, y, x)) * 2
+    a, t1 = create_parameter_tensor_tuples()
+    # c>1 is necessary for t1_star to be >= 0 and c<2 to ensure t1_star < t1
+    random_generator = RandomGenerator(seed=0)
+    c = random_generator.float32_tensor(size=a.shape, low=1.1, high=1.9)
 
     # Generate signal model and torch tensor for comparison
     model = MOLLI(ti)
-    (image,) = model.forward(a, b, t1)
+    (image,) = model.forward(a, c, t1)
 
-    # Assert closeness to a-b for large ti
-    if result == 'a-b':
-        torch.testing.assert_close(image[0, ...], a - b)
+    # Assert closeness to a(1-c) for large ti
+    if result == 'a(1-c)':
+        torch.testing.assert_close(image[0, ...], a * (1 - c))
     # Assert closeness to a for ti=0
     elif result == 'a':
         torch.testing.assert_close(image[0, ...], a)
@@ -42,6 +42,6 @@ def test_molli_shape(parameter_shape, contrast_dim_shape, signal_shape):
     """Test correct signal shapes."""
     (ti,) = create_parameter_tensor_tuples(contrast_dim_shape, number_of_tensors=1)
     model_op = MOLLI(ti)
-    a, b, t1 = create_parameter_tensor_tuples(parameter_shape, number_of_tensors=3)
-    (signal,) = model_op.forward(a, b, t1)
+    a, c, t1 = create_parameter_tensor_tuples(parameter_shape, number_of_tensors=3)
+    (signal,) = model_op.forward(a, c, t1)
     assert signal.shape == signal_shape
