8beaa9b677a57673db4682bb2b34baff12752eb2
Felix F Zimmermann <fzimmermann89@gmail.com>
2024-09-26 09:59:51 +0000

    finish

--- src/mrpro/operators/Functional.py
+++ src/mrpro/operators/Functional.py
@@ -1,15 +1,15 @@
-"""Base Class Functional."""
+"""Base Class (Elemetary)(Proximable)Functional and (Proximable)StackedFunctionals."""
 
 from __future__ import annotations
 
 import math
 from abc import ABC, abstractmethod
-from collections.abc import Sequence
+from collections.abc import Iterator, Sequence
+from typing import TypeVarTuple, cast, overload
 
 import torch
 
 from mrpro.operators.Operator import Operator
-from mrpro.operators.StackedFunctionals import StackedFunctionals
 
 
 class Functional(Operator[torch.Tensor, tuple[torch.Tensor]]):
@@ -47,6 +47,8 @@ class Functional(Operator[torch.Tensor, tuple[torch.Tensor]]):
         -------
             StackedFunctionals object
         """
+        if not isinstance(other, Functional):
+            return NotImplemented  # type: ignore[unreachable]
         return StackedFunctionals(self, other)
 
 
@@ -181,6 +183,32 @@ class ProximableFunctional(Functional, ABC):
         sigma[sigma < 1e-8] += 1e-6
         return (x - sigma * self.prox(x / sigma, 1 / sigma)[0],)
 
+    @overload
+    def __or__(self, other: ProximableFunctional) -> StackedProximableFunctionals[torch.Tensor, torch.Tensor]: ...
+    @overload
+    def __or__(self, other: Functional) -> StackedFunctionals[torch.Tensor, torch.Tensor]: ...
+
+    def __or__(
+        self, other: ProximableFunctional | Functional
+    ) -> StackedProximableFunctionals[torch.Tensor, torch.Tensor] | StackedFunctionals[torch.Tensor, torch.Tensor]:
+        """Create a StackedFunctionals object from two proximable functionals.
+
+        Parameters
+        ----------
+        other
+            second functional to be stacked
+
+        Returns
+        -------
+            StackedFunctionals object
+        """
+        if isinstance(other, ProximableFunctional):
+            return StackedProximableFunctionals(self, other)
+        if isinstance(other, Functional):
+            return StackedFunctionals(self, other)
+        else:
+            return NotImplemented  # type: ignore[unreachable]
+
 
 class ElementaryProximableFunctional(ElementaryFunctional, ProximableFunctional):
     r"""Elementary proximable functional base class.
@@ -192,3 +220,328 @@ class ElementaryProximableFunctional(ElementaryFunctional, ProximableFunctional)
     A proximable functional is a functional :math:`f(x)` that has a prox implementation,
     i.e. a function that solves the problem :math:`\min_x f(x) + 1/(2\sigma) ||x - y||^2`.
     """
+
+
+Tp = TypeVarTuple('Tp')
+
+
+class StackedFunctionals(Operator[*Tp, tuple[torch.Tensor]]):
+    """A class to stack multiple functionals together.
+
+    This is a separable sum of the functionals. The forward method returns the sum of the functionals.
+    """
+
+    @overload
+    def __init__(self: StackedFunctionals[torch.Tensor], f0: Functional, /): ...
+
+    @overload
+    def __init__(self: StackedFunctionals[torch.Tensor, torch.Tensor], f0: Functional, f1: Functional, /): ...
+
+    @overload
+    def __init__(
+        self: StackedFunctionals[torch.Tensor, torch.Tensor, torch.Tensor],
+        f0: Functional,
+        f1: Functional,
+        f2: Functional,
+        /,
+    ): ...
+
+    @overload
+    def __init__(
+        self: StackedFunctionals[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor],
+        f0: Functional,
+        f1: Functional,
+        f2: Functional,
+        f3: Functional,
+        /,
+    ): ...
+
+    @overload
+    def __init__(
+        self: StackedFunctionals[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor],
+        f0: Functional,
+        f1: Functional,
+        f2: Functional,
+        f3: Functional,
+        f4: Functional,
+        /,
+    ): ...
+
+    @overload
+    def __init__(self: StackedFunctionals, *funtionals: Functional): ...
+
+    def __init__(self: StackedFunctionals[*Tp], *functionals: Functional) -> None:
+        """Initialize the StackedFunctionals object.
+
+        Parameters
+        ----------
+        functionals
+            The functionals to be stacked.
+        """
+        super().__init__()
+        if not len(functionals):
+            raise ValueError('At least one functional is required')
+        self.functionals = functionals
+
+    def forward(self: StackedFunctionals[*Tp], *x: *Tp) -> tuple[torch.Tensor,]:
+        """Apply the functionals to the inputs and return the sum of the results.
+
+        Parameters
+        ----------
+        x
+            The inputs to the functionals.
+
+        Returns
+        -------
+        The sum of the results of the functionals.
+        """
+        ret = sum((f(xi)[0] for f, xi in zip(self.functionals, cast(tuple[torch.Tensor, ...], x), strict=True)))
+        assert isinstance(ret, torch.Tensor)  # Type hinting # noqa: S101
+        return (ret,)
+
+    def __iter__(self) -> Iterator[Functional]:
+        """Iterate over the functionals."""
+        return iter(self.functionals)
+
+    def __len__(self) -> int:
+        """Return the number of functionals."""
+        return len(self.functionals)
+
+    @overload
+    def __or__(self: StackedFunctionals[*Tp], other: Functional) -> StackedFunctionals[*Tp, torch.Tensor]: ...
+
+    @overload
+    def __or__(
+        self: StackedFunctionals[*Tp], other: StackedFunctionals[torch.Tensor]
+    ) -> StackedFunctionals[*Tp, torch.Tensor]: ...
+
+    @overload
+    def __or__(
+        self: StackedFunctionals[*Tp], other: StackedFunctionals[torch.Tensor, torch.Tensor]
+    ) -> StackedFunctionals[*Tp, torch.Tensor, torch.Tensor]: ...
+
+    @overload
+    def __or__(
+        self: StackedFunctionals[*Tp], other: StackedFunctionals[torch.Tensor, torch.Tensor, torch.Tensor]
+    ) -> StackedFunctionals[*Tp, torch.Tensor, torch.Tensor, torch.Tensor]: ...
+
+    @overload
+    def __or__(
+        self: StackedFunctionals[*Tp],
+        other: StackedFunctionals[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, *tuple[torch.Tensor, ...]],
+    ) -> StackedFunctionals[*tuple[torch.Tensor, ...]]: ...
+
+    def __or__(self: StackedFunctionals[*Tp], other: Functional | StackedFunctionals) -> StackedFunctionals:
+        """Stack functionals."""
+        if isinstance(other, StackedFunctionals):
+            return StackedFunctionals(*self.functionals, *other.functionals)
+
+        elif isinstance(other, Functional):
+            return StackedFunctionals(*self.functionals, other)
+        else:
+            return NotImplemented  # type: ignore[unreachable]
+
+    def __ror__(self: StackedFunctionals[*Tp], other: Functional) -> StackedFunctionals[torch.Tensor, *Tp]:
+        """Stack functionals."""
+        if isinstance(other, Functional):
+            return cast(StackedFunctionals[torch.Tensor, *Tp], StackedFunctionals(other, *self.functionals))
+        else:
+            return NotImplemented  # type: ignore[unreachable]
+
+
+class StackedProximableFunctionals(StackedFunctionals[*Tp]):
+    """Stacked Proximable Functionals.
+
+    This is a separable sum of the functionals. The forward method returns the sum of the functionals.
+    """
+
+    functionals: tuple[ProximableFunctional, ...]
+
+    @overload
+    def __init__(self: StackedProximableFunctionals[torch.Tensor], f0: ProximableFunctional, /): ...
+
+    @overload
+    def __init__(
+        self: StackedProximableFunctionals[torch.Tensor, torch.Tensor],
+        f0: ProximableFunctional,
+        f1: ProximableFunctional,
+        /,
+    ): ...
+
+    @overload
+    def __init__(
+        self: StackedProximableFunctionals[torch.Tensor, torch.Tensor, torch.Tensor],
+        f0: ProximableFunctional,
+        f1: ProximableFunctional,
+        f2: ProximableFunctional,
+        /,
+    ): ...
+
+    @overload
+    def __init__(
+        self: StackedProximableFunctionals[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor],
+        f0: ProximableFunctional,
+        f1: ProximableFunctional,
+        f2: ProximableFunctional,
+        f3: ProximableFunctional,
+        /,
+    ): ...
+
+    @overload
+    def __init__(
+        self: StackedProximableFunctionals[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor],
+        f0: ProximableFunctional,
+        f1: ProximableFunctional,
+        f2: ProximableFunctional,
+        f3: ProximableFunctional,
+        f4: ProximableFunctional,
+        /,
+    ): ...
+
+    @overload
+    def __init__(self: StackedProximableFunctionals, *funtionals: ProximableFunctional): ...
+
+    def __init__(self: StackedProximableFunctionals[*Tp], *functionals: ProximableFunctional) -> None:
+        """Initialize the StackedProximableFunctionals object.
+
+        Parameters
+        ----------
+        functionals
+            The functionals to be stacked.
+        """
+        super(StackedFunctionals, self).__init__()
+        self.functionals = functionals
+
+    def prox(self: StackedProximableFunctionals[*Tp], *x: *Tp, sigma: float | torch.Tensor = 1) -> tuple[*Tp]:
+        """Apply the proximal operators of the functionals to the inputs.
+
+        Parameters
+        ----------
+        x
+            The inputs to the proximal operators
+        sigma
+            The scaling factor for the proximal operators
+
+        Returns
+        -------
+            A tuple of the proximal operators applied to the inputs
+        """
+        ret = tuple(
+            f.prox(xi, sigma)[0] for f, xi in zip(self.functionals, cast(tuple[torch.Tensor, ...], x), strict=True)
+        )
+        return cast(tuple[*Tp], ret)
+
+    def prox_convex_conj(
+        self: StackedProximableFunctionals[*Tp], *x: *Tp, sigma: float | torch.Tensor = 1
+    ) -> tuple[*Tp]:
+        """Apply the proximal operators of the convex conjugate of the functionals to the inputs.
+
+        Parameters
+        ----------
+        x
+            The inputs to the proximal operators
+        sigma
+            The scaling factor for the proximal operators
+
+        Returns
+        -------
+            A tuple of the proximal convex conjugate operators applied to the inputs
+        """
+        ret = tuple(
+            f.prox_convex_conj(xi, sigma)[0]
+            for f, xi in zip(self.functionals, cast(tuple[torch.Tensor, ...], x), strict=True)
+        )
+        return cast(tuple[*Tp], ret)
+
+    @overload  # type: ignore[override]
+    def __or__(
+        self: StackedProximableFunctionals[*Tp], other: ProximableFunctional
+    ) -> StackedProximableFunctionals[*Tp, torch.Tensor]: ...
+
+    @overload
+    def __or__(
+        self: StackedProximableFunctionals[*Tp], other: StackedProximableFunctionals[torch.Tensor]
+    ) -> StackedProximableFunctionals[*Tp, torch.Tensor]: ...
+
+    @overload
+    def __or__(
+        self: StackedProximableFunctionals[*Tp], other: StackedProximableFunctionals[torch.Tensor, torch.Tensor]
+    ) -> StackedProximableFunctionals[*Tp, torch.Tensor, torch.Tensor]: ...
+
+    @overload
+    def __or__(
+        self: StackedProximableFunctionals[*Tp],
+        other: StackedProximableFunctionals[torch.Tensor, torch.Tensor, torch.Tensor],
+    ) -> StackedProximableFunctionals[*Tp, torch.Tensor, torch.Tensor, torch.Tensor]: ...
+
+    @overload
+    def __or__(
+        self: StackedProximableFunctionals,
+        other: StackedProximableFunctionals[
+            torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, *tuple[torch.Tensor, ...]
+        ],
+    ) -> StackedProximableFunctionals[*tuple[torch.Tensor, ...]]: ...
+
+    @overload
+    def __or__(self: StackedProximableFunctionals[*Tp], other: Functional) -> StackedFunctionals[*Tp, torch.Tensor]: ...
+
+    @overload
+    def __or__(
+        self: StackedProximableFunctionals[*Tp], other: StackedFunctionals[torch.Tensor]
+    ) -> StackedFunctionals[*Tp, torch.Tensor]: ...
+
+    @overload
+    def __or__(
+        self: StackedProximableFunctionals[*Tp], other: StackedFunctionals[torch.Tensor, torch.Tensor]
+    ) -> StackedFunctionals[*Tp, torch.Tensor, torch.Tensor]: ...
+
+    @overload
+    def __or__(
+        self: StackedProximableFunctionals[*Tp], other: StackedFunctionals[torch.Tensor, torch.Tensor, torch.Tensor]
+    ) -> StackedFunctionals[*Tp, torch.Tensor, torch.Tensor, torch.Tensor]: ...
+
+    @overload
+    def __or__(
+        self: StackedProximableFunctionals[*Tp],
+        other: StackedFunctionals[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, *tuple[torch.Tensor, ...]],
+    ) -> StackedFunctionals[*tuple[torch.Tensor, ...]]: ...
+
+    def __or__(  # type: ignore[misc]
+        self: StackedProximableFunctionals[*Tp],
+        other: Functional | StackedFunctionals | ProximableFunctional | StackedProximableFunctionals,
+    ) -> StackedProximableFunctionals | StackedFunctionals:
+        """Stack functionals."""
+        if isinstance(other, StackedProximableFunctionals):
+            return StackedProximableFunctionals(*self.functionals, *other.functionals)
+        if isinstance(other, ProximableFunctional):
+            return StackedProximableFunctionals(*self.functionals, other)
+        if isinstance(other, StackedFunctionals):
+            return StackedFunctionals(*self.functionals, *other.functionals)
+        if isinstance(other, Functional):
+            return StackedFunctionals(*self.functionals, other)
+
+        return NotImplemented  # type: ignore[unreachable]
+
+    @overload
+    def __ror__(
+        self: StackedProximableFunctionals[*Tp], other: ProximableFunctional
+    ) -> StackedProximableFunctionals[torch.Tensor, *Tp]: ...
+    @overload
+    def __ror__(
+        self: StackedProximableFunctionals[*Tp], other: Functional
+    ) -> StackedFunctionals[torch.Tensor, *Tp]: ...
+    def __ror__(
+        self: StackedProximableFunctionals[*Tp], other: Functional | ProximableFunctional
+    ) -> StackedProximableFunctionals[torch.Tensor, *Tp] | StackedFunctionals[torch.Tensor, *Tp]:
+        """Stack functionals."""
+        if isinstance(other, ProximableFunctional):
+            return StackedProximableFunctionals(other, *self.functionals)
+
+        if isinstance(other, Functional):
+            return StackedFunctionals(other, *self.functionals)
+        else:
+            return NotImplemented  # type: ignore[unreachable]
+
+    def __iter__(self) -> Iterator[ProximableFunctional]:
+        """Iterate over the functionals."""
+        return iter(self.functionals)
--- tests/operators/functionals/test_stackedfunctionals.py
+++ tests/operators/functionals/test_stackedfunctionals.py
@@ -1,7 +1,7 @@
 import pytest
 import torch
 from mrpro.operators import Functional, ProximableFunctional
-from mrpro.operators.StackedFunctionals import StackedFunctionals
+from mrpro.operators.Functional import StackedFunctionals, StackedProximableFunctionals
 
 from tests import RandomGenerator
 
@@ -12,57 +12,112 @@ class Dummy(Functional):
 
 
 class ProxDummy(ProximableFunctional):
-    def __init__(self):
+    def __init__(self, prox_scale: float = 1.0):
         super().__init__()
         self.register_buffer('dummy', torch.tensor(1.0))
+        self.prox_scale = prox_scale
 
     def forward(self, x: torch.Tensor) -> tuple[torch.Tensor]:
         return (x.max() + self.dummy,)
 
     def prox(self, x: torch.Tensor, sigma: torch.Tensor | float = 1.0) -> tuple[torch.Tensor]:
-        return (x,)
+        """This is not a real proximal operator, just a dummy function."""
+        return (self.prox_scale * x * sigma,)
 
     def prox_convex_conj(self, x: torch.Tensor, sigma: torch.Tensor | float = 1.0) -> tuple[torch.Tensor]:
-        return (x,)
+        """This is not a real proximal operator, just a dummy function."""
+
+        return (-self.prox_scale * x * sigma,)
 
 
 def test_stackedfunctions_forward():
-    functional1 = Dummy()
-    functional2 = ProxDummy()
+    a = Dummy()
+    b = ProxDummy()
     rng = RandomGenerator(123)
     x1 = rng.float32_tensor(3)
     x2 = rng.float32_tensor(3)
-    stacked = StackedFunctionals(functional1, functional2)
-    torch.testing.assert_close(stacked(x1, x2), functional1(x1) + functional2(x2))
+    stacked = StackedFunctionals(a, b)
+    torch.testing.assert_close(stacked(x1, x2)[0], a(x1)[0] + b(x2)[0])
 
 
 @pytest.mark.cuda()
 def test_stackedfunctions_forward_cuda():
-    functional1 = Dummy()
-    functional2 = ProxDummy()
+    a = Dummy()
+    b = ProxDummy()
     rng = RandomGenerator(123)
     x1 = rng.float32_tensor(3)
     x2 = rng.float32_tensor(3)
-    stacked = StackedFunctionals(functional1, functional2)
-    torch.testing.assert_close(stacked(x1, x2), functional1(x1) + functional2(x2))
+    stacked = StackedFunctionals(a, b)
+    torch.testing.assert_close(stacked(x1, x2)[0], a(x1)[0] + b(x2)[0])
 
 
 def test_stackedfunctions_shorthand():
-    functional1 = Dummy()
-    functional2 = ProxDummy()
+    a = Dummy()
+    b = ProxDummy()
 
     rng = RandomGenerator(123)
-    x1 = rng.float32_tensor(3)
-    x2 = rng.float32_tensor(3)
-    x3 = rng.float32_tensor(3)
-    x4 = rng.float32_tensor(3)
+    x1 = rng.float64_tensor(3)
+    x2 = rng.float64_tensor(3)
+    x3 = rng.float64_tensor(3)
+    x4 = rng.float64_tensor(3)
 
-    stacked = functional1 | functional2
-    mypy_test: StackedFunctionals[torch.Tensor, torch.Tensor] = stacked
-    torch.testing.assert_close(stacked(x1, x2), functional1(x1) + functional2(x2))
+    stacked = a | b
+    # mypy will complain about the type of the expression and the argument not matching
+    # if the types hints in StackedFunctional are not correct.
+    mypy_test_dummy1: StackedFunctionals[torch.Tensor, torch.Tensor] = stacked  # noqa F841
+    torch.testing.assert_close(stacked(x1, x2)[0], a(x1)[0] + b(x2)[0])
 
-    stacked2left = functional1 | stacked
+    stacked2left = b | stacked
+    mypy_test_dummy2: StackedFunctionals[torch.Tensor, torch.Tensor, torch.Tensor] = stacked2left  # noqa F841
+    torch.testing.assert_close(stacked2left(x1, x2, x3)[0], b(x1)[0] + a(x2)[0] + b(x3)[0])
 
-    stacked2right = stacked | functional1
+    stacked2right = stacked | b
+    mypy_test_dummy3: StackedFunctionals[torch.Tensor, torch.Tensor, torch.Tensor] = stacked2left  # noqa F841
+    torch.testing.assert_close(stacked2right(x1, x2, x3)[0], b(x1)[0] + a(x2)[0] + b(x3)[0])
 
-    stacked3 = stacked2left | stacked2right
+    stacked3 = stacked | stacked
+    mypy_test_dummy4: StackedFunctionals[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor] = stacked3  # noqa F841
+    torch.testing.assert_close(stacked3(x1, x2, x3, x4)[0], a(x1)[0] + b(x2)[0] + a(x3)[0] + +b(x4)[0])
+
+
+def test_stackedfunctionals_prox():
+    rng = RandomGenerator(123)
+    xs = rng.float32_tensor(4)
+    stackedprox = ProxDummy(1.0) | (ProxDummy(2.0) | ProxDummy(3.0)) | ProxDummy(4.0)
+    expected_prox = tuple(torch.tensor([1.0, 2.0, 3.0, 4.0]) * xs)
+    actual_prox = stackedprox.prox(*xs, sigma=1.0)
+    assert expected_prox == pytest.approx(actual_prox)
+    # mypy will complain about the type of the expression and the argument not matching
+    # if the types hints in StackedProximableFunctionals are not correct.
+    mypy_test_dummy1: StackedProximableFunctionals[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor] = stackedprox  # noqa F841
+    mypy_test_dummy2: tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor] = actual_prox  # noqa F841
+
+
+def test_stackedfunctionals_prox_convex_conj():
+    rng = RandomGenerator(123)
+    xs = rng.float32_tensor(4)
+    stackedprox = ProxDummy(1.0) | (ProxDummy(2.0) | ProxDummy(3.0)) | ProxDummy(4.0)
+    expected_proxcc = tuple(torch.tensor([1.0, 2.0, 3.0, 4.0]) * xs)
+    actual_prox = stackedprox.prox_convex_conj(*xs, sigma=-1.0)
+    assert expected_proxcc == pytest.approx(actual_prox)
+    # mypy will complain about the type of the expression and the argument not matching
+    # if the types hints in StackedProximableFunctionals are not correct.
+    mypy_test_dummy1: StackedProximableFunctionals[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor] = stackedprox  # noqa F841
+    mypy_test_dummy2: tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor] = actual_prox  # noqa F841
+
+
+def test_stackedfunctionals_iter_order():
+    a = Dummy()
+    b = ProxDummy()
+    c = ProxDummy()
+    assert tuple(a | b) == (a, b)
+    assert tuple(b | c) == (b, c)
+    assert tuple((b | c) | (b | c) | b) == (b, c, b, c, b)
+    assert tuple((a | c) | (b | c) | b) == (a, c, b, c, b)
+
+
+def test_stackedfunctionls_len():
+    a = Dummy()
+    b = ProxDummy()
+    assert len(a | b) == 2
+    assert len(b | b | b) == 3
