2eebebcd8e1c6e2c28f3115d56092e57c3060847
Felix Zimmermann <fzimmermann89@gmail.com>
2024-09-27 17:06:41 +0200

    Add SpatialDimension Arithmetic

--- src/mrpro/data/SpatialDimension.py
+++ src/mrpro/data/SpatialDimension.py
@@ -4,12 +4,13 @@ from __future__ import annotations
 
 from collections.abc import Callable
 from dataclasses import dataclass
-from typing import Generic, Protocol, TypeVar
+from typing import Generic, Protocol, TypeVar, overload
 
 import numpy as np
 import torch
 from numpy.typing import ArrayLike
 
+import mrpro.utils.typing as typeing_utils
 from mrpro.data.MoveDataMixin import MoveDataMixin
 
 T = TypeVar('T', int, float, torch.Tensor)
@@ -40,7 +41,7 @@ class SpatialDimension(MoveDataMixin, Generic[T]):
         data
             should implement .x .y .z. For example ismrmrd's matrixSizeType.
         conversion,  optional
-            will be called for each value to convert it
+            will be called for each other to convert it
         """
         if conversion is not None:
             return cls(conversion(data.z), conversion(data.y), conversion(data.x))
@@ -58,7 +59,7 @@ class SpatialDimension(MoveDataMixin, Generic[T]):
         data
             shape (..., 3) in the order (x,y,z)
         conversion
-            will be called for each value to convert it
+            will be called for each other to convert it
         """
         if not isinstance(data, np.ndarray | torch.Tensor):
             data = np.asarray(data)
@@ -88,7 +89,7 @@ class SpatialDimension(MoveDataMixin, Generic[T]):
         data
             shape (..., 3) in the order (z,y,x)
         conversion
-            will be called for each value to convert it
+            will be called for each other to convert it
         """
         data = torch.flip(torch.as_tensor(data), (-1,))
         return SpatialDimension.from_array_xyz(data, conversion)
@@ -101,3 +102,136 @@ class SpatialDimension(MoveDataMixin, Generic[T]):
     def __str__(self) -> str:
         """Return a string representation of the SpatialDimension."""
         return f'z={self.z}, y={self.y}, x={self.x}'
+
+    def __getitem__(
+        self: SpatialDimension[torch.Tensor], idx: typeing_utils.IndexerType
+    ) -> SpatialDimension[torch.Tensor]:
+        """Get SpatialDimension item."""
+        if not all(isinstance(el, torch.Tensor) for el in self.zyx):
+            raise IndexError('Cannot index SpatialDimension with non-tensor')
+        return SpatialDimension(self.z[idx], self.y[idx], self.x[idx])
+
+    def __setitem__(self: SpatialDimension[torch.Tensor], idx: typeing_utils.IndexerType, other: SpatialDimension):
+        """Set SpatialDimension item."""
+        if not all(isinstance(el, torch.Tensor) for el in self.zyx):
+            raise IndexError('Cannot index SpatialDimension with non-tensor')
+        self.z[idx] = other.z
+        self.y[idx] = other.y
+        self.x[idx] = other.x
+
+    @overload
+    def __mul__(self: SpatialDimension[int], other: int | SpatialDimension[int]) -> SpatialDimension[int]: ...
+    @overload
+    def __mul__(self: SpatialDimension[int], other: float | SpatialDimension[float]) -> SpatialDimension[float]: ...
+    @overload
+    def __mul__(self: SpatialDimension[float], other: float | SpatialDimension[float]) -> SpatialDimension[float]: ...
+
+    @overload
+    def __mul__(
+        self: SpatialDimension[torch.Tensor], other: float | T | SpatialDimension
+    ) -> SpatialDimension[torch.Tensor]: ...
+
+    def __mul__(self: SpatialDimension[T], other: T | float | SpatialDimension) -> SpatialDimension:
+        """Multiply SpatialDimension with numeric other or SpatialDimension."""
+        if isinstance(other, SpatialDimension):
+            return SpatialDimension(self.z * other.z, self.y * other.y, self.x * other.x)
+        return SpatialDimension(self.z * other, self.y * other, self.x * other)
+
+    @overload
+    def __rmul__(self: SpatialDimension[int], other: int | SpatialDimension[int]) -> SpatialDimension[int]: ...
+    @overload
+    def __rmul__(self: SpatialDimension[int], other: float | SpatialDimension[float]) -> SpatialDimension[float]: ...
+    @overload
+    def __rmul__(self: SpatialDimension[float], other: float | SpatialDimension[float]) -> SpatialDimension[float]: ...
+
+    @overload
+    def __rmul__(  # type: ignore[misc]
+        self: SpatialDimension[torch.Tensor], other: float | T | SpatialDimension
+    ) -> SpatialDimension[torch.Tensor]: ...
+
+    def __rmul__(self: SpatialDimension[T], other: T | float | SpatialDimension) -> SpatialDimension:  # type: ignore[misc]
+        """Right multiply SpatialDimension with numeric other or SpatialDimension."""
+        return self.__mul__(other)
+
+    @overload
+    def __truediv__(
+        self: SpatialDimension[float], other: float | SpatialDimension[float]
+    ) -> SpatialDimension[float]: ...
+
+    @overload
+    def __truediv__(
+        self: SpatialDimension[torch.Tensor], other: float | T | SpatialDimension
+    ) -> SpatialDimension[torch.Tensor]: ...
+
+    def __truediv__(self: SpatialDimension[T], other: T | float | SpatialDimension) -> SpatialDimension:
+        """Divide SpatialDimension with numeric other or SpatialDimension."""
+        if isinstance(other, SpatialDimension):
+            return SpatialDimension(self.z / other.z, self.y / other.y, self.x / other.x)
+        return SpatialDimension(self.z / other, self.y / other, self.x / other)
+
+    @overload
+    def __rtruediv__(
+        self: SpatialDimension[float], other: float | SpatialDimension[float]
+    ) -> SpatialDimension[float]: ...
+
+    @overload
+    def __rtruediv__(  # type: ignore[misc]
+        self: SpatialDimension[torch.Tensor], other: float | T | SpatialDimension
+    ) -> SpatialDimension[torch.Tensor]: ...
+
+    def __rtruediv__(self: SpatialDimension[T], other: T | float) -> SpatialDimension:  # type: ignore[misc]
+        """Right divide SpatialDimension with numeric other."""
+        return SpatialDimension(other / self.z, other / self.y, other / self.x)
+
+    @overload
+    def __add__(self: SpatialDimension[int], other: int | SpatialDimension[int]) -> SpatialDimension[int]: ...
+    @overload
+    def __add__(self: SpatialDimension[int], other: float | SpatialDimension[float]) -> SpatialDimension[float]: ...
+    @overload
+    def __add__(self: SpatialDimension[float], other: float | SpatialDimension[float]) -> SpatialDimension[float]: ...
+
+    @overload
+    def __add__(
+        self: SpatialDimension[torch.Tensor], other: float | T | SpatialDimension
+    ) -> SpatialDimension[torch.Tensor]: ...
+
+    def __add__(self: SpatialDimension[T], other: T | float | SpatialDimension) -> SpatialDimension:
+        """Add SpatialDimension or numeric other to SpatialDimension."""
+        if isinstance(other, SpatialDimension):
+            return SpatialDimension(self.z + other.z, self.y + other.y, self.x + other.x)
+        return SpatialDimension(self.z + other, self.y + other, self.x + other)
+
+    @overload
+    def __radd__(self: SpatialDimension[int], other: int | SpatialDimension[int]) -> SpatialDimension[int]: ...
+    @overload
+    def __radd__(self: SpatialDimension[int], other: float | SpatialDimension[float]) -> SpatialDimension[float]: ...
+    @overload
+    def __radd__(self: SpatialDimension[float], other: float | SpatialDimension[float]) -> SpatialDimension[float]: ...
+
+    @overload
+    def __radd__(  # type: ignore[misc]
+        self: SpatialDimension[torch.Tensor], other: float | T | SpatialDimension
+    ) -> SpatialDimension[torch.Tensor]: ...
+
+    def __radd__(self: SpatialDimension[T], other: T | float | SpatialDimension) -> SpatialDimension:  # type: ignore[misc]
+        """Right add numeric other to SpatialDimension."""
+        return self.__add__(other)
+
+    @overload
+    def __sub__(self: SpatialDimension[int], other: int | SpatialDimension[int]) -> SpatialDimension[int]: ...
+    @overload
+    def __sub__(self: SpatialDimension[int], other: float | SpatialDimension[float]) -> SpatialDimension[float]: ...
+    @overload
+    def __sub__(self: SpatialDimension[float], other: float | SpatialDimension[float]) -> SpatialDimension[float]: ...
+
+    def __sub__(self: SpatialDimension[T], other: T | float | SpatialDimension) -> SpatialDimension:
+        """Subtract SpatialDimension or numeric other to SpatialDimension."""
+        if isinstance(other, SpatialDimension):
+            return SpatialDimension(self.z - other.z, self.y - other.y, self.x - other.x)
+        return SpatialDimension(self.z - other, self.y - other, self.x - other)
+
+    def __rsub__(self: SpatialDimension[T], other: T | float | SpatialDimension) -> SpatialDimension:  # type: ignore[misc]
+        """Right subtract SpatialDimension or numeric other to SpatialDimension."""
+        if isinstance(other, SpatialDimension):
+            return SpatialDimension(other.z - self.z, other.y - self.y, other.x - self.x)
+        return SpatialDimension(other - self.z, other - self.y, other - self.x)
