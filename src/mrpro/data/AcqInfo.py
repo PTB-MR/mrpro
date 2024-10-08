a403f7217c8edb59f11a6a03c174fdf33af40d6e
ckolbPTB <christoph.kolbitsch@ptb.de>
2024-10-08 10:36:48 +0200

    outline for new info/idx structure

--- src/mrpro/data/AcqInfo.py
+++ src/mrpro/data/AcqInfo.py
@@ -1,40 +1,21 @@
 """Acquisition information dataclass."""
 
-import dataclasses
 from collections.abc import Callable, Sequence
 from dataclasses import dataclass
-from typing import Self, TypeVar
+from typing import Self
 
 import ismrmrd
 import numpy as np
 import torch
-from einops import rearrange
 
-from mrpro.data.MoveDataMixin import MoveDataMixin
+from mrpro.data.DataInfo import DataIdx, DataInfo
 from mrpro.data.Rotation import Rotation
 from mrpro.data.SpatialDimension import SpatialDimension
 from mrpro.utils.unit_conversion import mm_to_m
 
-T = TypeVar('T', torch.Tensor, Rotation, SpatialDimension)
-
-
-def rearrange_acq_info_fields(field: T, pattern: str, additional_info: dict[str, int] | None = None) -> T:
-    """Change the shape of the fields in AcqInfo."""
-    axes_lengths = {} if additional_info is None else additional_info
-    if isinstance(field, Rotation):
-        return Rotation.from_matrix(rearrange(field.as_matrix(), pattern, **axes_lengths))
-    elif isinstance(field, SpatialDimension):
-        return SpatialDimension(
-            z=rearrange(field.z, pattern, **axes_lengths),
-            y=rearrange(field.y, pattern, **axes_lengths),
-            x=rearrange(field.x, pattern, **axes_lengths),
-        )
-    else:
-        return rearrange(field, pattern, **axes_lengths)
-
 
 @dataclass(slots=True)
-class AcqIdx(MoveDataMixin):
+class AcqIdx(DataIdx):
     """Acquisition index for each readout."""
 
     k1: torch.Tensor
@@ -43,54 +24,9 @@ class AcqIdx(MoveDataMixin):
     k2: torch.Tensor
     """Second phase encoding."""
 
-    average: torch.Tensor
-    """Signal average."""
-
-    slice: torch.Tensor
-    """Slice number (multi-slice 2D)."""
-
-    contrast: torch.Tensor
-    """Echo number in multi-echo."""
-
-    phase: torch.Tensor
-    """Cardiac phase."""
-
-    repetition: torch.Tensor
-    """Counter in repeated/dynamic acquisitions."""
-
-    set: torch.Tensor
-    """Sets of different preparation, e.g. flow encoding, diffusion weighting."""
-
-    segment: torch.Tensor
-    """Counter for segmented acquisitions."""
-
-    user0: torch.Tensor
-    """User index 0."""
-
-    user1: torch.Tensor
-    """User index 1."""
-
-    user2: torch.Tensor
-    """User index 2."""
-
-    user3: torch.Tensor
-    """User index 3."""
-
-    user4: torch.Tensor
-    """User index 4."""
-
-    user5: torch.Tensor
-    """User index 5."""
-
-    user6: torch.Tensor
-    """User index 6."""
-
-    user7: torch.Tensor
-    """User index 7."""
-
 
 @dataclass(slots=True)
-class AcqInfo(MoveDataMixin):
+class AcqInfo(DataInfo):
     """Acquisition information for each readout."""
 
     idx: AcqIdx
@@ -129,18 +65,6 @@ class AcqInfo(MoveDataMixin):
     number_of_samples: torch.Tensor
     """Number of sample points per readout (readouts may have different number of sample points)."""
 
-    orientation: Rotation
-    """Rotation describing the orientation of the readout, phase and slice encoding direction."""
-
-    patient_table_position: SpatialDimension[torch.Tensor]
-    """Offset position of the patient table, in LPS coordinates [m]."""
-
-    physiology_time_stamp: torch.Tensor
-    """Time stamps relative to physiological triggering, e.g. ECG. Not in s but in vendor-specific time units"""
-
-    position: SpatialDimension[torch.Tensor]
-    """Center of the excited volume, in LPS coordinates relative to isocenter [m]."""
-
     sample_time_us: torch.Tensor
     """Readout bandwidth, as time between samples [us]."""
 
@@ -149,12 +73,6 @@ class AcqInfo(MoveDataMixin):
 
     trajectory_dimensions: torch.Tensor  # =3. We only support 3D Trajectories: kz always exists.
     """Dimensionality of the k-space trajectory vector."""
-
-    user_float: torch.Tensor
-    """User-defined float parameters."""
-
-    user_int: torch.Tensor
-    """User-defined int parameters."""
 
     version: torch.Tensor
     """Major version number."""
@@ -284,21 +202,3 @@ class AcqInfo(MoveDataMixin):
             version=tensor_2d(headers['version']),
         )
         return acq_info
-
-    def _apply_(self, modify_acq_info_field: Callable) -> None:
-        """Go through all fields of AcqInfo object and apply function in-place.
-
-        Parameters
-        ----------
-        modify_acq_info_field
-            Function which takes AcqInfo fields as input and returns modified AcqInfo field
-        """
-        for field in dataclasses.fields(self):
-            current = getattr(self, field.name)
-            if dataclasses.is_dataclass(current):
-                for subfield in dataclasses.fields(current):
-                    subcurrent = getattr(current, subfield.name)
-                    setattr(current, subfield.name, modify_acq_info_field(subcurrent))
-            else:
-                setattr(self, field.name, modify_acq_info_field(current))
-        return None
