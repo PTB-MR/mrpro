1787dd53393dfc5394332c2170d8d7833af5babf
Patrick Schuenke <patrick.schuenke@gmail.com>
2024-10-09 15:59:21 +0200

    move VERSION to package folder ensure its included in dist

--- src/mrpro/__init__.py
+++ src/mrpro/__init__.py
@@ -7,4 +7,4 @@ try:
     __version__ = version("package-name")
 except PackageNotFoundError:
     # package is not installed
-    __version__ = Path(__file__).parent.parent.parent.joinpath("VERSION").read_text().strip()
+    __version__ = Path(__file__).parent.joinpath("VERSION").read_text().strip()
