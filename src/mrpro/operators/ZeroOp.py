838c2b405253ed218412fe99774ab8a857a55b89
Felix Zimmermann <fzimmermann89@gmail.com>
2024-10-12 00:11:12 +0200

    allow scalar and vector zero as return

--- src/mrpro/operators/ZeroOp.py
+++ src/mrpro/operators/ZeroOp.py
@@ -12,8 +12,21 @@ class ZeroOp(LinearOperator):
     It is the neutral element of the addition of operators.
     """
 
-    def __init__(self):
-        """Initialize the Zero Operator."""
+    def __init__(self, keep_shape: bool = True):
+        """Initialize the Zero Operator.
+
+        Returns a constant zero, either as a scalar or as a tensor of the same shape as the input,
+        depending on the value of keep_shape.
+        Returning a scalar can save memory and computation time in some cases.
+
+        Parameters
+        ----------
+        keep_shape
+            If True, the shape of the input is kept.
+            If False, the output is, regardless of the input shape, an integer scalar 0,
+            which can broadcast to the input shape and dtype.
+        """
+        self.keep_shape = keep_shape
         super().__init__()
 
     def forward(self, x: torch.Tensor) -> tuple[torch.Tensor,]:
@@ -28,7 +41,10 @@ class ZeroOp(LinearOperator):
         -------
         zeros_like(x)
         """
-        return (torch.zeros_like(x),)
+        if self.keep_shape:
+            return (torch.zeros_like(x),)
+        else:
+            return (torch.tensor(0),)
 
     def adjoint(self, x: torch.Tensor) -> tuple[torch.Tensor,]:
         """Apply the adjoint of the operator to the input.
@@ -42,4 +58,7 @@ class ZeroOp(LinearOperator):
         -------
         zeros_like(x)
         """
-        return (torch.zeros_like(x),)
+        if self.keep_shape:
+            return (torch.zeros_like(x),)
+        else:
+            return (torch.tensor(0),)
--- tests/operators/test_zero_op.py
+++ tests/operators/test_zero_op.py
@@ -2,18 +2,29 @@ from typing import assert_type
 
 import torch
 from mrpro.operators import IdentityOp, LinearOperator, MagnitudeOp, Operator, ZeroOp
+from mrpro.operators.LinearOperator import LinearOperatorSum
 
 from tests import RandomGenerator
 from tests.helper import dotproduct_adjointness_test
 
 
-def test_zero_op():
+def test_zero_op_keepshape():
     """Test that the zero operator returns zeros."""
     generator = RandomGenerator(seed=0)
     x = generator.complex64_tensor(2, 3, 4)
-    operator = ZeroOp()
+    operator = ZeroOp(keep_shape=True)
     (actual,) = operator(x)
     expected = torch.zeros_like(x)
+    torch.testing.assert_close(actual, expected)
+
+
+def test_zero_op_scalar():
+    """Test that the zero operator returns zeros."""
+    generator = RandomGenerator(seed=0)
+    x = generator.complex64_tensor(2, 3, 4)
+    operator = ZeroOp(keep_shape=False)
+    (actual,) = operator(x)
+    expected = torch.tensor(0)
     torch.testing.assert_close(actual, expected)
 
 
@@ -47,10 +58,24 @@ def test_zero_op_neutral_op():
     assert_type(lsum, Operator[torch.Tensor, tuple[torch.Tensor]])
 
 
-def test_zero_op_adjoint():
+def test_zero_op_adjoint_keepshape():
     """Test that the adjoint of the zero operator is the zero operator."""
     generator = RandomGenerator(seed=0)
     u = generator.complex64_tensor(2, 3, 4)
     v = generator.complex64_tensor(2, 3, 4)
-    operator = ZeroOp()
+    operator = ZeroOp(keep_shape=True)
+    dotproduct_adjointness_test(operator, u, v)
+
+
+def test_zero_op_adjoint_scalar():
+    """Test that the adjoint of the zero operator is the zero operator."""
+    generator = RandomGenerator(seed=0)
+    u = generator.complex64_tensor(2, 3, 4)
+    v = generator.complex64_tensor(2, 3, 4)
+    # We can't test the operator directly, because the adjointness is only after
+    # broadcasting the scalar to the shape of the input and expading the dtype.
+    # We achieve this by instead testing ZeroOp + IdentityOp
+    # We can't use '+' because it would short-circuit to IdentityOp,
+    # as ZeroOp is the neutral element of the addition.
+    operator = LinearOperatorSum(ZeroOp(keep_shape=False), IdentityOp())
     dotproduct_adjointness_test(operator, u, v)
