7c1e3bdb178696f74e321e9cceea72abf8b16a2a
Patrick Schuenke <patrick.schuenke@gmail.com>
2024-10-11 12:17:50 +0200

    add _version.py

--- src/mrpro/__init__.py
+++ src/mrpro/__init__.py
@@ -1,10 +1,2 @@
-from pathlib import Path
-from importlib.metadata import version, PackageNotFoundError
+from mrpro._version import __version__
 from mrpro import algorithms, operators, data, phantoms, utils
-
-
-try:
-    __version__ = version("package-name")
-except PackageNotFoundError:
-    # package is not installed
-    __version__ = Path(__file__).parent.joinpath("VERSION").read_text().strip()
