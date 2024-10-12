3348f581216f7c73f627809c4981b91c71396474
Felix F Zimmermann <fzimmermann89@gmail.com>
2024-10-12 14:16:09 +0200

    Add L1NormViewAsReal Functional (#399)
    Co-authored-by: Stefan Martin <stefan.martin.93@gmx.de>
    Co-authored-by: Mara Guastini <mara.guastini@gmail.com>

--- src/mrpro/operators/functionals/__init__.py
+++ src/mrpro/operators/functionals/__init__.py
@@ -1,3 +1,3 @@
+from mrpro.operators.functionals.L1NormViewAsReal import L1NormViewAsReal
 from mrpro.operators.functionals.L2NormSquared import L2NormSquared
 from mrpro.operators.functionals.MSEDataDiscrepancy import MSEDataDiscrepancy
-
