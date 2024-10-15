7449ab6b364a7076e84dc4318a08a5a484be007a
Felix F Zimmermann <fzimmermann89@gmail.com>
2024-10-16 00:14:16 +0200

    typo

--- src/mrpro/operators/functionals/__init__.py
+++ src/mrpro/operators/functionals/__init__.py
@@ -1,5 +1,5 @@
 from mrpro.operators.functionals.L1NormViewAsReal import L1NormViewAsReal
 from mrpro.operators.functionals.L2NormSquared import L2NormSquared
 from mrpro.operators.functionals.MSEDataDiscrepancy import MSEDataDiscrepancy
-from mrpro.operators.functionals.ZeroFunctional import ZeroFunctionals
+from mrpro.operators.functionals.ZeroFunctional import ZeroFunctional
 __all__ = ["L1NormViewAsReal", "L2NormSquared", "MSEDataDiscrepancy", "ZeroFunctional"]
