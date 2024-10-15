ed37acebd698d1a357510583fd1acab414f1fb30
Felix F Zimmermann <fzimmermann89@gmail.com>
2024-10-16 00:11:48 +0200

    fix merge

--- src/mrpro/operators/functionals/__init__.py
+++ src/mrpro/operators/functionals/__init__.py
@@ -1,4 +1,5 @@
 from mrpro.operators.functionals.L1NormViewAsReal import L1NormViewAsReal
 from mrpro.operators.functionals.L2NormSquared import L2NormSquared
 from mrpro.operators.functionals.MSEDataDiscrepancy import MSEDataDiscrepancy
+from mrpro.operators.functionals.ZeroFunctional import ZeroFunctionals
 __all__ = ["L1NormViewAsReal", "L2NormSquared", "MSEDataDiscrepancy", "ZeroFunctional"]
