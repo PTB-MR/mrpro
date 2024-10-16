ad47837ffbff2b4bc2e5c45aeb327f39362f3f9e
ckolbPTB <christoph.kolbitsch@ptb.de>
2024-10-16 21:06:43 +0200

    remove and rename

--- src/mrpro/operators/PCACompressionOp.py
+++ src/mrpro/operators/PCACompressionOp.py
@@ -1,3 +1,5 @@
+"""PCA Compression Operator."""
+
 import einops
 import numpy as np
 import torch
@@ -6,11 +8,11 @@ from mrpro.data import Data
 from mrpro.operators import LinearOperator
 
 
-class CoilCompression(LinearOperator):
-    """PCA based soft coil compression operator."""
+class PCACompressionOp(LinearOperator):
+    """PCA based compression operator."""
 
-    def __init__(self, data: Data | torch.Tensor, n_components, separate_dims: tuple[int, ...] | None = None):
-        """Construct a PCA based soft coil compression operator.
+    def __init__(self, data: torch.Tensor, n_components: int, compression_dim: int = -4, shared_dims: tuple[int, ...] | None = None):
+        """Construct a PCA based compression operator.
 
         Parameters
         ----------
@@ -18,35 +20,33 @@ class CoilCompression(LinearOperator):
             Data to used to find the principal components.
         n_components
             Number of principal components to keep.
-        separate_dims
-            tuple of dimensions to construct separate compression matrices for.
-            None means one global compression matrix for all (but the coil) dimensions.
+        compression_dim
+            Dimension along which compression is applied. Default to coil dimension.
+        shared_dims
+            Dimensions which share the same compression matrix.
         """
         super().__init__()
-        if isinstance(data, Data):
-            d = data.data
-        else:
-            d = data
-        coil_dimension = d.ndim - 4
+
+        coil_dimension = data.ndim - 4
         if separate_dims is None:
             # global compression matrix
-            d = d.moveaxis(coil_dimension, -1).reshape(-1, d.shape[coil_dimension])
+            d = data.moveaxis(coil_dimension, -1).reshape(-1, data.shape[coil_dimension])
         else:
             # different compression matrices
             # reshape to (*separate dimensions, -1, coils)
-            separate_dims_normalized = [i % d.ndim for i in separate_dims]
+            separate_dims_normalized = [i % data.ndim for i in separate_dims]
             if coil_dimension in separate_dims_normalized:
                 raise ValueError('coil dimension must not be in separate_dims')
             permute_order = (
                 separate_dims_normalized
-                + [i for i in range(d.ndim) if i != coil_dimension and i not in separate_dims_normalized]
+                + [i for i in range(data.ndim) if i != coil_dimension and i not in separate_dims_normalized]
                 + [coil_dimension]
             )
-            d = d.permute(permute_order)
-            d = d.flatten(start_dim=len(separate_dims), end_dim=-2)  # keep separate dimensions and coil
+            d = data.permute(permute_order)
+            d = data.flatten(start_dim=len(separate_dims), end_dim=-2)  # keep separate dimensions and coil
 
-        d = d - d.mean(-1, keepdim=True)
-        correlation = einops.einsum(d, d.conj(), '... i coil1, ... i coil2 -> ... coil1 coil2')
+        d = d - data.mean(-1, keepdim=True)
+        correlation = einops.einsum(d, data.conj(), '... i coil1, ... i coil2 -> ... coil1 coil2')
         _, _, v = torch.svd(correlation)
         self.register_buffer('_compression_matrix', v[..., :n_components, :].clone())
         self._separate_dims = separate_dims
@@ -81,9 +81,29 @@ class CoilCompression(LinearOperator):
             return (data,)
 
     def forward(self, data: torch.Tensor) -> tuple[torch.Tensor,]:
-        """Apply the compression to the data."""
+        """Apply the compression to the data.
+
+        Parameters
+        ----------
+        data
+            data to be compressed
+
+        Returns
+        -------
+            compressed data
+        """
         return self._applymatrix(data, self._compression_matrix, self._separate_dims)
 
     def adjoint(self, data: torch.Tensor) -> tuple[torch.Tensor,]:
-        """Apply the adjoint compression to the data."""
+        """Apply the adjoint compression to the data.
+
+         Parameters
+        ----------
+        data
+            compressed data
+
+        Returns
+        -------
+            uncompressed data
+        """
         return self._applymatrix(data, self._compression_matrix.mH, self._separate_dims)
