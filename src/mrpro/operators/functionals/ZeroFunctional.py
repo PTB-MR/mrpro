67e8cdd1d4175cf6e2e05d50b421d2d46a0f4a8b
Felix F Zimmermann <fzimmermann89@gmail.com>
2024-10-16 17:53:05 +0200

    fix docstring

--- src/mrpro/operators/functionals/ZeroFunctional.py
+++ src/mrpro/operators/functionals/ZeroFunctional.py
@@ -64,8 +64,9 @@ class ZeroFunctional(ElementaryProximableFunctional):
     def prox_convex_conj(self, x: torch.Tensor, sigma: float | torch.Tensor = 1.0) -> tuple[torch.Tensor,]:
         """Apply the proximal operator of the convex conjugate of the functional to a tensor.
 
-        The convex conjugate of the zero functional is the indicator function over the set {0}. If sigma>0,
-        the proximal operator of the scaled convex conjugate is constant zero, otherwise it is the identity.
+        The convex conjugate of the zero functional is the indicator function over :math:`C^N \setminus {0}`,
+        which evaluates to infinity for all values of `x` except zero.
+        If sigma>0, the proximal operator of the scaled convex conjugate is constant zero, otherwise it is the identity.
 
         Parameters
         ----------
