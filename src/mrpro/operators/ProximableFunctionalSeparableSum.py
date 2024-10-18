623bb7c2dc72b78db82e4908cd9c6a16d7ecc687
Felix Zimmermann <fzimmermann89@gmail.com>
2024-10-18 16:11:20 +0200

    review

--- src/mrpro/operators/ProximableFunctionalSeparableSum.py
+++ src/mrpro/operators/ProximableFunctionalSeparableSum.py
@@ -14,12 +14,24 @@ from mrpro.operators.Operator import Operator
 
 
 class ProximableFunctionalSeparableSum(Operator[*tuple[torch.Tensor, ...], tuple[torch.Tensor]]):
-    """Stacked Proximable Functionals.
+    r"""Separabke Sum of Proximable Functionals.
 
-    This is a separable sum of the functionals. The forward method returns the sum of the functionals.
+    This is a separable sum of the functionals. The forward method returns the sum of the functionals
+    evaluated at the inputs, :math:`\sum_i f_i(x_i)`.
     """
 
     functionals: tuple[ProximableFunctional, ...]
+
+    def __init__(self, *functionals: ProximableFunctional) -> None:
+        """Initialize the separable sum of proximable functionals.
+
+        Parameters
+        ----------
+        functionals
+            The proximable functionals to be summed.
+        """
+        super().__init__()
+        self.functionals = functionals
 
     def forward(self, *x: torch.Tensor) -> tuple[torch.Tensor]:
         """Apply the functionals to the inputs.
@@ -38,17 +50,6 @@ class ProximableFunctionalSeparableSum(Operator[*tuple[torch.Tensor, ...], tuple
         result = reduce(operator.add, (f(xi)[0] for f, xi in zip(self.functionals, x, strict=True)))
         return (result,)
 
-    def __init__(self, *functionals: ProximableFunctional) -> None:
-        """Initialize the separable sum of proximable functionals.
-
-        Parameters
-        ----------
-        functionals
-            The proximable functionals to be summed.
-        """
-        super().__init__()
-        self.functionals = functionals
-
     def prox(self, *x: torch.Tensor, sigma: float | torch.Tensor = 1) -> tuple[torch.Tensor, ...]:
         """Apply the proximal operators of the functionals to the inputs.
 
@@ -63,10 +64,10 @@ class ProximableFunctionalSeparableSum(Operator[*tuple[torch.Tensor, ...], tuple
         -------
             A tuple of the proximal operators applied to the inputs
         """
-        prox = tuple(
+        prox_x = tuple(
             f.prox(xi, sigma)[0] for f, xi in zip(self.functionals, cast(tuple[torch.Tensor, ...], x), strict=True)
         )
-        return prox
+        return prox_x
 
     def prox_convex_conj(self, *x: torch.Tensor, sigma: float | torch.Tensor = 1) -> tuple[torch.Tensor, ...]:
         """Apply the proximal operators of the convex conjugate of the functionals to the inputs.
@@ -82,11 +83,11 @@ class ProximableFunctionalSeparableSum(Operator[*tuple[torch.Tensor, ...], tuple
         -------
             A tuple of the proximal convex conjugate operators applied to the inputs
         """
-        prox_convex_conj = tuple(
+        prox_convex_conj_x = tuple(
             f.prox_convex_conj(xi, sigma)[0]
             for f, xi in zip(self.functionals, cast(tuple[torch.Tensor, ...], x), strict=True)
         )
-        return prox_convex_conj
+        return prox_convex_conj_x
 
     def __or__(
         self,
