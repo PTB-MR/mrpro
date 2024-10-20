cf7b8761f1188ff3270350c0a47f37d1e2000a72
Felix Zimmermann <fzimmermann89@gmail.com>
2024-10-20 03:15:02 +0200

    pdhg

--- src/mrpro/algorithms/optimizers/__init__.py
+++ src/mrpro/algorithms/optimizers/__init__.py
@@ -2,3 +2,4 @@ from mrpro.algorithms.optimizers.OptimizerStatus import OptimizerStatus
 from mrpro.algorithms.optimizers.adam import adam
 from mrpro.algorithms.optimizers.cg import cg
 from mrpro.algorithms.optimizers.lbfgs import lbfgs
+from mrpro.algorithms.optimizers.pdhg import pdhg
