ed4d2ab22e19b59234a8c506393e87dfb3bf7496
Felix Zimmermann <fzimmermann89@gmail.com>
2024-10-24 01:10:13 +0200

    autofix __all__

--- src/mrpro/algorithms/reconstruction/__init__.py
+++ src/mrpro/algorithms/reconstruction/__init__.py
@@ -1,4 +1,8 @@
 from mrpro.algorithms.reconstruction.Reconstruction import Reconstruction
 from mrpro.algorithms.reconstruction.DirectReconstruction import DirectReconstruction
 from mrpro.algorithms.reconstruction.IterativeSENSEReconstruction import IterativeSENSEReconstruction
-__all__ = ["Reconstruction", "DirectReconstruction", "IterativeSENSEReconstruction"]
+__all__ = [
+    "DirectReconstruction",
+    "IterativeSENSEReconstruction",
+    "Reconstruction"
+]
--- src/mrpro/utils/__init__.py
+++ src/mrpro/utils/__init__.py
@@ -6,4 +6,15 @@ from mrpro.utils.zero_pad_or_crop import zero_pad_or_crop
 from mrpro.utils.modify_acq_info import modify_acq_info
 from mrpro.utils.split_idx import split_idx
 from mrpro.utils.reshape import broadcast_right, unsqueeze_left, unsqueeze_right
-__all__ = ["slice_profiles", "typing", "smap", "remove_repeat", "zero_pad_or_crop", "modify_acq_info", "split_idx", "broadcast_right", "unsqueeze_left", "unsqueeze_right"]
+__all__ = [
+    "broadcast_right",
+    "modify_acq_info",
+    "remove_repeat",
+    "slice_profiles",
+    "smap",
+    "split_idx",
+    "typing",
+    "unsqueeze_left",
+    "unsqueeze_right",
+    "zero_pad_or_crop"
+]
