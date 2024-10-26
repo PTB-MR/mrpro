0bbf766abfd8574cd128465380eb7a74a1a18985
ckolbPTB <christoph.kolbitsch@ptb.de>
2024-10-26 22:15:23 +0200

    encoding_matrix has to cover all trajectory points

--- tests/operators/test_fourier_op.py
+++ tests/operators/test_fourier_op.py
@@ -30,7 +30,11 @@ def test_fourier_op_fwd_adj_property(im_shape, k_shape, nkx, nky, nkz, sx, sy, s
 
     # create operator
     recon_matrix = SpatialDimension(im_shape[-3], im_shape[-2], im_shape[-1])
-    encoding_matrix = SpatialDimension(k_shape[-3], k_shape[-2], k_shape[-1])
+    encoding_matrix = SpatialDimension(
+        int(trajectory.kz.max() - trajectory.kz.min() + 1),
+        int(trajectory.ky.max() - trajectory.ky.min() + 1),
+        int(trajectory.kx.max() - trajectory.kx.min() + 1),
+    )
     fourier_op = FourierOp(recon_matrix=recon_matrix, encoding_matrix=encoding_matrix, traj=trajectory)
 
     # apply forward operator
