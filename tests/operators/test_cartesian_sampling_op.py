dc44f3a76ef2b1275add6d54f05b4dcc9b37b55e
Felix Zimmermann <fzimmermann89@gmail.com>
2024-11-11 10:53:21 +0100

    test with other dimension

--- tests/operators/test_cartesian_sampling_op.py
+++ tests/operators/test_cartesian_sampling_op.py
@@ -130,15 +130,16 @@ def test_cart_sampling_op_oversampling(k0_min, k0_max, k2_min, k2_max):
     # The indices are inverted to ensure CartesianSamplingOp acts on them
     kx = rearrange(torch.linspace(k0_max, k0_min, 20), 'kx->1 1 1 kx')
     ky = torch.ones(1, 1, 1, 1)
-    kz = rearrange(torch.linspace(k2_max, k2_min, 40), 'kz->1 kz 1 1')
+    kz = rearrange(torch.linspace(k2_max, k2_min, 40), 'kz-> kz 1 1')
+    kz = torch.stack([kz, -kz], dim=0)  # different kz values for two other elements
     trajectory = KTrajectory(kz=kz, ky=ky, kx=kx)
 
     with pytest.warns(UserWarning, match='K-space points lie outside of the encoding_matrix'):
         sampling_op = CartesianSamplingOp(encoding_matrix=encoding_matrix, traj=trajectory)
 
     random_generator = RandomGenerator(seed=0)
-    u = random_generator.complex64_tensor(size=(3, 5, kz.shape[-3], ky.shape[-2], kx.shape[-1]))
-    v = random_generator.complex64_tensor(size=(3, 5, *encoding_matrix.zyx))
+    u = random_generator.complex64_tensor(size=(3, 2, 5, kz.shape[-3], ky.shape[-2], kx.shape[-1]))
+    v = random_generator.complex64_tensor(size=(3, 2, 5, *encoding_matrix.zyx))
 
     assert sampling_op.adjoint(u)[0].shape[-3:] == encoding_matrix.zyx
     assert sampling_op(v)[0].shape[-3:] == (kz.shape[-3], ky.shape[-2], kx.shape[-1])
