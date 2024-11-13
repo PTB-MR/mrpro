82d09d4f179fe2fa114040578fb1e062120133f9
JoHa0811 <johanneshammacher1994@gmail.com>
2024-11-13 11:31:33 +0100

    Added manual creation for KTrajectoryRadial2D

--- src/mrpro/data/traj_calculators/KTrajectoryRadial2D.py
+++ src/mrpro/data/traj_calculators/KTrajectoryRadial2D.py
@@ -46,3 +46,32 @@ class KTrajectoryRadial2D(KTrajectoryCalculator):
         kz = torch.zeros(1, 1, 1, 1)
 
         return KTrajectory(kz, ky, kx)
+
+    def create_manually(self, n_spokes: int = 240, n_k0: int = 256, initial_angle: float = 0) -> KTrajectory:
+        """Generate KTrajectory object without header information.
+
+        Args:
+            n_spokes (int, optional): Number of radial spokes. Defaults to 240.
+            n_k0 (int, optional): Number of k0 points along each spoke. Defaults to 256.
+            initial_angle (float, optional): Initial rotation angle of the first spoke. Defaults to 0.
+
+        Returns
+        -------
+            radial 2D trajectory
+        """
+        rotation_angle = self.angle
+        center_sample = n_k0 // 2
+
+        # K-space locations along readout lines
+        radial = torch.arange(0, n_k0, dtype=torch.float32) - center_sample
+        spoke_idx = torch.arange(n_spokes)
+
+        # Angles of readout lines
+        kang = (spoke_idx * rotation_angle + initial_angle)[None, None, :, None]
+
+        # K-space radial coordinates
+        kx = radial * torch.cos(kang)
+        ky = radial * torch.sin(kang)
+        kz = torch.zeros(1, 1, 1, 1)
+
+        return KTrajectory(kz, ky, kx)
