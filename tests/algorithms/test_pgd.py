01a26bdbeda696b8ccfffc758e469102d13866e3
guastinimara <mara.guastini@gmail.com>
2024-11-14 11:32:29 +0100

    error message corrected

--- tests/algorithms/test_pgd.py
+++ tests/algorithms/test_pgd.py
@@ -1,14 +1,16 @@
 """Tests for the proximal gradient descent."""
 
+from typing import cast
+
 import torch
 from mrpro.algorithms.optimizers import pgd
 from mrpro.data.SpatialDimension import SpatialDimension
-from mrpro.operators import FastFourierOp, LinearOperatorMatrix, ProximableFunctionalSeparableSum
+from mrpro.operators import FastFourierOp, LinearOperatorMatrix, Operator, ProximableFunctionalSeparableSum
 from mrpro.operators.functionals import L1Norm, L2NormSquared
 from mrpro.phantoms import EllipsePhantom
 
 
-def test_pgd_convergence_fft_example():
+def test_pgd_convergence_fft_example() -> None:
     """Test if the proximal gradient descent algorithm converges for the
     problem min_x 1/2 ||Fx - target||^2 + ||x||_1,
     with F being the Fourier Transform."""
@@ -36,7 +38,7 @@ def test_pgd_convergence_fft_example():
     assert f(*pgd_solution)[0] + g(*pgd_solution)[0] < f(*initial_value)[0] + g(*initial_value)[0]
 
 
-def test_pgd_convergence_backtracking_fft_example():
+def test_pgd_convergence_backtracking_fft_example() -> None:
     """Test the backtracking stepsize rule in proximal gradient descent algorithm
     for the problem min_x 1/2 ||Fx - target||^2 + ||x||_1, with F being the
     Fourier Transform."""
@@ -65,7 +67,7 @@ def test_pgd_convergence_backtracking_fft_example():
     assert f(*pgd_solution)[0] + g(*pgd_solution)[0] < f(*initial_value)[0] + g(*initial_value)[0]
 
 
-def test_pgd_convergence_backtracking_denoising_example():
+def test_pgd_convergence_backtracking_denoising_example() -> None:
     """Test if the proximal gradient descent algorithm converges for denoising."""
     dim = SpatialDimension(x=100, y=100, z=1)
     ellipse = EllipsePhantom()
@@ -90,7 +92,7 @@ def test_pgd_convergence_backtracking_denoising_example():
     assert f(*pgd_solution)[0] + g(*pgd_solution)[0] < f(*initial_value)[0] + g(*initial_value)[0]
 
 
-def test_pgd_on_functionals_with_multiple_inputs():
+def test_pgd_on_functionals_with_multiple_inputs() -> None:
     """Test if the proximal gradient descent algorithm converges for
     the problem min_x f(x) + g(x), with f being a function with multiple inputs,
     g being a ProximableFunctionalSeparableSum."""
@@ -102,7 +104,7 @@ def test_pgd_on_functionals_with_multiple_inputs():
 
     # to check on multiple inputs, we will use the same input twice
     l2 = L2NormSquared(target=kspace, divide_by_n=True)
-    matrix_op = LinearOperatorMatrix(((fft, fft),))
+    matrix_op = cast(Operator[torch.Tensor, torch.Tensor, tuple[torch.Tensor]], LinearOperatorMatrix(((fft, fft),)))
     f = l2 @ matrix_op
 
     l1 = L1Norm(divide_by_n=True)
