38722bf9c9aad87bb30008d6ada488e69f239cd1
Lunin Leonid <leonid.lunin@ptb.de>
2024-11-14 18:14:43 +0100

    Examples typos (#449)
    Co-authored-by: Felix F Zimmermann <fzimmermann89@gmail.com>

--- examples/qmri_sg_challenge_2024_t1.py
+++ examples/qmri_sg_challenge_2024_t1.py
@@ -1,5 +1,5 @@
 # %% [markdown]
-# # QMRI Challenge ISMRM 2024 - T1 mapping
+# # QMRI Challenge ISMRM 2024 - $T_1$ mapping
 
 # %%
 # Imports
@@ -22,7 +22,7 @@ from mrpro.operators.models import InversionRecovery
 # %% [markdown]
 # ### Overview
 # The dataset consists of images obtained at 10 different inversion times using a turbo spin echo sequence. Each
-# inversion time is saved in a separate DICOM file. In order to obtain a T1 map, we are going to:
+# inversion time is saved in a separate DICOM file. In order to obtain a $T_1$ map, we are going to:
 # - download the data from Zenodo
 # - read in the DICOM files (one for each inversion time) and combine them in an IData object
 # - define a signal model and data loss (mean-squared error) function
@@ -53,15 +53,15 @@ if idata_multi_ti.header.ti is None:
 fig, axes = plt.subplots(1, 3, squeeze=False)
 for idx, ax in enumerate(axes.flatten()):
     ax.imshow(torch.abs(idata_multi_ti.data[idx, 0, 0, :, :]))
-    ax.set_title(f'TI = {idata_multi_ti.header.ti[idx]:.0f}ms')
+    ax.set_title(f'TI = {idata_multi_ti.header.ti[idx]:.3f}s')
 
 # %% [markdown]
 # ### Signal model and loss function
 # We use the model $q$
 #
-# $q(TI) = M_0 (1 - e^{-TI/T1})$
+# $q(TI) = M_0 (1 - e^{-TI/T_1})$
 #
-# with the equilibrium magnetization $M_0$, the inversion time $TI$, and $T1$. We have to keep in mind that the DICOM
+# with the equilibrium magnetization $M_0$, the inversion time $TI$, and $T_1$. We have to keep in mind that the DICOM
 # images only contain the magnitude of the signal. Therefore, we need $|q(TI)|$:
 
 # %%
@@ -76,7 +76,7 @@ mse = MSE(idata_multi_ti.data.abs())
 # %% [markdown]
 # Now we can simply combine the two into a functional to solve
 #
-# $ \min_{M_0, T1} || |q(M_0, T1, TI)| - x||_2^2$
+# $ \min_{M_0, T_1} || |q(M_0, T_1, TI)| - x||_2^2$
 # %%
 functional = mse @ model
 
@@ -88,17 +88,17 @@ functional = mse @ model
 # To increase our chances of reaching the global minimum, we can ensure that our starting
 # values are already close to the global minimum. We need a good starting point for each pixel.
 #
-# One option to get a good starting point is to calculate the signal curves for a range of T1 values and then check
+# One option to get a good starting point is to calculate the signal curves for a range of $T_1$ values and then check
 # for each pixel which of these signal curves fits best. This is similar to what is done for MR Fingerprinting. So we
 # are going to:
-# - define a list of realistic T1 values (we call this a dictionary of T1 values)
-# - calculate the signal curves corresponding to each of these T1 values
+# - define a list of realistic $T_1$ values (we call this a dictionary of $T_1$ values)
+# - calculate the signal curves corresponding to each of these $T_1$ values
 # - compare the signal curves to the signals of each voxel (we use the maximum of the dot-product as a metric of how
 # well the signals fit to each other)
 
 # %%
-# Define 100 T1 values between 100 and 3000 ms
-t1_dictionary = torch.linspace(100, 3000, 100)
+# Define 100 T1 values between 0.1 and 3.0 s
+t1_dictionary = torch.linspace(0.1, 3.0, 100)
 
 # Calculate the signal corresponding to each of these T1 values. We set M0 to 1, but this is arbitrary because M0 is
 # just a scaling factor and we are going to normalize the signal curves.
@@ -114,19 +114,19 @@ idx_best_match = torch.argmax(torch.abs(dot_product), dim=1)
 t1_start = rearrange(t1_dictionary[idx_best_match], '(y x)->1 1 y x', y=n_y, x=n_x)
 
 # %%
-# The image with the longest inversion time is a good approximation of the equilibrium magnetization
-m0_start = torch.abs(idata_multi_ti.data[torch.argmax(idata_multi_ti.header.ti), ...])
+# The maximum absolute value observed is a good approximation for m0
+m0_start = torch.amax(torch.abs(idata_multi_ti.data), 0)
 
 # %%
 # Visualize the starting values
 fig, axes = plt.subplots(1, 2, figsize=(8, 2), squeeze=False)
 colorbar_ax = [make_axes_locatable(ax).append_axes('right', size='5%', pad=0.05) for ax in axes[0, :]]
 im = axes[0, 0].imshow(m0_start[0, 0, ...])
-axes[0, 0].set_title('M0 start values')
+axes[0, 0].set_title('$M_0$ start values')
 fig.colorbar(im, cax=colorbar_ax[0])
-im = axes[0, 1].imshow(t1_start[0, 0, ...], vmin=0, vmax=2500)
-axes[0, 1].set_title('T1 start values')
-fig.colorbar(im, cax=colorbar_ax[1])
+im = axes[0, 1].imshow(t1_start[0, 0, ...], vmin=0, vmax=2.5)
+axes[0, 1].set_title('$T_1$ start values')
+fig.colorbar(im, cax=colorbar_ax[1], label='s')
 
 # %% [markdown]
 # ### Carry out fit
@@ -134,7 +134,7 @@ fig.colorbar(im, cax=colorbar_ax[1])
 # %%
 # Hyperparameters for optimizer
 max_iter = 2000
-lr = 1e0
+lr = 1e-1
 
 # Run optimization
 params_result = adam(functional, [m0_start, t1_start], max_iter=max_iter, lr=lr)
@@ -146,7 +146,7 @@ t1[torch.isnan(t1)] = 0
 # ### Visualize the final results
 # To get an impression of how well the fit has worked, we are going to calculate the relative error between
 #
-# $E_{relative} = \sum_{TI}\frac{|(q(M_0, T1, TI) - x)|}{|x|}$
+# $E_{relative} = \sum_{TI}\frac{|(q(M_0, T_1, TI) - x)|}{|x|}$
 #
 # on a voxel-by-voxel basis
 
@@ -158,11 +158,11 @@ relative_absolute_error = torch.sum(torch.abs(model(m0, t1)[0] - idata_multi_ti.
 fig, axes = plt.subplots(1, 3, figsize=(10, 2), squeeze=False)
 colorbar_ax = [make_axes_locatable(ax).append_axes('right', size='5%', pad=0.05) for ax in axes[0, :]]
 im = axes[0, 0].imshow(m0[0, 0, ...])
-axes[0, 0].set_title('M0')
+axes[0, 0].set_title('$M_0$')
 fig.colorbar(im, cax=colorbar_ax[0])
-im = axes[0, 1].imshow(t1[0, 0, ...], vmin=0, vmax=2500)
-axes[0, 1].set_title('T1')
-fig.colorbar(im, cax=colorbar_ax[1])
+im = axes[0, 1].imshow(t1[0, 0, ...], vmin=0, vmax=2.5)
+axes[0, 1].set_title('$T_1$')
+fig.colorbar(im, cax=colorbar_ax[1], label='s')
 im = axes[0, 2].imshow(relative_absolute_error[0, 0, ...], vmin=0, vmax=1.0)
 axes[0, 2].set_title('Relative error')
 fig.colorbar(im, cax=colorbar_ax[2])
