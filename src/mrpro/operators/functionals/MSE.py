f01c1fe29ddbb9d943780a688d11ccfaa4806beb
Felix F Zimmermann <fzimmermann89@gmail.com>
2024-11-14 13:40:51 +0100

    Update src/mrpro/operators/functionals/MSE.py

--- src/mrpro/operators/functionals/MSE.py
+++ src/mrpro/operators/functionals/MSE.py
@@ -23,7 +23,7 @@ class MSE(L2NormSquared):
         The MSE functional is given by
         :math:`f: C^N -> [0, \infty), x -> 1/N \| W (x-b)\|_2^2`,
         where :math:`W` is either a scalar or tensor that corresponds to a (block-) diagonal operator
-        that is applied to the input. The squared norm is then divided by N.
+        that is applied to the input. The division by `N` can be disabled by setting `divide_by_n=False`
         For more details also see :class:`mrpro.operators.functionals.L2NormSquared`
 
         Parameters
