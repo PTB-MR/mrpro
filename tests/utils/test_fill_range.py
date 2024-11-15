6958ba88efe703d1e655ccac08aab0bf24b5b8dd
Felix Zimmermann <fzimmermann89@gmail.com>
2024-11-15 04:07:36 +0100

    fix

--- tests/utils/test_fill_range.py
+++ tests/utils/test_fill_range.py
@@ -7,15 +7,15 @@ from mrpro.utils import fill_range_
 
 @pytest.mark.parametrize('dtype', [torch.float32, torch.int64], ids=['float32', 'int64'])
 def test_fill_range(dtype):
-    """Test functionality of arange_."""
-    tensor = torch.zeros(3, 4, dtype=dtype)
+    """Test functionality of fill_range."""
+    tensor = torch.zeros(3, rm 4, dtype=dtype)
     fill_range_(tensor, dim=1)
     expected = torch.tensor([[0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3]], dtype=tensor.dtype)
     torch.testing.assert_close(tensor, expected)
 
 
 def test_fill_range_dim_out_of_range():
-    """Test arange_ with a dimension out of range."""
+    """Test arange_ with a dimension out of fill_range_."""
     tensor = torch.zeros(3, 4)
     with pytest.raises(IndexError, match='Dimension 2 is out of range'):
         fill_range_(tensor, dim=2)
