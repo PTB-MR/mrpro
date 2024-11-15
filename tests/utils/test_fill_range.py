dbfd0da77cab5bdef5be41ab26eb438fe9da00e6
Felix Zimmermann <fzimmermann89@gmail.com>
2024-11-15 10:28:24 +0100

    fix

--- tests/utils/test_fill_range.py
+++ tests/utils/test_fill_range.py
@@ -8,7 +8,7 @@ from mrpro.utils import fill_range_
 @pytest.mark.parametrize('dtype', [torch.float32, torch.int64], ids=['float32', 'int64'])
 def test_fill_range(dtype):
     """Test functionality of fill_range."""
-    tensor = torch.zeros(3, rm 4, dtype=dtype)
+    tensor = torch.zeros(3, 4, dtype=dtype)
     fill_range_(tensor, dim=1)
     expected = torch.tensor([[0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3]], dtype=tensor.dtype)
     torch.testing.assert_close(tensor, expected)
