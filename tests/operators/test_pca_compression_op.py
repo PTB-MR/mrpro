6d5ffa7b1b67f4aeb4a91a38fa3ab47ea9625596
ckolbPTB <christoph.kolbitsch@ptb.de>
2024-11-16 21:15:56 +0100

    add pca comp

--- tests/operators/test_pca_compression_op.py
+++ tests/operators/test_pca_compression_op.py
@@ -3,21 +3,16 @@
 import pytest
 from mrpro.operators import PCACompressionOp
 
-from tests import RandomGenerator, dotproduct_adjointness_test
-
-
-@pytest.mark.parametrize(
-    ('init_data_shape', 'input_shape', 'n_components'),
-    [
-        ((40, 10), (100, 10), 6),
-        ((40, 10), (3, 4, 5, 100, 10), 3),
-        ((3, 4, 40, 10), (3, 4, 100, 10), 6),
-        ((3, 4, 40, 10), (7, 3, 4, 100, 10), 3),
-    ],
+from tests import (
+    RandomGenerator,
+    dotproduct_adjointness_test,
+    forward_mode_autodiff_of_linear_operator_test,
+    gradient_of_linear_operator_test,
 )
-def test_pca_compression_op_adjoint(init_data_shape, input_shape, n_components):
-    """Test adjointness of PCA Compression Op."""
 
+
+def create_pca_compression_op_and_domain_range(init_data_shape, input_shape, n_components):
+    """Create a pca compression operator and an element from domain and range."""
     # Create test data
     generator = RandomGenerator(seed=0)
     data_to_calculate_compression_matrix_from = generator.complex64_tensor(init_data_shape)
@@ -27,7 +22,40 @@ def test_pca_compression_op_adjoint(init_data_shape, input_shape, n_components):
 
     # Create operator and apply
     pca_comp_op = PCACompressionOp(data=data_to_calculate_compression_matrix_from, n_components=n_components)
-    dotproduct_adjointness_test(pca_comp_op, u, v)
+    return pca_comp_op, u, v
+
+
+SHAPE_PARAMETERS = pytest.mark.parametrize(
+    ('init_data_shape', 'input_shape', 'n_components'),
+    [
+        ((40, 10), (100, 10), 6),
+        ((40, 10), (3, 4, 5, 100, 10), 3),
+        ((3, 4, 40, 10), (3, 4, 100, 10), 6),
+        ((3, 4, 40, 10), (7, 3, 4, 100, 10), 3),
+    ],
+)
+
+
+@SHAPE_PARAMETERS
+def test_pca_compression_op_adjoint(init_data_shape, input_shape, n_components):
+    """Test adjointness of PCA Compression Op."""
+    dotproduct_adjointness_test(*create_pca_compression_op_and_domain_range(init_data_shape, input_shape, n_components))
+
+
+@SHAPE_PARAMETERS
+def test_pca_compression_op_grad(init_data_shape, input_shape, n_components):
+    """Test gradient of PCA Compression Op."""
+    gradient_of_linear_operator_test(
+        *create_pca_compression_op_and_domain_range(init_data_shape, input_shape, n_components)
+    )
+
+
+@SHAPE_PARAMETERS
+def test_pca_compression_op_forward_mode_autodiff(init_data_shape, input_shape, n_components):
+    """Test forward-mode autodiff of PCA Compression Op."""
+    forward_mode_autodiff_of_linear_operator_test(
+        *create_pca_compression_op_and_domain_range(init_data_shape, input_shape, n_components)
+    )
 
 
 def test_pca_compression_op_wrong_shapes():
