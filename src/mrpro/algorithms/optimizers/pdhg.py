c555ae79971556ba0b1225d758a0de8e5a86da58
Andreas Kofler <andreas.kofler@ptb.de>
2024-11-18 14:49:48 +0100

    fix the fix

--- src/mrpro/algorithms/optimizers/pdhg.py
+++ src/mrpro/algorithms/optimizers/pdhg.py
@@ -113,8 +113,8 @@ def pdhg(
         f_sum = ProximableFunctionalSeparableSum(*(ZeroFunctional(),) * rows)
     elif isinstance(f, ProximableFunctional):
         f_sum = ProximableFunctionalSeparableSum(f)
-        if len(f) != rows:
-            raise ValueError('Number of rows in operator does not match number of functionals in f')
+    elif len(f) != rows:
+        raise ValueError('Number of rows in operator does not match number of functionals in f')
     else:
         f_sum = f
 
@@ -123,8 +123,8 @@ def pdhg(
         g_sum = ProximableFunctionalSeparableSum(*(ZeroFunctional(),) * cols)
     elif isinstance(g, ProximableFunctional):
         g_sum = ProximableFunctionalSeparableSum(g)
-        if len(g) != cols:
-            raise ValueError('Number of columns in operator does not match number of functionals in g')
+    elif len(g) != cols:
+        raise ValueError('Number of columns in operator does not match number of functionals in g')
     else:
         g_sum = g
 
