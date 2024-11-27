54a4115b66e1eccc542213631b1e1c90718f61c5
koflera <andreas_kofler@gmx.net>
2024-11-27 12:04:58 +0100

    review tv example

--- examples/tv_minimization_reconstruction_pdhg.py
+++ examples/tv_minimization_reconstruction_pdhg.py
@@ -70,7 +70,7 @@ data_file.flush()
 # to be able to compute $\mathrm{prox}_{\tau g}$, one would need to solve a linear system at each
 # iteration. We will therefore use a way more efficient identification.
 #
-# In the following, we load some 2D radial MR data and PDHG to reconstruct it.
+# In the following, we load 2D radial MR data and set up problem (2) to use PDHG to reconstruct the data.
 
 # %%
 import mrpro
@@ -90,11 +90,12 @@ kdata.header.recon_matrix.y = 256
 # reconstruction and iterative SENSE as methods of comparison.
 
 # %%
-# Set up direct reconstruction class to estimate coil sensitivity maps, density compensation etc
+# Set up direct reconstruction class. The estimate coil sensitivity maps and density
+# compensation values can be reused later to save time.
 direct_reconstruction = mrpro.algorithms.reconstruction.DirectReconstruction(kdata)
 img_direct = direct_reconstruction(kdata)
 
-# run tierative SENSE to get an initiual guess of the solution
+# run iterative SENSE to get an initial guess of the solution
 iterative_sense_reconstruction = mrpro.algorithms.reconstruction.IterativeSENSEReconstruction(
     kdata,
     n_iterations=8,
@@ -103,18 +104,14 @@ iterative_sense_reconstruction = mrpro.algorithms.reconstruction.IterativeSENSER
 )
 img_iterative_sense = iterative_sense_reconstruction(kdata)
 
-# Define Fourier operator and csm-operator re-using the ones already constructed in the
+# Define Fourier operator and CSM operator re-using the ones already constructed in the
 # direct reconstruction class
 fourier_operator = direct_reconstruction.fourier_op
 assert direct_reconstruction.csm is not None
 csm_operator = direct_reconstruction.csm.as_operator()
 
-# Create the entire acquisition operator A to be used in the operator K=[A, \nabla]^T for PDHG
+# Create the entire acquisition operator A
 acquisition_operator = fourier_operator @ csm_operator
-
-# adjoint reconstruction as additional method of comparison
-(img_adjoint,) = acquisition_operator.H(kdata.data)
-
 
 # %% [markdown]
 # ### Recast the problem to be able to apply PDHG
@@ -136,15 +133,16 @@ acquisition_operator = fourier_operator @ csm_operator
 # Define the gradient operator \nabla to be used in the operator K=[A, \nabla]^T for PDHG
 from mrpro.operators import FiniteDifferenceOp
 
-# The operator computes the directional derivatives along the penultimate and last dimensions (x,y)
+# The operator computes the directional derivatives along the the last two dimensions (x,y)
 nabla_operator = FiniteDifferenceOp(dim=(-2, -1), mode='forward')
 
 from mrpro.algorithms.optimizers import pdhg
+from mrpro.algorithms.optimizers.pdhg import PDHGStatus
 from mrpro.operators import LinearOperatorMatrix, ProximableFunctionalSeparableSum
 from mrpro.operators.functionals import L1NormViewAsReal, L2NormSquared, ZeroFunctional
 
 # Regularization parameter for the $\ell_1$-norm
-regularization_parameter = 2
+regularization_parameter = 0.1
 
 # Set up the problem by using the previously described identification
 l2 = 0.5 * L2NormSquared(target=kdata.data, divide_by_n=True)
@@ -161,8 +159,21 @@ initial_values = (img_iterative_sense.data,)
 # ### Run PDHG for a certain number of iterations
 
 # %%
-max_iterations = 64
-(img_pdhg,) = pdhg(f=f, g=g, operator=K, initial_values=initial_values, max_iterations=max_iterations)
+max_iterations = 128
+
+
+# call backfunction to track the value of the objective functional f(K(x)) + g(x)
+def callback(optimizer_status: PDHGStatus) -> None:
+    """Print the value of the objective functional every 8th iteration."""
+    iteration = optimizer_status['iteration_number']
+    solution = optimizer_status['solution']
+    if iteration % 16 == 0:
+        print(optimizer_status['objective'](*solution).item())
+
+
+(img_pdhg,) = pdhg(
+    f=f, g=g, operator=K, initial_values=initial_values, max_iterations=max_iterations, callback=callback
+)
 
 # %%
 # ### Compare the results
@@ -171,7 +182,7 @@ import matplotlib.pyplot as plt
 
 fig, ax = plt.subplots(1, 3, squeeze=False)
 clim = [0, 1e-3]
-ax[0, 0].set_title('Adjoint Recon', fontsize=10)
+ax[0, 0].set_title('Adjoint (direct) Recon', fontsize=10)
 ax[0, 0].imshow(img_direct.data.abs()[0, 0, 0, :, :], clim=clim)
 ax[0, 1].set_title('Iterative SENSE', fontsize=10)
 ax[0, 1].imshow(img_iterative_sense.data.abs()[0, 0, 0, :, :], clim=clim)
