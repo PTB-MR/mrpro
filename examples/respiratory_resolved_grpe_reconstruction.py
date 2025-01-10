0fa9e38c4148357ff2574d76aa27b936b2fdd823
Christoph Kolbitsch <christoph.kolbitsch@ptb.de>
2025-01-10 18:01:27 +0100

    update reco with tv

--- examples/respiratory_resolved_grpe_reconstruction.py
+++ examples/respiratory_resolved_grpe_reconstruction.py
@@ -13,8 +13,9 @@ fname = '/home/kolbit01/data/GRPE/meas_MID00291_FID96729_20240520_fov288_288_200
 #fname = '/home/kolbit01/data/GRPE/meas_MID00044_FID09487_20241021_fov288_288_160mm_154_92_200_3d_grpe_itl_pf0_6.h5'
 path_out = '/home/kolbit01/data/GRPE'
 
-fname = '/echo/kolbit01/data/GRPE_Charite/2024_12_09/meas_MID00058_FID17527_grpe_seq_sag_short.mrd'
-fname = '/echo/kolbit01/data/GRPE_Charite/2024_12_09/meas_MID00060_FID17529_grpe_seq_sag_long.mrd'
+path_data = '/echo/kolbit01/data/GRPE_Charite/2024_12_09/'
+fname = path_data + 'meas_MID00058_FID17527_grpe_seq_sag_short.mrd'
+fname = path_data + 'meas_MID00060_FID17529_grpe_seq_sag_long.mrd'
 
 # ### Imports
 import matplotlib.pyplot as plt
@@ -23,16 +24,17 @@ import numpy as np
 from einops import rearrange
 from mrpro.data import CsmData, KTrajectory, DcfData, IData, KData
 from mrpro.data.traj_calculators import KTrajectoryRpe, KTrajectoryPulseq
-from mrpro.operators import FourierOp, FastFourierOp, SensitivityOp, CartesianSamplingOp, EinsumOp
-from mrpro.algorithms.optimizers import cg
+from mrpro.operators import FourierOp, FastFourierOp, SensitivityOp, IdentityOp, DensityCompensationOp, FiniteDifferenceOp, LinearOperatorMatrix, ProximableFunctionalSeparableSum
+from mrpro.algorithms.optimizers import cg, pdhg
 from mrpro.algorithms.reconstruction import DirectReconstruction, IterativeSENSEReconstruction
+from mrpro.operators.functionals import L1NormViewAsReal, L2NormSquared, ZeroFunctional
 from mrpro.utils import split_idx
 import os
 import nibabel as nib
 
 import time
 
-flag_plot = False
+flag_plot = True
 
 
 # Correct for partial fourier sampling, as Siemens scanner does not save trajectory correctly
@@ -50,7 +52,7 @@ if not os.path.exists(fname.replace('.mrd', '_pf.mrd')):
         ismrmrd_header.encoding[0].encodingLimits.kspace_encoding_step_1.maximum=111
         ismrmrd_header.encoding[0].encodingLimits.kspace_encoding_step_1.center=96
     # 92 ky
-    elif ismrmrd_header.encoding[0].encodedSpace.matrixSize.y == 92:
+    elif ismrmrd_header.encoding[0].encodedSpace.matrixSize.y == 92 or ismrmrd_header.encoding[0].encodedSpace.matrixSize.y == 154:
         ismrmrd_header.encoding[0].encodingLimits.kspace_encoding_step_1.maximum=54
         ismrmrd_header.encoding[0].encodingLimits.kspace_encoding_step_1.center=46
     # 64 ky
@@ -102,46 +104,95 @@ def save_image_data(img, suffix):
     
     for i in range(img.shape[0]):
         nii_resp = nib.Nifti1Image(img[i].numpy(), affine=torch.eye(4))
-        nib.save(nii_resp,os.path.join(path_out,suffix+str(i))) 
+        nib.save(nii_resp,os.path.join(path_data,suffix+str(i))) 
         
-        img_coronal_sagittal_axial = get_coronal_sagittal_axial_view(img_resp_resolved_abs, other=i, z=showz, y=showy, x=showx)
-        plt.imsave(os.path.join(path_out,suffix+f'{i}.png'), img_coronal_sagittal_axial, dpi=300, cmap='grey', vmin=0, vmax=0.25)
+        img_coronal_sagittal_axial = get_coronal_sagittal_axial_view(img, other=i, z=showz, y=showy, x=showx)
+        plt.imsave(os.path.join(path_data,suffix+f'{i}.png'), img_coronal_sagittal_axial, dpi=300, cmap='grey', vmin=0, vmax=0.25)
     
     
-    
-# Display slices
-showz, showy, showx = [50, 96, 115]
-#showz, showy, showx = [50, 96, 28]   
-#showz, showy, showx = [50, 50, 70] 
+def reg_it_sense(kdata, csm, dcf, n_iterations=4, reg_weight=0.0, reg_img=0.0):
+    fourier_operator = FourierOp.from_kdata(kdata)
+    csm_operator = SensitivityOp(csm)
+    acquisition_operator = fourier_operator @ csm_operator
+    dcf_operator = DensityCompensationOp(dcf)
 
-tstart = time.time()
+    right_hand_side = acquisition_operator.H(dcf_operator(kdata.data)[0])[0] + reg_weight * reg_img
+    operator = acquisition_operator.H @ dcf_operator @ acquisition_operator + IdentityOp() * torch.as_tensor(reg_weight)
+    #operator = acquisition_operator.gram + IdentityOp() * torch.as_tensor(reg_weight)
+
+    img = cg(operator, right_hand_side, initial_value=right_hand_side, max_iterations=n_iterations, tolerance=0.0)
+    return img
+
+def tv_denoising(img_target, img_initial, reg_weight=0.1, data_weight=1.0, tv_dim=(-2, -1), n_iterations=100):
+    nabla_operator = FiniteDifferenceOp(dim=tv_dim, mode='forward')
+
+    l2 = data_weight * 0.5 * L2NormSquared(target=img_target, divide_by_n=True)
+    l1 = reg_weight * L1NormViewAsReal(divide_by_n=True)
+
+    f = ProximableFunctionalSeparableSum(l2, l1)
+    g = ZeroFunctional()
+    K = LinearOperatorMatrix(((IdentityOp(),), (nabla_operator,)))
+
+    initial_values = (img_initial,)
+    max_iterations = n_iterations
+
+    (img,) = pdhg(
+        f=f, g=g, operator=K, initial_values=initial_values, max_iterations=max_iterations)
+    return img
+
+def tv_admm(img_initial, kdata, csm, dcf, reg_weight=0.2, data_weight=0.1, tv_it=100, sense_it=10):
+    img_x = img_initial.data.clone()
+    img_z = img_initial.data.clone()
+    img_u = torch.zeros_like(img_z)
+    
+    if img_x.shape[0] == 1:
+        tv_dim = (-3,-2,-1)
+    else:
+        tv_dim = (-5, -3,-2,-1)
+
+    for outer_iter in range(5):
+        # Denoising
+        tstart = time.time()
+        img_x = tv_denoising(img_z - img_u, img_z, reg_weight=reg_weight/data_weight, data_weight=1.0, tv_dim=tv_dim, n_iterations=tv_it)
+        print(f'It {outer_iter} - PDHG {(time.time()-tstart)/60}min')
+
+        # Iterative SENSE
+        tstart = time.time()
+        img_z = reg_it_sense(kdata, csm, dcf, n_iterations=sense_it, reg_weight=data_weight, reg_img=img_x+img_u)
+        print(f'It {outer_iter} - itSENSE {(time.time()-tstart)/60}min')
+
+        # Update u
+        img_u = img_u + img_x - img_z
+        
+    return IData(data=img_z, header=img_initial.header)
+    
 
 # %% Read data
 # We are going to reconstruct an image using all the acquired data.
 # Read the raw data, calculate the trajectory and dcf
-kdata = KData.from_file(fname.replace('.mrd', '_pf.mrd'), KTrajectoryRpe(angle=torch.pi * 0.618034))
+kdata = KData.from_file(fname.replace('.mrd', '_pf.mrd'), KTrajectoryRpe(angle=torch.pi * 0.618034, shift_between_rpe_lines=[0,]))
+
+# Display slices
+showz, showy, showx = [50, 96, 50] if kdata.header.recon_matrix.y == 192 else [50, 96, 28] 
 
 # Set the matrix sizes which are not encoded correctly in the pulseq sequence
 kdata.header.encoding_matrix.z = kdata.header.encoding_matrix.y
-kdata.header.recon_matrix.x = 192 #100 or 192
-kdata.header.recon_matrix.y = 192 #100 or 192
-kdata.header.recon_matrix.z = 192 #100 or 192
+kdata.header.recon_matrix.z = kdata.header.recon_matrix.y
 
-kdata.header.encoding_matrix.y = 192 #92 or 192
-kdata.header.encoding_matrix.z = 192 #92 or 192
+# Temp Fix
+kdata.header.encoding_matrix.y = 192
+kdata.header.encoding_matrix.z = 192
+kdata.header.recon_matrix.x = 192
+kdata.header.recon_matrix.y = 192
+kdata.header.recon_matrix.z = 192
 
 # Speed up things
 kdata = kdata.compress_coils(n_compressed_coils=6)
-kdata.header.recon_matrix.x = 192
 kdata = kdata.remove_readout_os()
-
 
 if flag_plot:
     plt.figure()
     plt.plot(kdata.traj.ky[0,:10,:,0], kdata.traj.kz[0,:10,:,0], 'ob')
-
-#idx_rpe_lines = torch.as_tensor(torch.arange(0,50))[None,:]
-#kdata_dynamic = kdata.split_k2_into_other(idx_rpe_lines, other_label='repetition')
 
 # %% Coil maps
 # Calculate coil maps
@@ -156,72 +207,33 @@ if flag_plot:
         cax.imshow(np.abs(img_coronal_sagittal_axial))
 
 
-# %% Iterative reconstruction of average
+# %% Average reconstruction
+tstart = time.time()
+direct_recon = DirectReconstruction(kdata, csm=csm_maps)
+img_direct = direct_recon.forward(kdata)
+print(f'Average reco direct {(time.time()-tstart)/60}min')
+
+tstart = time.time()
 itSENSE_recon = IterativeSENSEReconstruction(kdata, csm = csm_maps)
 img_itSENSE = itSENSE_recon.forward(kdata)
+print(f'Average reco itSENSE {(time.time()-tstart)/60}min')
+
+tstart_admm = time.time()
+img_pdhg = tv_admm(img_initial=img_itSENSE, kdata=kdata, csm=csm_maps, dcf=itSENSE_recon.dcf, reg_weight=0.2, data_weight=0.1)
+print(f'Average reco ADMM {(time.time()-tstart_admm)/60}min')
+
 
 if flag_plot:
-    img_coronal_sagittal_axial = get_coronal_sagittal_axial_view(img_itSENSE.rss(), z=showz, y=showy, x=showx)
-    img_coronal_sagittal_axial /= img_coronal_sagittal_axial.max()
+    img_save = []
+    for idx, img in enumerate([img_direct.rss(), img_itSENSE.rss(), img_pdhg.rss()]):
+        img_coronal_sagittal_axial = get_coronal_sagittal_axial_view(img, z=showz, y=showy, x=showx)
+        img_coronal_sagittal_axial /= img_coronal_sagittal_axial.max()
+        img_save.append(img_coronal_sagittal_axial)
+    img_save = np.concatenate(img_save)
     plt.figure()
-    plt.imshow(img_coronal_sagittal_axial, cmap='grey', vmin=0, vmax=0.35)
-    plt.imsave(os.path.join(path_out,f'average.png'), img_coronal_sagittal_axial, dpi=300, cmap='grey', vmin=0, vmax=0.35)
+    plt.imshow(img_save, cmap='grey', vmin=0, vmax=0.35)
+    plt.imsave(os.path.join(path_data,f'average.png'), img_save, dpi=300, cmap='grey', vmin=0, vmax=0.35)
 
-save_image_data(img_itSENSE, 'static_itSENSE_')
-# %%
-# Create the entire acquisition operator A
-acquisition_operator = itSENSE_recon.fourier_op @ SensitivityOp(csm_maps)
-
-# Define the gradient operator \nabla to be used in the operator K=[A, \nabla]^T for PDHG
-from mrpro.operators import FiniteDifferenceOp
-
-# The operator computes the directional derivatives along the the last two dimensions (x,y)
-nabla_operator = FiniteDifferenceOp(dim=(-3, -2, -1), mode='forward')
-
-from mrpro.algorithms.optimizers import pdhg
-from mrpro.algorithms.optimizers.pdhg import PDHGStatus
-from mrpro.operators import LinearOperatorMatrix, ProximableFunctionalSeparableSum
-from mrpro.operators.functionals import L1NormViewAsReal, L2NormSquared, ZeroFunctional
-
-# Regularization parameter for the $\ell_1$-norm
-regularization_parameter = 1.0
-
-# Set up the problem by using the previously described identification
-l2 = 0.5 * L2NormSquared(target=kdata.data, divide_by_n=True)
-l1 = regularization_parameter * L1NormViewAsReal(divide_by_n=True)
-
-f = ProximableFunctionalSeparableSum(l2, l1)
-g = ZeroFunctional()
-K = LinearOperatorMatrix(((acquisition_operator,), (nabla_operator,)))
-
-# initialize PDHG with iterative SENSE solution for warm start
-initial_values = (img_itSENSE.data,)
-
-# Run PDHG for a certain number of iterations
-max_iterations = 100
-
-
-# call backfunction to track the value of the objective functional f(K(x)) + g(x)
-def callback(optimizer_status: PDHGStatus) -> None:
-    """Print the value of the objective functional every 8th iteration."""
-    iteration = optimizer_status['iteration_number']
-    solution = optimizer_status['solution']
-    if iteration % 1 == 0:
-        print(optimizer_status['objective'](*solution).item())
-
-
-(img_pdhg,) = pdhg(
-    f=f, g=g, operator=K, initial_values=initial_values, max_iterations=max_iterations, callback=callback
-)
-
-if flag_plot:
-    img_coronal_sagittal_axial = get_coronal_sagittal_axial_view(img_pdhg.data[0].abs(), z=showz, y=showy, x=showx)
-    img_coronal_sagittal_axial /= img_coronal_sagittal_axial.max()
-    plt.figure()
-    plt.imshow(img_coronal_sagittal_axial, cmap='grey', vmin=0, vmax=0.35)
-    plt.imsave(os.path.join(path_out,f'average.png'), img_coronal_sagittal_axial, dpi=300, cmap='grey', vmin=0, vmax=0.35)
-
-save_image_data(img_pdhg, 'static_pdhg_')
 
 # %% Respiratory self-navigator
 # We are going to obtain a self-navigator from the k-space centre line ky = kz = 0
@@ -255,6 +267,12 @@ condition_below = f_Hz <=resp_freq_window_Hz[1]
 condition_above = f_Hz >= resp_freq_window_Hz[0] 
 condition_window = condition_below * condition_above 
 
+if flag_plot:
+    plt.figure() 
+    plt.plot(f_Hz, U_freq) 
+    plt.plot([resp_freq_window_Hz[0], resp_freq_window_Hz[0]], [0, U_freq.max()], '-k')
+    plt.plot([resp_freq_window_Hz[1], resp_freq_window_Hz[1]], [0, U_freq.max()], '-k')
+
 U_freq_window = U_freq[condition_window, :] 
 peak_in_window = torch.max(U_freq_window, dim=0) 
 peak_component = torch.argmax(peak_in_window.values) 
@@ -269,7 +287,7 @@ if flag_plot:
     rescaled_resp_nav = 80 * (resp_nav - resp_nav.min()) / (resp_nav.max() - resp_nav.min()) +80
 
     plt.figure()
-    plt.imshow(rearrange(torch.abs(nav_data_pre_rearrange[:,0,:]), 'angle x->x angle'), aspect='auto')
+    plt.imshow(rearrange(torch.abs(nav_data_pre_rearrange[:,2,:]), 'angle x->x angle'), aspect='auto')
     plt.plot(rescaled_resp_nav, ':w')
     plt.xlim(0,200)
 
@@ -285,112 +303,39 @@ resp_idx = split_idx(torch.argsort(torch.as_tensor(resp_nav_interpolated)), int(
 kdata_resp_resolved = kdata.split_k1_into_other(resp_idx, other_label='repetition')
 
 # %% Motion-resolved reconstruction
+tstart = time.time()
 direct_recon_resp_resolved = DirectReconstruction(kdata_resp_resolved, csm=itSENSE_recon.csm)
 img_direct_resp_resolved = direct_recon_resp_resolved.forward(kdata_resp_resolved)
+print(f'Dynamic reco direct {(time.time()-tstart)/60}min')
 
-save_image_data(img_direct_resp_resolved, 'dynamic_direct_')
-
-if flag_plot:
-    plot_resp_idx = [0, resp_idx.shape[0]-1]
-    fig, ax = plt.subplots(len(plot_resp_idx),1)
-    for nnd in range(len(plot_resp_idx)):
-        ax[nnd].imshow(get_coronal_sagittal_axial_view(img_direct_resp_resolved.rss(), other=plot_resp_idx[nnd], z=showz, y=showy, x=showx))
-
-
+tstart = time.time()
 itSENSE_recon_resp_resolved = IterativeSENSEReconstruction(kdata_resp_resolved, csm=itSENSE_recon.csm, n_iterations=10)
 img_itSENSE_resp_resolved = itSENSE_recon_resp_resolved.forward(kdata_resp_resolved)
+print(f'Dynamic reco itSENSE {(time.time()-tstart)/60}min')
+
+tstart_admm = time.time()
+img_pdhg_resp_resolved = tv_admm(img_initial=img_itSENSE_resp_resolved, kdata=kdata_resp_resolved, csm=csm_maps, dcf=itSENSE_recon_resp_resolved.dcf, reg_weight=2.0, data_weight=0.1)
+print(f'Dynamic reco ADMM {(time.time()-tstart_admm)/60}min')
+
 
 if flag_plot:
-    img_resp_resolved_abs = img_itSENSE_resp_resolved.rss()
-    img_resp_resolved_abs /= img_resp_resolved_abs.max()
-
-    plot_resp_idx = [0, resp_idx.shape[0]-1]
-    fig, ax = plt.subplots(len(plot_resp_idx),1)
-    for nnd in range(len(plot_resp_idx)):
-        img_coronal_sagittal_axial = get_coronal_sagittal_axial_view(img_resp_resolved_abs, other=plot_resp_idx[nnd], z=showz, y=showy, x=showx)
-        ax[nnd].imshow(img_coronal_sagittal_axial, cmap='grey', vmin=0, vmax=0.25)
-
-
-save_image_data(img_itSENSE_resp_resolved, 'dynamic_itSENSE_')
-print(f'Total time {(time.time()-tstart)/60}min')
-
+    img_dynamic = [img_direct_resp_resolved.rss(), img_itSENSE_resp_resolved.rss(), img_pdhg_resp_resolved.rss()]
+    for nnd in range(img_dynamic[0].shape[0]):
+        img_save = []
+        for idx, img in enumerate(img_dynamic):
+            img /= img.max()
+            img_save.append(get_coronal_sagittal_axial_view(img, other=nnd, z=showz, y=showy, x=showx))
+        img_save = np.concatenate(img_save)
+        plt.figure()
+        plt.imshow(img_save, cmap='grey', vmin=0, vmax=0.35)
+        plt.imsave(os.path.join(path_data,f'dynamic_{nnd}.png'), img_save, dpi=300, cmap='grey', vmin=0, vmax=0.35)
+  
 # %%
-tstart = time.time()
-
-# Create the entire acquisition operator A
-acquisition_operator = itSENSE_recon_resp_resolved.fourier_op @ SensitivityOp(itSENSE_recon_resp_resolved.csm)
-
-# Define the gradient operator \nabla to be used in the operator K=[A, \nabla]^T for PDHG
-from mrpro.operators import FiniteDifferenceOp
-
-# The operator computes the directional derivatives along the the last two dimensions (x,y)
-nabla_operator = FiniteDifferenceOp(dim=(0,), mode='forward')
-
-from mrpro.algorithms.optimizers import pdhg
-from mrpro.algorithms.optimizers.pdhg import PDHGStatus
-from mrpro.operators import LinearOperatorMatrix, ProximableFunctionalSeparableSum
-from mrpro.operators.functionals import L1NormViewAsReal, L2NormSquared, ZeroFunctional
-
-# Regularization parameter for the $\ell_1$-norm
-regularization_parameter = 1.0
-
-# Set up the problem by using the previously described identification
-l2 = 0.5 * L2NormSquared(target=kdata_resp_resolved.data, divide_by_n=True)
-l1 = regularization_parameter * L1NormViewAsReal(divide_by_n=True)
-
-f = ProximableFunctionalSeparableSum(l2, l1)
-g = ZeroFunctional()
-K = LinearOperatorMatrix(((acquisition_operator,), (nabla_operator,)))
-
-# initialize PDHG with iterative SENSE solution for warm start
-initial_values = (img_itSENSE_resp_resolved.data,)
-
-max_iterations = 100
-(img_pdhg,) = pdhg(
-    f=f, g=g, operator=K, initial_values=initial_values, max_iterations=max_iterations, callback=callback
-)
-
-if flag_plot:
-    img_resp_resolved_abs = img_pdhg.abs()[:,0,...]
-    img_resp_resolved_abs /= img_resp_resolved_abs.max()
-
-    plot_resp_idx = [0, resp_idx.shape[0]-1]
-    fig, ax = plt.subplots(len(plot_resp_idx),1)
-    for nnd in range(len(plot_resp_idx)):
-        img_coronal_sagittal_axial = get_coronal_sagittal_axial_view(img_resp_resolved_abs, other=plot_resp_idx[nnd], z=showz, y=showy, x=showx)
-        ax[nnd].imshow(img_coronal_sagittal_axial, cmap='grey', vmin=0, vmax=0.25)
-
-save_image_data(img_pdhg, 'dynamic_pdhg_')
-print(f'Total time TV {(time.time()-tstart)/60}min')
-# %%
-
-tstart = time.time()
-
-#### IMAGE DENOISING
-# Regularization parameter for the $\ell_1$-norm
-regularization_parameter = 1.0
-
-# Set up the problem by using the previously described identification
-l2 = 0.5 * L2NormSquared(target=img_itSENSE_resp_resolved.data, divide_by_n=True)
-l1 = regularization_parameter * L1NormViewAsReal(divide_by_n=True)
-
-f = ProximableFunctionalSeparableSum(l2, l1)
-g = ZeroFunctional()
-K = LinearOperatorMatrix(((acquisition_operator,), (nabla_operator,)))
-
-# initialize PDHG with iterative SENSE solution for warm start
-initial_values = (img_itSENSE_resp_resolved.data,)
-
-max_iterations = 100
-(img_admm_denoised,) = pdhg(
-    f=f, g=g, operator=K, initial_values=initial_values, max_iterations=max_iterations, callback=callback
-)
-
-save_image_data(img_admm_denoised, 'dynamic_admm_denoised_')
-
-#### DATA CONSISTENCY
-
-#### FINAL ITERATION
-
-print(f'Total time ADMM {(time.time()-tstart)/60}min')
+from PIL import Image, GifImagePlugin, ImageEnhance
+GifImagePlugin.LOADING_STRATEGY = GifImagePlugin.LoadingStrategy.RGB_AFTER_DIFFERENT_PALETTE_ONLY
+img_pil = []
+for nnd in range(img_dynamic[0].shape[0]):
+    img_pil.append(ImageEnhance.Brightness(Image.open(os.path.join(path_data,f'dynamic_{nnd}.png')).convert('L')).enhance(1.5))
+img_pil[0].save(fp=os.path.join(path_data,f'dynamic.gif'), format='GIF', append_images=img_pil[1:], save_all=True, 
+                duration=200, loop=0, optimize=False, lossless=True)
 # %%
