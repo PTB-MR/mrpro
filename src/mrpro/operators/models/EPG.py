17232dd23833b8c5119de97f1583760639e7d2d7
Felix Zimmermann <fzimmermann89@gmail.com>
2025-01-28 09:13:16 +0100

    docstring etc

--- src/mrpro/operators/models/EPG.py
+++ src/mrpro/operators/models/EPG.py
@@ -10,6 +10,60 @@ from mrpro.data.MoveDataMixin import MoveDataMixin
 from mrpro.operators.SignalModel import SignalModel
 
 
+class TensorAttributeMixin(torch.nn.Module):
+    def __setattr__(self, name: str, value: Any):
+        if isinstance(value, torch.Tensor):
+            if value.is_leaf and value.requires_grad:
+                self.register_parameter(name, torch.nn.Parameter(value))
+            elif value.is_leaf:
+                self.register_buffer(name, value)
+            else:
+                super().__setattr__(name, value)
+        else:
+            super().__setattr__(name, value)
+
+
+@dataclass
+class Parameters(MoveDataMixin):
+    """Tissue parameters for EPG simulation."""
+
+    t1: torch.Tensor
+    """T1 relaxation time [s]"""
+
+    t2: torch.Tensor
+    """T2 relaxation time [s]"""
+
+    m0: torch.Tensor
+    """Steay state magnetization (complex)"""
+
+    relative_b1: torch.Tensor | None = None
+    """Relative B1 scaling factor (complex)"""
+
+    @property
+    def device(self) -> torch.device:
+        """Device of the parameters."""
+        device = super().device
+        assert device is not None  # mypy # noqa: S101
+        return device
+
+    @property
+    def shape(self) -> torch.Size:
+        """Broadcasted shape of the parameters."""
+        shape = torch.broadcast_shapes(self.t1.shape, self.t2.shape, self.m0.shape)
+        if self.relative_b1 is not None:
+            shape = torch.broadcast_shapes(shape, self.relative_b1.shape)
+        return shape
+
+    @property
+    def dtype(self) -> torch.dtype:
+        """Promoted data type of the parameters."""
+        dtype = torch.promote_types(self.t1.dtype, self.t2.dtype)
+        dtype = torch.promote_types(dtype, self.m0.dtype)
+        if self.relative_b1 is not None:
+            dtype = torch.promote_types(dtype, self.relative_b1.dtype)
+        return dtype
+
+
 @torch.jit.script
 def rf_matrix(
     flip_angle: torch.Tensor,
@@ -40,6 +94,7 @@ def rf_matrix(
     sina2 = 1 - cosa2
     ejp = torch.polar(torch.tensor(1.0), 1j * phase)
     inv_ejp = 1 / ejp
+    new_shape = flip_angle.shape + (3, 3)
 
     return torch.stack(
         [
@@ -54,7 +109,7 @@ def rf_matrix(
             cosa + 0j,
         ],
         -1,
-    ).reshape(*flip_angle.shape, 3, 3)
+    ).reshape(new_shape)
 
 
 @torch.jit.script
@@ -90,7 +145,7 @@ def gradient_dephasing(state: torch.Tensor) -> torch.Tensor:
     -------
         EPG configuration states after gradient. Shape `..., 3, n`
     """
-    zero = state.new_zeros(*state.shape[:-2], 1)
+    zero = state.new_zeros(state.shape[:-2] + (1,))
     f_plus = torch.cat((state[..., 1, 1:2].conj(), state[..., 0, :-1]), dim=-1)
     f_minus = torch.cat((state[..., 1, 1:], zero), -1)
     z = state[..., 2, :]
@@ -143,54 +198,14 @@ def relax(state: torch.Tensor, relaxation_matrix: torch.Tensor, t1_recovery: boo
     return state
 
 
-class TensorAttributeMixin(torch.nn.Module):
-    def __setattr__(self, name: str, value: Any):
-        if isinstance(value, torch.Tensor):
-            if value.is_leaf and value.requires_grad:
-                self.register_parameter(name, torch.nn.Parameter(value))
-            elif value.is_leaf:
-                self.register_buffer(name, value)
-            else:
-                super().__setattr__(name, value)
-        else:
-            super().__setattr__(name, value)
-
-
-@dataclass
-class Parameters(MoveDataMixin):
-    t1: torch.Tensor
-    t2: torch.Tensor
-    m0: torch.Tensor
-    relative_b1: torch.Tensor | None = None
-
-    @property
-    def device(self) -> torch.device:
-        device = super().device
-        assert device is not None  # mypy
-        return device
-
-    @property
-    def shape(self) -> torch.Size:
-        shape = torch.broadcast_shapes(self.t1.shape, self.t2.shape, self.m0.shape)
-        if self.relative_b1 is not None:
-            shape = torch.broadcast_shapes(shape, self.relative_b1.shape)
-        return shape
-
-    @property
-    def dtype(self) -> torch.dtype:
-        dtype = torch.promote_types(self.t1.dtype, self.t2.dtype)
-        dtype = torch.promote_types(dtype, self.m0.dtype)
-        if self.relative_b1 is not None:
-            dtype = torch.promote_types(dtype, self.relative_b1.dtype)
-        return dtype
-
-
-def acquisition(state: torch.Tensor, parameters: Parameters) -> torch.Tensor:
-    return parameters.m0 * state[..., 0, 0]
+@torch.jit.script
+def acquisition(state: torch.Tensor, m0: torch.Tensor) -> torch.Tensor:
+    """Calculate the signal from the EPG state."""
+    return m0 * state[..., 0, 0]
 
 
 def initial_state(
-    shape: Sequence[int], n_sates: int = 20, device: torch.device | str = 'cpu', dtype: torch.dtype = torch.complex64
+    shape: Sequence[int], n_states: int = 20, device: torch.device | str = 'cpu', dtype: torch.dtype = torch.complex64
 ) -> torch.Tensor:
     """Generate initial EPG state.
 
@@ -209,9 +224,9 @@ def initial_state(
     -------
         Initial EPG state tensor. Shape `*shape,3, n_state)`
     """
-    if n_sates < 2:
+    if n_states < 2:
         raise ValueError('Number of states should be at least 2.')
-    return torch.zeros(*shape, 3, n_sates, device=device, dtype=dtype)
+    return torch.zeros(*shape, 3, n_states, device=device, dtype=dtype)
 
 
 class EPGBlock(TensorAttributeMixin):
@@ -229,8 +244,97 @@ class EPGBlock(TensorAttributeMixin):
         raise NotImplementedError
 
 
+class RFBlock(EPGBlock):
+    """RF pulse block."""
+
+    def __init__(self, flip_angle: torch.Tensor | float, phase: torch.Tensor | float) -> None:
+        super().__init__()
+        self.flip_angle = torch.as_tensor(flip_angle)
+        self.phase = torch.as_tensor(phase)
+
+    def forward(self, state: torch.Tensor, parameters: Parameters) -> tuple[torch.Tensor, list[torch.Tensor]]:
+        """Apply the RF to the EPG state.
+
+        Parameters
+        ----------
+        state
+            EPG configuration states
+        parameters
+            Tissue parameters
+
+        Returns
+        -------
+            EPG configuration states after the block and an empty list
+        """
+        matrix = rf_matrix(self.flip_angle, self.phase, parameters.relative_b1)
+        return rf(state, matrix), []
+
+    @property
+    def duration(self) -> torch.Tensor:
+        """Duration of the block in s."""
+        return torch.tensor(0.0)
+
+
+class AcquisitionBlock(EPGBlock):
+    """Acquisition block."""
+
+    def forward(self, state: torch.Tensor, parameters: Parameters) -> tuple[torch.Tensor, list[torch.Tensor]]:
+        """Apply the acquisition block to the EPG state.
+
+        Parameters
+        ----------
+        state
+            EPG configuration states
+        parameters
+            Tissue parameters
+
+        Returns
+        -------
+            EPG configuration states (unchanged) after the block and the acquired signal
+        """
+        return state, [acquisition(state, parameters.m0)]
+
+    @property
+    def duration(self) -> torch.Tensor:
+        """Duration of the block in s."""
+        return torch.tensor(0.0)
+
+
+class GradientDephasingBlock(EPGBlock):
+    """Gradient dephasing block."""
+
+    def forward(self, state: torch.Tensor, parameters: Parameters) -> tuple[torch.Tensor, list[torch.Tensor]]:
+        """Apply the gradient dephasing block to the EPG state.
+
+        Parameters
+        ----------
+        state
+            EPG configuration states
+        parameters
+            Tissue parameters
+
+        Returns
+        -------
+            EPG configuration states after the block and an empty list
+        """
+        return gradient_dephasing(state), []
+
+    @property
+    def duration(self) -> torch.Tensor:
+        """Duration of the block in s."""
+        return torch.tensor(0.0)
+
+
 class FispBlock(EPGBlock):
-    """FISP data acquisition block."""
+    """FISP data acquisition block.
+
+    The block consists of a series of RF pulses with different flip angles and phases followed by an acquisition:
+        - rf with flip_angle[i] and rf_phase[i]
+        - relax for te[i]
+        - acquisition
+        - gradient_dephasing
+        - relax for tr[i] - te[i]
+    """
 
     flip_angles: torch.Tensor
     rf_phases: torch.Tensor
@@ -243,28 +347,57 @@ class FispBlock(EPGBlock):
         rf_phases: torch.Tensor | float,
         te: torch.Tensor | float,
         tr: torch.Tensor | float,
-    ):
+    ) -> None:
+        """Initialize the FISP block.
+
+        Parameters
+        ----------
+        flip_angles
+            Flip angles of the RF pulses in rad
+        rf_phases
+            Phase of the RF pulses in rad
+        te
+            Echo time in s
+        tr
+            Repetition time in s
+        """
+        super().__init__()
+
+        flip_angles_, rf_phases_, te_, tr_ = map(torch.as_tensor, (flip_angles, rf_phases, te, tr))
         try:
             self.flip_angles, self.rf_phases, self.te, self.tr = torch.broadcast_tensors(
-                map(torch.as_tensor, (flip_angles, rf_phases, te, tr))
+                flip_angles_, rf_phases_, te_, tr_
             )
         except RuntimeError:
-            # Not broadcastable
             raise ValueError(
-                f'Shapes of flip_angles ({flip_angles.shape}), rf_phases ({rf_phases.shape}), te ({te.shape}) and '
-                f'tr ({tr.shape}) cannot be broadcasted.',
+                f'Shapes of flip_angles ({flip_angles_.shape}), rf_phases ({rf_phases_.shape}), te ({te_.shape}) and '
+                f'tr ({tr_.shape}) cannot be broadcasted.',
             ) from None
 
     @property
-    def duration(self):
+    def duration(self) -> torch.Tensor:
+        """Duration of the block in s."""
         return self.flip_angles.shape[-1] * self.tr
 
     def forward(self, state: torch.Tensor, parameters: Parameters) -> tuple[torch.Tensor, list[torch.Tensor]]:
+        """Apply the FISP block to the EPG state.
+
+        Parameters
+        ----------
+        state
+            EPG configuration states
+        parameters
+            Tissue parameters
+
+        Returns
+        -------
+            EPG configuration states after the block and the acquired signals
+        """
         signal = []
         for flip_angle, rf_phase, te, tr in zip(self.flip_angles, self.rf_phases, self.te, self.tr, strict=True):
             state = rf(state, rf_matrix(flip_angle, rf_phase, parameters.relative_b1))
             state = relax(state, relax_matrix(te, parameters.t1, parameters.t2))
-            signal.append(acquisition(state, parameters))
+            signal.append(acquisition(state, parameters.m0))
             state = gradient_dephasing(state)
             state = relax(state, relax_matrix((tr - te), parameters.t1, parameters.t2))
         return state, signal
@@ -274,15 +407,33 @@ class InversionBlock(EPGBlock):
     """T1 Inversion Preparation Block."""
 
     def __init__(self, inversion_time: torch.Tensor | float) -> None:
+        """Initialize the inversion block."""
+        super().__init__()
+
         self.inversion_time = torch.as_tensor(inversion_time)
 
     def forward(self, state: torch.Tensor, parameters: Parameters) -> tuple[torch.Tensor, list[torch.Tensor]]:
-        state = rf(state, rf_matrix(torch.pi, 0, parameters.relative_b1))
+        """Apply the inversion block to the EPG state.
+
+        Parameters
+        ----------
+        state
+            EPG configuration states
+        parameters
+            Tissue parameters
+
+        Returns
+        -------
+            EPG configuration states after the block and an empty list
+        """
+
+        state = rf(state, rf_matrix(torch.tensor(torch.pi), torch.tensor(0.0), parameters.relative_b1))
         state = relax(state, relax_matrix(self.inversion_time, parameters.t1, parameters.t2))
         return state, []
 
     @property
     def duration(self) -> torch.Tensor:
+        """Duration of the block in s."""
         return self.inversion_time
 
 
@@ -290,9 +441,31 @@ class T2PrepBlock(EPGBlock):
     """T2 Preparation Block."""
 
     def __init__(self, te: torch.Tensor | float) -> None:
+        """Initialize the T2 preparation block.
+
+        Parameters
+        ----------
+        te
+            Echo time of the T2 preparation block
+        """
+        super().__init__()
+
         self.te = torch.as_tensor(te)
 
     def forward(self, state: torch.Tensor, parameters: Parameters) -> tuple[torch.Tensor, list[torch.Tensor]]:
+        """Apply the T2 preperation block to the EPG state.
+
+        Parameters
+        ----------
+        state
+            EPG configuration states
+        parameters
+            Tissue parameters
+
+        Returns
+        -------
+            EPG configuration states after the block and an empty list
+        """
         # 90° pulse -> relaxation during TE/2 -> 180° pulse -> relaxation during TE/2 -> -90° pulse
         te2_relax = relax_matrix(self.te / 2, parameters.t1, parameters.t2)
         state = rf(state, rf_matrix(torch.pi / 2, 0, parameters.relative_b1))
@@ -306,6 +479,7 @@ class T2PrepBlock(EPGBlock):
 
     @property
     def duration(self) -> torch.Tensor:
+        """Duration of the block in s."""
         return self.te
 
 
@@ -313,14 +487,35 @@ class DelayBlock(EPGBlock):
     """Delay Block."""
 
     def __init__(self, delay_time: torch.Tensor | float) -> None:
+        """Initialize the delay block.
+        Parameters
+        ----------
+        delay_time
+            Delay time in s
+        """
+        super().__init__()
         self.delay_time = torch.as_tensor(delay_time)
 
     def forward(self, state: torch.Tensor, parameters: Parameters) -> tuple[torch.Tensor, list[torch.Tensor]]:
+        """Apply the delay block to the EPG state.
+
+        Parameters
+        ----------
+        state
+            EPG configuration states
+        parameters
+            Tissue parameters
+
+        Returns
+        -------
+            EPG configuration states after the block and an empty list
+        """
         state = relax(state, relax_matrix(self.delay_time, parameters.t1, parameters.t2))
         return state, []
 
     @property
-    def duration(self):
+    def duration(self) -> torch.Tensor:
+        """Duration of the block in s."""
         return self.delay_time
 
 
@@ -362,12 +557,27 @@ class EPGSequence(torch.nn.ModuleList, EPGBlock):
         - FispBlock with 48 flip_angles
         - DelayBlock with delay_time=1000-(t2_prep_block.duration + fisp_block.duration)
 
+    See also `CardiacFingerprinting` for an implementation.
+
     """
 
     def __init__(self, blocks: Sequence[EPGBlock] = ()):
         torch.nn.ModuleList.__init__(self, blocks)
 
     def forward(self, state: torch.Tensor, parameters: Parameters) -> tuple[torch.Tensor, list[torch.Tensor]]:
+        """Apply the sequence of blocks to the EPG state.
+
+        Parameters
+        ----------
+        state
+            EPG configuration states
+        parameters
+            Tissue parameters
+
+        Returns
+        -------
+            EPG configuration states after the sequence of blocks and the acquired signals
+        """
         signals: list[torch.Tensor] = []
         block: EPGBlock
         for block in self:
@@ -377,33 +587,94 @@ class EPGSequence(torch.nn.ModuleList, EPGBlock):
 
     @property
     def duration(self) -> torch.Tensor:
-        return sum(block.duration() for block in self)
+        """Duration of the block in s."""
+        return sum(block.duration for block in self)
 
 
 class EPGSignalModel(SignalModel[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor | None]):
     """EPG signal model."""
 
     def __init__(self, sequence: EPGSequence | Sequence[EPGBlock], n_states: int = 20):
+        """Initialize the EPG signal model.
+
+        Parameters
+        ----------
+        sequence
+            EPG sequence of blocks
+        n_states
+            Number of EPG configuration states. This model uses a fixed number of states for performance reasons. Should be large enough to capture the signal dynamics.
+            More states increase the accuracy of the simulation but also the computational cost.
+        """
+        super().__init__()
         self.n_states = n_states
         if not isinstance(sequence, EPGSequence):
             self.sequence = EPGSequence(sequence)
         else:
             self.sequence = sequence
+        self.sequence.compile()
 
     def forward(
         self, t1: torch.Tensor, t2: torch.Tensor, m0: torch.Tensor, relative_b1: torch.Tensor | None = None
     ) -> tuple[torch.Tensor]:
+        """Simulate the EPG signal.
+
+        Parameters
+        ----------
+        t1
+            T1 relaxation time [s]
+        t2
+            T2 relaxation time [s]
+        m0
+            Steady state magnetization (complex)
+        relative_b1
+            Relative B1 scaling factor (complex)
+
+        Returns
+        -------
+            Simulated EPG signal with the different acquisitions in the first dimension.
+        """
         parameters = Parameters(t1, t2, m0, relative_b1)
-        state = initial_state(parameters.shape, n_sates=self.n_states, device=parameters.device, dtype=parameters.dtype)
+        state = initial_state(
+            parameters.shape, n_states=self.n_states, device=parameters.device, dtype=parameters.dtype
+        )
         _, signals = self.sequence(state, parameters)
         signal = torch.stack(list(signals), dim=0)
         return (signal,)
 
 
 class CardiacFingerprinting(SignalModel[torch.Tensor, torch.Tensor, torch.Tensor]):
-    """Cardiac MR Fingerprinting signal model."""
+    """Cardiac MR Fingerprinting signal model.
+
+    This model simulates a cardiac MR fingerprinting sequence as described in Hamilton, J. I. et al. MR fingerprinting
+    for rapid quantification of myocardial T1 , T2 , and proton spin density. Magn. Reson. Med. 77, 1446-1458 (2017)
+    [http://doi.wiley.com/10.1002/mrm.26668].
+
+    It is a four-fold repetition of
+
+                Block 0                   Block 1                   Block 2                     Block 3
+       R-peak                   R-peak                    R-peak                    R-peak                    R-peak
+    ---|-------------------------|-------------------------|-------------------------|-------------------------|-----
+
+            [INV TI=20ms][ACQ]                     [ACQ]     [T2-prep TE=40ms][ACQ]    [T2-prep TE=80ms][ACQ]
+
+
+    """
 
     def __init__(self, acquisition_times: torch.Tensor, te: float) -> None:
+        """Initialize the Cardiac MR Fingerprinting signal model.
+
+        Parameters
+        ----------
+        acquisition_times
+            Acquisition times in s
+        te
+            Echo time in s
+
+        Returns
+        -------
+            Cardiac MR Fingerprinting signal with the different acquisitions in the first dimension.
+        """
+        super().__init__()
         sequence = EPGSequence()
         max_flip_angles = [12.5, 18.75, 25.0, 25.0, 25.0, 12.5, 18.75, 25.0, 25.0, 25.0, 12.5, 18.75, 25.0, 25.0, 25.0]
         for i in range(len(acquisition_times)):
@@ -428,4 +699,19 @@ class CardiacFingerprinting(SignalModel[torch.Tensor, torch.Tensor, torch.Tensor
         self.model = EPGSignalModel(sequence, n_states=20)
 
     def forward(self, t1: torch.Tensor, t2: torch.Tensor, m0: torch.Tensor) -> tuple[torch.Tensor]:
+        """Simulate the Cardiac MR Fingerprinting signal.
+
+        Parameters
+        ----------
+        t1
+            T1 relaxation time [s]
+        t2
+            T2 relaxation time [s]
+        m0
+            Steady state magnetization (complex)
+
+        Returns
+        -------
+            Simulated Cardiac MR Fingerprinting signal with the different acquisitions in the first dimension.
+        """
         return self.model(t1, t2, m0, None)
---
+++ tests/operators/models/test_epg.py
@@ -0,0 +1,15 @@
+"""Tests for EPG signal models."""
+
+import pytest
+import torch
+from mrpro.operators.models.EPG import CardiacFingerprinting, acquisition
+from tests.operators.models.conftest import SHAPE_VARIATIONS_SIGNAL_MODELS, create_parameter_tensor_tuples
+
+
+def test_cmrf_model():
+    """Test the CMRF model."""
+    acquisition_times = torch.linspace(0, 10, 15)
+    te = 0.05
+    model = CardiacFingerprinting(acquisition_times=acquisition_times, te=te)
+    t1, t2, m0 = create_parameter_tensor_tuples(number_of_tensors=3)
+    signal = model(t1, t2, m0)
