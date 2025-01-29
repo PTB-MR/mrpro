ef9cb100410f3aa9e6a658b8d408fa9ad4940f44
ckolbPTB <christoph.kolbitsch@ptb.de>
2025-01-29 17:32:19 +0100

    allow for 3D multi-frame data

--- src/mrpro/data/IData.py
+++ src/mrpro/data/IData.py
@@ -9,7 +9,6 @@ import torch
 from einops import repeat
 from pydicom import dcmread
 from pydicom.dataset import Dataset
-from pydicom.tag import TagType
 from typing_extensions import Self
 
 from mrpro.data.Data import Data
@@ -25,7 +24,7 @@ def _dcm_pixelarray_to_tensor(dataset: Dataset) -> torch.Tensor:
     their stored on disk representation to their in memory
     representation.     U = m*SV + b where U is in output units, m is
     the rescale slope, SV is the stored value, and b is the rescale
-    intercept. [RES]_
+    intercept [RES]_.
 
     References
     ----------
@@ -93,19 +92,28 @@ class IData(Data):
         filename
             path to DICOM file.
         """
-        dataset = dcmread(filename)
-        idata = repeat(_dcm_pixelarray_to_tensor(dataset), 'y x -> other coils z y x', other=1, coils=1, z=1)
-        header = IHeader.from_dicom_list([dataset])
-        return cls(data=idata, header=header)
+        return cls.from_dicom_files(filenames=(Path(filename),))
 
     @classmethod
     def from_dicom_files(cls, filenames: Sequence[str] | Sequence[Path] | Generator[Path, None, None]) -> Self:
         """Read multiple DICOM files and return IData object.
 
+        DICOM images can be saved as single-frame or multi-frame images [DCMMF]_.
+
+        If the DICOM files are single-frame, we treat each file separately and stack them along the `other` dimension.
+        If the DICOM files are multi-frame and the MRAcquisitionType is '3D' we treat the frame dimension as the `z`
+        dimension. Otherwise, we move the frame dimension to the `other` dimension. Multiple multi-frame DICOM
+        images are stacked along an additional `other` dimension before the frame dimension.
+        Providing the list of files sortey by filename usually leads to a reasonable sorting of the data.
+
         Parameters
         ----------
         filenames
             List of DICOM filenames.
+
+        References
+        ----------
+        .. [DCMMF] https://dicom.nema.org/medical/dicom/2020b/output/chtml/part03/sect_C.7.6.16.html
         """
         # Read in all files
         dataset_list = [dcmread(filename) for filename in filenames]
@@ -114,25 +122,29 @@ class IData(Data):
         if not dataset_list:
             raise ValueError('No dicom files specified')
 
-        # Ensure they all have the same orientation (same (0019, 1015) SlicePosition_PCS tag)
-        def get_unique_slice_positions(slice_pos_tag: TagType = 0x00191015):
-            if not dataset_list[0].get_item(slice_pos_tag):
-                return []
-            else:
-                slice_positions = [dataset_list[0].get_item(slice_pos_tag).value]
-                for ds in dataset_list[1:]:
-                    value = ds.get_item(slice_pos_tag).value
-                    if value not in slice_positions and not isinstance(value, bytes):
-                        slice_positions.append(value)
-                return slice_positions
-
-        if len(dataset_list) > 1 and len(get_unique_slice_positions()) > 1:
-            raise ValueError('Only dicoms with the same orientation can be read in.')
-        # stack required due to mypy: einops rearrange list[tensor]->tensor not recognized
-        idata = torch.stack([_dcm_pixelarray_to_tensor(ds) for ds in dataset_list])
-        idata = repeat(idata, 'other y x -> other coils z y x', coils=1, z=1)
-
         header = IHeader.from_dicom_list(dataset_list)
+
+        # Ensure that data has the same shape and can be stacked
+        if not all(ds.pixel_array.shape == dataset_list[0].pixel_array.shape for ds in dataset_list):
+            raise ValueError('Only dicom files with data of the same shape can be stacked.')
+
+        # Stack required due to mypy: einops rearrange list[tensor]->tensor not recognized
+        idata = torch.stack([_dcm_pixelarray_to_tensor(ds) for ds in dataset_list])
+
+        # Tag NumberofFrames (0028|0008) is used to determine if data is multi-frame and tag MRAcquisitionType
+        # (0018|0023) is used to determine if the data is 3D.
+        number_of_frames = [item.value for item in dataset_list[0].iterall() if item.tag == 0x00280008]
+        mr_acquisition_type = [item.value for item in dataset_list[0].iterall() if item.tag == 0x00180023]
+        if (
+            len(number_of_frames) > 0
+            and float(number_of_frames[0]) > 1
+            and len(mr_acquisition_type) > 0
+            and mr_acquisition_type[0] == '3D'
+        ):
+            idata = repeat(idata, 'other z y x -> other coils z y x', coils=1)
+        else:
+            idata = repeat(idata, 'other y x -> other coils z y x', coils=1, z=1)
+
         return cls(data=idata, header=header)
 
     @classmethod
@@ -153,7 +165,8 @@ class IData(Data):
         if len(file_paths) == 0:
             raise ValueError(f'No dicom files with suffix {suffix} found in {foldername}')
 
-        return cls.from_dicom_files(filenames=file_paths)
+        # Pass on sorted file list as order of dicom files is often the same as the required order
+        return cls.from_dicom_files(filenames=sorted(file_paths))
 
     def __repr__(self):
         """Representation method for IData class."""
