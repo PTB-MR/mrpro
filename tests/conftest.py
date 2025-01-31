d53c4a21b4752a3f402a37e1684cc2f823edfdaf
Felix Zimmermann <fzimmermann89@gmail.com>
2025-01-31 23:13:36 +0100

    partially adress oversamping in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -410,7 +410,7 @@ COMMON_MR_TRAJECTORIES = pytest.mark.parametrize(
             'zero',  # type_k2
         ),
         (  # (10) radial phase encoding (RPE), 3 coils, with oversampling in both FFT and non-uniform directions
-            (2, 3, 12, 14, 16),  # im_shape
+            (2, 3, 6, 8, 10),  # im_shape
             (2, 3, 8, 10, 12),  # k_shape
             (2, 1, 1, 12),  # nkx
             (2, 8, 10, 1),  # nky
@@ -423,7 +423,7 @@ COMMON_MR_TRAJECTORIES = pytest.mark.parametrize(
             'non-uniform',  # type_k2
         ),
         (  # (11) radial phase encoding (RPE), 2 coils with non-Cartesian sampling along readout
-            (2, 2, 12, 14, 16),  # im_shape
+            (2, 2, 6, 8, 10),  # im_shape
             (2, 2, 8, 10, 12),  # k_shape
             (2, 1, 1, 12),  # nkx
             (2, 8, 10, 1),  # nky
@@ -436,10 +436,10 @@ COMMON_MR_TRAJECTORIES = pytest.mark.parametrize(
             'non-uniform',  # type_k2
         ),
         (  # (12) stack of stars, 3 other, 2 coil, oversampling in both FFT and non-uniform directions
-            (3, 2, 16, 16, 24),  # im_shape
-            (3, 2, 12, 6, 10),  # k_shape
-            (3, 1, 6, 10),  # nkx
-            (3, 1, 6, 10),  # nky
+            (3, 2, 10, 8, 8),  # im_shape
+            (3, 2, 12, 8, 10),  # k_shape
+            (3, 1, 8, 10),  # nkx
+            (3, 1, 8, 10),  # nky
             (3, 12, 1, 1),  # nkz
             'non-uniform',  # type_kx
             'non-uniform',  # type_ky
@@ -449,7 +449,7 @@ COMMON_MR_TRAJECTORIES = pytest.mark.parametrize(
             'uniform',  # type_k2
         ),
         (  # (13) stack of stars, (2,2,4) other, 3 coil, trajectory is different along second other dimension
-            (2, 2, 4, 3, 16, 18, 20),  # im_shape
+            (2, 2, 4, 3, 8, 6, 6),  # im_shape
             (2, 2, 4, 3, 12, 6, 10),  # k_shape
             (2, 1, 1, 6, 10),  # nkx
             (2, 1, 1, 6, 10),  # nky
