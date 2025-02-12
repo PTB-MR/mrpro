54b2c3fa5b97e8824c7b42f35632941b907ce111
koflera <andreas_kofler@gmx.net>
2025-02-12 20:53:01 +0100

    adapt PGD example and notebook

--- examples/scripts/pgd_wavelet_reconstruction.py
+++ examples/scripts/pgd_wavelet_reconstruction.py
@@ -1,9 +1,11 @@
 # %% [markdown]
 # # Proximal gradient descent (FISTA) reconstruction of 2D golden angle radial data
+
+# %% [markdown]
+# ### Image reconstruction
 # Here, we use a proximal gradient descent algorithm to reconstruct an image
 # from 2D radial k-space data with wavelet regularization.
-# In particular, we use the accelerated version known as FISTA (Fast Iterative Shrinkage-Thresholding Algorithm)
-# [Beck \& Teboulle, SIAM Journal on Imaging Sciences 2009].
+# In particular, we use the accelerated version known as FISTA (Fast Iterative Shrinkage-Thresholding Algorithm).
 #
 # Let $y$ denote the k-space data of the image $x_{\mathrm{true}}$ sampled with an acquisition model $A$
 # (Fourier transform, coil sensitivity maps, etc.), i.e the forward problem is given as
@@ -11,28 +13,30 @@
 # $ y = Ax_{\mathrm{true}} + n, $
 #
 # where $n$ describes complex Gaussian noise.
-# An approximation of $x_{\mathrm{true}}$ can be carried out exploiting
-# the fact that wavelets provide a sparse representation of images
-# by minimizing
-# the following functional $\mathcal{F}$:
+#
+# As regularization method, here we employ wavelets, which are known to be suitable sparsifying transforms.
+# We consider the following functional $\mathcal{F}$:
 #
 # $ \mathcal{F}(x) = \frac{1}{2}||Ax - y||_2^2 + \lambda \| W x \|_1, \quad \quad \quad (1)$
 #
-# where $W$ is the discretized wavelet operator that maps image domain into wavelet domain,
-# and $\lambda >0$ appropriate weight.
+# where $W$ is the discretized wavelet operator that maps the image domain to the wavelet domain,
+# and $\lambda >0$ is an appropriate regularization weight.
 #
 # The minimization of the functional $\mathcal{F}$ is non-trivial due to
 # the non-differentiable $\ell_1$-norm and the presence of the wavelet operator.
-# We proceed first by reformulating $(1)$ to solve the minimization problem in the wavelet domain,
-# exploiting the orthonormality of $W$.
-# We set a new variable $\tilde{x} = Wx$. Thus,
-# the minimization problem becomes
+# A possible algorithm to approach the solution of (1) would be the primal dual hybrid gradient (PDHG)
+# algorithm [[Chambolle \& Pock, JMIV 2011](https://doi.org/10.1007%2Fs10851-010-0251-1)].
+# However, exploiting the orthonormality of $W$, we can reformulate $(1)$ to solve the minimization problem
+# in the wavelet domain and use a simpler algorithm. By defining the new variable $\tilde{x} = Wx$
+# (and thus, due to the orthonormality of $W$, $W^H \tilde{x} = x$), the minimization problem becomes
 #
-# $ \min_{\tilde{x}} \frac{1}{2}||AW^H\tilde{x} - y||_2^2 + \lambda \| \tilde{x} \|_1.  \quad \quad \quad (2)$
+# $ \min_{\tilde{x}} \frac{1}{2}||\tilde{A}\tilde{x} - y||_2^2 + \lambda \| \tilde{x} \|_1,  \quad \quad \quad (2)$
 #
-# A suitable algorithm to solve $(2)$ is the
-# FISTA-algorithm, consisting in an accelerated proximal gradient descent algorithm.
-# It solves problems of the form
+# where $\tilde{A}:=A W^H$. A suitable algorithm to solve $(2)$ is the FISTA-algorithm
+# [[Beck \& Teboulle, SIAM Journal on Imaging Sciences 2009](https://epubs.siam.org/doi/10.1137/080716542)],
+# which consists of an accelerated proximal gradient descent algorithm.
+#
+# In general, FISTA can be used to solve problems of the form
 #
 # $ \min_x f(x) + g(x)  \quad \quad \quad (3)$
 #
@@ -47,50 +51,33 @@
 # an appropriate stepsize, ideally $\sigma=\frac{1}{L}$, with $L$ being
 # the Lipschitz constant of $\nabla f$.
 #
-# In FISTA, there is an
-# additional step to accelerate the convergence. The following variable $y_{k+1}$
-# is added, consisting of a linear interpolation
-# of the previous two steps $x_{k}$ and $x_{k-1}$. So, for $t_1=1, y_1=x_0$:
+# Additionally, FISTA has an additional step to accelerate the convergence. The following variable $z_{k+1}$
+# is included, consisting of a linear interpolation
+# of the previous two steps $x_{k}$ and $x_{k-1}$. So, for $t_1=1, z_1=x_0$:
 #
-# $x_{k} = \mathrm{prox}_{\sigma g}(y_{k} - \sigma \nabla f({y}_{k}))$
+# $x_{k} = \mathrm{prox}_{\sigma g}(z_{k} - \sigma \nabla f({z}_{k}))$
 #
 # $t_{k+1} = \frac{1 + \sqrt{1 + 4t_k^2}}{2}$
 #
-# $y_{k+1} = x_{k} + \frac{t_k - 1}{t_{k+1}}(x_{k} - x_{k-1}).$
+# $z_{k+1} = x_{k} + \frac{t_k - 1}{t_{k+1}}(x_{k} - x_{k-1}).$
 #
 # As the Lipschitz constant $L$ is in general not known, and
-# the interval of the stepsize $\sigma\in ( 0, \frac{1}{|| W A^H A W^H||} )$ is crucial for the convergence,
-# a backtracking step can
-# be performed to update the stepsize $\sigma$ at every iteration. To do so,
-# $\sigma$ is iteratively reduced until reaching a stepsize that is the biggest one
-# for which the quadratic approximation of $f$ at $y_{k}$
+# the interval of the stepsize $\sigma\in ( 0, \frac{1}{|| \tilde{A}^H \tilde{A}||} )$ is crucial for the convergence,
+# a backtracking step can  be performed to update the stepsize $\sigma$ at every iteration. To do so,
+# $\sigma$ is iteratively reduced until reaching a stepsize that is the largest
+# for which the quadratic approximation of $f$ at $z_{k}$
 # is an upper bound for $f(x_{k})$.
-#
-# Functions $f$ ang $g$ from $(3)$ can be identified as
-#
-# $f(x) = \frac{1}{2}\|AW^Hx  - y\|_2^2,$
-#
-# $g(x) = \lambda \| x\|_1.$
-#
-# After running the algorithm for $N$ iterations, the optimal solution $x_{N}$
-# is in wavelet domain and needs to be mapped back to image domain.
-# Thus we apply the adjoint of the wavelet transform and obtain solution $x_{\text{opt}}$ as
-#
-# $x_{\text{opt}} := W^H x_{N}$.
-#
-# In the following, we load 2D radial MR data and set up problem (2) to use
-# FISTA to reconstruct the data.
 
 
-# ## Read-in the raw data
-# We read the raw k-space data and the trajectory from the ISMRMRD file
-# (see <project:comparison_trajectory_calculators.ipynb> for more information on the trajectory calculation).
-# Our example data contains three datasets:
+# ### Load data
+# Our example data contains three scans acquired with a 2D golden angle radial trajectory and
+# varying number of spokes:
+#
 # - ``radial2D_402spokes_golden_angle_with_traj.h5`` with 402 spokes
 # - ``radial2D_96spokes_golden_angle_with_traj.h5`` with 96 spokes
 # - ``radial2D_24spokes_golden_angle_with_traj.h5`` with 24 spokes
 #
-# We use the 24 spokes dataset for the reconstruction.
+# We will use the 402 spokes as a reference and try to reconstruct the image from the 24 spokes data.
 
 # %% tags=["hide-cell"] mystnb={"code_prompt_show": "Show download details"}
 # ### Download raw data from Zenodo
@@ -109,73 +96,101 @@ zenodo_get.zenodo_get([dataset, '-r', 5, '-o', data_folder])  # r: retries
 # Load in the data from the ISMRMRD file
 import mrpro
 
-trajectory_calculator = mrpro.data.traj_calculators.KTrajectoryIsmrmrd()
-kdata = mrpro.data.KData.from_file(data_folder / 'radial2D_24spokes_golden_angle_with_traj.h5', trajectory_calculator)
-
+# We have embedded the trajectory information in the ISMRMRD files.
+kdata_402spokes = mrpro.data.KData.from_file(
+    data_folder / 'radial2D_402spokes_golden_angle_with_traj.h5', mrpro.data.traj_calculators.KTrajectoryIsmrmrd()
+)
+kdata_24spokes = mrpro.data.KData.from_file(
+    data_folder / 'radial2D_24spokes_golden_angle_with_traj.h5', mrpro.data.traj_calculators.KTrajectoryIsmrmrd()
+)
 
 
 # %% [markdown]
-# ## Set up the operator $A$
-# Estimate coil sensitivity maps and density compensation function. Also run a direct (adjoint)
-# reconstruction and iterative SENSE as methods of comparison.
-#
-# Define wavelet operator $W$ and set $A = F C W^H $, where $F$ is the Fourier
+# ### Comparison reconstructions
+# Before running the wavelets-based reconstruction, we first run a direct (adjoint) reconstruction
+# using `~mrpro.algorithms.reconstruction.DirectReconstruction` (see <project:direct_reconstruction.ipynb>)
+# of both the 24 spokes and 402 spokes data to have a reference for comparison.
+
+# %%
+direct_reconstruction_402 = mrpro.algorithms.reconstruction.DirectReconstruction(kdata_402spokes)
+direct_reconstruction_24 = mrpro.algorithms.reconstruction.DirectReconstruction(kdata_24spokes)
+img_direct_402 = direct_reconstruction_402(kdata_402spokes)
+img_direct_24 = direct_reconstruction_24(kdata_24spokes)
+
+# %% [markdown]
+# We also run an iterative SENSE reconstruction (see <project:iterative_sense_reconstruction_radial2D.ipynb>) with early
+# stopping of the 24 spokes data. We use it as a comparison and as an initial guess for FISTA.
+
+# %%
+sense_reconstruction = mrpro.algorithms.reconstruction.IterativeSENSEReconstruction(
+    kdata_24spokes,
+    n_iterations=8,
+    csm=direct_reconstruction_24.csm,
+    dcf=direct_reconstruction_24.dcf,
+)
+img_sense_24 = sense_reconstruction(kdata_24spokes)
+
+
+# %% [markdown]
+# ### Set up the operator $A$
+# Define the wavelet operator $W$ and set $\tilde{A} = A W^H = F C W^H $, where $F$ is the Fourier
 # operator, $C$ the coil sensitivity maps and $W^H$ the adjoint wavelet operator.
 
 # %%
-# Set up direct reconstruction class. 
-direct_reconstruction = mrpro.algorithms.reconstruction.DirectReconstruction(kdata)
-img_direct = direct_reconstruction(kdata)
-
-# run iterative SENSE to compare the solution
-iterative_sense_reconstruction = mrpro.algorithms.reconstruction.IterativeSENSEReconstruction(
-    fourier_op=direct_reconstruction.fourier_op
-    n_iterations=8,
-    csm=direct_reconstruction.csm,
-    dcf=direct_reconstruction.dcf,
-)
-
-img_iterative_sense = iterative_sense_reconstruction(kdata)
-
-# Define Fourier operator and CSM operator re-using the ones already constructed in the
-# direct reconstruction class
-fourier_operator = direct_reconstruction.fourier_op
-assert direct_reconstruction.csm is not None
-csm_operator = direct_reconstruction.csm.as_operator()
+fourier_operator = direct_reconstruction_24.fourier_op
+assert direct_reconstruction_24.csm is not None
+csm_operator = direct_reconstruction_24.csm.as_operator()
 
 # Define the wavelet operator
-wavelet_operator = mrpro.operators.WaveletOp(domain_shape=img_direct.data.shape[-2:], dim=(-2, -1))
+wavelet_operator = mrpro.operators.WaveletOp(
+    domain_shape=img_direct_24.data.shape[-2:], dim=(-2, -1), wavelet_name='sym8', level=3
+)
 
 # Create the full acquisition operator A with wavelet operator
 acquisition_operator = fourier_operator @ csm_operator @ wavelet_operator.H
 
 # %% [markdown]
-# ### Apply FISTA
+# ### Set up the problem
+# In order to apply FISTA to solve $(2)$, we identify $f$ ang $g$ from $(3)$ as
+#
+# $f(\tilde{x}) = \frac{1}{2}\|\tilde{A}\tilde{x}  - y\|_2^2,$
+#
+# $g(\tilde{x}) = \lambda \| \tilde{x}\|_1.$
+#
+# From this, we see that FISTA is a good choice to solve $(2)$,
+# as $\mathrm{prox}_g$ is given by simple soft-thresholding.
+#
+# After having run the algorithm for $N$ iterations, the obtained solution $\tilde{x}_{N}$
+# is in the wavelet domain and needs to be mapped back to image domain.
+# Thus we apply the adjoint of the wavelet transform and obtain solution $x_{\text{opt}}$ as
+#
+# $x_{\text{opt}} := W^H x_{N}$.
 
-# In the following, we first identify the functionals $f$ and $g$
-# and then run FISTA.
 
 # %%
 # Regularization parameter for the $\ell_1$-norm
-regularization_parameter = 1e-6
+regularization_parameter = 3e-2
 
 # Set up the problem by using the previously described identification
-l2 = 0.5 * L2NormSquared(target=kdata.data, divide_by_n=False)
-l1 = L1Norm(divide_by_n=False)
+l2 = 0.5 * mrpro.operators.functionals.L2NormSquared(target=kdata_24spokes.data, divide_by_n=True)
+l1 = mrpro.operators.functionals.L1NormViewAsReal(divide_by_n=True)
 
 f = l2 @ acquisition_operator
 g = regularization_parameter * l1
 
 # initialize FISTA with adjoint solution
-initial_values = wavelet_operator(img_direct.data)
+initial_values = wavelet_operator(img_direct_24.data)
 
 # %% [markdown]
 # ### Run FISTA for a certain number of iterations
-
-max_iterations = 25
+# %%
+max_iterations = 128
 
 # callback function to track the value of the objective functional f(x) + g(x)
 # and stepsize update
+from mrpro.algorithms.optimizers.pgd import PGDStatus
+
+
 def callback(optimizer_status: PGDStatus) -> None:
     """Print the value of the objective functional every 10th iteration."""
     iteration = optimizer_status['iteration_number']
@@ -185,28 +200,39 @@ def callback(optimizer_status: PGDStatus) -> None:
             f'{iteration}: {optimizer_status["objective"](*solution).item()}, stepsize: {optimizer_status["stepsize"]}'
         )
 
-(img_wave_pgd,) = pgd(
+
+# compute the stepsize based on the operator norm of the acquisition operator
+import torch
+
+op_norm = acquisition_operator.operator_norm(
+    initial_value=torch.randn_like(initial_values[0]), dim=(-2, -1), max_iterations=36
+).item()
+
+# define step size with a security factor to ensure to
+# have stepsize \in (0, 1/||acquisition_operator||_2)
+stepsize = 0.98 * (1 / op_norm)
+
+(img_wave_pgd_24,) = mrpro.algorithms.optimizers.pgd(
     f=f,
     g=g,
     initial_value=initial_values,
-    stepsize=1,
+    stepsize=stepsize,
     max_iterations=max_iterations,
-    backtrack_factor=0.85,
+    backtrack_factor=1.0,
     callback=callback,
 )
 
 # map the solution back to image domain
-(img_pgd,) = wavelet_operator.H(img_wave_pgd)
+(img_pgd_24,) = wavelet_operator.H(img_wave_pgd_24)
 
 
 # %% [markdown]
-# #ä Result
-# We compare the result of the wavelet regularized reconstruction with the iterative SENSE and direct reconstruction
-# result.
+# ## Results
+# We compare the result of the wavelet regularized reconstruction with the
+# iterative SENSE and direct reconstruction result.
 
 # %% tags=["hide-cell"] mystnb={"code_prompt_show": "Show plotting details"}
 import matplotlib.pyplot as plt
-import torch
 
 
 def show_images(*images: torch.Tensor, titles: list[str] | None = None) -> None:
@@ -220,5 +246,24 @@ def show_images(*images: torch.Tensor, titles: list[str] | None = None) -> None:
             axes[0][i].set_title(titles[i])
     plt.show()
 
+
 # %%
-show_images(img_direct.rss[0,0], img_iterative_sense.rss()[0,0], img_pgd.abs()[0, 0],titles=["Direct Reconstruction", "Iterative SENSE", "Wavelet (PGD)"]
+# see the collapsed cell above for the implementation of show_images
+show_images(
+    img_direct_402.rss().squeeze(),
+    img_direct_24.rss().squeeze(),
+    img_sense_24.rss().squeeze(),
+    img_pgd_24.abs().squeeze(),
+    titles=['402 spokes', '24 spokes (Direct)', '24 spokes (SENSE)', '24 spokes (PGD)'],
+)
+
+
+# %% [markdown]
+# Congratulations! We have successfully reconstructed an image from 24 spokes using wavelets.
+#
+# ### Next steps
+# Not happy with the results? Play around with the regularization weight, the number of iterations, and the
+# number of levels in the wavelet decomposition to see how they affect the final image.
+# Still not happy? Maybe worth giving a try to total variation (TV)-minimization as an alternative
+# regularization method (see <project:tv_minimization_reconstruction_pdhg.ipynb>).
+# You can also try to use the 96 spokes data to see how the reconstruction quality improves with more spokes.
