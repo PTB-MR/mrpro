4b30d142e47c1a8c213d18ba6421d05a8c2310b6
Felix Zimmermann <fzimmermann89@gmail.com>
2025-02-17 16:25:07 +0100

    update test

--- tests/operators/test_dictionary_matching_op.py
+++ tests/operators/test_dictionary_matching_op.py
@@ -4,41 +4,27 @@ import pytest
 import torch
 from mrpro.operators import DictionaryMatchOp
 from mrpro.operators.models import InversionRecovery
-from tests.operators.models.conftest import create_parameter_tensor_tuples
-from mrpro.operators import DictionaryMatchOp
+
+from tests import RandomGenerator
 
 
-
-
-def test_real_matching():
+@pytest.mark.parametrize('dtype', [torch.float32, torch.complex64], ids=['float32', 'complex64'])
+@pytest.mark.parametrize('shape', [(5, 4, 3)])
+@pytest.mark.parametrize('scaling_argument', [None, 0], ids=['dont_predict_scale', 'predict_scale'])
+def test_dictionaryop_matching(shape: tuple[int], dtype: torch.dtype, scaling_argument: int | None):
     """Test dictionary matching with real-valued signals."""
-    model = InversionRecovery(20)
-    m0, t1 = create_parameter_tensor_tuples()
+    rng = RandomGenerator(2)
+    model = InversionRecovery(rng.float32_tensor(5))
+    m0 = rng.rand_tensor(shape, dtype=dtype, low=0.2, high=1.0)
+    t1 = rng.rand_tensor(shape, dtype=dtype.to_real(), low=0.1, high=1.0)
     (y,) = model(m0, t1)
-    dict_match_op = DictionaryMatchOp(model)
-    dictionary = dict_match_op.append(m0, y)
-    t1_matched = dict_match_op.forward(y)[1]
-    t1_matched = t1_matched.reshape(y.shape[1:])
 
+    operator = DictionaryMatchOp(model, scaling_argument=scaling_argument)
+    operator.append(m0[:1], t1[:1])
+    operator.append(m0[1:], t1[1:])
 
-    (y_matched,) = model(m0, t1_matched)
+    m0_matched, t1_matched = operator(y)
 
-    assert (y_matched-y).abs().square().mean() < 1e-5
-
-
-def test_complex_matching():
-    """Test dictionary matching with complex-valued signals."""
-    model = InversionRecovery(20)
-    m0, t1 = create_parameter_tensor_tuples()
-
-    (y,) = model(m0, t1)
-    y = torch.complex(y, torch.zeros_like(y))
-    dict_match_op = DictionaryMatchOp(model)
-    dictionary = dict_match_op.append(m0, y)
-    t1_matched = dict_match_op.forward(y)[1]
-    t1_matched = t1_matched.reshape(y.shape[1:])
-
-
-    (y_matched,) = model(m0, t1_matched)
-
-    assert (y_matched-y).abs().square().mean() < 1e-5
+    if scaling_argument is not None:
+        torch.testing.assert_close(m0_matched, m0, atol=1e-3, rtol=0.0)
+    torch.testing.assert_close(t1_matched, t1, atol=1e-4, rtol=0.0)
