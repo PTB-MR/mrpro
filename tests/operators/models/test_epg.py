e5a69167217945b9ae223b651d3a992298e27edd
Felix Zimmermann <fzimmermann89@gmail.com>
2025-03-02 01:34:23 +0100

    change test

--- tests/operators/models/test_epg.py
+++ tests/operators/models/test_epg.py
@@ -1,14 +1,19 @@
 """Tests for EPG signal models."""
 
+from collections.abc import Sequence
+
 import torch
 from mrpro.operators.models import CardiacFingerprinting
-from tests.operators.models.conftest import create_parameter_tensor_tuples
+from tests import RandomGenerator
 
 
-def test_cmrf_model():
+def test_cmrf_model(parameter_shape: Sequence[int] = (2, 5, 10, 10, 10)) -> None:
     """Test the CMRF model."""
     acquisition_times = torch.linspace(0, 10, 705)
     model = CardiacFingerprinting(acquisition_times=acquisition_times, echo_time=0.05)
-    t1, t2, m0 = create_parameter_tensor_tuples(number_of_tensors=3)
+    rng = RandomGenerator(0)
+    t1 = rng.float32_tensor(parameter_shape, low=1e-5, high=5)
+    t2 = rng.float32_tensor(parameter_shape, low=1e-5, high=0.5)
+    m0 = rng.complex64_tensor(parameter_shape)
     signal = model(t1, t2, m0)
     assert signal
