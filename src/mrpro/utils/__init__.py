38243170b89f2d55f544c5a36de25fd851726727
Felix Zimmermann <fzimmermann89@gmail.com>
2025-03-02 15:36:01 +0100

    Indexing helper
    ghstack-source-id: 36f63b927c982ad217eaa6cd44fe0ae38b6b72ed
    ghstack-comment-id: 2544442894
    Pull Request resolved: https://github.com/PTB-MR/mrpro/pull/580

--- src/mrpro/utils/__init__.py
+++ src/mrpro/utils/__init__.py
@@ -5,12 +5,16 @@ from mrpro.utils import typing
 from mrpro.utils import unit_conversion
 from mrpro.utils.fill_range import fill_range_
 from mrpro.utils.smap import smap
+from mrpro.utils.indexing import Indexer
 from mrpro.utils.reduce_repeat import reduce_repeat
 from mrpro.utils.zero_pad_or_crop import zero_pad_or_crop
 from mrpro.utils.split_idx import split_idx
 from mrpro.utils.reshape import broadcast_right, unsqueeze_left, unsqueeze_right, reduce_view, reshape_broadcasted, ravel_multi_index, unsqueeze_tensors_left, unsqueeze_tensors_right, unsqueeze_at, unsqueeze_tensors_at
 from mrpro.utils.TensorAttributeMixin import TensorAttributeMixin
+from mrpro.utils.zero_pad_or_crop import zero_pad_or_crop
+
 __all__ = [
+    "Indexer",
     "TensorAttributeMixin",
     "broadcast_right",
     "fill_range_",
