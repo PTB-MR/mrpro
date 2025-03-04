32be9b8692220f4e7d721fd116527c1a4971d440
lrlunin <leonid.lunin@ptb.de>
2025-03-04 17:00:20 +0100

    use github recommended installation of runner

--- docker/install_dependencies.sh
+++ docker/install_dependencies.sh
@@ -1,5 +1,6 @@
-# pre-install cpu-version of torch to avoid installation of cuda-version via dependencies
-python -m pip install --no-cache-dir torch --index-url https://download.pytorch.org/whl/cpu
+# pre-install cpu-version of torch by default
+# either use the 1st argument as specifier (cu118, cu124 or cu126)
+python -m pip install --no-cache-dir torch --index-url https://download.pytorch.org/whl/${1:-cpu}
 
 #parse dependencies
 python -m pip install --no-cache-dir toml
---
+++ docker/runner_entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+registration_url="https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPOSITORY}/actions/runners/registration-token"
+echo "Requesting registration URL at '${registration_url}'"
+
+payload=$(curl -sX POST -H "Authorization: token ${GITHUB_PERSONAL_TOKEN}" ${registration_url})
+export RUNNER_TOKEN=$(echo $payload | jq .token --raw-output)
+
+./config.sh \
+    --name $(hostname) \
+    --token ${RUNNER_TOKEN} \
+    -- labels my-runner \
+    --url https://github.com/${GITHUB_OWNER}/${GITHUB_REPOSITORY} \
+    --work "/work" \
+    --unattended \
+    --replace
+
+remove() {
+    ./config.sh remove --unattended --token "${RUNNER_TOKEN}"
+}
+
+trap 'remove; exit 130' INT
+trap 'remove; exit 143' TERM
+
+./run.sh "$*" &
+
+wait $!
