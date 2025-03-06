15060bfe60583e07bbae7545ec57e7d22f897db0
Christoph Kolbitsch <christoph.kolbitsch@ptb.de>
2025-03-06 10:56:11 +0100

    Add gradient and autodiff checks for linear operators (#536)
    Co-authored-by: Felix F Zimmermann <fzimmermann89@gmail.com>

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,10 @@
 from ._RandomGenerator import RandomGenerator
-from .helper import relative_image_difference, dotproduct_adjointness_test, operator_isometry_test, linear_operator_unitary_test, autodiff_test
+from .helper import (
+    relative_image_difference,
+    dotproduct_adjointness_test,
+    operator_isometry_test,
+    linear_operator_unitary_test,
+    autodiff_test,
+    gradient_of_linear_operator_test,
+    forward_mode_autodiff_of_linear_operator_test
+)
--- tests/operators/test_einsum_op.py
+++ tests/operators/test_einsum_op.py
@@ -1,14 +1,33 @@
 """Tests for Einsum Operator."""
 
+from collections.abc import Sequence
+
 import pytest
 import torch
 from mrpro.operators.EinsumOp import EinsumOp
 
-from tests import RandomGenerator, dotproduct_adjointness_test
+from tests import (
+    RandomGenerator,
+    dotproduct_adjointness_test,
+    forward_mode_autodiff_of_linear_operator_test,
+    gradient_of_linear_operator_test,
+)
 
 
-@pytest.mark.parametrize('dtype', ['float32', 'complex128'])
-@pytest.mark.parametrize(
+def create_einsum_op_and_range_domain(
+    tensor_shape: Sequence[int], input_shape: Sequence[int], rule: str, output_shape: Sequence[int], dtype: str
+) -> tuple[EinsumOp, torch.Tensor, torch.Tensor]:
+    """Create an Einsum operator and an element from range and domain."""
+    generator = RandomGenerator(seed=0)
+    generate_tensor = getattr(generator, f'{dtype}_tensor')
+    tensor = generate_tensor(size=tensor_shape)
+    u = generate_tensor(size=input_shape)
+    v = generate_tensor(size=output_shape)
+    einsum_op = EinsumOp(tensor, rule)
+    return einsum_op, u, v
+
+
+EINSUM_PARAMETERS = pytest.mark.parametrize(
     ('tensor_shape', 'input_shape', 'rule', 'output_shape'),
     [
         ((3, 3), (3), 'i j,j->i', (3)),  # matrix vector product
@@ -18,15 +37,39 @@ from tests import RandomGenerator, dotproduct_adjointness_test
         ((3, 5, 4, 2), (3, 2, 5), 'l ... i j, l j k -> k i l', (5, 4, 3)),  # general tensor contraction
     ],
 )
-def test_einsum_op(tensor_shape, input_shape, rule, output_shape, dtype) -> None:
+
+
+@pytest.mark.parametrize('dtype', ['float32', 'complex128'])
+@EINSUM_PARAMETERS
+def test_einsum_op(
+    tensor_shape: Sequence[int], input_shape: Sequence[int], rule: str, output_shape: Sequence[int], dtype: str
+) -> None:
     """Test adjointness and shape."""
-    generator = RandomGenerator(seed=0)
-    generate_tensor = getattr(generator, f'{dtype}_tensor')
-    tensor = generate_tensor(size=tensor_shape)
-    u = generate_tensor(size=input_shape)
-    v = generate_tensor(size=output_shape)
-    operator = EinsumOp(tensor, rule)
-    dotproduct_adjointness_test(operator, u, v)
+    dotproduct_adjointness_test(
+        *create_einsum_op_and_range_domain(tensor_shape, input_shape, rule, output_shape, dtype)
+    )
+
+
+@pytest.mark.parametrize('dtype', ['float32', 'complex128'])
+@EINSUM_PARAMETERS
+def test_einsum_op_grad(
+    tensor_shape: Sequence[int], input_shape: Sequence[int], rule: str, output_shape: Sequence[int], dtype: str
+) -> None:
+    """Test the gradient of the einsum operator."""
+    gradient_of_linear_operator_test(
+        *create_einsum_op_and_range_domain(tensor_shape, input_shape, rule, output_shape, dtype)
+    )
+
+
+@pytest.mark.parametrize('dtype', ['float32', 'complex128'])
+@EINSUM_PARAMETERS
+def test_einsum_op_forward_mode_autodiff(
+    tensor_shape: Sequence[int], input_shape: Sequence[int], rule: str, output_shape: Sequence[int], dtype: str
+) -> None:
+    """Test forward-mode autodiff of the einsum operator."""
+    forward_mode_autodiff_of_linear_operator_test(
+        *create_einsum_op_and_range_domain(tensor_shape, input_shape, rule, output_shape, dtype)
+    )
 
 
 @pytest.mark.parametrize(
@@ -39,7 +82,7 @@ def test_einsum_op(tensor_shape, input_shape, rule, output_shape, dtype) -> None
         '',  # empty string
     ],
 )
-def test_einsum_op_invalid(rule) -> None:
+def test_einsum_op_invalid(rule: str) -> None:
     """Test with different invalid rules."""
     with pytest.raises(ValueError, match='pattern should match'):
         EinsumOp(torch.tensor([]), rule)
