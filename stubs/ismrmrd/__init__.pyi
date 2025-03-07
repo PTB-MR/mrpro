4a909b6466209b16244f5e437a8ef542be58f463
Felix Zimmermann <fzimmermann89@gmail.com>
2025-03-07 21:19:04 +0100

    Update
    [ghstack-poisoned]

---
+++ stubs/ismrmrd/__init__.pyi
@@ -0,0 +1,8 @@
+from .constants import *
+from .acquisition import *
+from .image import *
+from .waveform import *
+from . import xsd as xsd
+from .file import File as File
+from .hdf5 import Dataset as Dataset
+from .meta import Meta as Meta
