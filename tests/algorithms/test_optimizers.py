133a11e9ffe057fc7aac0368eca5d10089ad775d
Stefan Martin <67423672+Stef-Martin@users.noreply.github.com>
2025-03-07 11:06:05 +0100

    Change iteration argument name in optimizers (#687)
    Co-authored-by: Felix F Zimmermann <fzimmermann89@gmail.com>

--- tests/algorithms/test_optimizers.py
+++ tests/algorithms/test_optimizers.py
@@ -10,7 +10,10 @@ from tests.operators import Rosenbrock
 @pytest.mark.parametrize('enforce_bounds_on_x1', [True, False])
 @pytest.mark.parametrize(
     ('optimizer', 'optimizer_kwargs'),
-    [(adam, {'lr': 0.02, 'max_iter': 2000, 'betas': (0.8, 0.999)}), (lbfgs, {'lr': 1.0, 'max_iter': 20})],
+    [
+        (adam, {'learning_rate': 0.02, 'n_iterations': 2000, 'betas': (0.8, 0.999)}),
+        (lbfgs, {'learning_rate': 1.0, 'max_iterations': 20}),
+    ],
 )
 def test_optimizers_rosenbrock(optimizer, enforce_bounds_on_x1, optimizer_kwargs) -> None:
     # use Rosenbrock function as test case with 2D test data
@@ -66,9 +69,6 @@ def test_callback_optimizers(optimizer) -> None:
     parameter2 = torch.tensor([3.14], requires_grad=True)
     parameters = [parameter1, parameter2]
 
-    # maximum number of iterations
-    max_iter = 10
-
     # callback function; if the function is called during the iterations, the
     # test is successful
     def callback(optimizer_status: OptimizerStatus) -> None:
@@ -76,7 +76,7 @@ def test_callback_optimizers(optimizer) -> None:
         assert True
 
     # run optimizer
-    _ = optimizer(rosen_brock, parameters, max_iter=max_iter, callback=callback)
+    _ = optimizer(rosen_brock, parameters, 10, callback=callback)
 
 
 @pytest.mark.parametrize('optimizer', [adam, lbfgs])
@@ -97,6 +97,6 @@ def test_callback_early_stop(optimizer) -> None:
     parameter1 = torch.tensor([a / 3.14], requires_grad=True)
     parameter2 = torch.tensor([3.14], requires_grad=True)
     parameters = [parameter1, parameter2]
-    _ = optimizer(rosen_brock, parameters, max_iter=100, callback=callback)
+    _ = optimizer(rosen_brock, parameters, 100, callback=callback)
 
     assert callback_check == 1
