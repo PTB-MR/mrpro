846014047d89589e07ba9d22bc0faef11c264cc7
Felix Zimmermann <fzimmermann89@gmail.com>
2025-03-15 00:53:02 +0100

    Update
    [ghstack-poisoned]

--- src/mrpro/data/AcqInfo.py
+++ src/mrpro/data/AcqInfo.py
@@ -9,8 +9,8 @@ import numpy as np
 import torch
 from typing_extensions import Self
 
-from mrpro.data.Dataclass import Dataclass
 from mrpro.data.CheckDataMixin import Annotation, string_to_size
+from mrpro.data.Dataclass import Dataclass
 from mrpro.data.Rotation import Rotation
 from mrpro.data.SpatialDimension import SpatialDimension
 from mrpro.utils.reshape import unsqueeze_at, unsqueeze_right
