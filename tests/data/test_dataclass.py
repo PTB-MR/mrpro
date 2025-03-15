38a43b9b4c5a0084504a2783c9c293290cda6a3c
Felix Zimmermann <fzimmermann89@gmail.com>
2025-03-15 01:05:33 +0100

    Update
    [ghstack-poisoned]

--- tests/data/test_dataclass.py
+++ tests/data/test_dataclass.py
@@ -4,8 +4,10 @@ from dataclasses import field
 
 import pytest
 import torch
-from mrpro.data import Dataclass, Rotation
+from mrpro.data import Dataclass, Rotation, SpatialDimension
 from typing_extensions import Any
+
+from tests import RandomGenerator
 
 
 class SharedModule(torch.nn.Module):
@@ -266,3 +268,36 @@ def test_dataclass_getitem() -> None:
     data = B()
     data2 = data[0]
     print(data2.shape)
+
+
+class Dummy(Dataclass):
+    a: torch.Tensor
+    b: SpatialDimension
+    c: Rotation
+
+    def __post_init__(self) -> None:
+        self.a.ravel()[0] = 1.0
+
+
+def test_dataclass_reducerepeat() -> None:
+    """Test ReduceRepeatMixin."""
+    rng = RandomGenerator(10)
+
+    a = rng.float32_tensor((5, 1, 1, 1))
+    a_expanded = a.expand(5, 2, 3, 1)
+
+    b = SpatialDimension(*rng.float32_tensor((3, 1, 1, 3)))
+    b_expanded = SpatialDimension(*[x.expand(1, 2, 3) for x in b.zyx])
+
+    c_matrix = torch.eye(3).reshape(1, 1, 3, 3)
+    c_expanded = Rotation.from_matrix(c_matrix.expand(5, 2, 3, 3))
+
+    test = Dummy(a_expanded, b_expanded, c_expanded)
+
+    torch.testing.assert_close(test.a, a)
+    torch.testing.assert_close(test.b.z, b.z)
+    torch.testing.assert_close(test.b.y, b.y)
+    torch.testing.assert_close(test.b.x, b.x)
+    torch.testing.assert_close(test.c.as_matrix(), c_matrix)
+
+    assert test.a[0, 0, 0, 0] == 1.0, 'subclass post_init not called'
