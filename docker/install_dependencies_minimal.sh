4a33a35bcbeec06b438a9ce49e38c614ae8f1442
Leonid Lunin <lunin.leonid@gmail.com>
2025-03-25 12:51:38 +0100

    remove shell script for minimal deps

--- docker/install_dependencies_minimal.sh
+++
@@ -1,5 +0,0 @@
-# install the minimal dependencies for the project from the minimal-requirements.txt file
-python -m pip install --no-cache-dir -r minimal-requirements.txt
-
-#clean up
-rm -rf /root/.cache
