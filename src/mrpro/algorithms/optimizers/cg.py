0c1ab57a8d7941d5dd6990840f7ddf47f0de6b04
Felix Zimmermann <fzimmermann89@gmail.com>
2025-04-07 12:51:42 +0200

    cg matrix op

--- src/mrpro/algorithms/optimizers/cg.py
+++ src/mrpro/algorithms/optimizers/cg.py
@@ -1,28 +1,66 @@
 """Conjugate Gradient for linear systems with self-adjoint linear operator."""
 
-from collections.abc import Callable
+from collections.abc import Callable, Sequence
 
 import torch
+from typing_extensions import TypeVarTuple, Unpack, overload
 
 from mrpro.algorithms.optimizers.OptimizerStatus import OptimizerStatus
 from mrpro.operators.LinearOperator import LinearOperator
+from mrpro.operators.LinearOperatorMatrix import LinearOperatorMatrix
+
+Tuple = TypeVarTuple('Tuple')
+
+
+def vdot(a: Sequence[torch.Tensor], b: Sequence[torch.Tensor]) -> torch.Tensor:
+    """Vector dot product."""
+    return sum(
+        (torch.vdot(a_i.flatten(), b_i.flatten()) for a_i, b_i in zip(a, b, strict=True)), start=torch.tensor(0.0)
+    )
 
 
 class CGStatus(OptimizerStatus):
     """Conjugate gradient callback base class."""
 
-    residual: torch.Tensor
+    residual: tuple[torch.Tensor, ...]
     """Residual of the current estimate."""
 
 
+@overload
 def cg(
     operator: LinearOperator,
     right_hand_side: torch.Tensor,
+    *,
     initial_value: torch.Tensor | None = None,
     max_iterations: int = 128,
     tolerance: float = 1e-4,
     callback: Callable[[CGStatus], bool | None] | None = None,
-) -> torch.Tensor:
+) -> torch.Tensor: ...
+
+
+@overload
+def cg(
+    operator: LinearOperatorMatrix | Callable[[Unpack[tuple[torch.Tensor, ...]]], tuple[torch.Tensor, ...]],
+    right_hand_side: Sequence[torch.Tensor],
+    *,
+    initial_value: Sequence[torch.Tensor] | None = None,
+    max_iterations: int = 128,
+    tolerance: float = 1e-4,
+    callback: Callable[[CGStatus], bool | None] | None = None,
+) -> tuple[torch.Tensor, ...]: ...
+
+
+def cg(
+    operator: LinearOperator
+    | LinearOperatorMatrix
+    | Callable[[Unpack[tuple[torch.Tensor, ...]]], tuple[torch.Tensor, ...]],
+    right_hand_side: torch.Tensor | Sequence[torch.Tensor],
+    *,
+    initial_value: torch.Tensor | Sequence[torch.Tensor] | None = None,
+    max_iterations: int = 128,
+    tolerance: float = 1e-4,
+    callback: Callable[[CGStatus], bool | None] | None = None,
+) -> tuple[torch.Tensor, ...] | torch.Tensor:
     r"""CG for solving a linear system :math:`Hx=b`.
 
     This algorithm solves systems of the form :math:`H x = b`, where :math:`H` is a self-adjoint linear operator
@@ -74,57 +112,70 @@ def cg(
     .. [Nocedal2006] Nocedal, J. (2006). *Numerical Optimization* (2nd ed.). Springer.
     .. [WikipediaCG] Wikipedia: Conjugate Gradient https://en.wikipedia.org/wiki/Conjugate_gradient
     """
-    if initial_value is not None and (initial_value.shape != right_hand_side.shape):
-        raise ValueError(
-            f'Shapes of starting_value and right_hand_side must match,got {initial_value.shape, right_hand_side.shape}'
-        )
+    if isinstance(operator, LinearOperator):
+        operator_ = LinearOperatorMatrix.from_diagonal(operator)
+    else:
+        operator_ = operator
 
-    # initial residual
-    residual = right_hand_side - operator(initial_value)[0] if initial_value is not None else right_hand_side.clone()
+    if not isinstance(right_hand_side, torch.Tensor):
+        right_hand_side_ = tuple(right_hand_side)
+    else:
+        right_hand_side_ = (right_hand_side,)
 
-    # initialize conjugate vector
-    conjugate_vector = residual.clone()
+    if initial_value is None:
+        solution: tuple[torch.Tensor, ...] = tuple(x.clone() for x in right_hand_side_)
+    else:
+        initial_value_ = (initial_value,) if isinstance(initial_value, torch.Tensor) else initial_value
+        if len(initial_value_) != len(right_hand_side_):
+            raise ValueError(
+                'Length of initial_value and right_hand_side must match. '
+                f'Got {len(initial_value_), len(right_hand_side_)}'
+            )
+        if any(i.shape != r.shape for i, r in zip(initial_value_, right_hand_side_, strict=True)):
+            raise ValueError(
+                'Shapes of initial_value and right_hand_side must match. '
+                f'Got {[i.shape for i in initial_value_], [r.shape for r in right_hand_side_]}'
+            )
 
-    # assign starting value to the solution
-    solution = initial_value.clone() if initial_value is not None else right_hand_side.clone()
+        solution = tuple(i.clone() for i in initial_value_)
 
-    # for the case where the residual is exactly zero
-    if torch.vdot(residual.flatten(), residual.flatten()) == 0:
-        return solution
+    residual = tuple(rhs - op_sol for rhs, op_sol in zip(right_hand_side_, operator_(*solution), strict=True))
+    conjugate_vector = tuple(r.clone() for r in residual)
 
     # dummy value. new value will be set in loop before first usage
     residual_norm_squared_previous: torch.Tensor | None = None
 
     for iteration in range(max_iterations):
-        # calculate the square norm of the residual
-        residual_flat = residual.flatten()
-        residual_norm_squared = torch.vdot(residual_flat, residual_flat).real
+        residual_norm_squared = vdot(residual, residual).real
 
-        # check if the solution is already accurate enough
-        if tolerance != 0 and (residual_norm_squared < tolerance**2):
-            return solution
+        if residual_norm_squared < tolerance**2:
+            break
 
         if residual_norm_squared_previous is not None:  # not first iteration
             beta = residual_norm_squared / residual_norm_squared_previous
-            conjugate_vector = residual + beta * conjugate_vector
+            conjugate_vector = tuple(r + beta * c for r, c in zip(residual, conjugate_vector, strict=True))
 
-        # update estimates of the solution and the residual
-        (operator_conjugate_vector,) = operator(conjugate_vector)
-        alpha = residual_norm_squared / (torch.vdot(conjugate_vector.flatten(), operator_conjugate_vector.flatten()))
-        solution = solution + alpha * conjugate_vector
-        residual = residual - alpha * operator_conjugate_vector
-
+        operator_conjugate_vector = operator_(*conjugate_vector)
+        alpha = residual_norm_squared / vdot(conjugate_vector, operator_conjugate_vector).real
+        solution = tuple(s + alpha * c for s, c in zip(solution, conjugate_vector, strict=True))
+        residual = tuple(r - alpha * op_c for r, op_c in zip(residual, operator_conjugate_vector, strict=True))
         residual_norm_squared_previous = residual_norm_squared
 
         if callback is not None:
             continue_iterations = callback(
                 {
-                    'solution': (solution,),
+                    'solution': solution,
                     'iteration_number': iteration,
                     'residual': residual,
                 }
             )
             if continue_iterations is False:
                 break
-
+    if (
+        isinstance(operator, LinearOperator)
+        and isinstance(right_hand_side, torch.Tensor)
+        and (initial_value is None or isinstance(initial_value, torch.Tensor))
+    ):
+        # For backward compatibility if called with a single tensor and operator.
+        return solution[0]
     return solution
