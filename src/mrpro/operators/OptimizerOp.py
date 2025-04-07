4662d5862d31e9e61f47c76317d449e456d7c38b
Felix Zimmermann <fzimmermann89@gmail.com>
2025-04-07 12:51:09 +0200

    types

--- src/mrpro/operators/OptimizerOp.py
+++ src/mrpro/operators/OptimizerOp.py
@@ -14,7 +14,7 @@ ArgumentType: TypeAlias = tuple[torch.Tensor, ...]
 VariableType: TypeAlias = tuple[torch.Tensor, ...]
 ObjectiveType: TypeAlias = Callable[[Unpack[VariableType]], tuple[torch.Tensor]]
 FactoryType: TypeAlias = Callable[[Unpack[ArgumentType]], ObjectiveType]
-OptimizeFunctionType: TypeAlias = Callable[[Unpack[VariableType]], VariableType]
+OptimizeFunctionType: TypeAlias = Callable[[ObjectiveType, VariableType], VariableType]
 
 default_lbfgs = functools.partial(
     lbfgs,
@@ -37,66 +37,88 @@ class OptimizeCtx(torch.autograd.function.FunctionCtx):
     saved_tensors: tuple[torch.Tensor, ...]
 
 
-class OptimizerImplicitBackward(torch.autograd.Function):
+class _OptimizerImplicitBackward(torch.autograd.Function):
     """Implicit Backward."""
 
     @staticmethod
     def forward(
         ctx: OptimizeCtx,
         factory: FactoryType,
-        x0: VariableType,
+        initial_values: VariableType,
         optimize: OptimizeFunctionType = default_lbfgs,
         *parameters: Unpack[ArgumentType],
     ) -> VariableType:
         """Optimize."""
         ctx.factory = factory
         f = factory(*parameters)
-        xprime = optimize(f, x0)
+        xprime = optimize(f, initial_values)
         for xp in xprime:
             xp.grad = None
         ctx.save_for_backward(*xprime, *parameters)
-        ctx.len_x = len(x0)
+        ctx.len_x = len(initial_values)
         return xprime
 
     @staticmethod
-    def backward(ctx: OptimizeCtx, grad: torch.Tensor) -> tuple[torch.Tensor]:
+    def backward(ctx: OptimizeCtx, *grad_outputs: torch.Tensor) -> tuple[torch.Tensor | None, ...]:
         """Calculate the backward pass using implicit differentiation."""
-        xprime: torch.Tensor = ctx.saved_tensors[: ctx.len_x]
+        xprime = tuple(xp.detach().clone().requires_grad_(True) for xp in ctx.saved_tensors[: ctx.len_x])
         parameters = ctx.saved_tensors[ctx.len_x :]
-        xprime = xprime.detach().clone().requires_grad_(True)
-        parameters = [
+        parameters = tuple(
             p.detach().clone().requires_grad_(True) if ctx.needs_input_grad[i + 3] else p.detach()
             for i, p in enumerate(parameters)
-        ]
+        )
         dparams = [p for p in parameters if p.requires_grad]
 
         objective = ctx.factory(*parameters)
-        hessian_inverse_grad = cg(lambda v: torch.autograd.functional.vhp(objective, xprime, v=v)[1], grad)
+
+        def hvp(*v: torch.Tensor) -> tuple[torch.Tensor, ...]:
+            return torch.autograd.functional.vhp(objective, xprime, v=v)[1:]
+
+        hessian_inverse_grad = cg(hvp, grad_outputs)
         with torch.enable_grad():
-            dobjective_dxprime = torch.autograd.grad(objective(xprime), xprime, create_graph=True)[0]
+            dobjective_dxprime = torch.autograd.grad(objective(*xprime), xprime, create_graph=True)[0]
             # - d^2_obective / d_xprime d_params Hessian^-1_grad
-            grad_params = list(torch.autograd.grad(dobjective_dxprime, dparams, -hessian_inverse_grad))
+            grad_params = list(torch.autograd.grad(dobjective_dxprime, dparams, hessian_inverse_grad))
         grad_inputs: list[torch.Tensor | None] = [None, None, None]  # factory, x0, optimize
         for need_grad in ctx.needs_input_grad[3:]:
             if need_grad:
-                w = grad_params.pop(0)
-                grad_inputs.append(w)
+                grad_inputs.append(-grad_params.pop(0))
             else:
-                grad_inputs.append(grad_inputs)
+                grad_inputs.append(None)
 
         return tuple(grad_inputs)
 
 
 class OptimizerOp(Operator):
-    def __init__(self, factory: FactoryType, optimize: OptimizeFunctionType, initializer):
-        r"""Initialize a differentiable Optimizer.
+    """Differentiable Optimization Operator.
 
-        Setup a differentiable argmin solver.
+    One of the building blocks of PINQI [ZIMM2024]_
+    Finds :math:`x^*=argmin_x f_p(x)
 
-        This is one of the building blocks of PINQI [ZIMM2024]_
+    References
+    ----------
+    .. [ZIMM2024] Zimmermann, Felix F., et al. (2024) PINQI. An End-to-End Physics-Informed Approach to Learned
+       Quantitative MRI Reconstruction. IEEE TCI. https://doi.org/10.1109/TCI.2024.3388869
+    """
 
+    def __init__(
+        self,
+        factory: FactoryType,
+        initializer: Callable[[Unpack[ArgumentType]], VariableType],
+        optimize: OptimizeFunctionType = default_lbfgs,
+    ):
+        r"""Initialize a differentiable argmin solver.
 
-        Find :math:`x^*=argmin_x f_p(x)
+        Parameters
+        ----------
+        factory
+            Function, that given the parameters of the problem returns an objective function.
+            The objective function should be a callable that takes the variable(s) as input and returns a scalar.
+        initializer
+            Function, that given the parameters of the problem returns a tuple of initial values for the variable(s)
+        optimize
+            Function used to perform the optimization, for example `lbfgs`.
+            Use `functools.partial` to setup up all settings besides the objective function and the initial values.
 
         Example
         -------
@@ -110,47 +132,32 @@ class OptimizerOp(Operator):
                 def initializer(_y, _lambda, _xreg):
                     return (x_reg,)
 
-
-        Parameters
-        ----------
-            factory
-                Function, that given the parameters of the problem returns an objective function
-            initialiazer
-                Function, that given the parameters of the problem creates initial values for the variable(s)
-            optimize
-                Function used to perform the optimization.
-                Use `functools.partial` to setup up all settings besides the objective function and the initial values.
-
-        References
-        ----------
-         .. [ZIMM2024] Zimmermann, Felix F., et al. (2024) PINQI. An End-to-End Physics-Informed Approach to Learned Quantitative
-                  MRI Reconstruction. IEEE TCI. https://doi.org/10.1109/TCI.2024.3388869
+        Returns
+        -------
+            The argmin `x^*`
         """
         self.factory = factory
         self.optimize = optimize
         self.initializer = initializer
 
     def forward(self, *parameters: torch.Tensor) -> tuple[torch.Tensor, ...]:
-        """Solve the argmin problem.
+        """Find the argmin.
 
         Parameters
         ----------
         parameters
             Parameters of the argmin problem.
         """
-        parameters = []
-        for x in parameters:
-            if isinstance(x, torch.Tensor):
-                parameters.append(x.detach().clone())
-            else:
-                raise NotImplementedError()
+        parameters = tuple(p.detach().clone() for p in parameters)
         initial_values = self.initializer(*parameters)
-        initial_values = [x.copy() if x in parameters else x for x in initial_values]
-        initial_values = [x.detach().requires_grad_(True) for x in initial_values]
+        initial_values = tuple(x.clone() if x in parameters else x for x in initial_values)
+        initial_values = tuple(x.detach().requires_grad_(True) for x in initial_values)
         if TYPE_CHECKING:
             # For  mypy
-            result = OptimizerImplicitBackward.forward(self.factory, initial_values, self.optimize, *parameters)
+            result = _OptimizerImplicitBackward.forward(
+                OptimizeCtx(), self.factory, initial_values, self.optimize, *parameters
+            )
         else:
-            # For pytorch at runtime
-            result = OptimizerImplicitBackward.apply(self.factory, initial_values, self.optimize, *parameters)
+            # Actually used at runtime
+            result = _OptimizerImplicitBackward.apply(self.factory, initial_values, self.optimize, *parameters)
         return result
