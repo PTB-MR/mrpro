50dcdccc262ad6ea19eb1ab99c1ea608bb1d475a
Felix Zimmermann <fzimmermann89@gmail.com>
2025-04-10 13:24:37 +0200

    fix

--- src/mrpro/data/traj_calculators/KTrajectoryCartesian.py
+++ src/mrpro/data/traj_calculators/KTrajectoryCartesian.py
@@ -7,6 +7,7 @@ import torch
 from mrpro.data.KTrajectory import KTrajectory
 from mrpro.data.SpatialDimension import SpatialDimension
 from mrpro.data.traj_calculators.KTrajectoryCalculator import KTrajectoryCalculator
+from mrpro.utils import RandomGenerator
 from mrpro.utils.reshape import unsqueeze_tensors_left
 
 
@@ -66,6 +67,7 @@ class KTrajectoryCartesian(KTrajectoryCalculator):
         n_center: int = 10,
         fwhm_ratio: float = 0.3,
         n_other: Sequence[int] = (1,),
+        seed: int = 0,
     ) -> KTrajectory:
         """
         Generate k-space Gaussian weighted variable density sampling.
@@ -73,7 +75,7 @@ class KTrajectoryCartesian(KTrajectoryCalculator):
         Parameters
         ----------
         encoding_matrix
-            encoded K-space size, must have ``encoding_matrix.z=1``.
+            Encoded K-space size, must have ``encoding_matrix.z=1``.
             If a single integer, a square k-space is considered.
         acceleration
             Acceleration factor (undersampling rate).
@@ -81,7 +83,12 @@ class KTrajectoryCartesian(KTrajectoryCalculator):
             Number of fully-sampled center lines to always include.
         fwhm_ratio
             Full-width at half-maximum of the Gaussian relative to encoding_matrix.y.
-            Larger values approach uniform sampling.
+            Larger values approach uniform sampling. Set to infinity for uniform sampling.
+        n_other
+            Batch size(s)
+        seed
+            Random seed for reproducibility.
+
 
         Returns
         -------
@@ -102,29 +109,14 @@ class KTrajectoryCartesian(KTrajectoryCalculator):
         else:
             n_k1, n_k0 = encoding_matrix.y, encoding_matrix.x
 
+        rng = RandomGenerator(seed)
         n_keep = min(int(n_k1 / acceleration), n_k1)
-        if n_center > n_keep:
-            raise ValueError(f'n_center ({n_center}) > total lines to keep ({n_keep})')
-
         k1_center = n_k1 // 2
-        k1_center_start = k1_center - n_center // 2
-        k1_center_end = k1_center_start + n_center
-        if n_center:
-            k1_idx = torch.arange(k1_center_start, k1_center_end).broadcast_to((*n_other, -1))
-
-        if (n_rand := n_keep - n_center) > 0:
-            sigma = fwhm_ratio / (2 * (2 * torch.log(torch.tensor(2.0))).sqrt())
-            x = torch.linspace(-0.5, 0.5, n_k1)
-            pdf = torch.exp(-(x**2) / (2 * sigma**2))
-            pdf[k1_center_start:k1_center_end] = 0
-            pdf = pdf.broadcast_to((*n_other, -1)).flatten(end_dim=-2)
-            idx_rand = pdf.multinomial(n_rand, False)
-            idx_rand = idx_rand.unflatten(0, n_other)
-            if n_center:
-                k1_idx = torch.cat([k1_idx, idx_rand], -1)
-            else:
-                k1_idx = idx_rand
-            k1_idx = torch.sort(k1_idx)[0]
+        center_start = k1_center - n_center // 2
+        center_end = center_start + n_center
+        k1_idx = rng.gaussian_variable_density_samples(
+            (*n_other, n_keep), low=0, high=n_k1, fwhm=fwhm_ratio * n_k1, always_sample=range(center_start, center_end)
+        )
 
         return cls()(
             n_k0=n_k0,
