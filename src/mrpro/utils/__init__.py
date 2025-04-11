5e50a316354cd9d6fc9ae9377a63e14214abc13c
Felix F Zimmermann <fzimmermann89@gmail.com>
2025-04-11 12:37:49 +0200

    merge?

--- src/mrpro/utils/__init__.py
+++ src/mrpro/utils/__init__.py
@@ -7,9 +7,9 @@ from mrpro.utils.fill_range import fill_range_
 from mrpro.utils.smap import smap
 from mrpro.utils.reduce_repeat import reduce_repeat
 from mrpro.utils.indexing import Indexer
+from mrpro.utils.pad_or_crop import pad_or_crop
 from mrpro.utils.split_idx import split_idx
 from mrpro.utils.sliding_window import sliding_window
-from mrpro.utils.reshape import broadcast_right, unsqueeze_left, unsqueeze_right, reduce_view, reshape_broadcasted, ravel_multi_index, unsqueeze_tensors_left, unsqueeze_tensors_right, unsqueeze_at, unsqueeze_tensors_at
 from mrpro.utils.reshape import broadcast_right, unsqueeze_left, unsqueeze_right, reduce_view, reshape_broadcasted, ravel_multi_index, unsqueeze_tensors_left, unsqueeze_tensors_right, unsqueeze_at, unsqueeze_tensors_at
 from mrpro.utils.TensorAttributeMixin import TensorAttributeMixin
 from mrpro.utils.zero_pad_or_crop import zero_pad_or_crop
@@ -21,6 +21,7 @@ __all__ = [
     "TensorAttributeMixin",
     "broadcast_right",
     "fill_range_",
+    "pad_or_crop",
     "ravel_multi_index",
     "reduce_repeat",
     "reduce_view",
