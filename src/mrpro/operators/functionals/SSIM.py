017ced9dd32097dcf218b67d89e2b50dca3aef02
Felix Zimmermann <fzimmermann89@gmail.com>
2025-04-14 16:59:12 +0200

    weight and complex

--- src/mrpro/operators/functionals/SSIM.py
+++ src/mrpro/operators/functionals/SSIM.py
@@ -1,10 +1,10 @@
 """Structural Similarity Index (SSIM) functional."""
 
-from typing import cast
+from typing import Literal, cast
 
 import torch
 
-from mrpro.operators.Functional import ElementaryFunctional
+from mrpro.operators.Functional import Functional
 from mrpro.utils.sliding_window import sliding_window
 
 
@@ -12,14 +12,17 @@ def ssim3d(
     target: torch.Tensor,
     prediction: torch.Tensor,
     *,
-    mask: torch.Tensor | None = None,
+    data_range: torch.Tensor | None = None,
+    weight: torch.Tensor | None = None,
     k1: float = 0.01,
     k2: float = 0.03,
     window_size: int = 7,
-    data_range: torch.Tensor | None = None,
-    reduction: bool = True,
+    reduction: Literal['full', 'volume', 'none'] = 'full',
 ) -> torch.Tensor:
     """Compute SSIM between two 3D volumes.
+
+    For complex inputs, the SSIM is calculated separately for the real and imaginary parts and the results are averaged.
+    For more details, see `SSIM`.
 
     Parameters
     ----------
@@ -27,11 +30,13 @@ def ssim3d(
         Ground truth tensor, shape `(... z, y, x)` or broadcastable with prediction.
     prediction
         Predicted tensor, same shape as target
-    mask
-        Mask tensor, same shape as target.
-        Only windows that are fully inside the mask are considered.
-        If None, all elements are considered.
-        If float, windows will be weighted by the average value of the mask in the window.
+    data_range
+        Value range if the data. If None, the max-to-min per volume of the target will be used.
+    weight
+        Weight (or mask) tensor, same shape as target.
+        Only windows with all weight values > 0 (or True) are considered.
+        Windows will be weighted by the average value of the weight in the window.
+        If None, all windows are considered without weighting.
     k1
         Constant for SSIM computation. Commonly 0.01.
     k2
@@ -40,43 +45,72 @@ def ssim3d(
         Size of the rectangular sliding window.
         If any of the last 3 dimensions of the target is of size 1, the window size in this dimension will
         also be set to 1.
-    data_range
-        Value range if the data. If `None`, max-to-min of the target will be used.
+
     reduction
-        If True, return the mean SSIM over all pixels. If False, return the SSIM map.
-        The map will be of shape `(... z - window_size,  y - window_size, x - window_size)`
+        If `full`, return the weighted mean SSIM over all windows, i.e. a scalar value.
+        If `volume`, return one value for each volume, i.e, an ``target.ndim - 3`` dimensional tensor.
+        If `none`, return the unpadded SSIM map of shape
+        ``(... z - window_size + 1 ,  y - window_size + 1 , x - window_size + 1)``.
 
     Returns
     -------
-        Scalar mean SSIM or SSIM map.
+        mean SSIM or SSIM map (see `reduction`)
     """
+    if target.is_complex() or prediction.is_complex():
+        real_ssim = ssim3d(
+            target.real,
+            prediction.real,
+            weight=weight,
+            k1=k1,
+            k2=k2,
+            window_size=window_size,
+            data_range=data_range,
+            reduction=reduction,
+        )
+        imag_ssim = ssim3d(
+            target.imag if target.is_complex() else torch.zeros_like(target),
+            prediction.imag if prediction.is_complex() else torch.zeros_like(prediction),
+            weight=weight,
+            k1=k1,
+            k2=k2,
+            window_size=window_size,
+            data_range=data_range,
+            reduction=reduction,
+        )
+        return (real_ssim + imag_ssim) / 2
     if target.ndim < 3:
         raise ValueError('Input must be at least 3D (z, y, x)')
 
-    if mask is not None:
-        if (mask < 0).any():
+    if weight is not None:
+        if (weight < 0).any():
             raise ValueError('Mask contains negative values')
-        target, prediction, mask = cast(
-            tuple[torch.Tensor, torch.Tensor, torch.Tensor], torch.broadcast_tensors(target, prediction, mask)
+        target, prediction, weight = cast(
+            tuple[torch.Tensor, torch.Tensor, torch.Tensor], torch.broadcast_tensors(target, prediction, weight)
         )
-        mask = sliding_window(mask, window_shape=window_size, dim=(-3, -2, -1)).movedim((0, 1, 2), (-6, -5, -4))
-        bool_mask = mask.all(dim=(-3, -2, -1))
-        if bool_mask.numel() == 0:
-            raise ValueError('Mask does not cover any pixels')
-        mask = mask[bool_mask]
-        mask = mask / mask.sum()
+        weight = sliding_window(weight, window_shape=window_size, dim=(-3, -2, -1))
+        # Set weights to 0 for windows that are not fully inside the mask
+        weight = weight * ~torch.isclose(weight, torch.tensor(0, dtype=weight.dtype)).any((-3, -2, -1), keepdim=True)
+        weight = weight.mean((-1, -2, -3), dtype=torch.float32).moveaxis((0, 1, 2), (-3, -2, -1))
+        weight /= weight.sum(dim=(-3, -2, -1), keepdim=True)  # Normlization for mean
 
     else:
-        bool_mask = None
         target, prediction = cast(tuple[torch.Tensor, torch.Tensor], torch.broadcast_tensors(target, prediction))
 
     window = tuple(window_size if s > 1 else 1 for s in target.shape[-3:])  # To support 1D and 2D uses
     target_window = sliding_window(target, window_shape=window, dim=(-3, -2, -1)).movedim((0, 1, 2), (-6, -5, -4))
 
-    if data_range is None and bool_mask is not None:
-        data_range_ = target_window[bool_mask].amax() - target_window[bool_mask].amin()
     if data_range is None:
-        data_range_ = target_window.amax() - target_window.amin()
+        if weight is None:
+            target_max = target_window.amax((-3, -2, -1)).amax((-3, -2, -1), keepdim=True)
+            target_min = target_window.amin((-3, -2, -1)).amin((-3, -2, -1), keepdim=True)
+        else:
+            target_max = torch.where(weight > 0, target_window.amax((-3, -2, -1)), -torch.inf).amax(
+                (-3, -2, -1), keepdim=True
+            )
+            target_min = torch.where(weight > 0, target_window.amin((-3, -2, -1)), torch.inf).amin(
+                (-3, -2, -1), keepdim=True
+            )
+        data_range_ = target_max - target_min
     else:
         data_range_ = data_range
 
@@ -104,39 +138,49 @@ def ssim3d(
 
     ssim_map = (a1 * a2) / (b1 * b2)
 
-    if reduction and bool_mask is not None and mask is not None:
-        return (ssim_map[bool_mask] * mask).sum()
-    elif reduction and bool_mask is None:
-        return ssim_map.mean()
-    elif not reduction and bool_mask is not None:
-        return torch.where(bool_mask, ssim_map, ssim_map[bool_mask].mean())
-    else:  # reduction and mask is None
+    if reduction == 'full':
+        if weight is not None:
+            return (ssim_map * weight).sum((-3, -2, -1)).mean()
+        else:
+            return ssim_map.mean()
+    elif reduction == 'volume':
+        if weight is not None:
+            return (ssim_map * weight).sum(dim=(-3, -2, -1))
+        else:
+            return ssim_map.mean(dim=(-3, -2, -1))
+    elif reduction == 'none':
         return ssim_map
 
 
-class SSIM(ElementaryFunctional):
+class SSIM(Functional):
     """(masked) SSIM functional."""
 
     def __init__(
         self,
         target: torch.Tensor,
         weight: torch.Tensor | None = None,
+        *,
         data_range: torch.Tensor | None = None,
         window_size: int = 7,
         k1: float = 0.01,
         k2: float = 0.03,
+        reduction: Literal['full', 'volume', 'none'] = 'full',
     ) -> None:
         """Initialize Volume SSIM.
 
         The Structural Similarity Index Measure [SSIM]_ is used to measure the similarity between two images.
         It considers luminance, contrast and structure differences between the images.
         SSIM values range from -1 to 1, where 1 indicates perfect structural similarity.
+        Two random images have an SSIM of 0.
 
-        Calculates the SSIM using a rectangular sliding window. If a mask is provided, only the windows
-        that are fully inside the mask are considered.
+        Calculates the SSIM using a rectangular sliding window. If a boolean mask is used as weight, only the windows
+        that are fully inside the mask are considered. This can be used to ignore the background of the volumes.
+        Calculates the SSIM for a volume, i.e, 3D patches of the last three dimensions of the input are used.
+        To apply it to 2D data, add a singleton dimension.
+        For complex inputs, the SSIM is calculated separately for the real and imaginary parts and the results
+        are averaged.
 
-        The SSIM is calculated for a volume, i.e, 3D patches of the last three dimensions of the input are used.
-        To apply it to purely 2D data, add a singleton dimension.
+        For stability, it is advised to provide the data range. Otherwise it is estimates per volume from the target.
 
         References
         ----------
@@ -145,45 +189,50 @@ class SSIM(ElementaryFunctional):
                IEEE TMI, 13(4), 600-612. https://doi.org/10.1109/TIP.2003.819861
 
 
-
         Parameters
         ----------
         target
-            Target volume. At least 3d.
+            Target volume. At least three dimensional.
         weight
             Either a boolean mask. Only windows with all values `True` will be considered.
-            `None` means all windows are used.
             Or a weight tensor of the same shape as the target. Each window will be weighted by
-            the average value of the weight in the window.
+            the average value of the weight in the window. Only windows with all weight values > 0 will be considered.
+            Or `None`, meaning all windows are used.
         data_range
-            Value range if the data. If None, the max-to-min of the target will be used.
+            Value range if the data. If None, the max-to-min per volume of the target will be used.
         window_size
-            Size of the windows used in SSIM. Usually 7 or 11.
-            Will be cropped to
+            Size of the windows used in SSIM. Usually ``7`` or ``11``.
+            If any of the last 3 dimensions of the target is of size 1, the window size in this dimension will
+            also be set to 1.
         k1
-            Constant. Usually 0.01
+            Constant. Usually ``0.01`` and rarely changed.
         k2
-            Constant. Usually 0.03
+            Constant. Usually ``0.03`` and rarely changed.
+        reduction
+            If ``full``, return the weighted mean SSIM over all windows, i.e. a scalar value.
+            If ``volume``, return one value for each volume, i.e, an ``target.ndim - 3`` dimensional tensor.
+            If ``none``, return the unpadded SSIM map.
         """
-        weight_ = 1 if weight is None else weight
-        super().__init__(target, weight_)
+        super().__init__()
+        self.target = target
+        self.weight = weight
         self.k1 = k1
         self.k2 = k2
         self.window_size = window_size
         self.data_range = data_range
+        self.reduction = reduction
 
     def forward(self, x: torch.Tensor) -> tuple[torch.Tensor]:
         """Calculate SSIM of an input."""
-        if x.is_complex():
-            return ((self.forward(x.real)[0] + self.forward(x.imag)[0]) / 2,)
-        return (
-            ssim3d(
-                self.target,
-                x,
-                mask=self.weight,
-                k1=self.k1,
-                k2=self.k2,
-                window_size=self.window_size,
-                data_range=self.data_range,
-            ),
+        ssim = ssim3d(
+            self.target.real,
+            x.real,
+            weight=self.weight,
+            k1=self.k1,
+            k2=self.k2,
+            window_size=self.window_size,
+            data_range=self.data_range,
+            reduction=self.reduction,
         )
+
+        return (ssim,)
--- tests/operators/functionals/test_ssim.py
+++ tests/operators/functionals/test_ssim.py
@@ -19,11 +19,26 @@ def test_ssim() -> None:
 def test_ssim_mask() -> None:
     """Test masking in the SSIM functional"""
     rng = RandomGenerator(0)
-    target = rng.float32_tensor((1, 8, 32, 32), low=0.0, high=1.0)
-    mask = torch.zeros(1, 8, 32, 32, dtype=torch.bool)
+    target = rng.float32_tensor((2, 12, 32, 32), low=0.0, high=1.0)
+    mask = torch.zeros(2, 12, 32, 32, dtype=torch.bool)
     mask[..., 4:-4, 4:-4] = True
     test = rng.rand_like(target) + 0.5 * target + rng.rand_like(target, high=100) * (~mask).float()
     (masked,) = SSIM(target, mask)(test)
     (cropped,) = SSIM(target[..., 4:-4, 4:-4])(test[..., 4:-4, 4:-4])
     torch.testing.assert_close(masked, cropped)
-    assert 0.4 < masked.item() < 0.6
+    assert 0.40 < masked.item() < 0.45
+
+
+def test_ssim_reduction() -> None:
+    """Test the reduction argument of the SSIM functional."""
+    rng = RandomGenerator(0)
+    target = rng.complex64_tensor((2, 3, 10, 10, 10), low=0.0, high=1.0)
+    test = rng.complex64_tensor(target.shape) + rng.float32_tensor((2, 3, 1, 1, 1), low=0.2, high=0.8) * target
+    (ssim_volume,) = SSIM(target, reduction='volume')(test)
+    (ssim_full,) = SSIM(target, reduction='full')(test)
+    (ssim_none,) = SSIM(target, reduction='none')(test)
+    torch.testing.assert_close(ssim_volume.mean(), ssim_full)
+    torch.testing.assert_close(ssim_none.mean(), ssim_full)
+    assert ssim_volume.shape == (2, 3)
+    assert ssim_full.shape == ()
+    assert ssim_none.shape == (2, 3, 4, 4, 4)
