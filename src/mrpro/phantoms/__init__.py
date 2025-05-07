e40adc7d43e6119a9023017a5985a93930a66e57
Felix Zimmermann <fzimmermann89@gmail.com>
2025-05-08 00:55:45 +0200

    m4raw

--- src/mrpro/phantoms/__init__.py
+++ src/mrpro/phantoms/__init__.py
@@ -3,5 +3,11 @@
 from mrpro.phantoms.EllipsePhantom import EllipsePhantom
 from mrpro.phantoms.phantom_elements import EllipseParameters
 from mrpro.phantoms import brainweb
+from mrpro.phantoms.m4raw import M4RawDataset
 
-__all__ = ["EllipseParameters", "EllipsePhantom", "brainweb"]
+__all__ = [
+    "EllipseParameters",
+    "EllipsePhantom",
+    "M4RawDataset",
+    "brainweb",
+]
