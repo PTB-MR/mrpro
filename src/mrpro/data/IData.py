5407e6d9bdd1305dba4e6ec2a78de278afa5d362
Christoph Kolbitsch <christoph.kolbitsch@ptb.de>
2025-06-19 21:40:56 +0200

    tests added

--- src/mrpro/data/IData.py
+++ src/mrpro/data/IData.py
@@ -1,5 +1,6 @@
 """MR image data (IData) class."""
 
+import warnings
 from collections.abc import Generator, Sequence
 from copy import deepcopy
 from pathlib import Path
@@ -16,7 +17,7 @@ from mrpro.data.Dataclass import Dataclass
 from mrpro.data.IHeader import IHeader
 from mrpro.data.KHeader import KHeader
 from mrpro.data.SpatialDimension import SpatialDimension
-from mrpro.utils.unit_conversion import m_to_mm, rad_to_deg, s_to_ms
+from mrpro.utils.unit_conversion import m_to_mm, s_to_ms
 
 
 def _dcm_pixelarray_to_tensor(dataset: Dataset) -> torch.Tensor:
@@ -175,12 +176,15 @@ class IData(Dataclass):
             Path to folder for DICOM files.
         series_description
             String to be saved as the series description in the DICOM files.
+        reference_patient_table_position
+            If provided, the image position is calculated relative to this table positiion. This ensures that the
+            image position is consistent across different scans even if the patient table has moved.
         """
         if not isinstance(foldername, Path):
             foldername = Path(foldername)
         foldername.mkdir(parents=True, exist_ok=False)
 
-        # We try to save 3D image data in each dicom file. This can either be a full 3D volume, multiple slices (M2D) or
+        # We save 3D image data in each dicom file. This can either be a full 3D volume, multiple slices (M2D) or
         # a combination of y and x image dimensions an one other dimension, e.g. multiple cardiac phases of a 2D image.
         mr_acquisition_type = '3D' if self.data.shape[-3] > 1 else '2D'
         frame_dimension = next((i for i in range(-3, -len(self.data.shape) - 1, -1) if self.data.shape[i] > 1), -3)
@@ -229,7 +233,7 @@ class IData(Dataclass):
         dataset.PatientPosition = 'HFS'
 
         for file_index, other in enumerate(np.ndindex(dcm_idata.shape[:-3])):
-            dcm_file_idata = dcm_idata[(*other, slice(None), slice(None), slice(None))]  # type: ignore[index]
+            dcm_file_idata = dcm_idata[(*other, slice(None), slice(None), slice(None))]
 
             dataset.MRAcquisitionType = mr_acquisition_type
             dataset.NumberOfFrames = number_of_frames
@@ -248,7 +252,7 @@ class IData(Dataclass):
 
             for frame in range(number_of_frames):
                 dcm_frame_idata = dcm_file_idata[..., frame, :, :]
-                if np.prod(dcm_frame_idata.header.shape) != 1:
+                if dcm_frame_idata.header.shape.numel() != 1:
                     raise ValueError('Only single image can be saved as a frame.')
                 directions = dcm_frame_idata.header.orientation[0].as_directions()
                 readout_direction = np.squeeze(np.asarray(directions[2].zyx[::-1]))
@@ -284,7 +288,19 @@ class IData(Dataclass):
                     plane_orientation_sequence
                 )
 
-                mr_echo_sequence[0].EffectiveEchoTime = s_to_ms(dcm_frame_idata.header.te)
+                def unique_parameter(parameter: torch.Tensor | list[float], parameter_name: str) -> float:
+                    """Return unique value of parameter tensor or list. Raise warning if not unique."""
+                    unique_parameter = torch.unique(torch.as_tensor(parameter))
+                    if unique_parameter.numel() > 1:
+                        warnings.warn(
+                            f'{parameter_name} is not unique. Using first value. To ensure all values are saved, '
+                            'split data into correct subsets first.',
+                            stacklevel=2,
+                        )
+                    return unique_parameter[0].item()
+
+                mr_echo_sequence[0].EffectiveEchoTime = s_to_ms(unique_parameter(dcm_frame_idata.header.te, 'te'))
+
                 dataset.PerFrameFunctionalGroupsSequence[-1].MREchoSequence = deepcopy(mr_echo_sequence)
 
                 pixel_measure_sequence[0].SliceThickness = m_to_mm(dcm_frame_idata.header.resolution[0].z)
@@ -293,12 +309,10 @@ class IData(Dataclass):
                 )
                 dataset.PerFrameFunctionalGroupsSequence[-1].PixelMeasuresSequence = deepcopy(pixel_measure_sequence)
 
-                mr_timing_parameters_sequence[0].FlipAngle = (
-                    rad_to_deg(dcm_frame_idata.header.fa).tolist()
-                    if isinstance(dcm_frame_idata.header.fa, torch.Tensor)
-                    else rad_to_deg(dcm_frame_idata.header.fa)
+                mr_timing_parameters_sequence[0].FlipAngle = s_to_ms(unique_parameter(dcm_frame_idata.header.fa, 'fa'))
+                mr_timing_parameters_sequence[0].RepetitionTime = s_to_ms(
+                    unique_parameter(dcm_frame_idata.header.tr, 'tr')
                 )
-                mr_timing_parameters_sequence[0].RepetitionTime = s_to_ms(dcm_frame_idata.header.tr)
                 dataset.PerFrameFunctionalGroupsSequence[-1].MRTimingAndRelatedParametersSequence = deepcopy(
                     mr_timing_parameters_sequence
                 )
