e39bab5951fff2ff56948ff9a96b880fb95c4012
Felix F Zimmermann <fzimmermann89@gmail.com>
2025-07-09 21:05:45 +0200

    Update src/mrpro/operators/PatchOp.py
    Co-authored-by: Christoph Kolbitsch <christoph.kolbitsch@ptb.de>

--- src/mrpro/operators/PatchOp.py
+++ src/mrpro/operators/PatchOp.py
@@ -84,7 +84,7 @@ class PatchOp(LinearOperator):
 
         .. note::
             Prefer calling the instance of the PatchOp operator as ``operator(x)`` over
-            directly calling this method.
+            directly calling this method. See this PyTorch `discussion <https://discuss.pytorch.org/t/is-model-forward-x-the-same-as-model-call-x/33460/3>`_.
         """
         domain_size = tuple(x.shape[dim] for dim in self.dim)
         if self.domain_size is None:
