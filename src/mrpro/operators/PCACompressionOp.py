d0d69a7112523774bb535555f8990a4fc3ebbdb6
Felix F Zimmermann <fzimmermann89@gmail.com>
2025-07-09 21:05:27 +0200

    Update src/mrpro/operators/PCACompressionOp.py
    Co-authored-by: Christoph Kolbitsch <christoph.kolbitsch@ptb.de>

--- src/mrpro/operators/PCACompressionOp.py
+++ src/mrpro/operators/PCACompressionOp.py
@@ -63,7 +63,7 @@ class PCACompressionOp(LinearOperator):
 
         .. note::
             Prefer calling the instance of the PCACompressionOp operator as ``operator(x)`` over
-            directly calling this method.
+            directly calling this method. See this PyTorch `discussion <https://discuss.pytorch.org/t/is-model-forward-x-the-same-as-model-call-x/33460/3>`_.
         """
         try:
             result = (self._compression_matrix @ data.unsqueeze(-1)).squeeze(-1)
