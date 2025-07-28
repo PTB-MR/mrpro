1aa6af5ce99ccacb502f44a13d9c795bc218feb1
Felix F Zimmermann <fzimmermann89@gmail.com>
2025-07-29 00:34:21 +0200

    Improve docstrings of __call__ (#819)
    Co-authored-by: Leonid Lunin <leonid.lunin@ptb.de>
    Co-authored-by: Christoph Kolbitsch <christoph.kolbitsch@ptb.de>

--- src/mrpro/operators/MagnitudeOp.py
+++ src/mrpro/operators/MagnitudeOp.py
@@ -9,16 +9,27 @@ class MagnitudeOp(EndomorphOperator):
     """Magnitude of input tensors."""
 
     @endomorph
-    def forward(self, *x: torch.Tensor) -> tuple[torch.Tensor, ...]:
-        """Magnitude of tensors.
+    def __call__(self, *x: torch.Tensor) -> tuple[torch.Tensor, ...]:
+        """Calculate the element-wise magnitude (absolute value) of input tensors.
 
         Parameters
         ----------
-        x
-            input tensors
+        *x
+            One or more input tensors.
 
         Returns
         -------
-            tensors with magnitude (absolute values) of input tensors
+            A tuple of tensors, where each tensor contains the element-wise
+            magnitude of the corresponding input tensor.
+        """
+        return super().__call__(*x)
+
+    @endomorph
+    def forward(self, *x: torch.Tensor) -> tuple[torch.Tensor, ...]:
+        """Apply forward of MagnitudeOp.
+
+        .. note::
+            Prefer calling the instance of the MagnitudeOp operator as ``operator(x)`` over
+            directly calling this method. See this PyTorch `discussion <https://discuss.pytorch.org/t/is-model-forward-x-the-same-as-model-call-x/33460/3>`_.
         """
         return tuple([torch.abs(xi) for xi in x])
