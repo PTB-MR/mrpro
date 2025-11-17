95112ec370637aff2d2ff2d6c062955ef7a1c123
Patrick Schuenke <patrick.schuenke@nvision-imaging.com>
2025-11-17 14:08:34 +0100

    mypy fixes

--- src/mrpro/algorithms/optimizers/pdhg.py
+++ src/mrpro/algorithms/optimizers/pdhg.py
@@ -166,6 +166,8 @@ def pdhg(
         # choose primal and dual step size such that their product is 1/|operator|**2
         # to ensure convergence
         operator_norm = operator_matrix.operator_norm(*[torch.randn_like(v) for v in initial_values]).amax()
+        primal_stepsize_: float | torch.Tensor
+        dual_stepsize_: float | torch.Tensor
         if primal_stepsize is None and dual_stepsize is None:
             primal_stepsize_ = dual_stepsize_ = 1.0 / operator_norm
         elif primal_stepsize is None and dual_stepsize is not None:
