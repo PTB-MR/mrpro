b2a31f27acbb26e027e24d54e3f59ca856cfd993
Felix Zimmermann <fzimmermann89@gmail.com>
2026-02-07 20:27:20 +0100

    add positional encodings and attention modules
    ghstack-source-id: 37cc9ec719d1071aea20f5ea65dd1f503dfc0c00
    ghstack-comment-id: 3865064364
    Pull-Request-resolved: https://github.com/PTB-MR/mrpro/pull/945

--- src/mrpro/nn/__init__.py
+++ src/mrpro/nn/__init__.py
@@ -1,6 +1,8 @@
 """Neural network modules and utilities."""
 
 from mrpro.nn.ComplexAsChannel import ComplexAsChannel
+from mrpro.nn.AbsolutePositionEncoding import AbsolutePositionEncoding
+from mrpro.nn.AxialRoPE import AxialRoPE
 from mrpro.nn.CondMixin import CondMixin
 from mrpro.nn.DropPath import DropPath
 from mrpro.nn.FiLM import FiLM
@@ -14,6 +16,7 @@ from mrpro.nn.ResBlock import ResBlock
 from mrpro.nn.Residual import Residual
 from mrpro.nn.SeparableResBlock import SeparableResBlock
 from mrpro.nn.Sequential import Sequential
+from mrpro.nn import attention
 from mrpro.nn import data_consistency
 from mrpro.nn import nets
 from mrpro.nn.ndmodules import (
@@ -27,6 +30,8 @@ from mrpro.nn.ndmodules import (
 )
 
 __all__ = [
+    'AbsolutePositionEncoding',
+    'AxialRoPE',
     'ComplexAsChannel',
     'CondMixin',
     'DropPath',
@@ -42,6 +47,7 @@ __all__ = [
     'SeparableResBlock',
     'Sequential',
     'adaptiveAvgPoolND',
+    'attention',
     'avgPoolND',
     'batchNormND',
     'convND',
