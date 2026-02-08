404cb628e5da710e1f1655f988a9183c35338891
Felix Zimmermann <fzimmermann89@gmail.com>
2026-02-08 18:06:25 +0100

    add uformer architecture and tests
    ghstack-source-id: 7b0a4d5e1f0d968525e43f04b8ad95d312654653
    ghstack-comment-id: 3865651637
    Pull-Request-resolved: https://github.com/PTB-MR/mrpro/pull/959

--- src/mrpro/nn/nets/__init__.py
+++ src/mrpro/nn/nets/__init__.py
@@ -2,6 +2,7 @@ from mrpro.nn.nets.BasicCNN import BasicCNN
 from mrpro.nn.nets.Restormer import Restormer
 from mrpro.nn.nets.SwinIR import SwinIR
 from mrpro.nn.nets.UNet import AttentionGatedUNet, UNet
+from mrpro.nn.nets.Uformer import Uformer
 
 __all__ = [
     'AttentionGatedUNet',
@@ -9,4 +10,5 @@ __all__ = [
     'Restormer',
     'SwinIR',
     'UNet',
+    'Uformer',
 ]
