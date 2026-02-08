ab3d86db30303853c533c30a900662206f3ca09f
Felix Zimmermann <fzimmermann89@gmail.com>
2026-02-08 18:06:14 +0100

    add core nn foundations, layers, and resize blocks
    ghstack-source-id: f91e13b1b63eab7f12924afeefed72743836d857
    ghstack-comment-id: 3865650347
    Pull-Request-resolved: https://github.com/PTB-MR/mrpro/pull/953

---
+++ src/mrpro/nn/__init__.py
@@ -0,0 +1,45 @@
+"""Neural network modules and utilities."""
+
+from mrpro.nn.ComplexAsChannel import ComplexAsChannel
+from mrpro.nn.CondMixin import CondMixin
+from mrpro.nn.DropPath import DropPath
+from mrpro.nn.FiLM import FiLM
+from mrpro.nn.FourierFeatures import FourierFeatures
+from mrpro.nn.GEGLU import GEGLU
+from mrpro.nn.GroupNorm import GroupNorm
+from mrpro.nn.LayerNorm import LayerNorm
+from mrpro.nn.PermutedBlock import PermutedBlock
+from mrpro.nn.RMSNorm import RMSNorm
+from mrpro.nn.Residual import Residual
+from mrpro.nn.Sequential import Sequential
+from mrpro.nn.ndmodules import (
+    adaptiveAvgPoolND,
+    avgPoolND,
+    batchNormND,
+    convND,
+    convTransposeND,
+    instanceNormND,
+    maxPoolND,
+)
+
+__all__ = [
+    'ComplexAsChannel',
+    'CondMixin',
+    'DropPath',
+    'FiLM',
+    'FourierFeatures',
+    'GEGLU',
+    'GroupNorm',
+    'LayerNorm',
+    'PermutedBlock',
+    'RMSNorm',
+    'Residual',
+    'Sequential',
+    'adaptiveAvgPoolND',
+    'avgPoolND',
+    'batchNormND',
+    'convND',
+    'convTransposeND',
+    'instanceNormND',
+    'maxPoolND',
+]
