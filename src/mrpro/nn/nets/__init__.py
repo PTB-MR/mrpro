650bfe5c3072df6957fe8b1211bec38f723f8721
Felix Zimmermann <fzimmermann89@gmail.com>
2026-02-10 14:37:29 +0100

    add swinir architecture and tests
    ghstack-source-id: 9b04f9fb1f47e38ab120aacd06524fc97b9e9e2c
    ghstack-comment-id: 3865651450
    Pull-Request-resolved: https://github.com/PTB-MR/mrpro/pull/958

--- src/mrpro/nn/nets/__init__.py
+++ src/mrpro/nn/nets/__init__.py
@@ -1,5 +1,6 @@
 from mrpro.nn.nets.BasicCNN import BasicCNN
 from mrpro.nn.nets.Restormer import Restormer
+from mrpro.nn.nets.SwinIR import SwinIR
 from mrpro.nn.nets.UNet import AttentionGatedUNet, UNet
 from mrpro.nn.nets.MLP import MLP
 
@@ -8,5 +9,6 @@ __all__ = [
     "BasicCNN",
     "MLP",
     "Restormer",
+    "SwinIR",
     "UNet",
 ]
