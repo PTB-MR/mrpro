36fec77ee7348cb39aa874cc2fd3adc42e836192
Felix Zimmermann <fzimmermann89@gmail.com>
2026-02-11 14:45:28 +0100

    add dit
    ghstack-source-id: ca9443237d66d0ed56e3a3ebf1df40c4a2102c36
    ghstack-comment-id: 3874745738
    Pull-Request-resolved: https://github.com/PTB-MR/mrpro/pull/964

--- src/mrpro/nn/nets/__init__.py
+++ src/mrpro/nn/nets/__init__.py
@@ -1,5 +1,6 @@
 from mrpro.nn.nets.BasicCNN import BasicCNN
 from mrpro.nn.nets.VAE import VAE
+from mrpro.nn.nets.DiT import DiT
 from mrpro.nn.nets.HourglassTransformer import HourglassTransformer
 from mrpro.nn.nets.Restormer import Restormer
 from mrpro.nn.nets.SwinIR import SwinIR
@@ -10,6 +11,7 @@ from mrpro.nn.nets.MLP import MLP
 __all__ = [
     "AttentionGatedUNet",
     "BasicCNN",
+    "DiT",
     "HourglassTransformer",
     "MLP",
     "Restormer",
