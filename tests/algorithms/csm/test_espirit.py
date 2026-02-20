9295784962d64d96955f2476dc4b6a2ec5b0914f
Felix Zimmermann <fzimmermann89@gmail.com>
2026-02-20 22:42:49 +0100

    fix

--- tests/algorithms/csm/test_espirit.py
+++ tests/algorithms/csm/test_espirit.py
@@ -7,11 +7,11 @@ from tests.helper import relative_image_difference
 
 
 def test_espirit(ellipse_phantom, random_kheader):
-    """Test the Walsh method."""
+    """Test the espirit algorithm."""
     idata, csm_ref = multi_coil_image(n_coils=4, ph_ellipse=ellipse_phantom, random_kheader=random_kheader)
 
     # Estimate coil sensitivity maps.
-    csm = espirit(idata.data[0, ...], img_shape=idata.data.shape[-3:], max_iter=20)
+    csm = espirit(idata.data[0, ...], img_shape=idata.data.shape[-3:])
     # Phase is only relative in csm calculation, therefore only the abs values are compared.
 
     assert relative_image_difference(torch.abs(csm), torch.abs(csm_ref[0, ...])) <= 0.01
