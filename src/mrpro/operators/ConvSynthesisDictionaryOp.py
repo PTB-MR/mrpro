94c65b41c4d2a6c32da2ed921ac32efba792713c
koflera <andreas_kofler@gmx.net>
2026-04-17 15:01:02 +0200

    review

---
+++ src/mrpro/operators/ConvSynthesisDictionaryOp.py
@@ -0,0 +1,113 @@
+"""Convolutional Synthesis Dictionary Operator."""
+
+from typing import Literal
+
+import torch
+from torchnd import adjoint_pad_nd, conv_nd, pad_nd  # type: ignore[import-not-found]
+
+from mrpro.operators.LinearOperator import LinearOperator
+
+
+class ConvSynthesisDictionaryOp(LinearOperator):
+    """Convolutional Synthesis Dictionary Operator."""
+
+    def __init__(
+        self,
+        kernel: torch.Tensor,
+        pad_mode: Literal['constant', 'reflect', 'replicate', 'circular'] = 'circular',
+    ) -> None:
+        """Convolutional Synthesis Dictionary Operator class.
+
+        The operator implements the application of a convolutional synthesis transform to an input tensor.
+        Thereby, the following cases are supported: if the filter is real-valued, the transform can
+        be applied to both real and complex-valued inputs. For complex-valued inputs, the same filter is applied to
+        the real and the imaginary part. If the filter is complex-valued, the transform can only be applied to
+        complex-valued inputs.
+
+        Parameters
+        ----------
+        kernel
+            Convolutional filter of shape (n_filters, *spatial_dims). The filter filter dimension is specified
+            by the number of dimension in the *spatial_dims.
+            Example: for 2D filters, the shape is (n_filters, ky, kx), for 3D filters, (n_filters, kz, ky, kx).
+            Note that, typically, odd spatial dimensions are used for the kernels to avoid spatial pixel-shift artefacts
+            in the result of the composition of the adjoint and the forward operator.
+        pad_mode
+            the mode to use for padding
+        """
+        super().__init__()
+        if pad_mode not in ('constant', 'reflect', 'replicate', 'circular'):
+            raise ValueError(
+                f"Pad mode must be either 'constant', 'reflect', 'replicate', or 'circular', but got {pad_mode}."
+            )
+        self.kernel = torch.nn.Parameter(kernel, requires_grad=kernel.requires_grad)
+        self.pad_mode = pad_mode
+
+    def __call__(self, x: torch.Tensor) -> tuple[torch.Tensor,]:
+        """Apply the convolutional synthesis dictionary transform.
+
+        This functions first appropriately reshapes the input tensor, then appropriately pads it and performs the
+        convolution. Finally, it undoes the reshaping to maintain the original spatial dimensions.
+
+        Parameters
+        ----------
+        x
+            Input tensor.
+
+        Returns
+        -------
+            The result of the convolutional analysis dictionary applied to the input.
+
+
+        """
+        return super().__call__(x)
+
+    def forward(self, x: torch.Tensor) -> tuple[torch.Tensor,]:
+        """Apply forward of the convolutional synthesis dictionary.
+
+        .. note::
+            Prefer calling the instance of the ConvSynthesisDictionaryOp operator as ``operator(x)`` over
+            directly calling this method. See this PyTorch `discussion <https://discuss.pytorch.org/t/is-model-forward-x-the-same-as-model-call-x/33460/3>`_.
+        """
+        if x.shape[0] != self.kernel.shape[0]:
+            raise ValueError('First dimension of input must match the number of filters in the kernel.')
+        if self.kernel.is_complex() and not x.is_complex():
+            raise ValueError('Input tensor must be complex-valued when the kernel is complex-valued.')
+
+        n_dim = self.kernel.ndim - 1
+        pad = tuple(p for k in self.kernel.shape[1:] for p in (k // 2, k // 2))
+
+        batch_shape = x.shape[1:-n_dim]
+        x = x.moveaxis(0, -n_dim - 1).flatten(0, -n_dim - 2)
+        x = pad_nd(x, pad=pad, dims=tuple(range(-n_dim, 0)), mode=self.pad_mode)
+        y = conv_nd(x, self.kernel.unsqueeze(0), dim=tuple(range(-n_dim, 0)))
+        y = y.squeeze(1)
+        if batch_shape:
+            y = y.unflatten(0, batch_shape)
+
+        return (y,)
+
+    def adjoint(self, x: torch.Tensor) -> tuple[torch.Tensor,]:
+        """Apply the adjoint of the convolutional synthesis dictionary transform.
+
+        Parameters
+        ----------
+        x
+            Input tensor.
+
+        Returns
+        -------
+            The transformed input tensor.
+        """
+        if self.kernel.is_complex() and not x.is_complex():
+            raise ValueError('Input tensor must be complex-valued when the kernel is complex-valued.')
+        n_dim = self.kernel.ndim - 1
+        pad = tuple(p for k in self.kernel.shape[1:] for p in (k // 2, k // 2))
+
+        batch_shape = x.shape[:-n_dim]
+        x = x.flatten(0, -n_dim - 1).unsqueeze(1)
+        y = conv_nd(x, self.kernel.unsqueeze(0).conj(), dim=tuple(range(-n_dim, 0)), transposed=True)
+        y = adjoint_pad_nd(y, pad=pad, dims=tuple(range(-n_dim, 0)), mode=self.pad_mode)
+        y = y.unflatten(0, batch_shape).moveaxis(-n_dim - 1, 0)
+
+        return (y,)
