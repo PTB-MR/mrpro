36f181f715026084892adbc7f8e8ba0da3ea2d84
koflera <andreas_kofler@gmx.net>
2026-06-17 13:40:49 +0200

    fix linear combination

--- src/mrpro/algorithms/optimizers/pdhg.py
+++ src/mrpro/algorithms/optimizers/pdhg.py
@@ -198,7 +198,8 @@ def pdhg(
         )
         primals_new = g_sum.prox(*primals_new, sigma=primal_stepsize_)
         primals_relaxed = tuple(
-            torch.lerp(primal, primal_new, relaxation) for primal, primal_new in zip(primals, primals_new, strict=False)
+            torch.lerp(primal, primal_new, -relaxation)
+            for primal, primal_new in zip(primals, primals_new, strict=False)
         )
 
         # check if the solution is already accurate enough
