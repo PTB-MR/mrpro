a1778188c63dd419ef3fac32aa58dbbeee3fc03a
Christoph Kolbitsch <christoph.kolbitsch@ptb.de>
2026-09-21 15:06:40 +0200

    change folder structure

--- src/mrpro/algorithms/__init__.py
+++ src/mrpro/algorithms/__init__.py
@@ -2,7 +2,7 @@
 
 from mrpro.algorithms import csm, dcf, optimizers, reconstruction
 from mrpro.algorithms.prewhiten_kspace import prewhiten_kspace
-from mrpro.algorithms.total_variation_denoising import total_variation_denoising
+from mrpro.algorithms.denoiser.total_variation_denoising import total_variation_denoising
 from mrpro.algorithms.varimax import varimax
 
 __all__ = [
